631620f8f3a4d0befa61bcb8875880bd092edd1f
mixelpixel <mixelpix@mac.com>
2017-08-24 01:32:33 -0400

    Schema √

--- patrick/src/user.js
+++ patrick/src/user.js
@@ -10,6 +10,20 @@ mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
   // TODO: fill in this schema
+  // ### `src/user.js`
+  // First, write the schema for the user model in `src/user.js`. Each user has two
+  // properties: `username`, a String, and `passwordHash`, also a String. Both
+  // properties are required, and the username should be unique (use the option
+  // `unique: true`).  This prevents two users from having the same username.
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
