41166ea5e5601fa9ec74ab440cb4215ac061fbeb
WizrdJim <wzrdjim@gmail.com>
2017-08-31 13:27:11 -0400

    updated some stuff

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const session = require('express-session');
-const User = require('./user');
-const bcrypt = require('bcrypt');
+const bodyParser = require("body-parser");
+const express = require("express");
+const session = require("express-session");
+const User = require("./user");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -10,9 +11,12 @@ const BCRYPT_COST = 11;
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
+server.use(cors());
+server.use(
+  session({
+    secret: "e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re"
+  })
+);
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -29,7 +33,7 @@ const sendUserError = (err, res) => {
 const middleVerification = (req, res, next) => {
   const sess = req.session;
   if (!sess.LoginId) {
-    sendUserError('Please log in', res);
+    sendUserError("Please log in", res);
     return;
   }
   User.findById(sess.LoginId, (err, user) => {
@@ -42,10 +46,10 @@ const middleVerification = (req, res, next) => {
   });
 };
 
-server.use('/restricted/*', (req, res, next) => {
+server.use("/restricted/*", (req, res, next) => {
   const sess = req.session;
   if (!sess.LoginId) {
-    sendUserError('Please log in', res);
+    sendUserError("Please log in", res);
     return;
   }
   User.findById(sess.LoginId, (err, user) => {
@@ -55,15 +59,15 @@ server.use('/restricted/*', (req, res, next) => {
     }
     req.user = user;
   });
-})
+});
 
 // TODO: implement routes
-server.post('/users', (req, res) => {
+server.post("/users", (req, res) => {
   // getting the username and password from a user
   const { username, password } = req.body;
   // Check if the password is empty
-  if (password === '') {
-    sendUserError('Please input a valid password', res);
+  if (password === "") {
+    sendUserError("Please input a valid password", res);
     return;
   }
   // hash the password
@@ -83,14 +87,14 @@ server.post('/users', (req, res) => {
     });
   });
 });
-server.post('/log-in', (req, res) => {
+server.post("/login", (req, res) => {
   const { username, password } = req.body;
   const sess = req.session;
-  if (!password || password === '') {
-    sendUserError('Please input a valid password', res);
+  if (!password || password === "") {
+    sendUserError("Please input a valid password", res);
   }
   if (!username) {
-    sendUserError('Please input username', res);
+    sendUserError("Please input username", res);
   }
   // hash the password
   User.findOne({ username }, (err, user) => {
@@ -99,7 +103,7 @@ server.post('/log-in', (req, res) => {
       return;
     }
     if (!user) {
-      sendUserError('Please input valid info', res);
+      sendUserError("Please input valid info", res);
       return;
     }
     if (bcrypt.compareSync(password, user.passwordHash)) {
@@ -107,18 +111,22 @@ server.post('/log-in', (req, res) => {
       res.json({ success: true });
       return;
     }
-    sendUserError('Crap password', res);
+    sendUserError("Crap password", res);
   });
 });
 
+server.post("/logout", (req, res) => {
+  sess.LoginId = null;
+});
+
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', middleVerification, (req, res) => {
+server.get("/me", middleVerification, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
 
-server.get('/restricted/*', (req, res) => {
-  res.json({ hidden: 'hidden'});
+server.get("/restricted/*", (req, res) => {
+  res.json({ hidden: "hidden" });
 });
 
 module.exports = { server };
