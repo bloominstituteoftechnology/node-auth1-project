49631c63517ceff791bba956bfb89b3e071b4295
Egrodo <egrodo1@gmail.com>
2017-09-13 22:25:31 -0400

    Finished proj + extra credit. My tests are borked but works for every case I can test.

--- src/server.js
+++ src/server.js
@@ -39,11 +39,8 @@ const sendServerError = (err, res) => {
   }
 };
 
-server.post('/users', (req, res) => {
-  // ### `POST /users`
-  // The `POST /users` route expects two parameters: `username` and `password`. When
-  // the client makes a `POST` request to `/users`, hash the given password and
-  // create a new user in MongoDB. Send the user object as a JSON response.
+// Start middleware
+const validateInfo = (req, res, next) => {
   const { username, password } = req.body;
   if (!username || !password) {
     let err;
@@ -53,16 +50,66 @@ server.post('/users', (req, res) => {
       err = "You're missing your password.";
     }
     sendUserError(err, res);
+    return;
   }
+  res.locals = { username, password };
+  next();
+};
 
-  // Use bcrypt to hash the password.
-  bcrypt.hash(password, BCRYPT_COST, (err, hash) => {
-    if (err) sendServerError(err, res);
-    // Create a new user in MongoDB and save it.
+const isLoggedIn = (req, res, next) => {
+  if (!req.session.username) {
+    sendUserError("You're not logged in, can't view your personal info.", res);
+    return;
+  }
+  next();
+};
+
+server.use('/restricted', (req, res, next) => {
+  isLoggedIn(req, res, next); // If this doesn't handle an error, we will continue onto wherever we were going as defined in isLoggedIn.
+});
+// End middleware
+
+
+// Start routes
+server.get('/DEBUG/:type', (req, res) => {
+  // DEBUG, REMOVE BEFORE PRODUCTION.
+  const { type } = req.params;
+  if (type === 'show-all') {
+    User.find({}, (err, user) => {
+      if (err) {
+        sendServerError(err, res);
+      }
+      res.json({ user, loggedIn: req.session.username });
+    });
+  }
+  if (type === 'remove-all') {
+    User.remove({}, (err, response) => {
+      if (err) {
+        sendServerError(err, res);
+      }
+      res.json(response);
+    });
+  }
+});
+
+server.post('/sign-up', validateInfo, (req, res) => {
+  const { username, password } = res.locals;
+
+  // Once I've used the middleware to validate my login info, I no longer need it.
+  delete res.locals; // Security m8.
+
+  // Use bcrypt to hash the password,
+  bcrypt.hash(password, BCRYPT_COST, (berr, hash) => {
+    if (berr) sendServerError(berr, res);
+    // Then create a new user in MongoDB and save it.
     const user = new User({ username, passwordHash: hash });
-    user.save((error) => {
-      if (error) {
-        sendServerError(error, res);
+    user.save((uerr) => {
+      if (uerr) {
+        if (uerr.code === 11000) {
+          sendUserError('Username taken.', res);
+        } else {
+          sendServerError(uerr, res);
+        }
       } else {
         res.json(user);
       }
@@ -70,38 +117,54 @@ server.post('/users', (req, res) => {
   });
 });
 
-server.post('/log-in', (req, res) => {
-  // ### `POST /log-in`
-  // The `POST /log-in` route expects two parameters: `username` and `password`. When
-  // the client makes a `POST` request to `/log-in`, check the given credentials and
-  // log in the appropriate user. Send the object `{ success: true }` as a JSON
-  // response if everything works out.
-
-  // You'll need to use a session to track who is logged in. Do **NOT** store the
-  // entire user object in the session; if the user in MongoDB gets updated or
-  // deleted, the session will not reflect the changes. Instead, store some
-  // information that will let you uniquely identify which user is logged in.
-
-  // Make sure to do proper validation and error checking. If there's any error, or
-  // if the credentials are invalid, respond with an appropriate status and error
-  // message using the `sendUserError()` helper function.
-  
-  const { username, password } = req.body;
-  if (!username || !password) {
-    let error;
-    if (!username) {
-      error = "You're missing your username.";
-    } else {
-      error = "You're missing your password.";
+server.post('/log-in', validateInfo, (req, res) => {
+  const { username, password } = res.locals;
+  delete res.locals;
+  // Retrieve user by username, hash password, compare passwords.
+  User.findOne({ username }, (ferr, user) => {
+    if (ferr) sendUserError(ferr, res);
+    if (!user) { // If someone who's not signed up tries to log in.
+      sendUserError('User not found. Try signing up first?', res);
+      // Not implicitly returning with the res.json in sendUserError?
+      return;
     }
-    sendUserError(error, res);
+
+    bcrypt.compare(password, user.passwordHash, (berr, isValid) => {
+      if (berr) sendServerError(berr, res);
+      if (isValid) {
+        // If the log-in info was valid, check if we're already logged in.
+        if (!req.session.username) {
+          // If we're not, log in.
+          req.session.username = username;
+          res.json({ success: true, user });
+        } else {
+          sendUserError(`${username} is already logged in.`, res);
+        }
+      } else {
+        sendUserError('Wrong password!', res);
+      }
+    });
+  });
+});
+
+server.get('/log-out', (req, res) => {
+  if (!req.session.username) {
+    sendUserError("You can't log out if you're not logged in dummy.", res);
+  } else {
+    const { username } = req.session;
+    delete req.session.username;
+    res.json({ success: `${username} logged out.` });
   }
 });
 
-// TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', isLoggedIn, (req, res) => { // Extra credit.
   // Do NOT modify this route handler in any way.
-  res.json(req.user);
+  res.json(req.session.username);
 });
 
+server.get('/restricted/test', (req, res) => { // For extra credit testing purposes.
+  res.json({ success: 'Ur hot shit m8 you made it to the restricted area.' });
+});
+
+// End routes
 module.exports = { server };
