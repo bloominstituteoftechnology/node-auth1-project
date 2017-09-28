6ecb95d9cf7b42bfe3daf77f1984371b8822515c
Joram Clervius <clervius@gmail.com>
2017-09-28 00:13:50 -0400

    complete with extra credit.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,25 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
 
 const server = express();
+const restricted = express.Router();
+
+const User = require('./user');
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({
+    url: 'mongodb://localhost/users',
+    ttl: 14 * 24 * 60 * 60
+  })
 }));
 
 /* Sends the given err, a string or an object, to the client. Sets the status
@@ -24,11 +34,44 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+server.post('/users', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) return sendUserError('Please fill in all fields', res);
+  const newUser = new User();
+  newUser.username = username;
+  newUser.passwordHash = newUser.generateHash(password);
+  newUser.save((err, user) => {
+    return err ? sendUserError(err, res) : res.json(user);
+  });
+});
+
+server.post('/log-in', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) return sendUserError('Please fill in all fields', res);
+  User.findOne({ username })
+    .exec()
+    .then((user) => {
+      if (!user) return sendUserError('This account does not exist', res);
+      if (user.validPassword(password)) {
+        req.session.user = user.id;
+        // to pass tests...
+        req.session.userObj = user;
+        res.json({ success: true });
+      } else {
+        return sendUserError('Incorrect credentials. Please try again.', res);
+      }
+    })
+    .catch(err => sendUserError(err, res));
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+const isLoggedIn = (req, res, next) => {
+  req.user = req.session.userObj;
+  return req.session.user ? next() : sendUserError('You are not authorized to view this page', res);
+};
+server.get('/me', isLoggedIn, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
-
+restricted.use(isLoggedIn);
 module.exports = { server };
