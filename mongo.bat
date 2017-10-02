bdc28e000ef1bfd3ee7976e0f4f5dcccbe9b29ad
Roland Canuto <rolandcanuto@outlook.com>
2017-10-02 11:48:45 -0700

    complete

---
+++ mongo.bat
@@ -0,0 +1,2 @@
+cd C:\Program Files\MongoDB\Server\3.4\bin
+mongod.exe --dbpath C:\Users\Roland\lambda\Auth\data\db
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const User = require('./user');
+const bcrypt = require('bcrypt');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -23,10 +25,76 @@ const sendUserError = (err, res) => {
   }
 };
 
-// TODO: implement routes
+// Create Users
+server.post('/users', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    sendUserError('You need username or password', res);
+    return;
+  }
+  bcrypt.hash(password, 11, (err, passwordHash) => {
+    if (err) {
+      sendUserError(err, res);
+      return;
+    }
+    const newUser = new User({ username, passwordHash });
+    newUser.save((savedError, user) => {
+      if (savedError) {
+        sendUserError(savedError, res);
+        return;
+      }
+      res.json(user);
+    });
+  });
+});
+
+// User login
+server.post('/log-in', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    sendUserError('You need username or password', res);
+    return;
+  }
+  User.findOne({ username }, (userError, user) => {
+    if (userError) {
+      sendUserError(userError, res);
+      return;
+    }
+    if (!user) {
+      return sendUserError('Invalid username or password', res);
+    }
+    bcrypt.compare(password, user.passwordHash, (passwordError, valid) => {
+      if (passwordError) {
+        return sendUserError(passwordError, res);
+      }
+      if (valid === false) {
+        return sendUserError('Invalid username or password', res);
+      }
+      req.session.username = user.username;
+      res.json({ success: true });
+    });
+  });
+});
+
+const middleware = (req, res, next) => {
+  const { username } = req.session;
+  if (!username) {
+    return sendUserError('User needs to be logged in', res);
+  }
+  User.findOne({ username }, (userError, user) => {
+    if (userError) {
+      sendUserError(userError, res);
+    } else if (!user) {
+      sendUserError('No user found', res);
+    } else {
+      req.user = user;
+      next();
+    }
+  });
+};
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', middleware, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
