5b3d9cff1e087436443b52efe6dee2eda4a2840f
sara fritz <krumsara@gmail.com>
2017-10-30 13:15:08 -0500

    HW

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
+
+const User = require('./user.js')
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -24,9 +27,70 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+server.post('/users', (req, res) => {
+  const { username, password } = req.body;
+  // checking for a password
+  if(!password || !username) {
+    sendUserError('valid username/password required', res);
+    return;
+  }
+  bcrypt.hash(password, BCRYPT_COST, (err, passwordHash) => {
+    if (err) {
+      sendUserError(err, res);
+      return;
+    }
+    const newuser = new User({ username, passwordHash });
+    newuser.save((err, user) => {
+      if (err) {
+        sendUserError(err, res);
+        return;
+      }
+      res.json(user);
+    });
+  });
+});
+
+server.post('/log-in', (req, res) => {
+  const { username, password } = req.body;
+  const session = req.session;
+  if (!username || !password) {
+    sendUserError('invalid username or password', res);
+  }
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      sendUserError('username not found', res);
+      return;
+    }
+    if(!user) {
+      sendUserError('username required', res);
+      return;
+    }
+    if(bcrypt.compareSync(password, user.passwordHash)) {
+      session.login = user.username;
+      res.json({ success: true });
+    } else {
+    sendUserError('password invalid', res);
+  }
+  });
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+const localMiddleware = (req, res, next) => {
+  const session = req.session;
+  if (!session.login) {
+    sendUserError('Login required', res);
+    return;
+  }
+  User.findOne({ username: session.login }, (err, user) => {
+    if (err) {
+      sendUserError(err, res);
+      return;
+    }
+    req.user = user;
+    next();
+  });
+}
+server.get('/me', localMiddleware, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
