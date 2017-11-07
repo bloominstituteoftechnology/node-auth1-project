8342c31b711f304d41fa28333e9b7453f9e8122e
Adam Lower <adamlowerj@gmail.com>
2017-11-06 21:02:36 -0500

    Finished Sprint

--- src/server.js
+++ src/server.js
@@ -15,6 +15,12 @@ server.use(session({
   secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
 }));
 
+const corsOptions = {
+  "origin": "http://localhost:3000",
+  "credentials": true
+};
+server.use(cors(corsOptions));
+
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
 const sendUserError = (err, res) => {
@@ -29,8 +35,8 @@ const sendUserError = (err, res) => {
 // TODO: implement routes
 server.post('/users', (req, res) => {
   const { username, password } = req.body;
-  if (!username || !password) {
-    sendUserError('Missing username or password', res);
+  if (!password) {
+    sendUserError('Must provide password', res);
     return;
   }
   bcrypt.hash(password, BCRYPT_COST, (err, hash) => {
@@ -46,7 +52,7 @@ server.post('/users', (req, res) => {
   });
 });
 
-server.post('/log-in', (req, res) => {
+server.post('/login', (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) return sendUserError('Missing username or password', res);
   User.findOne({ username })
@@ -64,16 +70,63 @@ server.post('/log-in', (req, res) => {
   });
 });
 
+server.post('/logout', (req, res) => {
+  if (!req.session.username) {
+    sendUserError('Must be logged in', res);
+    return;
+  }
+
+  req.session.username = null;
+  res.json({ success: true });
+});
+
 const isloggedIn = (req, res, next) => {
-  if (!req.session || !req.session.user) return sendUserError('Not logged in!', res);
-  req.user = req.session.user;
-  next();
+  const { username } = req.session;
+  if (!username) {
+    sendUserError('Must be logged in', res);
+    return;
+  }
+
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      sendUserError(err, res);
+    } else if (!user) {
+      sendUserError('Must be logged in', res);
+    } else {
+      req.user = user;
+      next();
+    }
+  });
 };
 
-// TODO: add local middleware to this route to ensure the user is logged in
 server.get('/me', isloggedIn, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
+});
+
+const checkRestricted = (req, res, next) => {
+  const path = req.path;
+  if (/restricted/.test(path)) {
+    if (!req.session.username) {
+      sendUserError('Must be logged in to access restricted path', res);
+      return;
+    }
+  }
+  next();
+};
+
+server.use(checkRestricted);
+
+server.get('/restricted/users', (req, res) => {
+  User.find({})
+  .exec()
+  .then((users) => {
+    res.json(users);
+  })
+  .catch((err) => {
+    sendUserError(err, res);
+    return;
+  });
 });
 
 module.exports = { server };
