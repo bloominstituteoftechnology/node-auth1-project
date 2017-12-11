5f86a90813ee94c3e3b11a1558f017d56fb0892f
dan volosnikov <danvolosnikov@gmail.com>
2017-12-11 18:43:33 -0500

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
+
+const User = require('./user');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -23,10 +26,90 @@ const sendUserError = (err, res) => {
   }
 };
 
+const hashingMiddleware = (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username) {
+    sendUserError({ message: 'must provide a username' }, res);
+    return;
+  } else if (!password) {
+    sendUserError({ message: 'must provide a password' }, res);
+    return;
+  }
+  bcrypt.hash(password, BCRYPT_COST, (err, hash) => {
+    if (err) throw err;
+    req.username = username;
+    req.passwordHash = hash; // better name it pw
+    next();
+  });
+};
+
 // TODO: implement routes
+server.post('/users', hashingMiddleware, (req, res) => {
+  const { username, passwordHash } = req;
+  const newUser = new User({ username, passwordHash });
+  newUser.save().then((createdUser) => {
+    res.status(200).json(createdUser);
+  })
+  .catch((err) => {
+    res.status(500).json({ err });
+    return;
+  });
+});
+
+const authenticate = (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username) {
+    sendUserError({ message: 'must provide a username' }, res);
+    return;
+  } else if (!password) {
+    sendUserError({ message: 'must provide a password' }, res);
+    return;
+  }
+  User.findOne({ username })
+    .exec()
+    .then((user) => {
+      const userPassword = user.passwordHash;
+      bcrypt.compare(password, userPassword, (error, isValid) => {
+        if (error) throw error;
+        if (!isValid) {
+          res.status(STATUS_USER_ERROR).json({ success: false });
+        }
+        req.session.loggedIn = isValid;
+        req.session.username = user.username;
+        next();
+      });
+    })
+    .catch((err) => {
+      res.status(STATUS_USER_ERROR).json({ errorMessage: err.message }); // user not found;
+      return;
+    });
+};
+
+server.post('/log-in', authenticate, (req, res) => {
+  if (req.session.loggedIn) {
+    res.status(200).json({ success: true });
+  }
+});
+
+const isUserLoggedIn = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    sendUserError({ message: 'you are not logged in' }, res);
+    return;
+  }
+  User.findOne({ username: req.session.username })
+    .exec()
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      sendUserError(err, res);
+      return;
+    });
+};
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', isUserLoggedIn, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
