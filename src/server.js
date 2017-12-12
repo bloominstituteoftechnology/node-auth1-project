073480b5625ec09d0bf1d85d3321d0f4ed397f9a
Sam Kim <sams404link@gmail.com>
2017-12-11 17:05:56 -0800

    sprint updated

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
+const User = require('./user.js');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -23,10 +25,85 @@ const sendUserError = (err, res) => {
   }
 };
 
-// TODO: implement routes
+// Middlewares
+const hashedPassword = (req, res, next) => {
+  const { passwordHash } = req.body;
+  bcrypt
+    .hash(passwordHash, 11)
+    .then((pw) => {
+      req.passwordHash = pw;
+      next();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
 
+const authenticate = (req, res, next) => {
+  const { username, passwordHash } = req.body;
+  User.findOne({ username })
+    .then((user) => {
+      const hashedPw = user.passwordHash;
+      bcrypt
+        .compare(passwordHash, hashedPw)
+        .then((pw) => {
+          if (!pw) {
+            throw new Error();
+          }
+          req.loggedUser = user.username;
+          next();
+        })
+        .catch((err2) => {
+          return sendUserError('Unknown error', res);
+        });
+    })
+  .catch((err) => {
+    res.status(422).json(err);
+  });
+};
+
+// TODO: implement routes
+server.post('/users', hashedPassword, (req, res) => {
+  const { username } = req.body;
+  const passwordHash = req.passwordHash;
+  const newUser = new User({ username, passwordHash });
+  newUser
+    .save()
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(422).json({ error: err.message });
+      return;
+    });
+});
+
+server.get('/users', (req, res) => {
+  User.find({})
+    .then((user) => {
+      res.status(422).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+server.delete('/users/:user_id', (req, res) => {
+  const { user_id } = req.params;
+  User.findById({ _id: user_id })
+    .then((user) => {
+      user.remove();
+      res.status(422).json({ success: true });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+server.post('/user/login', authenticate, (req, res) => {
+  res.status(200).json({ sucess: `${req.loggedUser} has successfully logged in.` });
+});
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', authenticate, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
