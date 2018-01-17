12e8474fd57bb92169e8355db1d501a92c58a130
sarika <sarikaresume@gmail.com>
2018-01-17 15:48:07 -0800

    signup

---
+++ signin/src/SignUp.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+render() {
+    return (
+        <form>
+            <inputtype="text"
+            onChange={}
+            />
+            </form>
+    );
+}
+}
+
+export default SignUp; 
