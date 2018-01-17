36f48795cb78ba45fa19b4e99195f2275e158887
sarika <sarikaresume@gmail.com>
2018-01-16 17:02:18 -0800

    hello

--- src/server.js
+++ src/server.js
@@ -1,66 +1,84 @@
-//Now start editing `src/server.js`. Note that we've provided you a helper
-//function `sendUserError()` that can send down either an object error or a string
-// error. You'll use this liberally in your routes.
-
-// We've also gone ahead and initialized the express-session middleware so you can
-// use the client-specific, persistent `req.session` object in your route handlers.const bodyParser = require('body-parser');
 const express = require('express');
-const session = require('express-session');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
-const STATUS_USER_ERROR = 422;
-const BCRYPT_COST = 11;
-
+const port = process.env.PORT || 3043; 
 const server = express();
-// to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
-
-/* Sends the given err, a string or an object, to the client. Sets the status
- * code appropriately. */
-const sendUserError = (err, res) => {
-  res.status(STATUS_USER_ERROR);
-  if (err && err.message) {
-    res.json({ message: err.message, stack: err.stack });
-  } else {
-    res.json({ error: err });
+const Schema = mongoose.Schema({
+const UserSchema = Schema({ 
+  email: {
+    type: String, 
+    required: true, 
+    unique: true
+  }, 
+  password: {
+    type: String, 
+    required: true
   }
-};
-
-// TODO: implement routes
-server.post ('/user', userName(req, res))
-server.post ('/user', userName)
-server.get ('/user', userName)
-server.post ('/server', passwordCreate)
-server.get ('/server', password)
-
-
-// TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
-  const userName = require('../models/username');
-  module.exports = userName = (req, res) => {
-     const {title, userName} = req.body;
-     const userName = new userName()
-     userName.save({title, userName}) 
-       .then(
-           (results) => {
-             //do something here are the results
-             res.status(200).json(results)
-             console.log('user has created userName');
-           }
-         )
-       .catch((err) => {
-           //do something theres be an error
-           res.status(500).json(err)
-           console.log('problem creating new userName');
-       })
-       .next. 
- 
- } 
-
-  // Do NOT modify this route handler in any way.
-  res.json(req.user);
 });
 
-module.exports = { server };
+const model = mongoose.model('User', UserSchema);
+//pre save hooks 
+
+server.use(
+  session({
+    secret: 'grizzlybear',
+    resave: false, 
+    saveUninitialized: true
+  })
+);
+
+server.listen(3000, () => {
+  console.log('Listening to 3000');
+});
+
+
+//pre save hooks 
+const User = mongoose.model('User',UserSchema);
+
+mongoose.Promise = global.Promise; 
+mongoose.connect('mongodb://localhost/bcrypt-users', { useMongoClient: true });
+
+/*  #################### Middlewares ######################## */
+
+const passwordEncrypt = (req, res, next) => {
+  const { password } = req.body; 
+  bcrypt
+    .hash(password, 11)
+    .then(hash => {
+      req.hash = hash; 
+      next();
+    })
+    .catch(err => {
+       throw new Error(err);
+    });
+  };
+ 
+ const passwordCompare = (req, res, next) => {
+   const { email, password } = req.body; 
+
+/* ###################### API #################### */
+
+server.post('/user/sign-up', passwordEncrypt, (req, res) => {
+  const { email } = req.body;
+  const { hash } = req;
+  const newUser = new User({ email, password: hash });
+  User.save((err, savedUser) => {
+    if (err) res.status(422).json(err);
+    res.json({ success: { savedUser: savedUser.username } });
+  });
+});
+    
+server.post ('/user/log-in', (req, res) => {
+  const { email, password } = req.body; 
+  // use req.session.. 
+  // if in db, send info back to client, 
+  // else sendUserError
+});
+
+server.listen(port, err => {
+  if (err) console.log(err);
+  console.log('server listening on ${port}');
+});
--- src/user.js
+++ src/user.js
@@ -1,9 +1,3 @@
-//`src/user.js`
-// First, write the schema for the user model in `src/user.js`. Each user has two
-// properties: `username`, a String, and `passwordHash`, also a String. Both
-// properties are required, and the username should be unique (use the option
-// `unique: true`).  This prevents two users from having the same username.
-
 const mongoose = require('mongoose');
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
@@ -14,12 +8,16 @@ mongoose.modelSchemas = {};
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   // TODO: fill in this schema
-});new Schema({
-     index: true, 
-     title: String, 
-     username: Sarika,
-     passwordHash: sarika1234
+username: {
+     type: String, 
+     required: true,
+     unique: true, 
+},
+     
+password: {
+  type: String, 
+  required: true,
    
 module.exports = mongoose.model('User', UserSchema);
