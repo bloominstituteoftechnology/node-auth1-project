88704eb4af9a8e51943b3ff7f0398967f24ac62a
Ting Wang <felix.wang.1026@gmail.com>
2018-01-16 19:19:36 -0500

    Finished the project

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const User = require('./user');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/user', { useMongoClient: true });
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -9,7 +15,9 @@ const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+  resave: false,
+  saveUninitialized: true
 }));
 
 /* Sends the given err, a string or an object, to the client. Sets the status
@@ -24,9 +32,80 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+const hashPasswordMiddle = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) return sendUserError(new Error('Please provide the password'), res);
+  bcrypt.hash(password, BCRYPT_COST)
+    .then((hash) => {
+      req.passwordHash = hash;
+      next();
+    })
+    .catch((error) => {
+      sendUserError(error, res);
+    });
+};
+
+const loginMiddle = (req, res, next) => {
+  if (!req.session.username) return sendUserError(new Error('No user logged in'), res);
+  const username = req.session.username;
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) return sendUserError(new Error('User not found'), res);
+      req.user = user;
+      next();
+    })
+    .catch((error) => {
+      sendUserError(error, res);
+    });
+};
+
+server.use('/restricted/*', (req, res, next) => {
+  if (!req.session.username) return sendUserError(new Error('No user logged in'), res);
+  const username = req.session.username;
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) return sendUserError(new Error('No user found'), res);
+      req.user = user;
+      next();
+    })
+    .catch((error) => {
+      sendUserError(error, res);
+    });
+});
+
+server.post('/users', hashPasswordMiddle, (req, res) => {
+  const { username } = req.body;
+  if (!username) return sendUserError(new Error('Please provide username'), res);
+  const { passwordHash } = req;
+  const newUser = new User({ username, passwordHash });
+  newUser.save()
+    .then((user) => {
+      if (!user) sendUserError(new Error('User created failed'), res);
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      sendUserError(error, res);
+    });
+});
+
+server.post('/log-in', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) return sendUserError(new Error('Please provide both username and password'), res);
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) return sendUserError(new Error('User not found'), res);
+      if (!bcrypt.compareSync(password, user.passwordHash)) return sendUserError(new Error('Invalid password'), res);
+      req.session.username = username;
+      req.user = user;
+      res.status(200).json({ success: true });
+    })
+    .catch((error) => {
+      sendUserError(error, res);
+    });
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', loginMiddle, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
