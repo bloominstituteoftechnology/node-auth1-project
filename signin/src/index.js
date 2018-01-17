d98bb90ddbd2a280de733879b83321c64ae8477c
sarika <sarikaresume@gmail.com>
2018-01-17 15:58:15 -0800

    index

--- signin/src/index.js
+++ signin/src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import Login from './Login';
+import SignUp from './SignUp';
+import UserPage from './UserPage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+     <Route exact path="/" component={App} />
+     <Route path="/login" component={Login} />
+     <Route path="/signup" component={SignUp} />
+     <Route path="/users" component={UserPage} />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+
+
+
+
+
+
+(<App />, document.getElementById('root'));
 registerServiceWorker();
