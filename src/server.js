2d496b522628be68fa2488d879835c920e042ddb
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-02-27 17:14:39 -0500

    Finished all routes and restricted stretch

--- src/server.js
+++ src/server.js
@@ -14,6 +14,17 @@ server.use(session({
   secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
 }));
 
+server.use((req, res, next) => {
+  const check = req.originalUrl.split('/restricted');
+  if (check.length > 1 && req.session.user) {
+    next();
+  } else if (check.length > 1 && !req.session.user) {
+    res.status(500).send({ error: 'You must be logged in for access to NSA documents.' });
+  } else {
+    next();
+  }
+});
+
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
 const sendUserError = (err, res) => {
@@ -25,7 +36,16 @@ const sendUserError = (err, res) => {
   }
 };
 
-// TODO: implement routes
+const logInCheck = (req, res, next) => {
+  const session = req.session;
+  if (session.user) {
+    req.user = session.user;
+    next();
+  } else {
+    sendUserError('Need to log in to access NSA data.', res);
+  }
+};
+
 
 server.post('/users', (req, res) => {
   const { username, password } = req.body;
@@ -35,27 +55,53 @@ server.post('/users', (req, res) => {
     return;
   }
 
-  const hashed = bcrypt.hash(password, BCRYPT_COST, (err, hash) => {
-    if (err) {
-      throw err;
-    }
-    console.log(hash);
-  });
-
-  User.create({ username, hashed })
-    .save()
-    .then((user) => {
-      res.status(200).send(user);
-    }).catch((err) => {
-      sendUserError('Could not save new user.', res);
+  bcrypt.hash(password, BCRYPT_COST)
+    .then((data) => {
+      const user = new User({ username, passwordHash: data })
+      user.save()
+        .then((newUser) => {
+          res.status(200).send(newUser);
+        }).catch((err) => {
+          sendUserError('Could not save new user.', res);
+        });
     });
-
-  // res.status(200).send({ username, password });
 });
 
-// TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
-  // Do NOT modify this route handler in any way.
+// to test stretch problem
+server.get('/restricted/users', (req, res) => {
+  User.find()
+    .then((user) => {
+      res.status(200).send(user);
+    }).catch((error) => {
+      res.status(500).send({ error: 'Unable to retrieve users.' });
+    });
+});
+
+server.post('/log-in', (req, res) => {
+  const { username, password } = req.body;
+  const session = req.session;
+
+  if (!username || !password) {
+    sendUserError('Must provide both username & password.', res);
+    return;
+  }
+
+  User.findOne({ username })
+    .then((data) => {
+      bcrypt.compare(password, data.passwordHash)
+       .then((match, err) => {
+         if (match) {
+           session.loggedIn = true;
+           session.user = username;
+           res.status(200).send({ success: session.loggedIn });
+         } else {
+           sendUserError({ success: false }, res);
+         }
+       });
+    });
+});
+
+server.get('/me', logInCheck, (req, res) => {
   res.json(req.user);
 });
 
