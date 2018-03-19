79a1dc731b1ca193dfbf9324299031596c09eac2
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-19 16:05:56 -0400

    First Commit/Schema Finished

--- src/server.js
+++ src/server.js
@@ -8,9 +8,13 @@ const BCRYPT_COST = 11;
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
+server.use(
+  session({
+    secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
--- src/user.js
+++ src/user.js
@@ -9,7 +9,15 @@ mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
-  // TODO: fill in this schema
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('User', UserSchema);
