b91adf0ca3d0a394033d7b3337ff3d7b437d3d86
cliffkang <cliffgkang@gmail.com>
2018-03-19 16:00:45 -0700

    got post working

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
-const bodyParser = require('body-parser');
+// import { constants } from 'fs';
+
 const express = require('express');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
+
+const User = require('./user.js');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
 
 const server = express();
 // to enable parsing of json bodies for post requests
-server.use(bodyParser.json());
+server.use(express.json());
 server.use(session({
   secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
 }));
@@ -24,6 +28,19 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+server.post('/users', (req, res) => {
+  const pass = req.body.password;
+  const userN = req.body.username;
+  if (!userN) sendUserError({ message: 'username not given' }, res);
+  if (pass === '') sendUserError({ message: 'password not given' }, res);
+  bcrypt.hash(pass, BCRYPT_COST, (err, hashedPw) => {
+    if (err) sendUserError({ message: 'error hashing password' }, res);
+    const user = new User({ username: userN, passwordHash: hashedPw });
+    user.save().then((savedUser) => {
+      res.status(200).json(savedUser);
+    }).catch(error => sendUserError(error));
+  });
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
 server.get('/me', (req, res) => {
