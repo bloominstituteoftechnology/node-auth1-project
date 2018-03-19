15a71747a6b57ebf6ddb55091413fda516406f53
Nathan Daniel <natdf@icloud.com>
2018-03-19 18:43:51 -0500

    Commit 1 in server.js: - continued work on Post /users - added bcrypt functionality

--- src/server.js
+++ src/server.js
@@ -9,11 +9,13 @@ const BCRYPT_COST = 11;
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
-  resave: true,
-  saveUninitialized: false,
-}));
+server.use(
+  session({
+    secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -27,18 +29,31 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
-const userMiddleware = (req,res,next) => {
-
-}
+const userMiddleware = (req, res, next) => {};
 
 // TODO: add local middleware to this route to ensure the user is logged in
+
 server.get('/me', (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
 
-server.post('/users', (req,res) => {
+server.post('/users', (req, res) => {
+  const { userName, userPass } = req.body;
+  const newUser = new User();
 
+  bcrypt.hash(userPass, BCRYPT_COST, (err, hash) => {
+    if (err) {
+      sendUserError('Error!', res);
+    }
+  });
+  res.json(newUser);
+
+  if (!userName || !userPass) {
+    sendUserError('Error: Invalid username or password', res);
+  } else {
+    res.json(newUser);
+  }
 });
 
 module.exports = { server };
