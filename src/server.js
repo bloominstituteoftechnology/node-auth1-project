ecadc92791ebb8b593454da79f41000c8cf8e870
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-19 17:07:44 -0500

    Begin POST Route
    in server.js:
    require User from schema file
    add POST route for '/users'
    require username and password via conditional
    save new user
    add basic structure to test route
    test route (success)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const User = require('./user.js');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -24,6 +25,23 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+server.post('/users', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    sendUserError('You must provide a valid username and password to sign up', res);
+  }
+  const passwordHash = password;
+  const user = new User({
+    username, passwordHash
+  });
+  user.save()
+    .then((newUser) => {
+      res.json(newUser);
+    })
+    .catch((err) => {
+      sendUserError(err, res);
+    });
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
 server.get('/me', (req, res) => {
