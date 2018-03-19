3fbf32d8954db70dc2011afe215cb2b8b956631d
shobanavv <shobanavv@gmail.com>
2018-03-19 15:29:57 -0700

    created schema

--- src/server.js
+++ src/server.js
@@ -26,9 +26,10 @@ const sendUserError = (err, res) => {
 // TODO: implement routes
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me',sendUserError, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
+  
 });
 
 module.exports = { server };
