2608729a350f663301017afef5f4bbce097a53e5
Christopher Beards <beardscm@gmail.com>
2018-03-19 18:14:23 -0400

    users Route is Functional

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
+const User = require('./user.js');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -30,11 +32,45 @@ const sendUserError = (err, res) => {
   }
 };
 
-// TODO: implement routes
+const hashedPwd = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    res.status(400).json({ message: 'A password is required!' });
+  }
+  bcrypt
+    .hash(password, BCRYPT_COST)
+    .then(password => {
+      req.password = password;
+      next();
+    })
+    .catch(err => {
+      res.status(500).sendUserError();
+    });
+};
 
 
 //==============================================================================
 //                                ROUTES
+//==============================================================================
+
+server.post('/users', hashedPwd, (req, res) => {
+  const { username } = req.body;
+  const passwordHash = req.password;
+  const newUser = new User({ username, passwordHash });
+  newUser
+    .save()
+    .then(obj => {
+      res.status(200).json(obj);
+    })
+    .catch(err => {
+      res.status(500).sendUserError();
+    });
+});
+
+
+
+//==============================================================================
+//                                SERVER INFORMATION
 //==============================================================================
 
 
