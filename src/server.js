4b94d06b4d4af3242fd9ba2ce2044b74eef595b4
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-19 19:03:35 -0400

    Add GET /me and Stretch Problem
    Added a GET route at the /me endpoint that utilizes local middleware which we created.
    Middleware first checks if req.session.isAuth property on session is true.
    If true, page returns user information. If false, user receives error.
    
    Ran all tests and debugged file until every test passed.
    
    Stretch Problem:
    Created a piece of global middleware that runs on any endpoint with the /restricted path.
    If req.session.isAuth property is true, user may proceed. If not, they are faced with
    an error.

--- src/server.js
+++ src/server.js
@@ -12,11 +12,13 @@ const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+  resave: true,
+  saveUninitialized: false,
 }));
 
 /* Sends the given err, a string or an object, to the client. Sets the status
- * code appropriately. */
+* code appropriately. */
 const sendUserError = (err, res) => {
   res.status(STATUS_USER_ERROR);
   if (err && err.message) {
@@ -26,26 +28,28 @@ const sendUserError = (err, res) => {
   }
 };
 
+
 // TODO: implement routes
 server.post('/users', (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
     res.status(422).json({ error: 'Username and Password required' });
-  }
-  bcrypt.hash(password, BCRYPT_COST, (err, passwordHash) => {
-    const newUser = { username, passwordHash };
-    const user = new User(newUser);
-    user.save()
+  } else {
+    bcrypt.hash(password, BCRYPT_COST, (err, passwordHash) => {
+      const newUser = { username, passwordHash };
+      const user = new User(newUser);
+      user.save()
       .then((response) => {
         // console.log('response', response);
         res.status(200).json({ username: response.username, passwordHash: response.passwordHash });
       })
       .catch(error => sendUserError(error, res));
-  });
+    });
+  }
 });
 
 server.post('/log-in', (req, res) => {
-  const UA = req.headers['cookie'];
+  const UA = req.headers.cookie;
   req.session.UA = UA;
   const { username, password } = req.body;
   if (!username || !password) {
@@ -53,12 +57,15 @@ server.post('/log-in', (req, res) => {
   }
   User.findOne({ username })
   .then((response) => {
+    if (!response) res.status(422).json({ error: 'Incorrect Username' });
     bcrypt.compare(password, response.passwordHash)
     .then((pass) => {
       if (pass) {
         req.session.username = username;
         req.session.isAuth = true;
         res.status(200).json({ success: true });
+      } else {
+        res.status(422).json({ error: 'Incorrect Password' });
       }
     })
     .catch(err => sendUserError(err, res));
@@ -66,10 +73,40 @@ server.post('/log-in', (req, res) => {
   .catch(err => err);
 });
 
-// TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+const userLoggedIn = (req, res, next) => {
+  if (!req.session.isAuth) {
+    res.status(422).json({ error: 'Not logged in!' });
+  } else {
+    User.findOne({ username: req.session.username })
+    .select('username passwordHash -_id')
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Error' });
+    });
+  }
+};
+const restrictedCheck = (req, res, next) => {
+  if (req.session) {
+    if (!req.session.isAuth) {
+      res.status(422).json({ error: 'Restricted! Not logged in!' });
+    } else {
+      next();
+    }
+  }
+};
+
+server.use('/restricted', restrictedCheck);
+
+server.get('/restricted/test', (req, res) => {
+  res.status(200).json({ success: "You're logged in!" });
+});
+
+  // TODO: add local middleware to this route to ensure the user is logged in
+server.get('/me', userLoggedIn, (req, res) => {
   // Do NOT modify this route handler in any way.
-  console.log(req.session);
   res.json(req.user);
 });
 
