b5328ba13d363fe76643e95be0dd9b600f7b4969
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-19 17:55:07 -0600

    Complete users post route
    Include password hashing middelware
    
    Save user to MongoDB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+
+const User = require('./user');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -25,19 +28,37 @@ const sendUserError = (err, res) => {
   }
 };
 
+const hashPasswordMiddleware = (req, res, next) => {
+  const { passwordHash } = req.body;
+  
+  if (!passwordHash) {
+    sendUserError('You must provide a password', res);
+    return;
+  }
+  bcrypt.hash(passwordHash, BCRYPT_COST)
+    .then(hashedPassword => {
+      req.password = hashedPassword;
+      next()
+    })
+    .catch(error => { error: 'There was an error', error });
+};
+
 // TODO: implement routes
-server.post('/users', (req, res) => {
-  const { username, password } = req.body;
+server.post('/users', hashPasswordMiddleware, (req, res) => {
+  const { username, passwordHash } = req.body;
+  const hashedPW = req.password;
+  const user = new User({ username: username, passwordHash: hashedPW });
 
   if (!username) {
     res.status(STATUS_USER_ERROR).json({ error: 'You must provide a username' });
   }
-  if (!password) {
-    res.status(STATUS_USER_ERROR).json({ error: 'You must provide a password' });
-  }
 
-  // hash password
   // save user in MongoDB (create)
+  user.save()
+  .then(savedUser => {
+    res.status(200).json(savedUser);
+  })
+  .catch(error => { error: 'There was an error saving user' });
 });
 
 // TODO: add local middleware to this route to ensure the user is logged in
