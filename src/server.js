48c5076151f84b83bc02dd6c128461afe1bbeca4
Dani Tacheny <dani.tacheny@gmail.com>
2018-03-20 14:32:14 -0500

    add global middleware that checks if user is logged in

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
@@ -11,18 +12,19 @@ const User = require('./user.js');
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
-  authed: false,
-  resave: true,
-  saveUninitialized: false,
-}));
+server.use(
+  session({
+    secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+    authed: false,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 
 const authenticateUserMW = (req, res, next) => {
   const session = req.session;
   if (session.UA === req.headers['cookie']) {
-    User
-      .findOne({ username: session.username })
+    User.findOne({ username: session.username })
       .then(foundUser => {
         req.user = foundUser;
         console.log('The value of req.user is:', req.user);
@@ -32,7 +34,24 @@ const authenticateUserMW = (req, res, next) => {
   } else {
     res.status(500).send({ errorMessage: 'Ya dun goofed' });
   }
-}
+};
+
+const restrictedAccess = (req, res, next) => {
+  const path = req.path.split('/');
+  if (path[1] === 'restricted') {
+    if (req.session.UA === req.headers['cookie']) {
+      next();
+    } else {
+      res.status(403).send({ message: "You're not logged in" });
+    }
+  } else {
+    next();
+  }
+};
+
+// localhost:3000/restricted/
+
+server.use(restrictedAccess);
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -50,38 +69,42 @@ server.post('/users/:username&:password', (req, res) => {
   const { username, password } = req.params;
   bcrypt.hash(password, BCRYPT_COST, (err, passwordHash) => {
     if (err) return sendUserError(err);
-    const newUser = new User({username, passwordHash})
+    const newUser = new User({ username, passwordHash });
     newUser
       .save()
-      .then((savedUser) => res.status(201).send(savedUser))
+      .then(savedUser => res.status(201).send(savedUser))
       .catch(err => sendUserError(err, res));
-  })
-})
+  });
+});
 
-server.post('/users/log-in/:username&:password', (req, res) => {
+server.post('/log-in/:username&:password', (req, res) => {
   const { username, password } = req.params;
   User.findOne({ username: username })
     .then(foundUser => {
-      bcrypt.compare(password, foundUser.passwordHash, (err, passwordsMatch) => {
-        if (passwordsMatch) {
-          const session = req.session;
-          session.username = username;
-          session.UA = req.headers['cookie'];
-          res.status(500).send({ success: true });
-        } else {
-          res.status(500).send({errorMessage: 'Passwords don\'t match!'});
+      bcrypt.compare(
+        password,
+        foundUser.passwordHash,
+        (err, passwordsMatch) => {
+          if (passwordsMatch) {
+            const session = req.session;
+            session.username = username;
+            session.UA = req.headers['cookie'];
+            res.status(500).send({ success: true });
+          } else {
+            res.status(500).send({ errorMessage: "Passwords don't match!" });
+          }
         }
-      })
+      );
     })
     .catch(err => sendUserError(err, res));
-})
+});
 
-server.get('/users/dev', (req, res) => {
+server.get('/restricted/dev', (req, res) => {
   res.send(req.session);
   User.find({})
     .then(results => res.status(200).send(results))
     .catch(err => sendUserError(err, res));
-})
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
 server.get('/me', authenticateUserMW, (req, res) => {
