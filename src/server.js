0e165c0e8e6b542e5b25156f388fcc6a1eb296ee
temp <temp@Debis-Home-Mac.local>
2018-03-20 16:22:33 -0400

    Finished main + stretch.

--- src/server.js
+++ src/server.js
@@ -3,14 +3,45 @@ const express = require('express');
 const session = require('express-session');
 
 const STATUS_USER_ERROR = 422;
-const BCRYPT_COST = 11;
+
+const User = require('./user.js');
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
+server.use(
+  session({
+    secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+
+// Stretch problem global middleware
+const restrictedAccess = (req, res, next) => {
+  const session = req.session;
+  if (session.UA === req.headers['cookie']) next();
+  else res.status(404).send({ msg: 'You must log in to view this page.' });
+};
+
+// Stretch problem solution line which pairs with restrictedAccess on line 22
+server.use('/restricted', restrictedAccess);
+
+const authenticateUserMW = (req, res, next) => {
+  const session = req.session;
+  console.log(session.UA);
+  console.log(req.headers['cookie']);
+  if (session.UA === req.headers['cookie']) {
+    User.findOne({ username: session.username })
+      .then(foundUser => {
+        req.user = foundUser;
+        next();
+      })
+      .catch(err => sendUserError(err, res));
+  } else {
+    res.status(500).send({ errorMessage: 'You are not logged in.' });
+  }
+};
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -24,9 +55,52 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+server.post('/users/:username&:password', (req, res) => {
+  const { username, password } = req.params;
+  const newUser = new User({ username, passwordHash: password });
+  newUser
+    .save()
+    .then(savedUser => res.status(201).send(savedUser))
+    .catch(err => sendUserError(err, res));
+});
+
+server.post('/log-in/:username&:password', (req, res) => {
+  const { username, password } = req.params;
+  User.findOne({ username: username })
+    .then(foundUser => {
+      if (foundUser === null)
+        res.status(404).send({ Error: 'Must use valid username/password' });
+      else {
+        foundUser.checkPassword(password, (err, validated) => {
+          if (err) return sendUserError(err);
+          else if (!validated)
+            res.status(404).send({ Error: 'Must use valid username/password' });
+          else if (validated) {
+            const session = req.session;
+            session.username = username;
+            session.UA = req.headers['cookie'];
+            console.log(session.UA);
+            console.log(req.headers['cookie']);
+            res.status(500).send({ success: true });
+          }
+        });
+      }
+    })
+    .catch(err => sendUserError(err, res));
+});
+
+server.get('/dev', (req, res) => {
+  User.find({})
+    .then(results => res.status(200).send(results))
+    .catch(err => sendUserError(err, res));
+});
+
+server.get('/restricted/hello', (req, res) => {
+  res.send('Hello world');
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', authenticateUserMW, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
--- src/user.js
+++ src/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
@@ -6,10 +7,45 @@ mongoose.models = {};
 mongoose.modelSchemas = {};
 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
+
+const BCRYPT_COST = 11;
+
+mongoose
+  .connect('mongodb://localhost/users', { useMongoClient: true })
+  .then(res => {
+    console.log('Successfully connected to MongoDB');
+  })
+  .catch(err => {
+    console.log('Database connection failed. Error: ', err);
+  });
 
 const UserSchema = new mongoose.Schema({
   // TODO: fill in this schema
+  username: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.methods.checkPassword = function(potentialPass, cb) {
+  bcrypt.compare(potentialPass, this.passwordHash, (err, passwordsMatch) => {
+    if (err) return cb(err);
+    cb(null, passwordsMatch);
+  });
+};
+
+UserSchema.pre('save', function(next) {
+  bcrypt.hash(this.passwordHash, BCRYPT_COST, (err, hashed) => {
+    if (err) return next(err);
+    this.passwordHash = hashed;
+    next();
+  });
 });
 
 module.exports = mongoose.model('User', UserSchema);
