5a677c0d5a030c4d1073d63d43816469f7ec0314
ashleijo <shuryjo@gmail.com>
2018-03-20 15:35:46 -0400

    adding functionality to hash password from server.js
    still trying to figure out the mongo pre middleware

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const User = require('./user');
+const bcrypt = require('bcrypt');
+
+const BCRYPT_COST = 11;
 
 const STATUS_USER_ERROR = 422;
-const BCRYPT_COST = 11;
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+  resave: true,
+  saveUninitialized: false,
 }));
 
 /* Sends the given err, a string or an object, to the client. Sets the status
@@ -24,11 +29,74 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+const checkUserLoggedIn = (req, res, next) => {
+  const thisSession = req.session;
+  if (thisSession.user) {
+    User.findOne({ username: thisSession.user })
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch((err) => {
+        sendUserError(err, res);
+      });
+  } else {
+    sendUserError('You must first log in.', res);
+  }
+};
+
+server.post('/users', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    sendUserError('You will need to enter a username & password.', res);
+    return;
+  }
+  bcrypt.hash(password, BCRYPT_COST).then((data) => {
+    const user = new User({ username, passwordHash: data });
+    user
+      .save()
+      .then((newUser) => {
+        res.status(200).send(newUser);
+      })
+      .catch((err) => {
+        sendUserError('Error when attemping to save user.', res);
+      });
+  });
+});
+
+/* server.post('/log-in', (req, res) => {
+  const { username, password } = req.body;
+  const thisSession = req.session;
+  if (!username || !password) {
+    sendUserError('You will need to enter a username & password.', res);
+    return;
+  }
+
+  User.findOne({ username })
+    .then((data) => {
+      bcrypt
+        .compare(password, data.passwordHash)
+        .then((match, err) => {
+          if (match) {
+            thisSession.user = username;
+            res.status(200).send({ success: true });
+          } else {
+            sendUserError({ success: false }, res);
+          }
+        })
+        .catch((error) => {
+          sendUserError({ error: 'Password was invalid.' });
+        });
+    })
+    .catch((err) => {
+      sendUserError({ error: 'Error when attempting to find user.' }, res);
+    });
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', checkUserLoggedIn, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
-});
+}); */
 
 module.exports = { server };
