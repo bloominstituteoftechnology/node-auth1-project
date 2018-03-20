45f98c030b9798b865ee8d063f0f1c842ec3f284
Smithface <kylewibblesmith@gmail.com>
2018-03-20 14:18:34 -0400

    added Ryan refactoring
    also fixed merge conflicts

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const bcrypt = require('bcrypt');
 const User = require('./user');
 
 const STATUS_USER_ERROR = 422;
-const BCRYPT_COST = 11;
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -25,76 +24,6 @@ const sendUserError = (err, res) => {
     res.json({ error: err });
   }
 };
-
-// TODO: implement routes
-<<<<<<< HEAD
-=======
-// server.post('/users', (req, res) => {
-//   if (!req.body.password) { sendUserError({ error: 'password required' }, res); }
-//   const userInfo = {
-//     username: req.body.username,
-//     passwordHash: req.body.password
-//   };
-//   const newUser = new User(userInfo);
-//   bcrypt.hash(newUser.passwordHash, BCRYPT_COST, (err, passwordHash) => {
-//     if (err) {
-//       sendUserError(err, res);
-//     } else {
-//       newUser.passwordHash = passwordHash;
-//       newUser.save()
-//         .then((user) => {
-//           res.status(200).json(user);
-//         })
-//         .catch((catchErr) => {
-//           sendUserError(catchErr, res);
-//         });
-//     }
-//   });
-// });
-
-// server.post('/log-in', (req, res) => {
-//   const { username, password } = req.body;
-//   User
-//     .findOne({ username }, '_id passwordHash')
-//     .then((user) => {
-//       bcrypt.compare(password, user.passwordHash, (err, resp) => {
-//         if (!resp) {
-//           sendUserError(err, res);
-//         } else {
-//           session({
-//             secret: user._id, // eslint-disable-line no-underscore-dangle
-//             resave: true,
-//             saveUninitialized: false
-//           });
-//           res.status(200).json({ success: true });
-//         }
-//       });
-//     })
-//     .catch((err) => {
-//       // console.log('failure to find the One');
-//       sendUserError(err, res);
-//     });
-// });
-
-// const loggedIn = (req, res, next) => {
-//   const { username } = req.session;
-//   console.log(req.session);
-//   if (!username) {
-//     sendUserError('User is not logged in', res);
-//     return;
-//   }
-//   User.findOne({ username }, (err, user) => {
-//     if (err) {
-//       sendUserError(err, res);
-//     } else if (!user) {
-//       sendUserError('User does not exist', res);
-//     } else {
-//       req.user = user;
-//       next();
-//     }
-//   });
-// };
->>>>>>> c788d3f... stretch attempted
 
 server.post('/users', (req, res) => {
   if (!(req.body.password && req.body.username)) {
@@ -151,10 +80,6 @@ server.post('/log-in', (req, res) => {
 });
 
 const middleWare = (req, res, next) => {
-<<<<<<< HEAD
-=======
-  console.log(req.session);
->>>>>>> c788d3f... stretch attempted
   if (req.session.user) {
     User
       .findById(req.session.user)
