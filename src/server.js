1003d2156418cab56fb8d86c197635ad485cbfd8
Res Yeg <shaqk702@gmail.com>
2018-03-19 20:58:09 -0700

    stretch attempted

--- src/server.js
+++ src/server.js
@@ -27,6 +27,74 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+<<<<<<< HEAD
+=======
+// server.post('/users', (req, res) => {
+//   if (!req.body.password) { sendUserError({ error: 'password required' }, res); }
+//   const userInfo = {
+//     username: req.body.username,
+//     passwordHash: req.body.password
+//   };
+//   const newUser = new User(userInfo);
+//   bcrypt.hash(newUser.passwordHash, BCRYPT_COST, (err, passwordHash) => {
+//     if (err) {
+//       sendUserError(err, res);
+//     } else {
+//       newUser.passwordHash = passwordHash;
+//       newUser.save()
+//         .then((user) => {
+//           res.status(200).json(user);
+//         })
+//         .catch((catchErr) => {
+//           sendUserError(catchErr, res);
+//         });
+//     }
+//   });
+// });
+
+// server.post('/log-in', (req, res) => {
+//   const { username, password } = req.body;
+//   User
+//     .findOne({ username }, '_id passwordHash')
+//     .then((user) => {
+//       bcrypt.compare(password, user.passwordHash, (err, resp) => {
+//         if (!resp) {
+//           sendUserError(err, res);
+//         } else {
+//           session({
+//             secret: user._id, // eslint-disable-line no-underscore-dangle
+//             resave: true,
+//             saveUninitialized: false
+//           });
+//           res.status(200).json({ success: true });
+//         }
+//       });
+//     })
+//     .catch((err) => {
+//       // console.log('failure to find the One');
+//       sendUserError(err, res);
+//     });
+// });
+
+// const loggedIn = (req, res, next) => {
+//   const { username } = req.session;
+//   console.log(req.session);
+//   if (!username) {
+//     sendUserError('User is not logged in', res);
+//     return;
+//   }
+//   User.findOne({ username }, (err, user) => {
+//     if (err) {
+//       sendUserError(err, res);
+//     } else if (!user) {
+//       sendUserError('User does not exist', res);
+//     } else {
+//       req.user = user;
+//       next();
+//     }
+//   });
+// };
+>>>>>>> c788d3f... stretch attempted
 
 server.post('/users', (req, res) => {
   if (!(req.body.password && req.body.username)) {
@@ -83,6 +151,10 @@ server.post('/log-in', (req, res) => {
 });
 
 const middleWare = (req, res, next) => {
+<<<<<<< HEAD
+=======
+  console.log(req.session);
+>>>>>>> c788d3f... stretch attempted
   if (req.session.user) {
     User
       .findById(req.session.user)
@@ -103,5 +175,31 @@ server.get('/me', middleWare, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
+
+// stretch
+const restrictedMw = (req, res, next) => {
+  if (req.url.startsWith('/restricted')) {
+    if (req.session.user) {
+      User
+        .findById(req.session.user)
+        .then((user) => {
+          req.user = user;
+          next();
+        })
+        .catch((err) => {
+          sendUserError(err, res);
+        });
+    } else {
+      sendUserError({ message: 'No user is currently logged in.' }, res);
+    }
+  }
+};
+
+server.use(restrictedMw);
+
+server.get('/restricted/', (req, res) => {
+  res.send('You\'re accessing restricted content!');
+});
+
 
 module.exports = { server };
