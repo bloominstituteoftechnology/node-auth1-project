b6c5423ec54bf688693aa91e0238d3809bdfc5f0
ashleijo <shuryjo@gmail.com>
2018-03-21 14:03:10 -0400

    commiting after completeing todos for client-auth-mini

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const cors = require('cors');
 const User = require('./user');
 const bcrypt = require('bcrypt');
 
@@ -9,6 +10,11 @@ const BCRYPT_COST = 11;
 const STATUS_USER_ERROR = 422;
 
 const server = express();
+const corsOptions = {
+  "origin": "http://localhost:3000",
+  "credentials": true
+};
+server.use(cors(corsOptions));
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 server.use(session({
@@ -50,18 +56,17 @@ server.post('/users', (req, res) => {
   if (!username || !passwordHash) {
     sendUserError('You will need to enter a username & password.', res);
   }
-//  bcrypt.hash(password, BCRYPT_COST).then((data) => {
   const user = new User({ username, passwordHash });
   user.save()
-    .then((newUser) => {
-      res.status(200).json(newUser);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: 'There was a server error while signing up', err });
-    });
+  .then((newUser) => {
+    res.status(200).json(newUser);
+  })
+  .catch((err) => {
+    res.status(500).json({ error: 'There was a server error while signing up', err });
   });
+});
 
-/* server.post('/log-in', (req, res) => {
+server.post('/login', (req, res) => {
   const { username, password } = req.body;
   const thisSession = req.session;
   if (!username || !password) {
@@ -70,30 +75,51 @@ server.post('/users', (req, res) => {
   }
 
   User.findOne({ username })
-    .then((data) => {
-      bcrypt
-        .compare(password, data.passwordHash)
-        .then((match, err) => {
-          if (match) {
-            thisSession.user = username;
-            res.status(200).send({ success: true });
-          } else {
-            sendUserError({ success: false }, res);
-          }
-        })
-        .catch((error) => {
-          sendUserError({ error: 'Password was invalid.' });
-        });
-    })
-    .catch((err) => {
-      sendUserError({ error: 'Error when attempting to find user.' }, res);
-    });
+  .then((data) => {
+    bcrypt
+      .compare(password, data.passwordHash)
+      .then((match, err) => {
+        if (match) {
+          thisSession.user = username;
+          res.status(200).send({ success: true });
+        } else {
+          sendUserError({ success: false }, res);
+        }
+      })
+      .catch((error) => {
+        sendUserError({ error: 'Password was invalid.' });
+      });
+  })
+  .catch((err) => {
+    sendUserError({ error: 'Error when attempting to find user.' }, res);
+  });
 });
+
+
+server.post('/logout', (req, res) => {
+  if (!req.session.user) {
+    sendUserError('User is not logged in', res);
+    return;
+  }
+  req.session.user = null;
+  res.json(req.session);
+});
+
+server.get('/restricted/users', checkUserLoggedIn, (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      sendUserError('500', res);
+      return;
+    }
+    res.json(users);
+  });
+});
+
 
 // TODO: add local middleware to this route to ensure the user is logged in
 server.get('/me', checkUserLoggedIn, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
-}); */
+});
 
 module.exports = { server };
