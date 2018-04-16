99d975026316f1ca33e6796e5226fe160b171f05
Carlos Andrade <carlos.andrade86@outlook.com>
2018-04-16 18:19:41 -0400

    stupid linter

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
 
+const User = require('./user');
+
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
 
@@ -24,6 +26,15 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+server.post('/users', (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(savedUser => res.status(200).json(savedUser))
+    .catch(err => res.status(500).json(err));
+})
+
 
 // TODO: add local middleware to this route to ensure the user is logged in
 server.get('/me', (req, res) => {
--- src/user.js
+++ src/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
@@ -10,6 +11,35 @@ mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
   // TODO: fill in this schema
+  
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      index: true,
+      lowercase: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    }
+
 });
+
+UserSchema.pre('save', function(next) {
+  console.log('pre save hook');
+  bcrypt.hash(this.passwordHash, 12, (err, hash) => {
+    if(err) {
+      return next(err);
+    }
+
+    this.passwordHash = hash;
+    return next();
+  })
+})
+
+UserSchema.methods.isPasswordValid = function(passwordGuess) {
+  return bcrypt.compare(passwordGuess, this.passwordHash);
+}
 
 module.exports = mongoose.model('User', UserSchema);
