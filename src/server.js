a248c334b72246270ee5cfee6b57524f5f68a208
Justin Kaseman <justinkaseman@live.com>
2018-04-16 15:42:47 -0700

    Finish

--- src/server.js
+++ src/server.js
@@ -1,34 +1,60 @@
-const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
 const session = require('express-session');
 
-const STATUS_USER_ERROR = 422;
-const BCRYPT_COST = 11;
+const User = require('./user.js');
+
+mongoose.connect('mongodb://localhost/authDB');
 
 const server = express();
-// to enable parsing of json bodies for post requests
-server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
 
-/* Sends the given err, a string or an object, to the client. Sets the status
- * code appropriately. */
-const sendUserError = (err, res) => {
-  res.status(STATUS_USER_ERROR);
-  if (err && err.message) {
-    res.json({ message: err.message, stack: err.stack });
-  } else {
-    res.json({ error: err });
-  }
+server.use(express.json());
+server.use(
+  session({
+    secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// add local middleware to this route to ensure the user is logged in
+const isLoggedIn = (req, res, next) => {
+  if (!req.session.auth) res.status(500).json('not allowed');
+  next();
 };
 
-// TODO: implement routes
+server.post('/users', (req, res) => {
+  if (!req.body.username && req.body.password) {
+    res.status(400);
+  } else {
+    User.create(req.body)
+      .then(saved => res.status(201).json(saved))
+      .catch(error => res.status(500).json(error));
+  }
+});
 
-// TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
-  // Do NOT modify this route handler in any way.
+server.post('/log-in', (req, res) => {
+  if (!req.body.username && req.body.password) {
+    res.status(400);
+  } else {
+    User.findOne({ username: req.body.username })
+      .then((user) => {
+        if (user) {
+          user
+            .isPasswordValid(req.body.password)
+            .then((response) => {
+              req.session.auth = true;
+              res.status(200).json({ success: true });
+            })
+            .catch(error => res.status(400).json({ success: false }));
+        }
+      })
+      .catch(error => res.status(500).json(error));
+  }
+});
+
+server.get('/me', isLoggedIn, (req, res) => {
   res.json(req.user);
 });
 
-module.exports = { server };
+server.listen(5000, () => console.log('Server running'));
