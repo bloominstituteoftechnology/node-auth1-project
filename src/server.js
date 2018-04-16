2035425c20a7778fe9e61fd455a6c13689bb08e2
joshuacoyne <josh.j.coyne@gmail.com>
2018-04-16 18:49:49 -0400

    Server runnning and connected to mongodb

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcrypt');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
+
+const User = require('./user.js');
+
+mongoose
+  .connect('mongodb://localhost/data', { useMongoClient: true })
+  .then(() => {
+    console.log('\n=== Connected to MongoDb ===\n');
+  })
+  .catch(error => console.log('Error Connecting to the database', error));
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -24,6 +36,23 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+server
+  .post('/users', (req, res) => {
+    const { username, password } = req.body;
+    const passwordHash = bcrypt.hash(password, BCRYPT_COST, (err, hash) => {
+      if (err) {
+        return err;
+      }
+      console.log(hash);
+      return hash;
+    });
+    console.log(passwordHash);
+    const user = new User({ username, passwordHash });
+    user
+      .save()
+      .then(savedUser => res.status(201).json(savedUser))
+      .catch(err => res.status(500).json(err));
+  });
 
 // TODO: add local middleware to this route to ensure the user is logged in
 server.get('/me', (req, res) => {
