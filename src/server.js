caf636b8896fe638207b7be52c43be6cf23f9453
Juber Ahmed <a.juber@hotmail.com>
2018-04-16 16:34:14 -0400

    Completed /users

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
 
+const User = require('./user.js');
+
 const server = express();
 // to enable parsing of json bodies for post requests
-server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
+server.use(express.json());
+
+server.use(
+  session({
+    secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+  })
+);
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -24,6 +29,16 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+server.post('/users', (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => console.log('error'));
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
 server.get('/me', (req, res) => {
--- src/user.js
+++ src/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
 mongoose.models = {};
@@ -10,6 +10,26 @@ mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
   // TODO: fill in this schema
+  username: {
+    type: String,
+    unique: true,
+    required: true
+    // index: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  }
 });
 
+UserSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, 12, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+
+    return next();
+  });
+});
 module.exports = mongoose.model('User', UserSchema);
