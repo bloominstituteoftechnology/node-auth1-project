3c5d3fe81aa7690d9514afc57e1626dfc11aa735
Cristina Laryea <crispad06@gmail.com>
2018-04-17 16:24:45 -0400

    added methods, and fixed session

--- src/server.js
+++ src/server.js
@@ -1,18 +1,22 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const session = require('express-session');
+//const bodyParser = require("body-parser");
+const express = require("express");
+const session = require("express-session");
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
 
-const User = require('./user.js');
+const User = require("./user.js");
 
 const server = express();
 // to enable parsing of json bodies for post requests
-server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
+// server.use(bodyParser.json());
+server.use(
+  session({
+    secret: "e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -28,22 +32,28 @@ const sendUserError = (err, res) => {
 // TODO: implement routes
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get("/me", (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
 
-server.post('/users', (req, res) => {
+server.get("/", (req, res) => {
+  User.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post("/users", (req, res) => {
   const user = new User(req.body);
 
-  user  
+  user
     .save()
     .then(user => {
       res.status(200).json(user);
     })
-    .catch(err => 
-      res.status(500).json(err));
+    .catch(err => res.status(500).json(err));
 });
-
 
 module.exports = { server };
--- src/user.js
+++ src/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
@@ -12,13 +13,27 @@ const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     unique: true,
-    required: true,
-    index: true
+    required: true
   },
   passwordHash: {
     type: String,
     required: true
   }
 });
+
+UserSchema.pre("save", function(next) {
+  bcrypt.hash(this.passwordHash, 12, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.passwordHash = hash;
+
+    return next();
+  });
+});
+
+UserSchema.methods.isPasswordValid = function (passwordGuess) {
+  return bcrypt.compare(passwordGuess, this.password);
+};
 
 module.exports = mongoose.model("User", UserSchema);
