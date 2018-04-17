5e96ba14320c0aae3877e42fa437f68ee220ab8b
joshuacoyne <josh.j.coyne@gmail.com>
2018-04-17 18:41:41 -0400

    All tests passing

--- src/server.js
+++ src/server.js
@@ -22,8 +22,8 @@ const server = express();
 server.use(bodyParser.json());
 server.use(session({
   secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
-
+})
+);
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
 const sendUserError = (err, res) => {
@@ -35,29 +35,66 @@ const sendUserError = (err, res) => {
   }
 };
 
+const checkStatus = (req, res, next) => {
+  if (req.session.username) {
+    User.findOne({ username: req.session.username })
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch(err => res.status(500).json(err));
+  } else {
+    sendUserError({ message: 'NOT LOGGED IN' }, res);
+  }
+};
+
 // TODO: implement routes
+
+// function getHashedPassword(password) {
+//   let hashedPassword = "0";
+//   bcrypt.hash(password, 11, function(err, hash) {
+//     console.log(hash);
+//     if (hash) {
+//       hashedPassword = hash;
+//     }
+//   });
+//   return hashedPassword;
+// }
+
+// async function f1(password) {
+//   return await getHashedPassword(password)
+// }
 server
   .post('/users', (req, res) => {
     const { username, password } = req.body;
-    const passwordHash = bcrypt.hash(password, BCRYPT_COST, (err, hash) => {
-      if (err) {
-        return err;
-      }
-      console.log(hash);
-      return hash;
-    });
-    console.log(passwordHash);
-    const user = new User({ username, passwordHash });
+    const user = new User({ username, passwordHash: password });
     user
       .save()
-      .then(savedUser => res.status(201).json(savedUser))
-      .catch(err => res.status(500).json(err));
+      .then(savedUser => res.status(200).json(savedUser))
+      .catch(err => sendUserError(err, res));
   });
-
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', checkStatus, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
+});
+
+server.post('/log-in', (req, res) => {
+  const { username, password } = req.body;
+  User.findOne({ username })
+  .then((user) => {
+    bcrypt.compare(password, user.passwordHash)
+    .then((ans) => {
+      if (ans) {
+        req.session.username = username;
+        res.status(200).json({ success: true });
+      } else {
+        sendUserError({ message: 'Passwords didn\'t match' }, res);
+      }
+    })
+    .catch(err => sendUserError(err, res));
+  })
+  .catch(err => sendUserError(err, res));
 });
 
 module.exports = { server };
