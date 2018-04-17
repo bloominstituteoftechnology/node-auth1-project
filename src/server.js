d7ab71a17e78e8510f08e4dfed7dd061463d7db4
Nick Coultas <nncoultas@yahoo.com>
2018-04-17 11:39:52 -0700

    Project Complete

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const User = require('./user.js');
+const User = require('./user');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -13,8 +15,9 @@ server.use(bodyParser.json());
 server.use(
   session({
     secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
-  })
-);
+    saveUninitialized: false,
+    resave: false,
+  }));
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -27,43 +30,75 @@ const sendUserError = (err, res) => {
   }
 };
 
+const usernameTest = function (req, res, next) {
+  const { username } = req.body;
+
+  if (!username || username.trim() === '') {
+    return sendUserError('Username needs to be filled out', res);
+  }
+  next();
+};
+
+const passwordTest = function (req, res, next) {
+  const { password } = req.body;
+
+  if (!password || password.trim() === '') {
+    return sendUserError('Password needs to be filled out', res);
+  }
+  next();
+};
+
 // TODO: implement routes
 
-server.post('/users', (req, res) => {
-  const user = new User(req.body);
-  const { username, passwordHash } = req.body;
+server.post('/users', usernameTest, passwordTest, (req, res) => {
+  const { username, password } = req.body;
+  const passwordHash = password;
 
-  if (!username || !passwordHash) {
-    return sendUserError('A username and password is needed', res);
-  }
-  user.save((err, saved) => {
-    if (err) {
-      return sendUserError(err, res);
-    }
-    res.status(200).json(saved);
-  });
-});
+  const user = new User({ username, passwordHash });
 
-server.post('/log-in', (req, res) => {
-  const { username, passwordHash } = req.body;
-  User.findOne({ username })
-    .then(user => {
-      if (user) {
-        user.isPasswordValid(passwordHash);
-        res.json({ success: true });
-      } else {
-        res
-          .status(404)
-          .json({ message: 'A valid username and password are needed.' });
-      }
+  user
+    .save()
+    .then((savedUser) => {
+      res.status(200).json(savedUser);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
 
+server.post('/log-in', usernameTest, passwordTest, (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username })
+    .then(user => user.isPasswordValid(password))
+    .then((result) => {
+      if (!result) {
+        return sendUserError('Provide a Valid Password', res);
+      }
+      req.session.name = username;
+      return res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      sendUserError('Provide a valid username', res);
+    });
+});
+
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+const checkIfLoggedIn = function (req, res, next) {
+  const username = req.session.name;
+
+  if (username) {
+    User.findOne({ username })
+      .then((user) => {
+        req.user = user;
+        next();
+      });
+  } else {
+    return sendUserError('Must be logged in to gain access', res);
+  }
+};
+
+server.get('/me', checkIfLoggedIn, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
