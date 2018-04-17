7d5e67c2bc6efc57a6440b11dbb092a2793c704f
runranron <williamrpelton@gmail.com>
2018-04-17 18:04:39 -0500

    worked out everything but a couple of post issues and the get

--- src/server.js
+++ src/server.js
@@ -1,19 +1,33 @@
+/*eslint-disable*/
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const mongoose = require('mongoose');
+
+const User = require('./user');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
-
 const server = express();
-// to enable parsing of json bodies for post requests
+
+mongoose
+  .connect('mongodb://localhost/authdb')
+  .then(() => {
+    console.log('\n=== connected to MongoDB ===\n');
+  })
+  .catch(err => console.log('database connection failed', err));
+
 server.use(bodyParser.json());
 server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+  cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 },
+  secure: false,
+  name: 'auth',
 }));
 
 /* Sends the given err, a string or an object, to the client. Sets the status
- * code appropriately. */
+   code appropriately. */
 const sendUserError = (err, res) => {
   res.status(STATUS_USER_ERROR);
   if (err && err.message) {
@@ -25,8 +39,61 @@ const sendUserError = (err, res) => {
 
 // TODO: implement routes
 
+server.get('/users', (req, res) => {
+  User
+    .find()
+    .then(users => res.status(200).json(users))
+    .catch(err => sendUserError(err, res));
+});
+
+server.post('/users', (req, res) => {
+  const { username, password } = req.body;
+  const user = new User({ username, passwordHash: password });
+
+  user
+    .save()
+    .then(savedUser => res.status(200).json(savedUser))
+    .catch(err => sendUserError(err, res));
+});
+
+server.post('/log-in', (req, res) => {
+  const { username, password } = req.body;
+
+  if (username && password) {
+    User
+      .findOne({ username })
+      .then((user) => {
+        user.isPasswordValid(password)
+          .then(isValid => {
+            if(isValid) {
+              req.session.id = user._id;
+              res.status(200).json({success: true});
+            } else {
+              sendUserError({message: "Username/password match not found."}, res);
+            }
+          })
+          .catch(err => sendUserError(err, res));
+        }).catch(err => sendUserError(err, res));
+    } else {
+      sendUserError({message: "You must enter both a username and a password."}, res);
+  }
+});
+
+const validate = function() {
+  return function(req, res, next) {
+    console.log("req.id: ", req);
+    console.log("session: ", req.session.id);
+    if (req.session.id === req._id) {
+      next();
+    } else {
+      sendUserError({ error: "wrong user" }, res);
+      return;
+    }
+  };
+};
+
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', validate(), (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
--- src/user.js
+++ src/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
@@ -6,10 +7,32 @@ mongoose.models = {};
 mongoose.modelSchemas = {};
 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
-  // TODO: fill in this schema
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  }
 });
+
+UserSchema.pre('save', function (next) {
+  bcrypt.hash(this.passwordHash, 12, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.passwordHash = hash;
+    return next();
+  });
+});
+
+UserSchema.methods.isPasswordValid = (passwordGuess) => {
+  return bcrypt.compare(this.passwordHash, passwordGuess);
+};
+
 
 module.exports = mongoose.model('User', UserSchema);
