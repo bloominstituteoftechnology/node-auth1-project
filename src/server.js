3285f886f60ba01aaa802a66d7ccd5f01d7f544d
KJ Sourcing <tmkogwork@gmail.com>
2018-04-16 18:08:05 -0700

    got some work done

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+
+const User = require('./user.js');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
 
 const server = express();
 // to enable parsing of json bodies for post requests
-server.use(bodyParser.json());
+server.use(express.json());
 server.use(session({
   secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
 }));
+
+const authenticate = function (req, res, next) {
+  req.hello = `hello ${User}!`;
+
+  next();
+};
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -23,12 +30,50 @@ const sendUserError = (err, res) => {
   }
 };
 
-// TODO: implement routes
+server.post('/log-in', (req, res) => {
+  const { username, password } = req.body;
+  User
+    .findOne({ username })
+    .then((users) => {
+      if (User.isPasswordValid(password)) {
+        res.status(200).json({ success: true });
+        return;
+      }
+    })
+    .catch((err) => {
+      sendUserError(err, res);
+    });
+});
+
+
+
+server.post('/users', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = new User({ username, password });
+  newUser
+    .save()
+    .then(savedUser => {
+      res.status(200).json(savedUser);
+    })
+    .catch((err) => {
+      sendUserError(err, res);
+    });
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
 server.get('/me', (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
+});
+
+server.get('/', (req, res) => {
+  User.find()
+  .then(users => {
+      res.status(200).json(users);
+  })
+    .catch((err) => {
+      sendUserError(err, res);
 });
 
 module.exports = { server };
--- src/user.js
+++ src/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
@@ -9,7 +10,33 @@ mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
-  // TODO: fill in this schema
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+	index: true,
+	lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
+
+UserSchema.pre('save', function(next) {
+  console.log('pre save hook');
+  bcrypt.hash(this.password, 12, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+
+    return next();
+  });
+});
+
+UserSchema.methods.isPasswordValid = function(passwordGuess) {
+  return bcrypt.compare(passwordGuess, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
