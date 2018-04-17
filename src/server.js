1083281537ef4ed4c51fbd90efba7b48c6e77664
Abraham Andujo <foxtara00@gmail.com>
2018-04-17 16:13:15 -0700

    had all working except one but a code rewrite broke it

--- src/server.js
+++ src/server.js
@@ -1,34 +1,75 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
-
+const bcrypt = require('bcrypt');
+const User = require('./user');
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
-
 const server = express();
-// to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
+server.use(
+    session({
+        secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+    })
+);
 
-/* Sends the given err, a string or an object, to the client. Sets the status
- * code appropriately. */
-const sendUserError = (err, res) => {
-  res.status(STATUS_USER_ERROR);
-  if (err && err.message) {
-    res.json({ message: err.message, stack: err.stack });
-  } else {
-    res.json({ error: err });
-  }
-};
+server.post('/users', (req, res) => {
+    const { username, password } = req.body;
 
-// TODO: implement routes
-
-// TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
-  // Do NOT modify this route handler in any way.
-  res.json(req.user);
+    if (username && password) {
+        bcrypt.hash(password, BCRYPT_COST, (err, hash) => {
+            if (err) {
+                res.status(STATUS_USER_ERROR).json(err);
+            }
+            const user = new User({ username, passHash: hash });
+            user.save()
+                .then((item) => {
+                    res.status(200).json(item);
+                })
+                .catch((error) => {
+                    res.status(STATUS_USER_ERROR).json(err);
+                });
+        });
+    } else {
+        res.status(STATUS_USER_ERROR).json('Username and Password Required.')
+    }
 });
 
-module.exports = { server };
+server.post('/log-in', (req, res) => {
+    const { username, password } = req.body;
+    if (username && password) {
+        User.findOne({ username })
+            .then((item) => {
+                item.checkPassword(password).then((response) => {
+                    if (response) {
+                        res.status(200).json(item);
+                    } else {
+                        res.status(STATUS_USER_ERROR).json('Incorrect Login Details');
+                    }
+                });
+            })
+            .catch((err) => {
+                res.status(STATUS_USER_ERROR).json(err);
+            });
+    } else {
+        res.status(STATUS_USER_ERROR).json('Username or Password Missing');
+    }
+});
+
+const isLoggedIn = function (req, res, next) {
+    const username = req.session.name;
+
+    if (username) {
+        User.findOne({ username })
+            .then((item) => {
+                req.user = item;
+                next();
+            });
+    } else {
+        return res.status(STATUS_USER_ERROR).json('Must be logged in')
+    }
+}
+
+server.get('/me', isLoggedIn, (req, res) => {
+    res.json(req.user);
+});
--- src/user.js
+++ src/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
@@ -10,6 +11,17 @@ mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
   // TODO: fill in this schema
+  username: {
+    type: String,
+    min: [1, 'Password Length not Acceptable'],
+    unique: true,
+    required: true
+  },
+  passHash: { type: String, required: true }
 });
+
+UserSchema.methods.checkPassword = function (passGuess) {
+  return bcrypt.compare(passGuess, this.passHash);
+};
 
 module.exports = mongoose.model('User', UserSchema);
