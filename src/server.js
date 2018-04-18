87b4f28a052687634a1214e6c20841783e475e76
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-04-18 10:58:52 -0700

    11/11 tests working :)

--- src/server.js
+++ src/server.js
@@ -15,6 +15,24 @@ server.use(
   })
 );
 
+//local middleware
+const protected = function(msg) {
+  return function(req, res, next) {
+    if (req.session && req.session.name) {
+      User.findOne({ username: req.session.name })
+        .then((user) => {
+          req.user = user;
+          next();
+        })
+        .catch((err) => sendUserError(err, res));
+    } else {
+      sendUserError({ message: 'You are not logged in' }, res);
+    }
+  };
+};
+
+server.use(express.json()); //global middleware
+
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
 const sendUserError = (err, res) => {
@@ -28,11 +46,11 @@ const sendUserError = (err, res) => {
 
 // TODO: implement routes
 server.post('/users', (req, res) => {
-  const { username, passwordHash } = req.body;
-  if (!username || !passwordHash)
+  const { username, password } = req.body;
+  if (!username || !password)
     // return res.status(422).json({ err: 'You must enter a valid username and password' });
     return sendUserError('Enter valid username and password', res);
-  const user = new User(req.body);
+  const user = new User({ username, passwordHash: password });
   user
     .save()
     .then((savedUser) => res.status(200).json(savedUser))
@@ -41,18 +59,39 @@ server.post('/users', (req, res) => {
 
 server.post('/log-in', (req, res) => {
   const { username, password } = req.body;
+  if (!username) return sendUserError('Enter valid username and password', res);
   User.findOne({ username })
-    .then((user) => {
-      if (user) {
-        user.isPasswordValid(password);
-        res.status(200).json({ success: true });
+    .then((response) => {
+      if (response) {
+        // user is a truthy value obj then run code
+        response
+          .isPasswordValid(password)
+          .then((response) => {
+            if (response) {
+              req.session.name = username;
+              res.status(200).json({ success: true });
+            } else {
+              res.status(422).json({ err: "Password didn't match" });
+            }
+            // response
+            //   ? res.status(200).json({ success: true })
+
+            //   : res.status(422).json({ err: "Password didn't match" });
+          })
+          .catch((err) => {
+            console.log(err.message, 'err message');
+            res.status(422).json({ err: "Couldn't check password" });
+          });
+      } else {
+        // when user is null falsey statement
+        res.status(422).json({ err: 'User is null' });
       }
     })
-    .catch((err) => res.status(500).json(sendUserError(err)));
+    .catch((err) => res.status(500).json({ err: 'Server is not connected' }));
 });
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', protected('please login before proceeding'), (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
