f9299fc550717ff09a476e7f51468aa4436d492f
nourmhassan <nour-pop1@hotmail.com>
2018-04-18 08:56:19 -0700

    nour hassan... Auth

--- src/user.js
+++ src/user.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose');
-
-// Clear out mongoose's model cache to allow --watch to work for tests:
-// https://github.com/Automattic/mongoose/issues/1251
-mongoose.models = {};
-mongoose.modelSchemas = {};
-
-mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-  // TODO: fill in this schema
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
+
+UserSchema.pre('save', function (next) {
+  bcrypt.hash(this.password, 12, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+    return next();
+  });
+});
+
+UserSchema.methods.isPasswordValid = function (passwordGuess) {
+  return bcrypt.compare(passwordGuess, this.password);
+  // make into promise
+};
 
 module.exports = mongoose.model('User', UserSchema);
