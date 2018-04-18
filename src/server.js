980f970df1202691f7c16b86f98487ba2723b0ba
Megan Williamson <meganawilliamson@gmail.com>
2018-04-18 13:08:02 -0700

    completed mini, signup.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cors = require('cors');
 
 const User = require('./user.js');
 
@@ -8,6 +10,13 @@ const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
 
 const server = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+server.use(cors(corsOptions));
+
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 server.use(
@@ -16,15 +25,19 @@ server.use(
     name: 'Auth',
     cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 },
     secure: false,
+    // store: new MongoStore({
+    //   url: 'http://localhost'
+    //   ttl: 10 * 60; // seconds
+    // })
   })
 );
 server.use('/restricted', function(req, res, next) {
   if (req.session.name) {
     req.user = req.session.name;
+    next();
   } else {
     return sendUserError('Must be logged in to access this page.', res);
   }
-  next();
 });
 
 /* Sends the given err, a string or an object, to the client. Sets the status
@@ -41,10 +54,10 @@ const sendUserError = (err, res) => {
 const isInSession = function(req, res, next) {
   if (req.session.name) {
     req.user = req.session.name;
+    next();
   } else {
     return sendUserError('User must be logged in.', res);
   }
-  next();
 };
 
 // TODO: implement routes
@@ -65,7 +78,7 @@ server.post('/users', (req, res) => {
   });
 });
 
-server.post('/log-in', (req, res) => {
+server.post('/login', (req, res) => {
   const { username, password } = req.body;
 
   User.findOne({ username })
@@ -82,15 +95,22 @@ server.post('/log-in', (req, res) => {
           }
         });
       } else {
-        return sendUserError(
-          'Please provide valid username and password.',
-          res
-        );
+        return sendUserError('Please provide a valid username.', res);
       }
     })
     .catch(err => {
       res.status(500).json(err);
     });
+});
+
+server.post('/logout', (req, res) => {
+  if (req.session.name) {
+    req.session.destroy();
+    return res.status(200).json({ message: `Successfully logged out.` });
+  }
+  res
+    .status(500)
+    .json({ errorMessage: 'There was a problem in completing your request' });
 });
 
 // TODO: add local middleware to this route to ensure the user is logged in
