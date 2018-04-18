ddc7fffdb5df24aefe4cd54fc0c1e43ea558dfa5
Shaun Kelly <Shaun@redskelly.local>
2018-04-18 00:34:28 -0400

    added validation middleware,  fixed the /log-in route, add a GET /users

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
 const session = require('express-session');
 
 const User = require('./user');
@@ -8,26 +8,43 @@ const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
 
 const server = express();
-// to enable parsing of json bodies for post requests
-server.use(bodyParser.json());
+
+server.use(express.json());
 server.use(
   session({
+    name: 'auth',
     secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: false,
   })
 );
 
+const loggedIn = function(check) {
+  return function(req, res, next) {
+    if (req.session && req.session.name) {
+      next();
+    } else {
+      res.status(401).json({ error: err });
+    }
+  };
+};
+
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
-const sendUserError = (err, res) => {
-  res.status(STATUS_USER_ERROR);
-  if (err && err.message) {
-    res.json({ message: err.message, stack: err.stack });
-  } else {
-    res.json({ error: err });
-  }
-};
+// const sendUserError = (err, res) => {
+//   res.status(STATUS_USER_ERROR);
+//   if (err && err.message) {
+//     res.json({ message: err.message, stack: err.stack });
+//   } else {
+//     res.json({ error: err });
+//   }
+// };
+
+server.get('/users', (req, res) => {
+  User.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json(err));
+});
 
 server.post('/users', (req, res) => {
   const { username, password } = req.body;
@@ -39,7 +56,7 @@ server.post('/users', (req, res) => {
     user
       .save()
       .then(newUser => res.status(201).json(newUser))
-      .catch(err => sendUserError(err, res));
+      .catch(err => res.status(401).json(err));
   }
 });
 
@@ -48,24 +65,27 @@ server.post('/log-in', (req, res) => {
   if (!username || !password) {
     res.status(STATUS_USER_ERROR).json({ Error: 'Enter a username and password' });
   } else {
-    User.find({ username })
+    User.findOne({ username })
       .then(user => {
         if (user) {
           user.isPasswordValid(password).then(response => {
+            if (response) {
+              req.session.name = user.username;
+            }
             res.status(200).json({ success: true });
           });
         } else {
           res
-            .status(400)
+            .status(401)
             .json({ Error: 'The username/password combination you entered is invalid' });
         }
       })
-      .catch(err => sendUserError(err, res));
+      .catch(err => res.status(500).json(err));
   }
 });
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get('/me', loggedIn(), (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
--- src/user.js
+++ src/user.js
@@ -10,7 +10,7 @@ mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
 });
 
