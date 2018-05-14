6387d0bb6fbeb500ad6c47f95dbfd75dcd3f62ac
Jared Cuffe <jcuffe@gmail.com>
2018-05-14 13:14:50 -0700

    User schema created with pre-save hash hook

---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.pre('save', function (next) {
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    if (err) {
+      return next(err)
+    }
+
+    this.password = hash
+
+    return next()
+  })
+})
+
+module.exports = mongoose.model('User', userSchema)
