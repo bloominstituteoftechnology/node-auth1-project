6b7ca0b544098f11458b78ff7bef3ff4ae620723
Mark Wong <fmarkwong@gmail.com>
2018-05-14 15:32:49 -0700

    auth middleware

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ mongoose.connect('mongodb://localhost/authdb')
 server.listen(5000, () => console.log('server running on port 5000'));
 
 server.use(express.json());
+
+const auth = (req, res, next) => {
+  if (req.body.password === 'password') return next();
+
+  res.status(401).send("You shall not pass!");
+}
+
+server.post('/api/login', auth, (req, res) => {
+  res.status(200).send("You're logged in!"); 
+});
