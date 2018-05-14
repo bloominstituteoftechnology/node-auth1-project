c7aa4f011036e3284d27263688d8a89172524bb7
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-14 15:57:24 -0400

    Created new schemas

--- User/User.js
+++ User/User.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    'user': {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    'password': {
+        type: String,
+        required: true
+    }
+})
