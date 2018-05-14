40a7dfe20e009fbdf619ac29d7dceed1e4d5a644
Devon Bacon <devon.bacon@gmail.com>
2018-05-14 13:07:17 -0700

    User.js - used .pre schema methodto hash password before save

--- users/User.js
+++ users/User.js
@@ -14,5 +14,13 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    if(err) return next(err);
+    
+    this.password = hash;
+    return next();
+  })
+});
 
 module.exports - mongoose.model('User', userSchema);
