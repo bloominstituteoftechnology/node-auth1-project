0050e089583c1e92d7f632fe897710d3f67b9305
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-14 15:36:10 -0400

    user schema defined

---
+++ users/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  }
+  
+});
+
+module.exports = mongoose.model('User', userSchema);
