2044dda6b0a776933ade55f05459da993cd0d40c
Velsu <riftmage50@gmail.com>
2018-05-14 22:57:30 +0200

    Add sessions, configure login endpoint, save user sessions

--- server.js
+++ server.js
@@ -3,13 +3,23 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const User = require("./src/user.js");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 const server = express();
 
-const authenticate = (req, res, next) => {
-  if (req.body.password === "mellon") next();
-  else res.status(401).send("Wrong password");
-};
+// const authenticate = (req, res, next) => {
+//   if (req.body.password === "mellon") next();
+//   else res.status(401).send("Wrong password");
+// };
+
+//use sessions for tracking logins
+server.use(
+  session({
+    secret: "My super secret",
+    resave: true,
+    saveUninitialized: false
+  })
+);
 
 server.use(bodyParser.json());
 
@@ -29,10 +39,26 @@ server.get("/", (req, res, next) => {
 server.post("/register", (req, res, next) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(user => res.status(201).send(user))
-    .catch(err => res.status(500).send(err));
+  if (user.username && user.password)
+    user
+      .save()
+      .then(user => res.status(201).send(user))
+      .catch(err => res.status(500).send(err));
+  else res.status(400).json({ error: "Fields can't be empty" });
+});
+
+server.post("/login", (req, res, next) => {
+  User.authenticate(req.body.username, req.body.password, function(
+    error,
+    user
+  ) {
+    if (error || !user) {
+      res.status(401).json({ error: "You shall not pass" });
+    } else {
+      req.session.userId = user._id;
+      res.status(200).json("Logged in successfully!");
+    }
+  });
 });
 
 const port = process.env.PORT || 5000;
--- src/user.js
+++ src/user.js
@@ -14,6 +14,25 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
+UserSchema.statics.authenticate = function(user, password, callback) {
+  User.findOne({ username: user }).exec(function(err, user) {
+    if (err) {
+      return callback(err);
+    } else if (!user) {
+      var err = new Error("User not found.");
+      err.status = 401;
+      return callback(err);
+    }
+    bcrypt.compare(password, user.password, function(err, result) {
+      if (result === true) {
+        return callback(null, user);
+      } else {
+        return callback();
+      }
+    });
+  });
+};
+
 UserSchema.pre("save", function(next) {
   bcrypt.hash(this.password, 11, (err, hash) => {
     if (err) {
@@ -24,4 +43,6 @@ UserSchema.pre("save", function(next) {
   });
 });
 
-module.exports = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
