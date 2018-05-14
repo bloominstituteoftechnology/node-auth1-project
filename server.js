be3469d000e415d03093455fa03759a2ce2e8edc
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-14 15:39:39 -0700

    Applied POST endpoint /api/register

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-// const User = require('./users/User');
+const User = require('./users/User');
 
 mongoose
     .connect('mongodb://localhost/authdb')
@@ -24,28 +24,30 @@ function authenticate(req, res, next) {
     if (req.body.password === 'kiwi') {
         next();
     } else {
-        res.status(401).send('The Sky is Falling!!!');
+        res.status(401).send('You shall not pass!');
     }
 }
 
 // server.use(greet);
 server.use(express.json());
 
-server.get('/', (req, res) => {
+server.get('/api', (req, res) => {
     res.send({ route: 'Directing to', message: 'Fruit Farm' });
 });
 
-// server.post('/register', function(req, res) {
-//     const user = new User(req.body);
+server.post('/api/register', function(req, res) {
+    const user = new User(req.body);
 
-//     user
-//         .save()
-//         .then(user => res.status(201).send(user))
-//         .catch(err => res.status(500).send(err));
-// });
-
-server.post('/login', authenticate, (req, res) => {
-    res.send('Welcome to the Land of Strawberry Fields');
+    user
+        .save()
+        .then(user => res.status(201).send(user))
+        .catch(err => res.status(500).send(err));
 });
 
-server.listen(8000, () => console.log('\n=== API Running on 8K ===\n'));
+server.post('/api/login', authenticate, (req, res) => {
+    res.send('Logged In');
+});
+
+server.get('')
+
+server.listen(5000, () => console.log('\n=== API Running on 5K ===\n'));
--- users/User.js
+++ users/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        lowercase: true, //Kyle => kylecd
+        lowercase: true,
     },
     password: {
         type: String,
