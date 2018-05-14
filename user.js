c9c1bf81999c148fa7d869be38ceb3ec9399c6ec
Paul Fang <paulfang@pauls-mbp.home>
2018-05-14 16:01:36 -0700

    working on user.js

---
+++ user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+
+const userSchema = new mongoose.Schema({
+username:{
+    type: String,
+    uniqure: true,
+    required: true,
+    lowercase:true,
+},
+password:{
+    type: String,
+    required: true,
+
+},
+})
+
+userSchema.pre('save', function(next){
+    console.log('pre save hook')
+    bcrypt.hash(this.password, 11, (err,hash)=>{
+        if (err){
+            return next(err);
+        }
+        this.password = hash;
+        return next();
+    })
+
+next()
+})
+module.exports = mongoose.model('User', userSchema)
