4e85b31655901c2bbb41b906fae9982c1d95ad0a
sadrak105 <sadrak10@gmail.com>
2018-05-14 15:43:11 -0400

    setup port and dependencies

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const server = express();
+
 mongoose
 .connect('mongodb://localhost/authpassdb')
 .then(conn => {
@@ -9,3 +11,13 @@ mongoose
 .catch(err => {
     console.log('error connecting to mongo', err)
 })
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', function(req, res) {
+    res.status(200).json({ api: 'running' })
+})
+
+server.listen(9000, () => {console.log('/n=== Server running on 9k ===/n')})
