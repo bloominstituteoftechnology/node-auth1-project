c932156e5b57941d19435a77e438aff8e9170970
Devon Bacon <devon.bacon@gmail.com>
2018-05-14 13:19:52 -0700

    server.js - created get request to retrieve users

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const User = require('./users/User');
 mongoose
   .connect('mongodb://localhost/authdb')
   .then(conn => {
-    console.log('\n=== connected to mongo ===\n');
+    console.log('=== connected to mongo ===');
   })
   .catch(err => console.log('error connecting to mongo', err));
 
@@ -15,7 +15,15 @@ const server = express();
 server.use(express.json());
 
 server.get('/api/users', (req, res) => {
-
+  User.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: 'Could not retrieve users',
+      });
+    });
 });
 
-server.listen(8000, () => console.log('\n=== api running on port 8000===\n'));
+server.listen(8000, () => console.log('=== api running on port 8000 ==='));
