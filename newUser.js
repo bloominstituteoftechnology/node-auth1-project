18b1c0391f9131cba099a6dc09abf4924f3bd9c9
K Haag <kh@Ks-MacBook-Pro.local>
2018-05-14 14:25:13 -0600

    Successfully create new users with hashed password

---
+++ newUser.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+
+const User = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+User.pre('save', function(next){
+    bcrypt.hash(this.password, 12, (err, hash) => {
+        if(err) { 
+          return next(err)
+        }
+        this.password = hash;
+          return next()
+    })
+})
+
+
+
+module.exports = mongoose.model('User', User)
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const User = require('./newUser');
 
 const server = express();
 
@@ -10,13 +11,25 @@ mongoose
   .then(connect => console.log("\n connected to AuthDB"))
   .catch(err => console.log("error connecting to AuthDB", err))
 
+function authenticate(req, res, next) {
+    if(req.body.password === 'bananas') {
+        next()
+    } else {
+        res.status(401).send('Incorrect information. Try again')
+    }
+}
 
 server.get('/', (req, res) => {
   res.send({ msg: 'Test is successful. GET works' })
 })
 
 server.post("/api/register", (req, res) => {
-    res.send("I think it POSTed the new user")
+    const userInput = req.body;
+    const user = new User(userInput);
+    user
+    .save()
+    .then(user => res.status(201).json(user))
+    .catch(err => res.status(500).json(err))
 })
   
 
