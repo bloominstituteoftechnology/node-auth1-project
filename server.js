b7dde8ac49252a30107c9a9d0e6160ed19db9811
sadrak105 <sadrak10@gmail.com>
2018-05-14 15:56:11 -0400

    added express-session

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 
 const server = express();
 
