3295d45fcf739154adefaf5d3c9b127076820ab3
Henry K <mykim1891@hotmail.com>
2018-05-14 13:31:29 -0700

    Installed yarn; server.js and user.js modified

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const session = require('express-session');
+const bodyParser = require("body-parser");
+const express = require("express");
+const session = require("express-session");
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -8,9 +8,11 @@ const BCRYPT_COST = 11;
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
+server.use(
+  session({
+    secret: "e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re"
+  })
+);
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -24,9 +26,17 @@ const sendUserError = (err, res) => {
 };
 
 // TODO: implement routes
+server.post("/users", function(req, res) {
+  const user = new user(req.body);
+
+  user
+    .save()
+    .then(user => res.staus(201).send(user))
+    .catch(err => res.status(500).send(err));
+});
 
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+server.get("/me", (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
--- src/user.js
+++ src/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = requrie("bcrypt");
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
@@ -6,10 +7,30 @@ mongoose.models = {};
 mongoose.modelSchemas = {};
 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
+mongoose.connect("mongodb://localhost/users", { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
   // TODO: fill in this schema
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.pre('save', function(next) {
+  console.log('pre save hook');
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+    return next();
+  })
+})
+
+module.exports = mongoose.model("User", UserSchema);
