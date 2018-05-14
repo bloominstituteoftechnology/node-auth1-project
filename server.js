73fa9da50ba8be3ef22462850b4254280e78793a
Devon Bacon <devon.bacon@gmail.com>
2018-05-14 12:51:05 -0700

    server.js - boilerplate for server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose');
 
+const server = express();
+
+server.use(express.json());
+
+server.listen(8000, () => console.log('\n=== api running on port 8000===\n'));
