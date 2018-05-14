2bd8c1196ee3b4b3a2c4e57e52e9381ed8e4c4b1
Yasin Shuman <email@yasin.io>
2018-05-14 12:34:44 -0700

    added stuff we will prob delete

--- server.js
+++ server.js
@@ -16,6 +16,37 @@ mongoose
 server.use(helmet());
 server.use(express.json());
 
+//auth function
+function authenticate(req, res, next) {
+  if (req.body.password === "mellon") {
+    next();
+  } else {
+    res.status(401).send("You shall not pass!!!");
+  }
+}
+
+//routes
+
+// server.use(greet);
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send({ route: "/", message: req.message });
+});
+
+server.post("/register", function(req, res) {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(user => res.status(201).send(user))
+    .catch(err => res.status(500).send(err));
+});
+
+server.post("/login", authenticate, (req, res) => {
+  res.send("Welcome to the Mines of Moria");
+});
+
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
   console.log(`\n\nAPI running on http://localhost:${port}`)
---
+++ user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt"); //< ===========
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true // Kyle => kyle
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.pre("save", function(next) {
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+
+    this.password = hash;
+
+    return next(); // goes on to save to the db
+  });
+});
+
+// userSchema.post('save', function(next) {
+//   console.log('post save hook');
+
+//   next();
+// });
+
+module.exports = mongoose.model("User", userSchema);
