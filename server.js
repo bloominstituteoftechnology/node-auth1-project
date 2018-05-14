eb7af5ecd6ded72be0f5dc8ef903333ec9850a6b
Eric Andrade <eric.andrade@gmail.com>
2018-05-14 16:31:57 -0400

    added initial authentication code

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/authdb').then(conn => {console.log('\n=== connected to mongo ===\n')}).catch(err => console.log("error connecting to mongo", err));
+mongoose
+  .connect('mongodb://localhost/authdb')
+  .then(conn => {
+    console.log('\n=== connected to mongo ===\n')
+  })
+  .catch(err => console.log("error connecting to mongo", err));
 
 const server = express();
 
-function greet(req, res, next) {
-  req.message = "Hello world";
-
-  next();
+function authenticate(req, res, next) {
+  if(req.body.password === 'infinity') {
+    next();
+} else {
+  res.status(401).send('Sorry bub. Wrong password.');
+  }
 }
 
-server.use(greet);
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send({ route: '/', message: req.message });
 });
 
-server.get('/hello', (req, res) => {
-  res.send({ route: 'hello', message: req.message });
+server.post('api/login', authenticate, (req, res) => {
+  res.send('Welcome to the Mines of Moria!');
 });
 
 server.listen(8000, () => console.log('\n=== api running on db ===\n'))
