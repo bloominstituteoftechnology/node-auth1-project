c13e56f3f3132dbfe83cac0377ea43428a8906c4
sadrak105 <sadrak10@gmail.com>
2018-05-14 15:36:21 -0400

    connect db with mongoose

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+mongoose
+.connect('mongodb://localhost/authpassdb')
+.then(conn => {
+    console.log('\n=== connected to mongo ===\n')
+})
+.catch(err => {
+    console.log('error connecting to mongo', err)
+})
