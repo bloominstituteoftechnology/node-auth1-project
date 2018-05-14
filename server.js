9e229506b49e3119a981433435f6dfc81a17b2af
totsuya <supertatortots@gmail.com>
2018-05-14 17:09:36 -0400

    added basic server running for testing

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-  const
+const mongoose = require('mongoose');
+const express = require('express');
+
+mongoose
+  .connect('mongodb://localhost/authdb')
+  .then(conn => {
+    console.log('n\ ===connected to mongo === \n')
+  })
+  .catch(error => console.log('issues connecting to mongo', error));
+
+
+  const server = express();
+
+  server.get('/', (req, res) => {
+    res.send({api: 'running'})
+  })
+
+  server.listen(5000, () => console.log('n\ === API Running! === \n'))
