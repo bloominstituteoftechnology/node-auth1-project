6ad5092b7313c8c68f5c196a749c17ecae238c68
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-14 15:58:23 -0400

    post a new user

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,12 +2,26 @@ const express = require('express');
 const User = require('./userModel');
 const router = express.Router();
 
+const authenticateUser = (req, res, next) => {
+  const { username, password } = req.body;
+  if (username && password) { next() }
+  if (!username && pasword) { res.send("must provide a username") }
+  if (username && !password) { res.send("must provide a password") }
+}
+
 router.route('/')
   .get((req, res) => {
     User
       .find()
       .then(users => res.status(200).json("here are some users"))
       .catch(err => res.status(500).json("error fetching users"))
+  })
+
+  .post(authenticateUser, (req, res) => {
+    User
+      .create(req.body)
+      .then(user => res.status(201).json(user))
+      .catch(err => res.status(400).json({ error: "There was a problem creating this user." }))
   })
 
 module.exports = router;
