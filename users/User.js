575381d8421e8086de9825e6a054d3f61000d270
Devon Bacon <devon.bacon@gmail.com>
2018-05-14 13:05:19 -0700

    Users.js - built out user schema

--- users/User.js
+++ users/User.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
+
 
 module.exports - mongoose.model('User', userSchema);
