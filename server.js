44feaaf924fbefade45d5f145a4b0fd70d489460
Devon Bacon <devon.bacon@gmail.com>
2018-05-14 13:08:42 -0700

    server.js - imported user schema

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const User = require('./users/User');
+
 const server = express();
 
 server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+
+});
 
 server.listen(8000, () => console.log('\n=== api running on port 8000===\n'));
