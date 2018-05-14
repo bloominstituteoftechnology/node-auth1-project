df2c0f644822c9ab0eb1c8efc745863fe4d60754
totsuya <supertatortots@gmail.com>
2018-05-14 18:04:55 -0400

    fixed schema issue

--- User.js
+++ User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.SchemaType({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
