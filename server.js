a1b1ce95ce30cac4a84cd92ab483341b1795248a
K Haag <kh@Ks-MacBook-Pro.local>
2018-05-14 13:45:17 -0600

    server.get working, bcrypt added

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const server = express();
+
 server.use(express.json());
 
 mongoose
   .connect('mongodb://localhost/AuthDB')
   .then(connect => console.log("\n connected to AuthDB"))
   .catch(err => console.log("error connecting to AuthDB", err))
+
+
+server.get('/', (req, res) => {
+  res.send({ msg: 'Test is successful. GET works' })
+})
+
+server.post("/api/register", (req, res) => {
+    res.send("I think it POSTed the new user")
+})
+  
 
 
 
