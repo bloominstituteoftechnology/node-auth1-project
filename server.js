fb9f0f9ab1d9248e064d6288298f9edbc920c6cf
Devon Bacon <devon.bacon@gmail.com>
2018-05-14 13:10:38 -0700

    server.js - connected server to mongo database

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 
 const User = require('./users/User');
 
+mongoose
+  .connect('mongodb://localhost/authdb')
+  .then(conn => {
+    console.log('\n=== connected to mongo ===\n');
+  })
+  .catch(err => console.log('error connecting to mongo', err));
+
 const server = express();
 
 server.use(express.json());
