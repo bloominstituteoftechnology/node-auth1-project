b022c1b580d5bb078bdb0a4a4b3242b910a4b5b6
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-14 15:54:45 -0400

    Connected to mongo

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+mongoose
+.connect('mongodb://localhost/authN')
+.then(connects => {
+    console.log('You got connected');
+})
+.catch(err => {
+    console.log('Can\'t connect');
+})
