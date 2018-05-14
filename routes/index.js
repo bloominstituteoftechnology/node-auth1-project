f26f64a7eb9a866508c545566054d7c6b97b34c3
Nedu Robert <nedumrobert@gmail.com>
2018-05-15 00:58:23 +0100

    added endpoint for logging in users

--- routes/index.js
+++ routes/index.js
@@ -13,5 +13,26 @@ routes.post('/register', (req, res) => {
     })
 })
 
+routes.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    User.findOne({ username })
+    .then(user => {
+        if(user) {
+            user.authenticate(password)
+            .then(isValid => {
+                if(isValid) {
+                    req.session.userId = user._id;
+                    res.status(200).json({ response: 'Successfully logged in.'})
+                } else {
+                    res.status(401).json({ message: 'You shall not pass!' })
+                }
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
 
 module.exports = routes;
