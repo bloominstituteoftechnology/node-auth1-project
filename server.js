27a8bdc2a44285218a59c22fe24d053073b45ee3
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-05-15 16:23:09 -0400

    finished routes and prepping for front end

--- server.js
+++ server.js
@@ -1,29 +1,78 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./users/User.js');
-const port = 3333;
+const session = require('express-session');
+
+const User = require('./users/User');
 
 mongoose
   .connect('mongodb://localhost/authdb')
   .then(conn => {
-    console.log('> connected to mongo');
+    console.log('> connected to mongo ');
   })
-  .catch(err => console.log('error connecting to mongo'));
+  .catch(err => console.log('error connecting to mongo', err));
 
 const server = express();
 
+function authenticate(req, res, next) {
+  if (req.body.password === 'mellon') {
+    next();
+  } else {
+    res.status(401).send('You shall not pass!!!');
+  }
+}
+
 server.use(express.json());
+server.use
 
 server.get('/', (req, res) => {
-  res.send({ api: 'running' });
+  res.send({ route: '/', message: req.message });
 });
 
 server.post('/api/register', function(req, res) {
   const user = new User(req.body);
+
   user
     .save()
     .then(user => res.status(201).send(user))
     .catch(err => res.status(500).send(err));
 });
 
-server.listen(port, () => console.log(' > API running on 3333.'));
+server.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username })
+    .then(user => {
+      if (user) {
+        // compare the passwords
+        user.isPasswordValid(password).then(isValid => {
+          if (isValid) {
+            req.session.username = user.username;
+            res.send('have a cookie');
+          } else {
+            res.status(401).send('invalid credentials');
+          }
+        });
+      } else {
+        res.status(401).send('invalid credentials');
+      }
+    })
+    .catch(err => res.send(err));
+});
+
+server.get('/api/users', authenticate, (req, res) => {
+  User.find().then(users => res.send(users));
+});
+
+server.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(function(err) {
+      if (err) {
+        res.send('error');
+      } else {
+        res.send('good bye');
+      }
+    });
+  }
+});
+
+server.listen(3333, () => console.log('> api running on 3333 '));
--- users/User.js
+++ users/User.js
@@ -1,16 +1,35 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); //< ===========
 
-const userModel = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true,
-    lowercase: true
+    lowercase: true, // Kyle => kyle
   },
   password: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('User', userModel);
+userSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+
+    this.password = hash;
+
+    return next(); // goes on to save to the db
+  });
+});
+
+// userSchema.post('save', function(next) {
+//   console.log('post save hook');
+
+//   next();
+// });
+
+module.exports = mongoose.model('User', userSchema);
