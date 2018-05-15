df7752d63cb4e390f548c15370c0a5381a17a0ea
cgoez <goez2001@gmail.com>
2018-05-15 13:59:37 -0700

    usersRouter GET /

--- user/usersRouter.js
+++ user/usersRouter.js
@@ -4,7 +4,19 @@ const User = require("./User");
 
 const router = express.Router();
 
-// /users
+// /api/users
+// GET
+router.route("/").get((req, res) => {
+  User.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error getting users."
+      });
+    });
+});
 
 // POST
 router.route("/").post((req, res) => {
