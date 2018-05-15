3132e8103d4010b2e70eaf4065f7ee4fca586a4d
David Alexander Morales <damorales89@gmail.com>
2018-05-15 16:41:38 -0400

    sessions implemented

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const User = require('./user/User');
 
@@ -13,23 +15,40 @@ mongoose
     });
 
 const server = express();
+const sessionConfig = {
+    secret: 'Nobody tosses a dwarf!',
+    cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+    }, // 1 day in milliseconds
+    httpOnly: true,
+    secure: false,
+    resave: true,
+    saveUninitialized: false,
+    name: 'noname',
+    store: new MongoStore({
+        url: 'mongodb://localhost/sessions',
+        ttl: 60 * 10,
+    }),
+}
 
 server.use(express.json());
-
-server.get('/', (req, res) => {
-    res.send('API IS LIT FAM')
-});
+server.use(session(sessionConfig));
 
 function authenticate(req, res, next) {
-    const userSchema = req.params;
-    const password = userSchema(password);
-
-    if (req.body.password === ({ password })) {
+    if (req.session && req.session.username) {
         next();
     } else {
         res.status(401).send('You shall not pass!');
     }
 }
+
+server.get('/', (req, res) => {
+    if (req.session && req.session.username) {
+        res.send(`Welcome Back ${req.session.username}`);
+    } else {
+        res.send('DOES NOT COMPUTE!');
+    }
+});
 
 server.post('/api/register', function(req, res) {
     const user = new User(req.body);
@@ -44,13 +63,50 @@ server.post('/api/register', function(req, res) {
         });
 })
 
-server.post('/api/login', authenticate, (req, res) => {
-    res.send('Logged in');
-})
+server.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
 
-// server.get('/api/user', function(req, res) {
-//     .then()
-//     .catch()
-// })
+    User.findOne({ username })
+        .then(user => {
+            if (user) {
+                //compare the passwords
+                user.isPasswordValid(password).then(isValid => {
+                    if (isValid) {
+                        req.session.username = user.username;
+                        res.send('Login Successful - Here is a Cookie!');
+                    } else {
+                        res.status(401).send('Invalid Password');
+                    }
+                })
+            } else {
+                res.status(401).send('Invalid Username');
+            }
+        })
+        .catch(err => res.send(err));
+});
+
+server.get('/api/user', authenticate, (req, res) => {
+
+    User
+        .find()
+        .then(users => {
+            res.status(200).json(users);
+        });
+        // .catch(err => {
+        //     res.status(500).json(err);
+        // });
+});
+
+server.get('/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(function(err) {
+            if (err) {
+                res.send('error');
+            } else {
+                res.send('Good Riddance!');
+            }
+        });
+    }
+});
 
 server.listen(8000, () => console.log('Listening...'));
