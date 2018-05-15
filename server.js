52f3ee627678a48ec3eab98bfec44fcbf2e2dced
Devon Bacon <devon.bacon@gmail.com>
2018-05-15 12:37:07 -0700

    server.js - created basic login functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = require('./users/User');
 
@@ -12,16 +13,18 @@ mongoose
 
 const server = express();
 
+function authenticate(req, res, next) {
+  
+}
+
 server.use(express.json());
 
 // Register new user
 server.post('/api/register', (req, res) => {
+  // Create new user from req body and save
   const newUser = new User(req.body);
-
   newUser.save()
-    .then(user => {
-      res.status(201).json(user);
-    })
+    .then(user => res.status(201).json(user))
     .catch(err => {
       res.status(500).json({
         error: 'Could not complete registration'
@@ -30,13 +33,30 @@ server.post('/api/register', (req, res) => {
 });
 
 // Login exsiting user
+server.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  User.findOne({ username: username })
+    .then(user => {
+      // If user is not found then return error
+      if(!user) res.status(404).json({ error: 'Invalid credentials' });
+      // Check if password matches hash
+      bcrypt.compare(password, user.password, function(err, match) {
+        // Store cookie to track match
+        if(match) res.json(`Logged in as ${username}`);
+        else res.status(401).json('You shall not pass!!!');
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: 'User could not be logged in'
+      })
+    });
+});
 
 // Retrieve users
 server.get('/api/users', (req, res) => {
   User.find()
-    .then(users => {
-      res.json(users);
-    })
+    .then(users => res.json(users))
     .catch(err => {
       res.status(500).json({
         error: 'Could not retrieve users',
