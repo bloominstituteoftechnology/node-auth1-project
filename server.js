b4b0d601d75d21a765a32e5695a6c7d79989a651
Anthony <anthony@anthonys-mbp.lan>
2018-05-15 18:04:23 -0400

    Finished endpoints

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+// Import all of the packages we installed: Mongoose, express, session, Mongostore
+const mongoose = require('mongoose'); //to communicate easily with our DB
+const express = require('express'); // to utilize node more easily
+const session = require('express-session'); //to develop sessions and cookies
+const MongoStore = require('connect-mongo')(session); // to store sessions
 
-const User = require('./users/User');
+//Import the User Schema
+const User = require('./users/userSchema');
 
+//to connect our localhost server to our mongo database
 mongoose
   .connect('mongodb://localhost/authdb')
   .then(connection => {
@@ -14,11 +17,13 @@ mongoose
     console.log(error)
   });
 
-const port = 3000;
-const server = express();
-server.use(express.json());
-server.use(session(sessionConfig));
 
+const port = 3000;//to declare a server port for our _**what**
+const server = express(); //to tell our server we are using express/node
+server.use(express.json()); //to tell our server to use json syntax
+server.use(session(sessionConfig)); //to tell our server to use the sessionConfig library **why**
+
+//define our session config 
 const sessionConfig = {
   cookie: {
     key: value,
@@ -34,6 +39,8 @@ const sessionConfig = {
   }),
 };
 
+// create an authentication function which can be used as local middleware
+// check if a session/username are present, if so, move on to the next middleware
 function authenticate(req, res, next) {
   if (req.session && req.session.username) {
     next();
@@ -42,7 +49,8 @@ function authenticate(req, res, next) {
     res.send('Login failed, please try again');
   };
 }
-
+//**is this necessary?**
+//endpoint HTTP Request: at default path '/', check if the session/username are present. 
 server.get('/', (req, res) => {
   if (req.session && req.session.username) {
     res.send(`welcome back ${req.session.username}`);
@@ -51,6 +59,9 @@ server.get('/', (req, res) => {
   }
 })
 
+//HTTP Request endpoint at path '/register'
+// create and save a new user based on the inputs of the request body
+// hashes the password using the presave lifecycle hook in userschema.js
 server.post('/register', function (req, res) {
   const user = new User(req.body);
 
@@ -60,6 +71,10 @@ server.post('/register', function (req, res) {
     .catch(err => res.status(500).send(err));
 });
 
+// HTTP Request Endpoint at path '/login':
+// assign the username and password based on the request's body
+// check the database to see if there is a User with the username passed from the request body.
+// if so, check if the request password matches the hashed password on the user object in the database
 server.post('/login', (req, res) => {
   const { username, password } = req.body;
 
@@ -69,16 +84,47 @@ server.post('/login', (req, res) => {
         user.isPasswordValid(password).then(validPassword => {
           if (validPassword) {
             req.session.username = user.username;
-            res.send('have a cookie!');
+            res.send('Login Successful!');
+            //**add a cookie**
           } else {
             res.status(401).send('invalid password');
           }
         });
       } else {
-        res.status(401).send('invalid password');
+        res.status(401).send('invalid username');
       }
     })
     .catch(err => res.send(err));
 });
 
+// HTTP Request endpoint at path '/users/'
+// run the authenticate middleware above. it checks for a session/username.
+// if it passes, find the username in the user database, and send the user object back
+server.get('/users', authenticate, (req, res) => {
+  User.find().then(users => {
+    res.send(users)})
+});
+
+//HTTP Request endpoint at path '/logout'
+// check if a session is active, if so, destroy it and logout.
+server.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(function(err) {
+      if (err) {
+        res.send('error');
+      } else {
+        res.send('logout successful');
+      }
+    });
+  }
+});
+
+
+// Gglobal middleware that ensures a user is logged in when accessing any route 
+//prefixed by /api/restricted/. For instance, /api/restricted/something, /api/restricted/other, 
+// and /api/restricted/a should all be protected by the middleware; only logged in users should be 
+// able to access these routes.
+
+
+// tell our server to listen to server.js on a port. 
 server.listen(port, () => console.log(`\n == HOMIE YOU ARE NOW listening on port ${port}`));
---
+++ users/userSchema.js
@@ -0,0 +1,55 @@
+// require the packages we need: Mongoose and bCrypt
+const mongoose = require('mongoose'); // to communicate the schema with the database
+const bcrypt = require('bcrypt'); // to hash passwords
+
+const userSchema = new mongoose.Schema({ 
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// Can the schema above be written as follows:
+
+// const definitions = {
+//     username: username: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     }
+// }
+// const options = {
+//     ...
+// }
+
+//const userSchema = new mongoost.Schema({ definitions,options })
+
+// create a lifecycle hook that starts on the pre-save function of server.js, which hashs the password
+// used in server.js line 64-71
+userSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 11, (err, hash) => {
+        if (err) {
+            return next(err);
+        } 
+            this.password = hash;
+            return next();
+    });
+});
+
+// create a function to compare passwords for authentication purposes 
+// used in line 77
+userSchema.methods.isPasswordValid = function(passwordAttempt) {
+    return bcrypt.compare(passwordAttempt, this.password);
+}
+
+// Export the schema as a model for use in other files
+module.exports = mongoose.model('User', userSchema, 'users')
