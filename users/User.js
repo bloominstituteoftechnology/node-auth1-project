c7ea0bd68486b923f5cf7eebcd95c0a7b1107870
cgoez <goez2001@gmail.com>
2018-05-15 14:20:40 -0700

    User.js add pw check method

--- users/User.js
+++ users/User.js
@@ -14,15 +14,21 @@ const User = new mongoose.Schema({
   }
 });
 
+// Hash pw before saving to db
 User.pre("save", function(next) {
   bcrypt.hash(this.password, 11, (error, hash) => {
     if (error) {
       return next(error);
     }
     this.password = hash;
-
     return next();
   });
 });
+
+// User schema methods
+// Check if entered pw matches db pw
+User.methods.isPasswordValid = function(passwordGuess) {
+  return bcrypt.compare(passwordGuess, this.password);
+};
 
 module.exports = mongoose.model("User", User);
