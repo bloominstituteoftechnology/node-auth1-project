70df8b04c054f597a3e1f1c65a815a2b886f5325
Imran Ahmed <jappleleaf@gmail.com>
2018-05-14 20:40:06 -0400

    finished api/users get

--- server.js
+++ server.js
@@ -41,9 +41,52 @@ function authenticate(req, res, next) {
 }
 
 
+
+
+function checkLogin(req, res, next){
+  const username = req.body.username;
+  const passBody = req.body.password;
+
+
+  User.find({ username: username })
+    .then(user => {
+      bcrypt.compare(passBody, user[0].password)
+        .then(function (ifPass) {
+          // res == true
+          if (ifPass) {
+            console.log('from if pass', user[0]._id)
+            if (!user[0].session) {
+              User.findByIdAndUpdate(user[0]._id, { session: 1 }, { new: true }, function (err, model) {
+              })
+            } else {
+              User.findByIdAndUpdate(user[0]._id, { session: user[0].session + 1 }, { new: true }, function (err, model) {
+              })
+            }
+            console.log('after update', user.session)
+            next();
+          } else {
+            res.status(401).send('You shall not pass!!!');
+          }
+        }).catch(err => console.log(err))
+    }).catch(err => console.log(err))
+}
+
 server.get('/api/users', (req, res) => {
-  User.find()
-    .then(resp => res.json(resp))
+  let query =  User.find();
+
+  query.where('session').gt(0)
+  .then( user => {
+    console.log('line 78',user)
+    if(user.length){
+
+      User.find()
+      .then(resp => res.json(resp))
+      .catch(err => console.log(err))
+      console.log(user)
+    } else {
+      res.status(404).send('You shall not pass!');
+    }
+  })
     .catch(err => console.log(err))
 })
 
