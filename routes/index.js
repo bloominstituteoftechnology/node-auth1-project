b31940c74452758fec423a32b1e3c2f8b972b209
Nedu Robert <nedumrobert@gmail.com>
2018-05-15 09:19:17 +0100

    added logout and restricted endpoint

--- routes/index.js
+++ routes/index.js
@@ -22,11 +22,15 @@ routes.post('/login', (req, res) => {
             .then(isValid => {
                 if(isValid) {
                     req.session.userId = user._id;
+                    req.session.name = user.username;
                     res.status(200).json({ response: 'Successfully logged in.'})
                 } else {
                     res.status(401).json({ message: 'You shall not pass!' })
                 }
-            });
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
         }
     })
     .catch(err => {
@@ -34,5 +38,20 @@ routes.post('/login', (req, res) => {
     })
 })
 
+routes.get('/logout', (req, res) => {
+    if(req.session) {
+        req.session.destroy(function(err){
+            if(err) {
+                res.status(500).json({ message: 'could not log you out!'})
+            } else {
+                res.status(200).json({ message: 'Logged out successfully! '})
+            }
+        })
+    }
+})
+
+routes.get('/restricted/name', (req, res) => {
+    res.send({ greeting: `welcome back ${req.session.name}`})
+})
 
 module.exports = routes;
