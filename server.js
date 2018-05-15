b3c3af577da45a75e2b78926861cc61ebd5580df
Anthony <anthony@anthonys-mbp.lan>
2018-05-15 16:11:28 -0400

    Added the User schema and set up the password hashing with bcrypt.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const port = 3000;
-const server = express();
+const User = require('./users/User');
 
 mongoose
   .connect('mongodb://localhost/authdb')
@@ -12,17 +11,50 @@ mongoose
     console.log(error)
   });
 
+const port = 3000;
+const server = express();
 server.use(express.json());
 
-
-server.post('/log-in', (res, res) => {
-  res.send(
-    //   { route: '/log-in', message: 'Login successful!' })
-    { money: 'bank' });
+function authenticate(req, res, next) {
+  if (req.body.password === 'mellon') {
+    next();
+  } else {
+    res.status(401);
+    res.send('Incorrect Login, please try again');
+  };
+}
 
 server.get('/', (req, res) => {
-    res.send({ api: 'running' });
-  })
+  res.send({ route: '/', message: req.message });
+})
+
+
+server.post('/register', function (res, res) {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(user => res.status(201.send(user)))
+    .catch(err => res.status(500).send(err))
+});
+
+server.post('/login', (res, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username })
+  .then(user => {
+    if (user) {
+      user.isPasswordValid
+    } else {
+      res.status(401).send('invalid password');
+    }
+  });
+  .catch(err => res.status(500).send(err))
+  user
+    .save()
+    .then(user => res.status(201.send(user)))
+    .catch(err => res.status(500).send(err))
+});
 
 
 server.listen(port, () => console.log(`\n == HOMIE YOU ARE NOW listening on port ${port}`));
---
+++ users/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({ 
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 11, (err, hash) => {
+        if (err) {
+            return next(err);
+        } 
+            this.password = hash;
+            return next();
+    });
+});
+
