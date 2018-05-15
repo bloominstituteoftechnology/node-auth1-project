4ac703baaafb433fa61ee899c71598aab008d110
Natalie <natalieschennum@gmail.com>
2018-05-15 13:46:13 -0700

    Added Some Styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Route } from 'react-router';
-import Login from './Login';
-
+import React, { Component } from "react";
+import "./App.css";
+import { Route } from "react-router";
+import Login from "./Login";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-         
           <h1 className="App-title">Authentication</h1>
         </header>
-       <Route path='/login'component={Login}/>
+        <Route path="/login" component={Login} />
       </div>
     );
   }
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,43 +1,47 @@
-import React from 'react'; 
-import axios from 'axios'; 
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 export default class Login extends React.Component {
-    state = {
-        username: '', 
-        password: '',
-        response: ''
-    }
+  state = {
+    username: "",
+    password: "",
+    response: ""
+  };
 
-    handleChange = (event) => {
-        this.setState({[event.target.name]: event.target.value})
-    }
-    handleSubmit = (event) => {
-        event.preventDefault(); 
-        axios
-      .post("http://localhost:5000/api/login", {username: this.state.username, password: this.state.password} )
-      
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", {
+        username: this.state.username,
+        password: this.state.password
+      })
+
       .then(response => {
-        console.log( response )
+        console.log(response);
         if (response.data.username) {
-            this.setState({response:'logged in'})
+          this.setState({ response: "logged in" });
         }
       })
       .catch(err => {
         console.log(err);
-        this.setState({response:'login error'})
+        this.setState({ response: "login error" });
       });
-    }
+  };
 
-    render = () => {
-        return(
-            <div>
-                <form onSubmit={ this.handleSubmit }>
-                <input name='username' onChange={ this.handleChange }/>
-                <input name='password' type='password' onChange={ this.handleChange }/>
-                <button type='submit'>Login</button>
-                </form>
-                <p>{this.state.response}</p>
-                </div>
-        )     
-    }
+  render = () => {
+    return (
+      <div className="loginForm">
+        <form onSubmit={this.handleSubmit}>
+          <input name="username" onChange={this.handleChange} />
+          <input name="password" type="password" onChange={this.handleChange} />
+          <button type="submit">Login</button>
+        </form>
+        <p>{this.state.response}</p>
+      </div>
+    );
+  };
 }
