647a9c89ea1ace127f18778d4caddb8450217e39
axhon <axhonruiz@gmail.com>
2018-05-15 09:56:38 -1000

    added some session config

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express'
 import mongoose from 'mongoose'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import session from "express-session";
+import connectMongo from 'connect-mongo'
+
+const MongoStore = connectMongo(session)
 
 import mainRouter from './routes'
 
@@ -18,14 +22,35 @@ app.use(express.json())
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('\n=== MongoDB Connected ===\n'))
-  .catch(err => console.error(err))
+  .catch(err => console.error(err.message, '\n', '\n=======\n', err.stack))
+
+const authenticate = (req, res, next) =>
+  (req.session && req.session.username
+    ? next()
+    : res.status(401).send('Mithrandil looks down upon you'))
+
+const sessionConfig = {
+  secret: process.env.MONGO_SECRET,
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000
+  },
+  httpOnly: true,
+  secure: false,
+  resave: true,
+  saveUninitialized: false,
+  name: 'ad',
+  store: new MongoStore({
+    url: process.env.MONGO_URIBASE + 'sessions',
+    ttl: 60 * 10
+  })
+}
 
 app.get('/', (req, res) => {
   res.send('love')
 })
 
 app.use('/api', mainRouter)
-
-app.listen(8080, () =>
-  console.log('\n=== server listening on http://localhost:8080 ===\n')
+const port = 8080
+app.listen(port, () =>
+  console.log(`\n=== server listening on http://localhost:${port} ===\n`)
 )
