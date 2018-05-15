11c9b7b5a34f465a4992af283a24d0964b936aa4
sadrak105 <sadrak10@gmail.com>
2018-05-15 18:22:41 -0400

    post to login and create cookie

--- users/loginRouter.js
+++ users/loginRouter.js
@@ -1,12 +1,33 @@
 const router = require('express').Router();
+const session = require('express-session');
 
 const User = require('./users.js');
 
+router.post('/', (req, res) => {
+    const { username, password } = req.body;
 
-
-
-
-
+    User.findOne({ username })
+    .then(user => {
+        if (user) {
+            user.isPasswordValid(password)
+            .then(isValid => {
+                if (isValid) {
+                    req.session.username = user.username;
+                    res.send('Logged In');
+                }
+                else {
+                    res.status(401).json({ message:'You Shall Not Pass!' })
+                }
+            });
+        }
+        else {
+            res.status(401).json({ message:'You Shall Not Pass!' });
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error:'You Shall Not Pass!' })
+    })
+})
 
 
 module.exports = router;
