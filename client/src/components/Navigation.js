fd1832e842c71f0ce82a045f6856a2365419b695
Justin Hong <jhong2009@gmail.com>
2018-05-15 22:07:22 -0700

    in Navigation.js: created basic links in navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// styles
+import "../styles/navigation.css";
 
 const Navigation = () => {
-	return <h3>Navigation here</h3>;
+	return (
+		<div className="Navigation">
+			<Link to={"/"}>
+				<h3>Register</h3>
+			</Link>
+			<Link to={"/userprofile"}>
+				<h3>Profile</h3>
+			</Link>
+			<Link to={"/users"}>
+				<h3>Users</h3>
+			</Link>
+		</div>
+	);
 };
 
 export default Navigation;
