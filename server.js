17d72c5386bfd922322bdef257af7e58937a891d
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-12 05:19:05 +0800

    Installed all dependencies. Added endpoints. POST method to /api/registed passed Postman test.

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('./auth/UserModel.js');
+
+mongoose
+    .connect('mongodb://localhost/auth-i')
+    .then(() => {
+        console.log('\n ===== Connected to database ===== \n');    
+});
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'API is running...' });
+});
+
+server.post('/api/register', (req, res) => {
+    User.create(req.body)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+server.listen(8333, () => {
+    console.log('\n ===== API running on Port 8000 ===== \n');
+});
