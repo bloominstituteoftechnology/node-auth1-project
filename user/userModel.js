8248bc73636d868a07f3fbee034d5fd0d246cdf1
David <AquilaVirtual>
2018-06-11 15:03:11 -0700

    Added password hashing

--- user/userModel.js
+++ user/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const UserSchema = mongoose.Schema({
     username: {
@@ -13,5 +14,18 @@ const UserSchema = mongoose.Schema({
         min: 5
     }
 })
+
+UserSchema.pre('save', function(next) {   
+  
+    bcrypt.hash(this.password, 12, (err, hash) => {      
+      if (err) {
+        return next(err);
+      }  
+      this.password = hash;
+  
+      next();
+    });
+
+  });
 const UserModel = mongoose.model('User', UserSchema)
 module.exports = UserModel;
