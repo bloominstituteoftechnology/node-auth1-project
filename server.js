64f55ee4798ddb3b42d271f51a3899cafa1b2f1c
NandoTheessen <nando.theessen@gmail.com>
2018-06-11 23:52:18 +0200

    feat: boilerplated front-end, feat: restricted access to /api/restricted and all following to logged in users

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ server.use(session({
     saveUninitialized: false,
     secret: `I sexually Identify as an Attack Helicopter. Ever since I was a boy I dreamed of soaring over the oilfields dropping hot sticky loads on disgusting foreigners. People say to me that a person being a helicopter is Impossible and I'm fucking retarded but I don't care, I'm beautiful.I'm having a plastic surgeon install rotary blades, 30 mm cannons and AMG-114 Hellfire missiles on my body. From now on I want you guys to call me "Apache" and respect my right to kill from above and kill needlessly. If you can't accept me you're a heliphobe and need to check your vehicle privilege. Thank you for being so understanding.`
 }))
+server.use('/api/restricted', (req, res, next) => {
+    console.log("fire middleware")
+    req.session.loggedIn ? next() : res.status(403).json({ error: "Please login to view this site" })
+})
 
 server.get('/', (req, res) => {
     // console.log(req)
