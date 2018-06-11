2ca83f50b47fc2978118bdf64de51bac2e24f625
maribelcuales <maribelcuales@gmail.com>
2018-06-12 06:58:21 +0800

    added schema, post method, and hash for password

--- auth/UserModel.js
+++ auth/UserModel.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt'); 
 
-const UserModel = new.mongoose.Schema({
+const UserModel = new mongoose.Schema({
     username: {
         type: String,
         unique: true,
-        required
-    }
-})
+        required: true,
+        lowercase: true 
+    },
+    password: {
+        type: String,
+        required: true,
+        minlenght: 5
+    },
+});
+
+UserModel.pre('save', function(next) {
+    bcrypt.hash(this.password, 12, (err, hash) => {
+        if (err) {
+            return next (err); 
+        }
+        this.password=hash; 
+        next(); 
+    });
+}); 
+
+module.exports = mongoose.model('User', UserModel); 
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const mongoose = require ('mongoose'); 
+const bodyParser = require ('body-parser'); 
+const session = require ('expess-session'); 
 
 const User = require ('./auth/UserModel'); 
 
 mongoose.connect('mongodb://localhost/authIDb')
     .then(()=> {
-    console.log('/n*** Connected to database ***\n');
+    console.log('\n*** Connected to database ***\n');
 });
 
 const server = express(); 
 
 server.use(express.json()); 
+server.use(bodyParser.json()); 
+// server.use(session({
+//     secret: '$2b$12$UyBPjlgZlmPDyNUyxeQazuAyA5stlTkXBQrkaRZlRCLY5glkqmPBi'
+// }));
 
 server.get('/', (req, res) => {
     res.status(200).json({ api: 'running...'});
@@ -26,6 +32,22 @@ server.post('/api/register', (req, res) => {
         });
 }); 
 
-server.listen(8000, () => {
+
+server.post('/api/login', (req, res) => {
+    User.create(req.body)
+        .then(user => {
+            res.status(200).json({ api: 'Logged in' }); 
+        })
+        .catch(error => {
+            res.status(401).json({ error: 'You shall not pass!' });
+        });
+    });
+
+server.get('/api/users', (req, res) => {
+    res.status(200).json(res); 
+})
+
+
+server.listen(5000, () => {
     console.log('\n*** API running on port 5000 ***\n'); 
 }); 
