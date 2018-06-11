3b499ccd6375fb4d45cbd0bc5da82b50dfccc362
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-06-11 15:45:49 -0700

    added login methods and tested them sucessfully

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const userRouter = require('./users/UserRouter');
+const User = require('./users/UserModel');
 const localHost = 'localhost:27017';
 const database = 'usersdb';
 const server = express();
@@ -15,14 +17,61 @@ mongoose
         console.log("Connection Failed")
     });
 
+function authenticate(req, res, next) {
+    if(req.body.password === 'mellon') {
+        next();
+    } else {
+        res.status(401).send("You Shall Not Pass!")
+    }
+}
+
 server.use(express.json());
 server.use('/api', userRouter);
+server.use(
+    session({
+        secret: 'nobody tosses a dwarf!',
+        cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 },
+        httpOnly: true,
+        secure: false,
+        resave: true,
+        saveUninitialized: false,
+        name: "noname"
+    })
+);
 
 server.get('/', (req, res) => {
-    res.status(200).json({ api: 'running' });
+    if (req.session && req.session.username) {
+        res.send(`Welcome Back, ${req.session.username}`)
+    } else {
+        res.send("Login Attempt Unsuccessful")
+    }
 });
-  
+
+server.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+    User
+        .findOne({ username })
+        .then(user => {
+            if(user) {
+                user
+                    .isPasswordValid(password)
+                    .then(isValid => {
+                        if(isValid) {
+                            req.session.username = user.username;
+                            res.send("Login Successful")
+                        } else {
+                            res.status(401).send("Invalid Credentials")
+                        }
+                })
+            } else {
+                res.status(401).send("Invalid Credentials")
+            }
+        })
+        .catch(error => {
+            res.status(500).error({ error: error.message })
+        })
+})
+
 server.listen(port, () => {
     console.log(`\n=== API up on port: ${port} ===\n`);
 });
-
--- users/UserRouter.js
+++ users/UserRouter.js
@@ -10,7 +10,7 @@ router
                 res.status(200).json(users)
             })
             .catch(error => {
-                res.status(500).error({ errorMessage: "You Shall Not PASS!", error: error.message })
+                res.status(500).error({ error: error.message })
             })
     })
 router
@@ -25,29 +25,5 @@ router
                 res.status(500).error({ error: error.message })
             })
     })
-router
-    .route('/login')
-    .post((req, res) => {
-        const { username, password } = req.body;
-        User
-            .findOne({ username })
-            .then(user => {
-                if(user) {
-                    user
-                        .isPasswordValid(password)
-                        .then(isValid => {
-                            if(isValid) {
-                                res.send("Login Successful")
-                            } else {
-                                res.status(401).send("Invalid Credentials")
-                            }
-                    })
-                } else {
-                    res.status(401).send("Invalid Credentials")
-                }
-            })
-            .catch(error => {
-                res.status(500).error({error: error.message})
-            })
-    })
+
 module.exports = router;
