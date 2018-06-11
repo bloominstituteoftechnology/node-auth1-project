016827731ac07af98a60368afe6c459c0c0ad354
Zach Campbell <beazlebubbles@gmail.com>
2018-06-11 14:58:09 -0600

    Added session, began work on authentication

--- auth/userModel.js
+++ auth/userModel.js
@@ -17,15 +17,23 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', function(next) {
   console.log('pre save hook');
-  bcrypt.hash(this.password, 12, (err, hash) => {
+  bcrypt.hash(this.password, 16, (err, hash) => {
     if (err) next(err);
     else {
       this.password = hash;
       next();
     };
   });
-
 });
+
+//from https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
 
 
 
--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./auth/userModel.js');
 
-mongoose.connect('mongodb://localhost/cs10').then(() => {
+const session = require('express-session');
+
+
+
+mongoose.connect('mongodb://localhost/auth-i').then(() => {
   console.log('*** Connected to database ***');
 });
 
@@ -10,9 +14,24 @@ const server = express();
 
 server.use(express.json());
 
+server.use(session({
+  secret: 'Q4gG3XU929munG39JPhl3HavYkhqTkdv9E3rjaDKtexrZRxfM7'
+}));
+
 server.get('/', (req, res,) => {
   res.json({ api: 'running...' })
 });
+
+server.get('/view-counter', (req, res) => {
+	const session = req.session
+	if (!session.viewCount) {
+		session.viewCount = 0;
+	}
+	session.viewCount++;
+	res.json({ viewCount: session.viewCount })
+	// req.session is a persistent object that you'll see across requests for the same client
+// contains all the session variables that you set
+}) // First get request is 1, second is 2 / Doesn't just persist across one route
 
 server.post('/api/register', (req, res) => {
   User.create(req.body)
@@ -24,6 +43,6 @@ server.post('/api/register', (req, res) => {
   });
 });
 
-server.listen(8000, () => {
-  console.log('\n*** API running on port 8k ***\n');
+server.listen(5000, () => {
+  console.log('\n*** API running on port 5000 ***\n');
 });
