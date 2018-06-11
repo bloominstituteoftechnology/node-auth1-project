8bfdbca4e3e6cc1ade469fc952db090e6eda5ecd
Chris Honda <christopher.honda92@gmail.com>
2018-06-11 13:36:19 -0700

    Added UserModel to auth directory

---
+++ auth/UserModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 4
+    },
+});
+
+userSchema.pre('save', function(next) {
+    console.log('pre save hook');
+
+    bcrypt.hash(this.password, 12, (err, hash) => {
+        if(err) {
+            return next(err);
+        }
+
+        this.password = hash;
+
+        return next();
+    });
+});
+
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-//Model here
+const User = require('./auth/UserModel');
 
 mongoose.connect('mongodb://localhost/auth-i')
+    .then(
+        () => {
+            console.log('\n*** Connected to Database***\,');
+    })
+
+const app = express();
+
+app.use(express.json());
+
+app
+    .get('/', (req, res) => {
+        res.status(200).json({ api: 'running' });
+    })
+
+app
+    .post('/api/register', (req, res) => {
+        User.create(req.body)
+            .then(user => {
+                res.status(201).json(user);
+            })
+            .catch(error => {
+                res.status(500).json(error);
+            });
+    })
+
+port = 5001;
+
+app.listen(port, () => {
+    console.log(`API running on port ${port}`);
+});
