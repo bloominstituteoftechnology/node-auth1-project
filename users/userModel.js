8530d213f136be400d6e607288d4a21a27213b89
maxwashington <mnwashington18@gmail.com>
2018-06-11 17:37:37 -0500

    User Routes done username hash

---
+++ users/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userModel = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+});
+
+userModel.pre("save", function(next) {
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+    next();
+  });
+});
+
+module.exports = mongoose.model("User", userModel);
