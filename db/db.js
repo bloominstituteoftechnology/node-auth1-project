0b513a0cc15b7ee065303b5855f23b95081fd2d8
jesuarva <jean.ariza@jesuarva.com>
2018-06-11 22:12:33 +0200

    SMALL refactroring

--- db/db.js
+++ db/db.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 module.exports = {
   connectTo: function(database = 'sandbox', host = 'localhost') {
-    return mongoose.connect(`mongodb://${host}/${database}`);
+    return mongoose.connect(
+      `mongodb://${host}/${database}`,
+      () => {
+        console.log('\n**** Et voilà, we have direct line with the database! ****\n');
+      }
+    );
   },
 };
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const connectionToDB = require('./db/db');
 
-const server = express();
-
 connectionToDB.connectTo('auth_i');
+const server = express();
 
 const PORT = 6666;
 server.listen(PORT, () => console.log(`\n**** Yuhuuu! Server listening at port ${PORT} ****\n`));
