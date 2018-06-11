75fcaafd86a1577ea13a4d04ac7cda9d6ae7bc37
NandoTheessen <nando.theessen@gmail.com>
2018-06-12 01:07:47 +0200

    more commenting in the server.js file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ server.use(express.json())
 server.use(session({
     resave: true,
     saveUninitialized: true,
-    secret: `I sexually Identify as an Attack Helicopter. Ever since I was a boy I dreamed of soaring over the oilfields dropping hot sticky loads on foreigners. People say to me that a person being a helicopter is Impossible and I'm fucking retarded but I don't care, I'm beautiful.I'm having a plastic surgeon install rotary blades, 30 mm cannons and AMG-114 Hellfire missiles on my body. From now on I want you guys to call me "Apache" and respect my right to kill from above and kill needlessly. If you can't accept me you're a heliphobe and need to check your vehicle privilege. Thank you for being so understanding.`
+    secret: `RANDOM STRING WILL BE IN HERE!`
 }))
 
 // global middleware that restricts access to restricted and following to non-logged in users
