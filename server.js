51ab83e6019fecca565c71623fb031668e4d09d4
maxwashington <mnwashington18@gmail.com>
2018-06-11 16:50:51 -0500

    Created login,user, and register models and schema folders and files Added Global Middleware Added Routes in server

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const server = express()
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+
+//Routes
+
+const registerController = require('./register/registerController')
+const userController = require('./users/userController')
+const loginController = require('./login/loginController')
+
+//Gobal Middleware
+
+server.use(halmet())
+server.use(cors())
+server.use(express.json())
+
+//Endpoints
+
+//Mongoose
 mongoose.connect('mongodb://localhost/CS10', () => {
     console.log(`*** Connected to the Database ***`);
   })
 
-const server = express()
 
-server.use(express.json())
 
 server.get('/', (req,res) =>{
     res.status(200).json({API: "is up and running"})
