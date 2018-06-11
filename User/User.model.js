b0c3dc39f917508809d42c5f5dfe8ed8c7a84137
jesuarva <jean.ariza@jesuarva.com>
2018-06-11 22:11:52 +0200

    ADD starting User.model.js file

---
+++ User/User.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
