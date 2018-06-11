85fe556a0694464544a34311052eef8f696dbe4e
Steve <shalomlandsman@gmail.com>
2018-06-11 14:49:28 -0400

    server boilerplate

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express'); // remember to install your npm packages
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'running' });
+  });
+
+const port = process.env.PORT || 5000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/dbBudget', {}, err => {
+  if (err) console.log(err);
+  console.log('Mongoose connected us to our DB');
+});
+
+server.listen(port, () => {
+    console.log(`Server up and running on ${port}`);
+  });
