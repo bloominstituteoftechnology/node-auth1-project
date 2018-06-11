c1670ef2fbf7e4b9baf80031c55b779368e4ff40
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-06-11 12:23:08 -0700

    set up server and api

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const localHost = 'localhost:27017';
+const database = 'usersdb';
+const server = express();
+const port = process.env.PORT || 8000;
+
+mongoose
+    .connect(`mongodb://${localHost}/${database}`)
+    .then(response => {
+        console.log("Connection Successful")
+    })
+    .catch(error => {
+        console.log("Connection Failed")
+    });
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'running' });
+  });
+  
+  server.listen(port, () => {
+    console.log(`\n=== API up on port: ${port} ===\n`);
+  });
