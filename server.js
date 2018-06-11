b98afe3bc6e18612845c46f68577bc94443fe8be
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-11 19:01:54 -0400

    Setting up session

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const register = require('./routes/register');
 const login = require('./routes/login');
 
 server.use(require('express').json());
+server.use(require('express-session')({
+  secret: '6CLkzW9rSGmuk8ALKXc68zkuvYQSEDBeTS4v8LkPLYkr6Ljc4M',
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000
+  },
+  resave: true,
+  saveUninitialized: false
+}));
 server.use('/api/register', register);
 server.use('/api/login', login);
 
