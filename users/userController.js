4f27faa8f81e69d2e9ddf34d1592cd3fb39312db
Marco A Guzman <marcoguzman16@gmail.com>
2018-06-11 16:20:34 -0700

    .post

--- users/userController.js
+++ users/userController.js
@@ -1,17 +1,28 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
 
 const User = require('./userModel');
 
 
-router.route('/api/users').get((req, response) => {
+router.route('/api/users').get((req, res) => {
     User.find()
     then(users => {
         res.status(200).json(users);
     })
         .catch(error => {
-            res.status(500).json({ err });
+            res.status(500).json({ error: 'You shall not Pass' });
         })
 });  
+
+router.route('/api/register').post((req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+});
+
+
 
 
 
