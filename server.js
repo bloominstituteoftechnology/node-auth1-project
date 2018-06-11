e039f80510d94580079d17f45c37cf8023ca33d8
Xang Thao <xangthao24@yahoo.com>
2018-06-11 12:19:56 -0700

    Connected mongoose to mongo

--- server.js
+++ server.js
@@ -3,9 +3,22 @@ const mongoose = require('mongoose');
 const server = express();
 const port = 5555;
 
+server.use(express.json());
+
+mongoose.connect("mongodb://localhost/userdb")
+  .then(() => {
+    console.log("Connected to Mongo");
+  })
+  .catch(() => {
+    console.log("Error can't connect to Mongo");
+  })
+
+
 server.get("/", (req, res) => {
   res.json("Home");
 })
+
+
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
