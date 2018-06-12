06e8a272b1839487a225a956a6cd09d210a7a227
Danny Vail <danny_vail@outlook.com>
2018-06-12 16:50:39 -0400

    Using local middleware to restrict api/users GET endpoint to logged in users only

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const User = require('../models/user');
 
+const restrictAccess = (req, res, next) => {
+  (req.session && req.session.userId)
+    ? next()
+    : res.status(401).json({ error: 'You shall not pass!' });
+};
+
 router
-  .get('/', (req, res) => {
+  .get('/', restrictAccess, (req, res) => {
     User.find()
       .then(users => {
         res.status(200).json(users);
