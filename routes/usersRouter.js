b35a8ec8286340d5e7b0f7c217f49a7660ce3d00
Xang Thao <xangthao24@yahoo.com>
2018-06-12 11:59:51 -0700

    Completed assignment but not stretch

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,6 +3,14 @@ const Router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/UserModel");
 
+const protected = (req, res, next) => {
+  if(req.session && req.session.username){
+    next();
+  } else {
+    res.status(401).json({message: "Who are you again"});
+  }
+} 
+
 Router.post("/api/register", (req, res) => {
   const { username, password } = req.body;
   User.create({username, password})
@@ -21,18 +29,24 @@ Router.post("/api/login", (req, res) => {
       if(user) {
         user.compareHash(password, user.password, function(err, isMatch) {
           if(err) {
-            return res.json(err);
+            return res.status(401).json({message:" Wrong password or username"});
           }
           if(!isMatch) {
             return res.json({msg: "Incorrect info"})
           }
-          res.json({user, msg: "Success login"});
+          req.session.username = user.username;
+          res.json({user, msg: `Success login ${req.session.username}`});
         })
+      } else {
+        res.status(401).json({msg: "Wrong password or username"});
       }    
+    })
+    .catch(err => {
+      res.status(500).json(err);
     })
 })
 
-Router.get("/api/users", (req, res) => {
+Router.get("/api/users", protected, (req, res) => {
   User.find()
     .then(users => {
       res.json(users);
@@ -42,4 +56,15 @@ Router.get("/api/users", (req, res) => {
     })
 })
 
+Router.get("/api/logout", (req, res) => {
+  if(req.session) {
+    req.session.destroy(err => {
+      if(err) {
+        res.json({msg: "error logging out"})
+      } else {
+        res.json({msg: "Good bye"});
+      }
+    })
+  }
+})
 module.exports = Router;
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const server = express();
+const session = require("express-session");
 const port = 5555;
 const usersRouter = require("./routes/usersRouter");
 
-
+const sessionOptions = {
+  secret: "you can't see me",
+  cookie: {
+    maxAge: 1000 * 60 * 60
+  },
+  httpOnly: true,
+  secure: false,
+  resave: true,
+  saveUninitialized: false,
+  name: "tryagain"
+};
 
 server.use(express.json());
+server.use(session(sessionOptions));
 
 server.use("/", usersRouter);
 
@@ -20,7 +32,11 @@ mongoose.connect("mongodb://localhost/userdb")
 
 
 server.get("/", (req, res) => {
-  res.json("Home");
+  if(req.session && req.session.username) {
+    res.json({message: `Welcome back ${req.session.username}`});
+  } else {
+    res.status(401).json({message: "Please log in first"});
+  }
 })
 
 
