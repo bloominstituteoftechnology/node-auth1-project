b9ccaba7860784fbb6ffe11c6cd50ebd56bde115
Chris Honda <christopher.honda92@gmail.com>
2018-06-12 13:37:53 -0700

    Added cookies, login, logout, and session functionality

--- auth/UserModel.js
+++ auth/UserModel.js
@@ -30,4 +30,8 @@ userSchema.pre('save', function(next) {
     });
 });
 
+userSchema.methods.validatePassword = function(passwordGuess) {
+    return bcrypt.compare(passwordGuess, this.password)
+}
+
 module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
-
+const session = require('express-session');
 const User = require('./auth/UserModel');
 
 mongoose.connect('mongodb://localhost/auth-i')
@@ -13,14 +13,34 @@ mongoose.connect('mongodb://localhost/auth-i')
 
 const app = express();
 
+const sessionOptions = {
+    secret: 'nobody tosses a dwarf!',
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    },
+    httpOnly: true,
+    secure: false,
+    resave: true,
+    savedUninitialized: false
+}
+
 app.use(express.json());
+app.use(session(sessionOptions));
 app.use(cors());
 app.use(helmet());
+
+function protected(req, res, next) {
+    if (req.session) {
+        next();
+    } else {
+        res.status(401).json({ message: 'Please try again.' })
+    }
+}
 
 
 app
     .get('/', (req, res) => {
-        res.status(200).json({ api: 'running' });
+        res.status(200).json({ message: `Welcome back ${ req.session.user }` });
     })
 
 app
@@ -34,12 +54,54 @@ app
             });
     })
 
-// app
-//     .post('/api/login', (req, res) => {
-//         const { username, password } = req.body;
-//         User.save({ username, password })
-//             .then
-//     })
+app
+    .post('/api/login', (req, res) => {
+        const { username, password } = req.body;
+        User
+            .findOne({ username })
+            .then(user => {
+                if (user) {
+                    user.validatePassword(password)
+                        .then(passwordsMatch => {
+                            if(passwordsMatch) {
+                                req.session.username = user.username;
+                                res.send('Have a cookie.');
+                            } else {
+                                res.send('Invalid credentials.');
+                            }
+                        })
+                } else {
+                    res.status(401).send('Invalid credentials.')
+                }
+            })
+            .catch(error => {
+                res.send(error);
+            })
+    })
+
+app
+    .get('/api/logout', (req, res) => {
+        if(req.session) {
+            req.session.destroy(error => {
+                if(error) {
+                    res.send('Error logging out.');
+                } else {
+                    res.send('Goodbye.');
+                }
+            })
+        }
+    })
+
+app
+    .get('/api/users', protected, (req, res) => {
+        User.find()
+            .then(users => {
+                res.json(users)
+            })
+            .catch(error => {
+                res.json(error)
+            })
+    })
 
 port = 5001;
 
