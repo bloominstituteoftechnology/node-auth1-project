e51f52e3191ed45e2d13a3723849a7d590d91739
kelly <kelfro@fastmail.com>
2018-06-12 15:31:16 -0700

    began working on react app
    doesn't work yet, as I'm not sure how to get something that requires authorization into state.

--- thing/src/App.js
+++ thing/src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users:[]
+    };
+  }
+
+  componentDidMount() {
+    this.gatherUsers();
+  }
+
+  gatherUsers = () => {
+    axios.get('http://localhost:8000/api/users')
+      .then(res => {
+	console.log(res);
+	this.setState({ users: res.users });
+      })
+      .catch(error => console.log(error));
+  }
+  
   render() {
     return (
       <div className="App">
@@ -10,9 +31,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+	<div>
+	  {this.state.users.map(user => {
+	    return (
+	      {user}
+	    );
+	  })};
+	  </div>
       </div>
     );
   }
