291ba0c23684841857c1f56619913f0f731239e9
maribelcuales <maribelcuales@gmail.com>
2018-06-13 05:42:54 +0800

    added login, logout, and users

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require ('mongoose'); 
-const bodyParser = require ('body-parser'); 
-const session = require ('expess-session'); 
+const session = require ('express-session'); 
 
 const User = require ('./auth/UserModel'); 
 
@@ -12,14 +11,42 @@ mongoose.connect('mongodb://localhost/authIDb')
 
 const server = express(); 
 
+//middleware
+const sessionOptions = {
+    secret: 'nobody tosses a dwarf!', 
+    cookie: {
+        maxAge: 1000 * 60 * 60, // an hour
+    },
+    httpOnly: true, 
+    secure: false,
+    resave: true,
+    saveUninitialized: fales,
+    name: 'noname', 
+}; 
+
+function protected(req, res, next) {
+    if (req.session && req.session.username) {
+        next(); 
+    } else {
+        res.status(401).json({ message: 'you shall not pass!' });
+    }
+}
+
 server.use(express.json()); 
-server.use(bodyParser.json()); 
-// server.use(session({
-//     secret: '$2b$12$UyBPjlgZlmPDyNUyxeQazuAyA5stlTkXBQrkaRZlRCLY5glkqmPBi'
-// }));
+server.use(session(sessionOptions)); 
+
+server.get('/api/users', protected, (req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.json(err)); 
+}); 
 
 server.get('/', (req, res) => {
-    res.status(200).json({ api: 'running...'});
+    if (req.session && req.session.username) {
+        res.status(200).json({ message: `welcome back ${req.session.username}` });         
+    } else {
+        res.status(401).json({ message: 'speak friend and enter '});
+    }
 }); 
 
 server.post('/api/register', (req, res) => {
@@ -34,19 +61,44 @@ server.post('/api/register', (req, res) => {
 
 
 server.post('/api/login', (req, res) => {
-    User.create(req.body)
+    const { username, password } = req.body;
+
+    User.findOne({ username })
         .then(user => {
-            res.status(200).json({ api: 'Logged in' }); 
+            if (user) {
+                user
+                    .validatePassword(password)
+                    .then(passwordMatch => {
+                        if(passwordMatch) {
+                            req.session.username = user.username;
+                            res.send('have a cookie'); 
+                        } else {
+                            res.status(401).send('invalid credentials'); 
+                        }
+                    })
+                    .catch(err => {
+                        res.send('error comparing passwords'); 
+                    }); 
+            } else {
+                res.status(401).json('invalid credentials');                 
+            }
         })
-        .catch(error => {
-            res.status(401).json({ error: 'You shall not pass!' });
+        .catch(err => {
+            res.send(err);
         });
-    });
+});
 
-server.get('/api/users', (req, res) => {
-    res.status(200).json(res); 
-})
-
+server.get('/api/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.send('error logging out'); 
+            } else {
+                res.send('goodbye');
+            }
+        });
+    }
+});
 
 server.listen(5000, () => {
     console.log('\n*** API running on port 5000 ***\n'); 
