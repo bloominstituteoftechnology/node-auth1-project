ef1047d307f41942f5bd3418eb499b1ea5417057
bbenefield89 <bsquared18@gmail.com>
2018-06-12 16:49:36 -0400

    feat(app.js): compare user/password with user/hashedPassword for secure login feat(/client/): add React frontend

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
 
 const User = require('./User.js');
 
 const app = express();
 
 app.use(express.json());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+/*************************
+** CUSTOM MIDDLEWARE **
+*************************/
+// checkLogin
+const checkLogin = (req, res, next) => {
+  
+}
 
 /*************************
 ** /api/users **
@@ -42,12 +57,22 @@ app.post('/api/register', (req, res) => {
 app.post('/api/login', (req, res) => {
   const user = ({ username, password } = req.body);
 
-  User.findOne(user, (err, raw) => {
-    if (err)
-      return res.status(500).json(err);
+  User.findOne()
+    .where({ username })
+    .then(user => {
+      if (!user)
+        return res.status(404).json({ err: 'No user was found' });
+      
+      bcrypt.compare(password, user.password)
+        .then(match => {
+          if (!match)
+            return res.status(401).json({ err: 'Wrong password' });
 
-    res.json(raw);
-  });
+          return res.json({ succ: 'Authenticated' });
+        })
+        .catch(err => res.json(err));
+    })
+    .catch(err => res.json(err));
 });
 
 mongoose.connect('mongodb://localhost/auth-i', () => console.log('\n===== DATABASE STATUS: 200 =====\n'));
