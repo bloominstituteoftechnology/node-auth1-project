9e886b7a31d9e575ce02170a2f2d649bc295ff08
unknown <sam.park.email@gmail.com>
2018-06-13 00:39:29 -0400

    added redirect on successful log in

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Register from './components/register'
 import Login from './components/login';
 import Home from './components/home';
+import Logged from './components/loggedIn'
 
 class App extends Component {
 
@@ -18,6 +19,7 @@ class App extends Component {
         <Route exact path="/" component={Home} />
         <Route exact path="/api/login" component={Login} />
         <Route exact path="/api/register" component={Register} />
+        <Route exact path="/api/loggedin" component={Logged} />
       </div>
     );
   }
---
+++ client/src/components/loggedIn.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Logged extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                You have been logged in!
+                </div>
+         )
+    }
+}
+ 
+export default Logged;
