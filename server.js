2bdd38901bbdea1f938e822accf65206b7b29ead
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-06-14 12:53:50 -0500

    Goig to start reorganizing logic.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const server = express();
 const database = require('./data/database');
 
 const routes = require('./user/routes');
+const user = require('./user/User');
 
 database.connectTo('AuthMini')
   .then(() => {
@@ -23,6 +24,19 @@ database.connectTo('AuthMini')
   const port = process.env.PORT || 5000;
   server.listen(port, () => {
       console.log(`Server up on port ${port}`);
+  })
+
+  server.get('/', (req, res) => {
+      res.status(200).json({ message: 'Connected to server!' });
+  })
+
+  server.post('/register', (req, res) => {
+      const { username, password } = req.body;
+      if (!username || !password) res.sendStatus(400);
+      const user = new user(req.body);
+      user.save()
+        .then( user => res.status(201).json(user))
+        .catch( err => res.status(500).send(err));
   })
 
   
