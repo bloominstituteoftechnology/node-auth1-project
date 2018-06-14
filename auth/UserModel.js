401a07b29570d528c2c9bf669dc117085c9ea02b
sagardesai90 <saagardesai@gmail.com>
2018-06-14 12:17:39 -0500

    getting closer

--- auth/UserModel.js
+++ auth/UserModel.js
@@ -23,7 +23,6 @@ userSchema.pre('save', function(next){
             return next(err);
         }
         this.password = hash;
-
         next();
     });
 });
--- server.js
+++ server.js
@@ -4,6 +4,42 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
 const User = require('./auth/UserModel.js');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+server.use(
+    session({
+        secret: "secret1",
+        cookie: {maxAge: 1 * 24 * 60 * 60 * 1000},
+        httpOnly: true,
+        secure: false,
+        resave: true,
+        saveUninitialized: false,
+        name: 'none',
+        store: new MongoStore({
+            url: 'mongodb://localhost/sessions',
+            ttl: 60 * 10
+        })
+    })
+);
+
+const restrictAuth = (req, res, next) => {
+    if(req.path.startsWith('/restricted')){
+        if(req.session ** req.session.username){
+            next();
+        }else{
+            res.status(422).json({message: "This content is restricted to logged in users" })
+        }
+    } else{
+        next();
+    }
+};
+server.use(restrictAuth);
+
+server.get(`/restricted/:username`, (req, res, next) => {
+    const username = req.params.username;
+    res.status(200).json({message: `Welcome to our VIP page, ${username}`})
+})
 
 mongoose.connect('mongodb://localhost/user')
     .then(() => {
@@ -16,7 +52,7 @@ mongoose.connect('mongodb://localhost/user')
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.status(200).json({api: "running!"});
+    res.status(200).json({api: "running well!"});
 });
 
 server.post('/register', (req, res) => {
@@ -38,11 +74,12 @@ server.post('/login', (req, res) => {
     const {username, password} = req.body;
     User.findOne({username})
         .then(user => {
+            console.log(req.session);
             if(user){
                 user.isPasswordValid(password).then(isValid => {
                     if(isValid){
                         req.session.username = user.username
-                        res.send('have a cookie')
+                        res.send('login successful');
                     } else {
                         res.status(401).send('invalid creds');
                     }
@@ -61,7 +98,7 @@ server.get('/users', (req, res, next) => {
                 res.status(200).json(users);
             })
             .catch(error => {
-                res.status(200).json(error);
+                res.status(500).json(error);
             });
     } else {
         res.status(401).json({error: "Please try again."});
