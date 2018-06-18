e155dc68d22c0cdae8a6cf2eb75d53e9b7debf0c
CMVNK <Kopecky12112@gmail.com>
2018-06-18 13:14:33 -0700

    passwords are hashed!

---
+++ UserModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        required: true
+    }
+});
+
+//hash functionality using bcrypt
+UserSchema.pre('save', function(next) {
+    console.log("pre save hook");
+    bcrypt.hash(this.password, 12, (err, hash) => {
+        if(err){
+            return next(err);
+        }
+        this.password = hash;
+        next();
+
+    });
+});
+
+const UserModel = mongoose.model("User", UserSchema, "users");
+
+module.exports = UserModel;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const User = require("./UserModel");
+const server = express();
+
+mongoose.connect("mongodb://localhost:27017/userAuthdb")
+    .then(() => {
+        console.log('\n*** Connected to database ***\n');
+    })
+    .catch(err =>{
+        console.log(err.message);
+    });
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.status(200).json({Success: "API is running . . ."});
+});
+
+server.post("/api/register", (req, res) => {
+    User.create(req.body)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json(err.message);
+        });
+});
+
+
+
+let port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
