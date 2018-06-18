1d2b8a86e6b7c74ca76ae4069fb630d440e8bc0d
JSNavey <jsnavey@gmail.com>
2018-06-18 18:27:43 -0400

    implement credentials to login

--- users/UserModel.js
+++ users/UserModel.js
@@ -15,15 +15,21 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-userSchema.pre('save', function(next) {
-    bcrypt.hash(this.password, 12, (error, hash) => {
+userSchema.pre('save', function(next) { //need to use function, not fat arrow becoz it won't look inside userSchema.
+    bcrypt.hash(this.password, 12, (error, hash) => { //"this.password" comes from line 11
         if(error) {
             return next(error);
         }
         this.password = hash;
-        next();
-    })
-})
+        next(); 
+    });
+});
+
+//compare inputPassword and the correct password that stored in model
+//then pass validatePassword method to userRouter to check if the findOne username input the matched password
+userSchema.methods.validatePassword = function(inputPassword) {
+    return bcrypt.compare(inputPassword, this.password);
+};
     
 
 module.exports = mongoose.model('User', userSchema);
--- users/UserRouter.js
+++ users/UserRouter.js
@@ -15,6 +15,39 @@ router
             .catch(error => {
                 res.status(500).json(error);
             });
-    });
+});
+
+// POST login 
+//grab credential
+//find the user to get access to the store password
+//compare the password guess to the stored one from the model.
+//if not found, give 'You shall NOT pass'.
+router
+    .route('/login')
+    .post((req, res) => {
+        const { username, password } = req.body;
+        User
+            .findOne({ username })
+            .then(user => {
+                if(user) {
+                   user.validatePassword(password)
+                   .then(matchedPwd => {
+                       if(matchedPwd) {
+                           res.send('Logged In Successfully')
+                       } else {
+                           res.send('You shall NOT pass!')
+                       }
+                   })
+                   .catch(error => {
+                       res.send('Comparing Password Error.')
+                   })
+                } else {
+                    res.status(404).json('User Not found');
+                }
+            })
+            .catch(error => {
+                res.status(500).json(error)
+            })
+    })
 
 module.exports = router;
