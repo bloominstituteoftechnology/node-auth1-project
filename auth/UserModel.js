136d9851ab229ed6fb2d8199b7e310dbf4dd191c
april <martinezapril56@yahoo.com>
2018-06-19 06:50:07 -0400

    commit

--- auth/UserModel.js
+++ auth/UserModel.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema( {
         type: String,
         unique: true,
         required: true,
-        lowercase: true
+        lowercase: true 
         
     },
     password: {
