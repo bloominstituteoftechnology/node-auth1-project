273e905899fcb33cffffc07b9ebe8a5f516b79ca
Tyson Gushiken <tysonism@gmail.com>
2018-08-06 12:37:35 -0700

    Create POST /api/register endpoint

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const db = require('../data/dbConfig.js');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 router.post('/register', (req, res) => {
-  res.status(200).json('register');
+  const user = {...req.body};
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  db('users')
+    .insert(user)
+    .then(id => {
+      return res.status(200).json({'message':`${user.username} added!`});
+    })
+    .catch(e => {
+      return res.status(500).json(e);
+    });
 });
 
 router.post('/login', (req, res) => {
