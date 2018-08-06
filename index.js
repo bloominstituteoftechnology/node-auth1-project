2db3256ae3fb0710d76b76087ef024194dcfe2b5
Alec <afroenygam@gmail.com>
2018-08-06 17:39:29 -0400

    Authentication updated, POSTs and GET updated, cookie needed

--- index.js
+++ index.js
@@ -3,19 +3,31 @@ const server  = express();
 const PORT    = 8000;
 const bcrypt  = require('bcryptjs');
 const db      = require('./data/dbConfig');
+let isValid   = null;
 
 server.use(express.json());
+const authenticate = async (req, res, next) => {
+  const credentials = req.body;
+  const foundUser = await db('users').where('username', credentials.username).first();
+  const userHash = foundUser.password;
+  isValid = bcrypt.compareSync(credentials.password, userHash);
+  next();
+};
 
 server.get('/', (req, res) => {
    res.status(200).send('HellO!');
  });
 
  server.get('/api/users', (req, res) => {
-   const users = db('users').then(response => {
-     res.status(200).json(response);
-   }).catch(err => {
-     res.status(500).json(`${err}`);
-   });
+   if (isValid) {
+       const users = db('users').then(response => {
+         res.status(200).json(response);
+       }).catch(err => {
+         res.status(500).json(`${err}`);
+       });
+ } else {
+   res.status(401).json({status:`401 Denied`, message: "You are not logged in"});
+ }
  })
 
  server.post('/api/register', async (req, res) => {
@@ -36,10 +48,12 @@ server.get('/', (req, res) => {
    }
  })
 
- server.post('/api/login', (req, res) => {
-   const user = req.body;
-   //should not need this const hash = bcrypt.hashSync(user.password, 14);
-   const isValid = bcrypt.compareSync(user.password, /*hash which was retrieved from db*/);
+ server.post('/api/login', authenticate, async (req, res) => {
+   if (isValid) {
+     res.status(200).json('Logged In')
+   } else {
+     res.status(401).json({status:401, message: 'You shall not pass!'})
+   }
  });
 
  server.get('/api/users', (req, res) => {
