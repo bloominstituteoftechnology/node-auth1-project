bb1f4378841ed7a6d23c94c722a28db4d790b1b8
Joe Stanfield <jlstan06@hotmail.com>
2018-08-06 16:21:52 -0400

    first endpoint working

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ server.use(express.json());
 
 ///endpoints go here
 
+server.get('/', (req, res) => {
+    db('users')
+        .then(proj => res.status(200).json(proj))
+        .catch(err => res.status(500).json({error:'These are not the projects you are looking for'})
+    )
+})
+
+server.post('/api/register', (req, res) => {
+    const register = req.body;
+    const hash = bcrypt.hashSync(register.password, 14);
+    register.password = hash;
+    if (!register.username || !register.password)
+    res.status(400).json({errorMessage:"Required username and password"});
+    db('users')
+        .insert(register)
+        .then(user => res.status(201).json(register))
+        .catch(err => res.status(400).json({error: 'Error posting'}))
+})
+
 const port = 3300;
 server.listen(port, function() {
  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
