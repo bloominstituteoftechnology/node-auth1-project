96ae76c58daf459eec9d8841da1bb765604921e9
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-06 14:43:23 -0600

    Created register and login .post endpoints which includes hashing passwords

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const db = require('./data/db.js');
+const bcrypt = require('bcryptjs');
 
 const server = express();
 
 server.use(express.json());
 
 // Endpoints Here
+server.get('/users', (req, res) => {
+  db('users')
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({error: 'GET USERS'}))
+})
+server.post('/register', (req, res) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
 
-server.listen(3300, function() {console.log('API is running on 3300')});
+  db('users')
+    .insert(user)
+    .then(function(ids) {
+      db('users')
+        .where({ id: ids[0] })
+        .first()
+        .then(user => {
+          res.status(201).json(user);
+        })
+    })
+    .catch(err => {
+      res.status(500).json({ error });
+    })
+})
+server.post('/login', (req, res) => {
+  const credentials = req.body;
+  db('users')
+    .where({ username: credentials.username })
+    .first()
+    .then(function(user) {
+      if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
+        return res.status(401).json({ error: 'Incorrect credentials' });
+      } res.status(200).json('Hello, welcome')
+    })
+    .catch(err => {
+      res.status(500).json({ error });
+    })
+})
+
+server.listen(3300, function () { console.log('API is running on 3300') });
