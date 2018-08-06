602794b958c6aa84d3889342052ecdab86ceb144
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-06 11:42:17 -0700

    added some  dependencies

--- migrations/20180806112331_user_table.js
+++ migrations/20180806112331_user_table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTabel('user', function(tbl) {
+      tbl.increment().unique();
+      tbl.string('userName', 50).notNullable();
+      tbl.text('password').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('user')
 };
--- server.js
+++ server.js
@@ -1,41 +1,68 @@
 const bcrypt = require('bcryptjs');
+const express = require('express');
+const db = require('./data/db');
 
-// ******  to hash a password *******
-
-const credentials = req.body;
-
-const hash = bcrypt.hashSync(credentials.password, 14);
-
-credentials.password = hash;
-
-// move on to save the user.
-
-// ******  to hash a password *******
-const credentials = req.body;
-
-// find the user in the database by it's username then
-if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
-  return res.status(401).json({ error: 'Incorrect credentials' });
-}
-
-// the user is valid, continue on
-
+const port = 8002;
 const server = express();
 
-//hash password
-const hash = bcrypt.hashSync(user.password, 14);
-user.password = hash
+server.use(express.json());
 
-debug('users')
-    .insert(user)
-    .then(function(ids) {
-        db('users')
-        .where({ id: ids[0] })
-        .first()
-        .then(user => {
-            res.status(201).json(user);
-        });
-    })
-    .catch(function(err) {
-        res.status(500).json({ err });
-    })
+server.listen(port, () => console.log(`Server is listening port ${port}`))
+
+server.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1>')
+})
+
+// // ******  to hash a password *******
+
+// const credentials = req.body;
+
+// const hash = bcrypt.hashSync(credentials.password, 14);
+
+// credentials.password = hash;
+
+// // move on to save the user.
+
+// // ******  to hash a password *******
+// const credentials = req.body;
+
+// // find the user in the database by it's username then
+// if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
+//   return res.status(401).json({ error: 'Incorrect credentials' });
+// }
+
+// // the user is valid, continue on
+
+
+// //hash password
+// const hash = bcrypt.hashSync(user.password, 14);
+// user.password = hash
+
+// debug('users')
+//     .insert(user)
+//     .then(function(ids) {
+//         db('users')
+//         .where({ id: ids[0] })
+//         .first()
+//         .then(user => {
+//             res.status(201).json(user);
+//         });
+//     })
+//     .catch(function(err) {
+//         res.status(500).json({ err });
+//     })
+
+
+//     // login 
+
+//     server.post('/login', function(req, res) {
+//         const credentials = req.body;
+
+
+//     })
+//     db('users')
+//     .where({ username: user.username}).first()
+//     .then(function(user) {
+
+//         if()
+//     })
