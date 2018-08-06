8ecf53b0a1eda37bef007080d5a40fe14a789856
Simon <Simon_Freelance@hotmail.com>
2018-08-06 17:18:14 -0400

    Made a GET request to get all users in the db

--- api/userRouter.js
+++ api/userRouter.js
@@ -1,9 +1,17 @@
-const codes = require('../data/statusCodes');
+const codes = require("../data/statusCodes");
 
-const express = require('express');
-const db = require('../data/dbConfig');
+const express = require("express");
+const db = require("../data/dbConfig");
 
 const router = express.Router();
 
-
+router.get("/", (req, res, next) => {
+  db("users")
+    .then(response => {
+      res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 module.exports = router;
