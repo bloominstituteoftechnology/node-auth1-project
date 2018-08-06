e43d348cbeecf5b82115b4afde50f79ceda8fb32
Wdifulvio523 <w.difulvio523@gmail.com>
2018-08-06 16:44:23 -0400

    Minor changes and refactoring. MVP Completed

--- index.js
+++ index.js
@@ -1,42 +1,60 @@
-const express = require('express');
-const db = require('./data/db.js');
-const bcrypt = require('bcryptjs'); 
+const express = require("express");
+const db = require("./data/db.js");
+const bcrypt = require("bcryptjs");
 const server = express();
 server.use(express.json());
 
-
-server.get('/users', (req, res) => {
-   db('users')
-   .then(users => {res.status(200).json(users)})
-   .catch(err => {
-       res.status(500).json(err)
-   })
-  })
-
-server.post('/register', (req, res) => {
-    const user = req.body;
-    const hash = bcrypt.hashSync(user.password, 14);    
-    user.password = hash;
-
-    db('users')
-    .insert(user)
-    .then(ids => {
-        db('users')
-            .where({ id: ids[0]})
-            .first()
-            .then(user => {
-                res.status(201).json(user);
-            })
+server.get("/api/users", (req, res) => {
+  db("users")
+    .then(users => {
+      res.status(200).json(users);
     })
     .catch(err => {
-        res.status(500).json({
-          error: "There was an error while saving the user to the database"
-        });
-      });
-})
+      res.status(500).json(err);
+    });
+});
 
+server.post("/api/register", (req, res) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+
+  db("users")
+    .insert(user)
+    .then(ids => {
+      db("users")
+        .where({ id: ids[0] })
+        .first()
+        .then(user => {
+          res.status(201).json(user);
+        });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
+
+server.post("/api/login", (req, res) => {
+  const credentials = req.body;
+
+  db("users")
+    .where({ username: credentials.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        res.send("Welcome!");
+      } else {
+        return res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
- console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
--- migrations/20180806162617_users.js
+++ migrations/20180806162617_users.js
@@ -1,17 +1,13 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('users', function(register) {
-        register.increments();
+  return knex.schema.createTable("users", function(register) {
+    register.increments();
 
-        register
-            .string("username")
-            .notNullable()
+    register.string("username").notNullable();
 
-        register
-            .string("password")
-            .notNullable()
-    });
+    register.string("password").notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('users');
+  return knex.schema.dropTableIfExists("users");
 };
