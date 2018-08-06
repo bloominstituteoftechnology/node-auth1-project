1ab4bdfe71b80da7a453ee038ffc945205fac940
emaquino44 <prexy44@yahoo.com>
2018-08-06 14:17:00 -0700

    created register endpoint - implemented hash functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./data/db');
+const bcrypt = require('bcryptjs');
 const server = express();
 
 server.use(express.json());
@@ -9,7 +10,24 @@ server.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+//********GET USERS ENDPOINT*********************
+server.get('/users', (req, res) => {
+	db('users').then(users=> {
+		res.status(200).json(users);
+	}).catch(err => res.status(500).json(err));
+});
 
+//********CREATE REGISTER ENDPOINTS*********************
+server.post('/register', (req, res) => {
+	const user = req.body;
+	const hash = bcrypt.hashSync(user.password, 14);
+	user.password = hash;
+	db.insert(user).into('users')
+	.then(ids => {
+		const id = ids[0];
+		res.status(201).json({ id, ...user })
+	}).catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
