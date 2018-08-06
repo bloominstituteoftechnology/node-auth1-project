1815641d4d8ef80d1ea06f5b2b4cbf8b63e8bd80
Cesar Mejia <cesar@Cesars-iMac.local>
2018-08-06 12:54:27 -0700

    clean start

--- src/app.js
+++
@@ -1,5 +0,0 @@
-// Do NOT modify this file; make your changes in server.js.
-const { server } = require('./server.js');
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => console.log('server running'));
