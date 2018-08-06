115da7d15b7ef48db4e76f1957dae8b0daff844d
Tyson Gushiken <tysonism@gmail.com>
2018-08-06 11:58:51 -0700

    Create basic routes

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -1,4 +1,17 @@
 const express = require('express');
-const router = express.Router(); 
+const db = require('../data/dbConfig.js');
+const router = express.Router();
+
+router.post('/register', (req, res) => {
+  res.status(200).json('register');
+});
+
+router.post('/login', (req, res) => {
+  res.status(200).json('login');
+});
+
+router.get('/users', (req, res) => {
+  res.status(200).json('users');
+});
 
 module.exports = router;
