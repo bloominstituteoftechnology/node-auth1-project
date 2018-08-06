47958f79a2d0881b1173a049a69e026386944eb4
Simon <Simon_Freelance@hotmail.com>
2018-08-06 17:11:43 -0400

    Creation of user with password completed

--- api/registerRouter.js
+++ api/registerRouter.js
@@ -1,9 +1,23 @@
-const codes = require('../data/statusCodes');
+const codes = require("../data/statusCodes");
 
-const express = require('express');
-const db = require('../data/dbConfig');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const db = require("../data/dbConfig");
 
 const router = express.Router();
 
+router.post("/", (req, res, next) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+  db("users")
+  .insert(user)
+    .then(response => {
+      res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = router;
