7105461a19d35e5004ff917ac13def9f03ee46c0
chainchompa <chainchompa@gmail.com>
2018-08-06 18:17:39 -0400

    added post to api/login endpoint

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ server.post('/api/register', (req, res) => {
   })
 });
 
+server.post('/api/login', (req, res) => {
+  const credentials = req.body;
+
+  db('users')
+  .where({ username: credentials.username })
+  .first()
+  .then(user => {
+    if (user && bcrypt.compareSync(credentials.password, user.password)) {
+      return res.status(200).json({'message': 'You are now logged in.'})
+    }
+    return res.status(401).json({'errorMessage': 'The username and password you entered did not match our records. You shall not pass!'})
+  })
+  .catch(err => {
+    res.status(500).json({'error': 'Could not login user'})
+  })
+});
+
+
 const port = 8080;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
