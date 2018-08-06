1f02ecb1c0e308f4610085b10965b27db39eec58
DirupT <iamcooled@gmail.com>
2018-08-06 15:47:47 -0500

    added restricted route

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const userRoutes = require('./users/userRoutes');
 const loginRoutes = require('./login/loginRoutes');
 const registerRoutes = require('./register/registerRoutes');
+const restrictedRoutes = require('./restricted/restrictedRoutes');
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
 router.use('/login', loginRoutes);
 router.use('/register', registerRoutes);
+router.use('/restricted', restrictedRoutes);
 
 module.exports = router;
