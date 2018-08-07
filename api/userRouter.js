0c339991ad03c0fbf9f6c48b35e542763e40a1d6
Simon <Simon_Freelance@hotmail.com>
2018-08-07 17:22:49 -0400

    User can logout

--- api/userRouter.js
+++ api/userRouter.js
@@ -34,4 +34,17 @@ router.get("/", restricted('normal'), (req, res, next) => {
     });
 });
 
+router.get("/logout", (req, res, next) => {
+  if(req.session) {
+    req.session.destroy(function(err) {
+      if (err) {
+        res.send("Logging out of session did not happen");
+      }
+      else {
+        res.send("You are now logged out");
+      }
+    });
+  }
+})
+
 module.exports = router;
