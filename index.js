ef7248323d6fd9c78df8c5063792dda6e8d73cb0
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-07 14:49:25 -0700

    Build basic functionality without session for post /login

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ server.use(express.json())
 
 server.listen(3300, () => console.log('API running on port 3300'))
 
+
+server.use(
+    session({
+        name: 'yeehaw',
+        secret: 'Then after the show, it\'s the after party. And after the party, it\'s the hotel lobby.',
+        cookie: { maxAge: 1 * 24 * 60 * 60 * 1000},
+        httpOnly: true,
+        secure: false,  // Set to true when not in development
+        resave: true,
+        saveUninitialized: false,   // to automatically set cookies
+    })
+)
 server.get('/', (req, res) => {
     console.log("Hello")
     res.send("It's alive")
@@ -40,24 +52,16 @@ server.post('/api/register', (req, res) => {
 server.post('/api/login', (req, res) => {
     const credentials = req.body
     console.log("Credentials", credentials)
-    const user = db('users')
-    //                     .where({userName: credentials.userName})
-    //                     .then(user => {
-    //                         console.log("User", user)
-    //                     })
-    //                     .catch( err => res.status(500).json("Testing123"))
-    console.log(user.password)
-    console.log(credentials.password)
-    if(!user || !bcrypt.compareSync(credentials.password, user.password)){
-        return res.status(401).json({error: "Incorrect Credentials"})
-    }else{
-        // const session = req.session
-        // if(!session.logins){
-        //     session.logins = 0
-        // }
-        // session.logins++;
-
-        res.status(200).json({ logins: session.logins })
-    }
-    const hash = bcrypt.hachSync(credentials.password, 14)
+    
+    db('users')
+        .where({ userName: credentials.userName })
+        .first()
+        .then(user => {
+            if(!user || !bcrypt.compareSync(credentials.password, user.password)){
+                return res.status(401).json({error: "Incorrect Credentials"})
+            }else{
+                res.status(200).json(`Welcome ${user.userName}`)
+            }
+        })
+        .catch( err => res.status(500).json(err.message))
 })
