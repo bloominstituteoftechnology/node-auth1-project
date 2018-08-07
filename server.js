6ed92463eb7a244dc23741effaa86692d1927feb
Simon <Simon_Freelance@hotmail.com>
2018-08-07 15:51:47 -0400

    Modified/Added  session options

--- server.js
+++ server.js
@@ -12,7 +12,23 @@ const loginRoutes = require('./api/loginRouter');
 
 server.use(express.json());
 server.use(session({
-    secret: 'gahniNoh2Eeve8jiweerie9aequu5Tha8jahm8lie3iecha7La'
+    //default is connect.sid, need to change the name so the attacker won't know the library we used
+    name: 'tanglewood',
+    //the secret is a private key in order to decrypt our cookie
+    secret: 'gahniNoh2Eeve8jiweerie9aequu5Tha8jahm8lie3iecha7La',
+
+    cookie: {
+        //one day in milliseconds
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        //only set cookies over https. Server will not send back a cookie over http
+        secure: false,
+    }, 
+    //don't let JS code access cookies. Browser extensions run JS code on your browser
+    httpOnly: true,
+    //forces the session to be saved back to the session store, even if the session was never modified during the request
+    resave: false,
+    //forces a session that is "uninitialized" to be saved to the store. A session is uninitialized when it is new but not modified
+    saveUninitialized: true,
 }));
 server.use('/api/users', userRoutes);
 server.use('/api/register', registerRoutes);
