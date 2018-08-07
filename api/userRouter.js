26b8da88f0eb4c4cf29f9dacefbdb222b136daba
Simon <Simon_Freelance@hotmail.com>
2018-08-07 17:07:17 -0400

    Added restriction user access middleware

--- api/userRouter.js
+++ api/userRouter.js
@@ -1,22 +1,30 @@
 const codes = require("../data/statusCodes");
 
 const express = require("express");
-const session = require('express-session');
+const session = require("express-session");
 
 const db = require("../data/dbConfig");
 
 const router = express.Router();
-function restricted (req, res, next) {
-  console.log(req.session)
-  
-  if(req.session && req.session.username) {
-    next();
-  } else {
-    return res.status(codes.BAD_REQUEST).json({ error: 'Incorrect credentials '})
-  }
+function restricted(role) {
+  return function(req, res, next) {
+    const accessCodes = {
+      admin: 2,
+      normal: 1,
+      free: 0,
+    };
+    const userRole = req.session.user.role;
+    const hasAccess = accessCodes[userRole] >= accessCodes[role] ? true : false;
+    if (req.session && hasAccess) {
+      next();
+    } else {
+      return res
+        .status(codes.BAD_REQUEST)
+        .json({ error: "Incorrect credentials" });
+    }
+  };
 }
-router.get("/", restricted, (req, res, next) => {
-  
+router.get("/", restricted('normal'), (req, res, next) => {
   db("users")
     .then(response => {
       res.status(codes.OK).json(response);
@@ -25,4 +33,5 @@ router.get("/", restricted, (req, res, next) => {
       next(err);
     });
 });
+
 module.exports = router;
