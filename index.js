b9861e6f1e44c54875c7642f377b79547072c156
Kevin <brackkevin@gmail.com>
2018-08-07 14:49:34 -0500

    Add express-session dependency and add session for login/get users

--- index.js
+++ index.js
@@ -3,18 +3,43 @@ const express = require("express");
 const db = require("./data/db");
 const server = express();
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
 // import routers
 
 // use middleware
 server.use(express.json());
 
+// configure express session middleware
+// server.use(
+//   session({
+//     name: "notsession",
+//     secret: "nobody tosses a dwarf",
+//     cookie: {
+//       maxAge: 1 * 24 * 60 * 60 * 1000,
+//       secure: false
+//     },
+//     httpOnly: true,
+//     resave: false,
+//     saveUninitialized: true
+//   })
+// );
+
 // endpoints
 server.get("/", (req, res) => {
   res.send("Up and running...");
 });
 
-let loggedIn = false;
+// in class session example
+server.get("/setname", (req, res) => {
+  req.session.name = "frodo";
+  res.send("got it");
+});
+
+server.get("/getname", (req, res) => {
+  const name = req.session.name;
+  res.send(`hello ${req.session.name}`);
+});
 
 // POST /api/register
 server.post("/api/register", (req, res) => {
@@ -37,11 +62,10 @@ server.post("/api/login", function(req, res) {
     .first()
     .then(function(user) {
       if (user && bcrypt.compareSync(credentials.password, user.password)) {
-        loggedIn = true;
-        res.send("welcome");
+        req.session.user_name = user.user_name;
+        res.send(`welcome ${user.user_name}`);
       } else {
         return res.status(401).json({ error: "Incorrect credentials" });
-        loggedIn = false;
       }
     })
     .catch(function(error) {
@@ -51,7 +75,7 @@ server.post("/api/login", function(req, res) {
 
 // GET /api/users
 server.get("/api/users", (req, res) => {
-  if (loggedIn) {
+  if (req.session && req.session.user_name === "Lord Nikon") {
     db("users")
       .then(users => {
         res.status(200).json(users);
