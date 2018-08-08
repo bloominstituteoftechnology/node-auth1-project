4892838a19c9ac3b139dbcd648c2edc751132cd5
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-08 11:54:48 -0600

    converted from sessions to JWTs

--- index.js
+++ index.js
@@ -2,47 +2,75 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const db = require('./data/db.js');
+const jwt = require('jsonwebtoken');
  
 const server = express();
 
+const secret = "nobody tosses dwarf!";
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  }
+  const options = {
+    expiresIn: '1h',
+  }
+  return jwt.sign(payload, secret, options);
+}
+
+// JWTs:
 function protected(req, res, next) {
-  if (req.session && req.session.username) {
-    next();
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if(err) {
+        return res.status(401).json ({ error: 'Token Invalid'})
+      }
+      req.jwtToken = decodedToken;
+      next();
+    })
   } else {
     return res.status(401).json({ error: 'Incorrect credentials' });
   }
 }
 
+// Sessions:
 // function protected(req, res, next) {
-//   db('users')
-//     .then(users => {
-//       if (req.session && req.session.username === users.username) {
-//         next();
-//       } else {
-//         return res.status(401).json({ error: 'Incorrect credentials' });
-//       }
-//     })
-//     .catch(err => res.send(err));
+//   if (req.session && req.session.username) {
+//     next();
+//   } else {
+//     return res.status(401).json({ error: 'Incorrect credentials' });
+//   }
 // }
 
-server.use(
-  session({
-    name: 'notsession',
-    secret: 'nobody tosses a dwarf!',
-    cookie: {
-      maxAge: 1 * 24 * 60 * 60 * 1000,
-      secure: false,
-    },
-    httpOnly: true,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+// server.use(
+//   session({
+//     name: 'notsession',
+//     secret: 'nobody tosses a dwarf!',
+//     cookie: {
+//       maxAge: 1 * 24 * 60 * 60 * 1000,
+//       secure: false,
+//     },
+//     httpOnly: true,
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
 
 server.use(express.json());
 
+
+// Endpoints
 server.get('/', (req, res) => {
   res.send('API is running!');
+});
+
+server.get('/users', protected, (req, res) => {
+  db('users')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.status(500).json({error: 'Cannot GET Users'}));
 });
 
 server.post('/register', function(req, res) {
@@ -59,8 +87,9 @@ server.post('/register', function(req, res) {
         .where({ id: ids[0] })
         .first()
         .then(user => {
-          req.session.username = user.username;
-          res.status(201).json(user);
+          const token = generateToken(user);
+          // req.session.username = user.username;
+          res.status(201).json(token);
         });
     })
     .catch(function(error) {
@@ -75,8 +104,9 @@ server.post('/login', function(req, res) {
     .first()
     .then(function(user) {
       if (user && bcrypt.compareSync(credentials.password, user.password)) {
-        req.session.username = user.username;
-        res.send(`welcome ${user.username}`);
+        const token = generateToken(user);
+        // req.session.username = user.username;
+        res.send({token});
       } else {
         return res.status(401).json({ error: 'Incorrect credentials' });
       }
@@ -84,14 +114,6 @@ server.post('/login', function(req, res) {
     .catch(function(error) {
       res.status(500).json({ error });
     });
-});
-
-server.get('/users', protected, (req, res) => {
-  db('users')
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
 });
 
 server.get('/logout', (req, res) => {
