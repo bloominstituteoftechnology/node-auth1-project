1e01355823ffa02adac7d7fa908cc6858af0388e
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-07 17:52:56 -0700

    Build basic functionality for get /users and /logout hard coded name

--- index.js
+++ index.js
@@ -87,6 +87,26 @@ server.post('/api/login', getUser, (req, res) => {
     }
 })
 
-// server.get('/api/users', (req, res) => {
-//     if(req.session && req.session.username === )
-// })
+server.get('/api/users', (req, res) => {
+    if(req.session && req.session.userName === "Alex"){
+        db('users')
+            .then(users => {
+                res.status(200).json(users)
+            })
+            .catch(err => res.status(500).json("You shall not pass"))
+    }else{
+        res.status(500).json("You Shall not pass!")
+    }
+})
+
+server.get('/api/logout', (req, res) => {
+    if(req.session) {
+        req.session.destroy(err => {
+            if(err) {
+                res.send('error logging out')
+            }else{
+                res.send('good bye')
+            }
+        })
+    }
+})
