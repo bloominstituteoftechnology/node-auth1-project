17e84c624f62969585a96804dde981ee859cfa58
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-08-13 16:30:12 -0600

    hash password

--- index.js
+++ index.js
@@ -20,11 +20,24 @@ server.get('/users', (req, res) => {
 
 server.post('./register', function(req, res) {
     const user = req.body;
+    
+    const credentials = req.body;
+    const hash = bcrypt.hashSync(credentials.password, 14);
+    credentials.password = hash;
 
     db('users')
     .insert(user)
     .then(function(ids) {
         db('users')
-        .where
+        .where({ id: ids[0] })
+        .first()
+        .then(user => {
+            res.status(201).json(user);
+        });
     })
-})
+    .catch(function(error) {
+        res.status(500).json({ error });
+    });
+});
+
+server.listen(3300, () => console.log('\n running on port 3300 \n'));
