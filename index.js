58f2c40bccc8c416c36074bbbadb1e9d91d5bbe1
Daniel Chamorro <dan@dcitllc.com>
2018-08-13 16:09:44 -0400

    GET users back on /register

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ server.get("/", (req, res) => {
   res.send("up and running...");
 });
 
+//* GET authentications
+
+server.get("/register", (req, res) => {
+  db("users")
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
