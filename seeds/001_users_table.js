912c38089f61a176c37ad8710787446fed475b53
Daniel Chamorro <dan@dcitllc.com>
2018-08-13 16:00:46 -0400

    Created seed for users table

---
+++ seeds/001_users_table.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("users")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("users").insert([
+        { username: "user1", password: "password1" },
+        { username: "user2", password: "password2" },
+        { username: "user3", password: "password3" }
+      ]);
+    });
+};
