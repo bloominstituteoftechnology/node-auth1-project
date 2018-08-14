9f05a3fd0fb011da3e06c7dc296d6a428150d615
Daniel Chamorro <dan@dcitllc.com>
2018-08-14 13:17:31 -0400

    Added express-session and included it in index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const db = require("./data/db");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
 const server = express();
+
+// configure express-session middleware
+server.use(
+  session({
+    name: "notsession", // default is connect.sid
+    secret: "nobody tosses a dwarf!",
+    cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 }, // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    secure: true, // only set cookies over https. Server will not send back a cookie over http.
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 server.use(express.json());
 
