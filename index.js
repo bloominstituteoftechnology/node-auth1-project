de2d190fd0a9528afce03b7df84ab93401ce401e
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-08-14 14:53:25 -0600

    logout endpoint and cookies

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const db = require('./database/dbConfig.js');
 
@@ -7,9 +8,48 @@ const server = express();
 
 server.use(express.json());
 
+// configure express-session middleware
+server.use(
+  session({
+    name: 'notsession', // default is connect.sid
+    secret: 'nobody tosses a dwarf!',
+    cookie: {
+       maxAge: 1 * 24 * 60 * 60 * 1000, 
+       secure: false,
+    },
+     // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    //secure: true, // only set cookies over https. Server will not send back a cookie over http.
+     resave: false,
+    saveUninitialized: true,
+    //saveUninitialized: false,
+    
+  })
+);
+
+
+
 server.get('/', (req, res) => {
     res.send('We have liftoff!');
 });
+
+server.get('/setname', (req, res) => {
+    req.session.name = 'Frodo';
+    res.send('got it');
+});
+
+server.get('/getname', (req, res) => {
+    const name = req.session.name;
+    res.send('hello ${req.session.name}');
+});
+
+function protected(req, res, next) {
+    if (req.session && req.session.userId) {
+      next();
+    } else {
+      res.status(401).json({ message: 'you shall not pass!!' });
+    }
+  }
 
 server.get('/users', (req, res) => {
     db('users')
@@ -18,6 +58,12 @@ server.get('/users', (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+server.get('/api/users', protected, (req, res) => {
+    db('users')
+      .then(users => res.json(users))
+      .catch(err => res.json(err));
+  });
 
 server.post('./register', function(req, res) {
     const user = req.body;
@@ -48,7 +94,8 @@ server.post('/login', function(req, res) {
     .first()
     .then(function(user) {
         if (user && bcrypt.compareSync(credentials.password, user.password)) {
-            res.send('welcome');
+          req.session.userId = user.id;
+            res.send('welcome ${user.username}');
         } else {
             return res.status(401).json({ error: 'Incorrect credentials' });
         }
@@ -56,6 +103,18 @@ server.post('/login', function(req, res) {
     .catch((function(error) {
         res.status(500).json({ error });
     }));
+});
+
+server.get('/logout', (req, res) => {
+    if(req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.send('error logging out');
+            } else {
+                res.send('goodbye')
+            }
+        });
+    }
 });
 
 server.listen(3300, () => console.log('\n running on port 3300 \n'));
--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './users.sqlite3'
     },
     useNullAsDefault: true,
   },
