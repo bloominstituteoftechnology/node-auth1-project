e24c7e3c95d4c81755e4230f1e04bfa14500b09f
Ashwin <aswinjeganath@gmail.com>
2018-09-11 01:12:06 +0530

    Design endpoint to login users

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post('/api/register', function(req, res, next) {
 
   if (!username || !password)
     return res
-      .json(400)
+      .status(400)
       .json({ message: 'Please provide a username and a password' });
 
   password = bcrypt.hashSync(password, SALT_ROUNDS);
@@ -26,8 +26,38 @@ app.post('/api/register', function(req, res, next) {
     .catch(next);
 });
 
+app.post('/api/login', function(req, res, next) {
+  const { username, password } = req.body;
+
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ message: 'Please provide a username and password ' });
+
+  db('users')
+    .where('username', username)
+    .first()
+    .then(user => {
+      if (!user)
+        return res
+          .status(400)
+          .json({ message: 'Username or password is invalid ' });
+
+      if (!bcrypt.compareSync(password, user.password))
+        return res
+          .status(400)
+          .json({ message: 'Username or password is invalid ' });
+
+      res.status(200).json({ message: 'Login successful' });
+    })
+    .catch(next);
+});
+
 app.use(function(err, _, res, _) {
   console.log(err);
+  if (err.errno === 19)
+    return res.status(400).json({ message: 'username is already taken' });
+
   res.status(500).json({ message: 'Something went wrong' });
 });
 
