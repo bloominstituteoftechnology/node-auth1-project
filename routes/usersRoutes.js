280461092d136a948a3ad6370650da8cd71656a4
Trevor <trevorfehrman@gmail.com>
2018-09-10 13:34:42 -0700

    hased post working

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../dbConfig");
+const bcrypt = require("bcryptjs");
+
+router.post("/register", (req, res) => {
+	const credentials = req.body;
+	const hash = bcrypt.hashSync(credentials.password, 14);
+	credentials.password = hash;
+
+	db("users")
+		.insert(credentials)
+		.then(ids => {
+			const id = ids[0];
+
+			res.status(201).json(id);
+		})
+		.catch(err => res.status(500).send(err));
+});
 
 module.exports = router;
