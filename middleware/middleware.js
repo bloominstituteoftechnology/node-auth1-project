9193d811fa005b6e41a41021d2bab59c2d700317
szincone <sawyerzincone@gmail.com>
2018-09-10 14:22:25 -0600

    added morgan and helmet

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -2,16 +2,16 @@
 // dependencies
 const express = require("express");
 const cors = require("cors");
-// const helmet = require("helmet");
-// const morgan = require("morgan");
+const helmet = require("helmet");
+const morgan = require("morgan");
 // routes
 const userRoutes = require("../routes/userRoutes.js");
 
 module.exports = server => {
   // server.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-  // server.use(helmet());
+  server.use(helmet());
   server.use(express.json());
-  // server.use(morgan("dev"));
+  server.use(morgan("dev"));
   server.use("/api", userRoutes);
   // server.use(errorHandler);
 };
