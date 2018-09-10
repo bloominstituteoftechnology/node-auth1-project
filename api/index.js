3eecd6a1f0a39c19a9f632301f9fc6208b2ce7a7
johnoro <officialjohnorourke@gmail.com>
2018-09-10 15:53:55 -0400

    Added get route for users.
    I might wait until tomorrow to mess with the cookies/logged in part.

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 router.post('/register', (req, res) => {
   let { name, pass } = req.body;
   if (!name || !pass) {
-    res.status(404).json({ message: 'You need to provide a username and password!' }).end();
+    res.status(404).json({ message: 'You need to provide a unique username and password!' }).end();
   }
   pass = bcrypt.hashSync(pass, 10);
   db('users').insert({ name, pass })
@@ -30,7 +30,10 @@ router.post('/login', (req, res) => {
 });
 
 router.get('/users', (req, res) => {
-  // logged in ? users arr : 'You shall not pass!'
+  // if logged in
+    db('users')
+      .then(users => res.status(200).json(users))
+      .catch(err => res.status(500).json({ error: 'Something went wrong fetching the users.' }))
 });
 
 module.exports = router;
