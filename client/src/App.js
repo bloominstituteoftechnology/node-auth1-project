d3fe0e586261545cd194d31a4df35827491b0d61
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-09-10 12:41:31 -0700

    implement LoginPage client

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import { Route } from 'react-router-dom';
 
 import './App.css';
 import RegisterPage from './components/RegisterPage';
-// import LoginPage from './components/LoginPage';
+import LoginPage from './components/LoginPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route path="/register" component={RegisterPage} />
+        <Route path="/login" component={LoginPage} />
       </div>
     );
   }
---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class LoginPage extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			username: '',
+			password: ''
+		}
+	}
+
+	handleInputChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
+	handleFormSubmit = (e) => {
+		e.preventDefault();
+		const { username, password } = this.state;
+		axios.post('http://localhost:9000/api/login', {
+			username,
+			password
+		})
+			.then(res => {
+				console.log(res);
+			})
+			.catch(err => {
+				console.log(err);
+			})
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleFormSubmit}>
+					<input
+						type="text"
+						name="username" 
+						placeholder="Username" 
+						onChange={this.handleInputChange} 
+						value={this.state.username} 
+					/>
+					<input
+						type="password"
+						name="password" 
+						placeholder="Password" 
+						onChange={this.handleInputChange} 
+						value={this.state.password} 
+					/>
+					<button>Submit</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default LoginPage;
