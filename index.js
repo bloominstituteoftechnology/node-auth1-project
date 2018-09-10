1d6518ca4f7cb1367ad8ef667f3a873f9d3c49e8
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-09-10 17:12:48 -0400

    MVP completed

--- index.js
+++ index.js
@@ -3,13 +3,43 @@ const cors = require("cors");
 const bcrypt = require("bcryptjs");
 
 const db = require("./dbConfig");
-
 const server = express();
+
 server.use(express.json());
 server.use(cors());
 
 server.get("/", (req, res) => {
   res.send("Working");
+});
+
+server.post("/api/register", (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 3);
+
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json(id);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(users => {
+      if (users && bcrypt.compareSync(creds.password, users.password)) {
+        res.status(200).send("Authenticated");
+      } else {
+        res.status(401).json({ message: "Access Denied" });
+      }
+    });
 });
 
 server.get("/api/users", (req, res) => {
