527571a2e14aed894288739b84506dc50a308869
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-10 16:19:33 -0500

    added POST for api/login and GET for api/users endpoints
    Monday MVP complete

--- index.js
+++ index.js
@@ -15,18 +15,40 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/register', (req, res) => {
+    // grab creds
     const creds = req.body;
 
+    // hash the password
     const hash = bcrypt.hashSync(creds.password, 9);
 
+    // replace user's password with the hash
     creds.password = hash;
 
+    // save the user
     db('users')
         .insert(creds)
         .then(ids => {
             const id = ids[0];
 
             res.status(201).json(id);
+        })
+        .catch(err => res.status(500).send(err));
+});
+
+server.post('/api/login', (req, res) => {
+    const creds = req.body;
+
+    // find the user
+    db('users')
+        .where({ username: creds.username })
+        .first()
+        .then(user => {
+            // check creds
+            if (user && bcrypt.compareSync(creds.password, user.password)) {
+                res.status(200).send('Wilkommen');
+            } else {
+                res.status(401).json({ message: 'You shall not pass...' });
+            }
         })
         .catch(err => res.status(500).send(err));
 });
