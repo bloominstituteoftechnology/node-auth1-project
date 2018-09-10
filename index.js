4c48d5024271aba225b2490c10ca6cc8177f516a
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-09-10 15:52:32 -0500

    MVP finsihed

--- index.js
+++ index.js
@@ -12,5 +12,41 @@ server.get("/", (req, res) => {
   res.send("Its Alive!");
 });
 
+server.post("/api/register", (req, res)=>{
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, 12); 
+    creds.password = hash; 
+    db("users")
+        .insert(creds)
+        .then(ids =>{
+            const id = ids[0]; 
+            res.status(201).json(id);
+        })
+        .catch(err=>res.status(500).send(err)); 
+})
+
+server.post("/api/login", (req, res)=>{
+    const creds = req.body; 
+    db("users")
+    .where({username: creds.username})
+    .first()
+    .then(user=>{
+        if(user && bcrypt.compareSync(creds.password, user.password)){
+            res.status(201).json({message: "Welcome"})
+        }else{
+            res.status(404).json({ message: "No User Matching" })
+        }
+    })
+    .catch(err => res.status(500).send(err)); 
+})
+
+server.get("/api/users", (req, res) => {
+    db("users")
+      .select("id", "username")
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
 
 server.listen(3001, () => console.log("\nrunning on port 3001\n"));
