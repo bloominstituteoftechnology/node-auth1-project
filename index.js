dc014ef2340a7c117d1fa496beeb100882965864
codejoncode <jonathanjholloway@gmail.com>
2018-09-10 14:51:36 -0500

    connected the routers to the index.js file featuring the main server

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require("morgan");
-
+const loginRouter = require("./Authentication/routes/loginRoutes");
+const registerRouter = require("./Authentication/routes/registerRoutes");
+const userRouter = require("./Authentication/routes/usersRoutes");
 const server = express();
 
 //Middleware
@@ -10,12 +12,20 @@ server.use(helmet());
 server.use(morgan("short"));
 server.use(express.json());
 server.use(cors());
+//Routers
+const LOGIN = "/api/login";
+const REGISTER = "/api/register";
+const USERS = "/api/users";
 
+server.use(LOGIN, loginRouter)
+server.use(REGISTER, registerRouter)
+server.use(USERS, userRouter)
+//Routers^
 //Middleware^
 
-server.get('/', (req, res) => {
-  res.send("Server started")
-})
+server.get("/", (req, res) => {
+  res.send("Server started");
+});
 
 PORT = 9000;
 
