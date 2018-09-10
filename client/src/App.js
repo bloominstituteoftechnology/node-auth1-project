5c07f43abc32dec50e5d5cf769260bed0702b37c
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-09-10 12:38:20 -0700

    implement RegisterPage client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+
 import './App.css';
+import RegisterPage from './components/RegisterPage';
+// import LoginPage from './components/LoginPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/register" component={RegisterPage} />
       </div>
     );
   }
