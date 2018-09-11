711666749acada52c0dd2a41a278f7223bfa7f5f
David Pok <david.pok@gmail.com>
2018-09-11 16:05:11 -0700

    implements sessions, logout, and protected functions

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express");
+const session = require("express-session");
 
 const userRoutes = require("./routes/userRoutes");
 
 const server = express();
 
+//2 WAYS OF DOING THIS
+// server.use(
+//   session({
+//       name: "monkey", // default is connect.sid
+//       secret: "nobody tosses a dwarf!",
+//       cookie: {
+//         maxAge: 1 * 24 * 60 * 60 * 1000, // a day
+//         secure: false // only set cookies over https. Server will not send back a cookie over http.
+//       }, // 1 day in milliseconds
+//       httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+//       resave: false,
+//       saveUninitialized: false
+//   })
+// );
+const sessionConfig = {
+  secret: "im so bad at coding",
+  cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 },
+  httpOnly: true,
+  secure: true,
+  resave: false,
+  saveUninitialized: false
+};
+server.use(session(sessionConfig));
+
 server.use(express.json());
 server.use("/users", userRoutes);
+
 
 server.get("/", (req, res) => {
   res.send("API running");
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const db = require("../db/dbConfig");
 const bcrypt = require("bcryptjs");
 
+//MIDDLEWARE, should put this in own folder
+function protected(req, res, next) {
+  if (req.session && req.session.username) {
+    next();
+  } else {
+    res.status(401).json({ message: "denied" });
+  }
+};
+
+//CREATE A USERNAME/PASSWORD HERE AND GET PW HASHED
 router.post("/register", (req, res) => {
   const creds = req.body;
   const hash = bcrypt.hashSync(creds.password, 3);
@@ -17,15 +27,17 @@ router.post("/register", (req, res) => {
     .catch(err => res.status(500).send(err));
 });
 
+//DISPLAY ALL THE USERS SO FAR
 router.get("/", (req, res) => {
   db("users")
-    .select("id", "username")
+    .select("id", "username", "password")
     .then(ids => {
       res.json(ids);
     })
     .catch(err => res.status(500).send(err));
 });
 
+//LOG IN TO THE SESSION HERE
 router.post("/login", (req, res) => {
   const creds = req.body;
   db("users")
@@ -33,12 +45,26 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
-        res.status(200).send("Welcome");
+        req.session.username = user.username;
+        res.status(200).send(`Welcome ${req.session.username}`);
       } else {
         res.status(401).json({ message: "Wrong creds" });
       }
     })
     .catch(err => res.status(500).send(err));
+});
+
+//LOGOUT OF THE SESSION HERE
+server.get('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send('logout error');
+      } else {
+        res.send('bye!');
+      }
+    });
+  }
 });
 
 module.exports = router;
