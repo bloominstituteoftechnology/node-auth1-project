82e3fe3869a510de7ea8763a02f5b258d6738f48
David Pok <david.pok@gmail.com>
2018-09-11 00:54:01 -0700

    completes mvp with small bug

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+// const userRoutes = require("./routes/userRoutes");
+
+const server = express();
+
+server.use(express.json());
+// server.use("/users", userRoutes);
+
+server.get("/", (req, res) => {
+  res.send("API running");
+});
+
+const port = 8000;
+server.listen(port, function() {
+  console.log(`\n=API on ${port}=\n`);
+});
---
+++ routes/userRoutes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db/dbConfig");
+const bcrypt = require("bcryptjs");
+
+router.post("/register", (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 3);
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json(id);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+router.get("/", (req, res) => {
+  db("users")
+    .select("id", "username")
+    .then(ids => {
+      res.json(ids);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+router.post("/login", (req, res) => {
+  const creds = req.body;
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        res.status(200).send("Welcome");
+      } else {
+        res.status(401).json({ message: "Wrong creds" });
+      }
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+module.exports = router;
