243512176c921bda7202c69cfbf1213ab8edf9f9
MichaelBoegner <cmikeb@gmail.com>
2018-09-11 16:33:09 -0400

    Wired to Allow Register Session Username and Only Allows Registered Users to Access Users Data

--- database/migrations/20180910152733_users.js
+++ database/migrations/20180910152733_users.js
@@ -1,13 +1,20 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('users', users => {
-        users.increments();
+        users
+          .increments();
     
         users
           .string('username', 128)
           .notNullable()
           .unique();
-        users.string('password', 128).notNullable();
+        
+        users
+          .string('password', 128)
+          .notNullable();
+
+        users
+          .boolean('logged_in')
       });
     };
     
--- index.js
+++ index.js
@@ -1,62 +1,89 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
-const dbConfig = require('./knexfile');
+const dbConfig = require("./knexfile");
 
 const db = knex(dbConfig.development);
 
 const server = express();
+const sessionConfig = {
+  name: "fatdog", // default is connect.sid
+  secret: "fat cats are the worst!",
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000, // a day
+    secure: false // only set cookies over https. Server will not send back a cookie over http.
+  }, // 1 day in milliseconds
+  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+  resave: false,
+  saveUninitialized: false
+};
 
+server.use(session(sessionConfig));
 server.use(express.json());
 server.use(helmet());
 
 
-server.get('/api/users', (req, res) => {
-    db('users')
-    .then(users => {
-        res.status(201).json(users);
-    })
-    .catch(err =>{
-        res.status(500).json({error: "The users could not be retrieved."});
-    })
-});
+server.post("/api/register", (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10); //should be no lower than 12 generally speaking
+  creds.password = hash;
 
-
-
-server.post('/api/register', (req, res) => {
-    const creds = req.body; 
-    const hash = bcrypt.hashSync(creds.password, 10);//should be no lower than 12 generally speaking
-    creds.password = hash; 
-
-    db('users').insert(creds)
+  db("users")
+    .insert(creds)
     .then(ids => {
-        const id = ids[0];
-        res.status(201).json(id);
-        })
-    .catch(err => res.status(500).send(err))
+      const id = ids[0];
+      res.status(201).json(id);
+    })
+    .catch(err => res.status(500).send(err));
 });
 
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
 
-server.post('/api/login', (req, res) => {
-    const creds = req.body;
-
-    db('users')
-        .where({username: creds.username})
-        .first()
-        .then(user => {
-            if(user && bcrypt.compareSync(creds.password, user.password)) {
-                res.status(200).send("Welcome!")
-            } else{
-                res.status(401).json({message: "No passing you thief!"})
-            }
-        })
-        .catch(err => {
-            res.status(500).send(err);
-        })
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        req.session.username = user.username;
+        res.status(200).send(`Welcome ${req.session.username}!`);
+      } else {
+        res.status(401).json({ message: "No passing you thief!" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error posting" });
+    });
 });
 
+server.get("/api/admins", (req, res) => {
+  db("users")
+    .then(users => {
+      req.status(200).send(users);
+    })
+    .catch(err => res.send(err));
+});
+
+server.get("/api/users", (req, res) => {
+  if (req.session && req.session.username) {
+    db("users")
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(err => 
+        res.status(500).json({message: 'Error retrieving users.'}));
+  } else {
+    res.status(401).json({ message: "Nope. No passing for you!" });
+  }
+});
+
+server.get("/api/greet", (req, res) => {
+  const name = req.session.username;
+  res.send(`Hey there, ${name}`);
+});
 
 const port = 8800;
 server.listen(port, function() {
