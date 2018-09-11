6a363c33716e27b286cc59449c07f88a9715f771
LizBaker <lizbaker.pdx@gmail.com>
2018-09-11 13:07:56 -0700

    Added session configuration/cookies and middleware to keep users visible only when logged in -added logout for session

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 const express = require("express");
+const session = require('express-session');
 const bcrypt = require("bcryptjs");
 const db = require("./dbconfig.js");
-
 const server = express();
-
+const KnexSessionStore = require('connect-session-knex')(session);
+const sessionConfig = {
+    name: 'logincookie', 
+    secret: 'i like turtles 746374 !!!',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000, 
+      secure: false, 
+    }, 
+    httpOnly: true, 
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+      tablename: 'sessions',
+      sidfieldname: 'sid',
+      knex: db,
+      createtable: true,
+      clearInterval: 1000 * 60 * 60,
+    }),
+  };
+  
 server.use(express.json());
+server.use(session(sessionConfig));
+
+function protected(req, res, next) {
+    if (req.session && req.session.username) {
+      next();
+    } else {
+      res.status(401).json({ message: 'you must be logged in to view this page' });
+    }
+  }
 
 server.get("/", (req, res) => {
   res.send("helo!");
@@ -23,7 +51,7 @@ server.post("/register", (req, res) => {
     .catch(err => res.status(500).send(err));
 });
 
-server.get("/users", (req, res) => {
+server.get("/users", protected, (req, res) => {
   db("users")
     .select("id", "username")
     .then(users => {
@@ -39,12 +67,25 @@ server.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
-        res.status(200).send("welcome");
+        req.session.username = user.username;
+        res.status(200).send(`Welcome ${req.session.username}`);
       } else {
-        res.status(401).json({ message: "creds incorrect" });
+        res.status(401).json({ message: "login info incorrect" });
       }
     })
-    .catch(err => res.send(err));
+    .catch(err => resstatus(500).send(err));
 });
+
+server.get('/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.send('error logging out');
+        } else {
+          res.send('logged out');
+        }
+      });
+    }
+  });
 
 server.listen(3000, () => console.log("\nrunning on port 3000\n"));
