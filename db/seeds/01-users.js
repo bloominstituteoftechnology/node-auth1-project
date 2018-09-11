7219e3b29c0cc8e39365d1469d8aaf1669bcd2a8
Alberto-Funk <laptopreneur@gmail.com>
2018-09-11 16:11:05 -0700

    middleware, authorization, roles, seeds

---
+++ db/seeds/01-users.js
@@ -0,0 +1,11 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {username: 'Frank', password: 'The main Dude', role: 'CEO'}
+      ]);
+    });
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const knex = require('knex');
 const bycrypt = require('bcryptjs');
+const session = require('express-session')
 
 const server = express();
 const port = 3333;
@@ -15,13 +16,60 @@ server.use(cors());
 server.use(helmet());
 
 
+
+// middleware
+const sessionConfig = {
+  name: 'noneOfYoBiz',
+  secret: 'this is not a secret',
+  cookie: {
+    maxAge: 1 * 12 * 60 * 60 * 1000,
+    secure: false,
+  }, 
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+}
+
+const checkForUser = (req, res, next) => {
+  req.session && 
+  req.session.username && 
+  req.session.role === 'user' ?
+  next()
+  :
+  res.status(401).json({message: 'YU SHALL NOT PAWSS'});
+}
+
+const checkForAdmin = (req, res, next) => {
+  req.session && 
+  req.session.username && 
+  req.session.role === 'admin' ?
+  next()
+  :
+  res.status(401).json({message: 'YU SHALL NOT PAWSS'});
+}
+
+const checkForHr = (req, res, next) => {
+  req.session &&
+  req.session.username &&
+  req.session.role === 'hr' ?
+  next()
+  :
+  res.status(401).json({message: 'YU SHALL NOT PAWSS'});
+}
+
+
+server.use(session(sessionConfig));
+
+
+
+// routes
 server.get('/', (req, res) => {
   res.send('hello');
 });
 
 
 server.get('/api/users', (req, res) => {
-  db('users').select('id', 'username')
+  db('users').select('username')
   .then(users => {
     res.status(200).json(users)
   })
@@ -29,10 +77,11 @@ server.get('/api/users', (req, res) => {
 });
 
 
+
 server.post('/api/register', (req, res) => {
 
-  !req.body.username || !req.body.password ?
-  res.status(400).json({message: 'You need a username AND password'})
+  !req.body.username || !req.body.password || !req.body.role ?
+  res.status(400).json({message: 'You need a username AND password AND role'})
   :
   null
 
@@ -64,6 +113,32 @@ server.post('/api/login', (req, res) => {
   db('users').where({username: credentials.username}).first()
   .then(user => {
     if (user && bycrypt.compareSync(credentials.password, user.password)) {
+      req.session.username = user.username
+      req.session.role = user.role
+      res.status(200).json({message: `Success! Welcome, ${req.session.username}`})
+    } else {
+      res.status(401).json({message: 'cannot pass'})
+    }
+  })
+  .catch(err => {
+    console.log(err)
+  });
+});
+
+
+server.get('/api/logout', (req, res) => {
+  
+  !req.body.username || !req.body.password ?
+  res.status(400).json({message: 'You need a username AND password'})
+  :
+  null
+
+  const credentials = req.body;
+
+  db('users').where({username: credentials.username}).first()
+  .then(user => {
+    if (user && bycrypt.compareSync(credentials.password, user.password)) {
+      req.session.username = user.username
       res.status(200).json({message: 'success'})
     } else {
       res.status(401).json({message: 'cannot pass'})
@@ -72,6 +147,33 @@ server.post('/api/login', (req, res) => {
   .catch(err => {
     console.log(err)
   });
+});
+
+
+server.get('/api/user/users', checkForUser, (req, res) => {
+  db('users').select('id', 'username')
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => console.log(err));
+});
+
+
+server.get('/api/admin/users', checkForAdmin, (req, res) => {
+  db('users').select()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => console.log(err));
+});
+
+
+server.get('/api/hr/users', checkForHr, (req, res) => {
+  db('users').select('id', 'username', 'role')
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => console.log(err));
 });
 
 
