52d1a1b5f296487d244db472b6da577b9ae5a6ae
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-11 14:35:15 -0600

    Added sessions and cookies and users endpoint

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const dbConfig = require('./knexfile');
 const db = knex(dbConfig.development); 
 const bcrypt = require('bcryptjs'); 
 const Joi = require('joi'); 
+const session = require('express-session'); 
 
 const server = express(); 
 server.use(express.json()); 
+server.use(
+    session({
+        name: 'notsession', 
+        secret: 'This is a secret key', 
+        cookie: { 
+            maxAge: 1 * 24 * 60 * 60 * 1000, 
+            secure: false}, 
+        httpOnly: true,  
+        resave: false, 
+        saveUninitialized: true
+    })
+)
 
 server.post("/api/register", (req, res) => {
     const creds = req.body;
@@ -31,10 +44,16 @@ server.post("/api/register", (req, res) => {
     })
 }); 
 
-server.get("/api/register", (req, res) => {
-    db('users').then(users => {
-        res.status(200).json(users); 
-    })
+server.get("/api/users", (req, res) => {
+    if(req.session.userId){
+        db('users').then(users => {
+            res.status(200).json(users)
+        }).catch(err => {
+            res.status(500).json({error: "Error retrieving data from the database"})
+        });
+    }else {
+        res.status(403).json({message: "Access to this information is forbidden. Please login to view!"})
+    }
 }); 
 
 server.post("/api/login", (req, res) => {
@@ -44,12 +63,14 @@ server.post("/api/login", (req, res) => {
         .first()
         .then(user => {
         if(user && bcrypt.compareSync(creds.password, user.password)){
-            res.status(200).send("Welcome to the site");
+            req.session.userId = user.id;
+            res.status(200).json({message: `Logged in! Welcome to the site, ${user.username}`});
         }else {
             res.status(401).json({message: "YOU SHALL NOT PASS! Please try again. Username or Password is incorrect"})
         }
     }).catch(err => res.send(err)); 
 })
+
 
 
 server.listen(3400, () => {
