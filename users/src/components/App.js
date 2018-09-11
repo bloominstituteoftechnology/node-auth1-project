c5283248a5e28a8a4b6a4efec101c7cbf52792bf
dasgma <dasgrigoma@gmail.com>
2018-09-11 13:33:29 -0700

    Creating users app.

---
+++ users/src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Application = styled.div`
+  
+  text-align: center;
+
+`
+
+class App extends Component {
+  render() {
+    return (
+      <Application>
+        app is running
+        
+      </Application>
+    );
+  }
+}
+
+export default App;
