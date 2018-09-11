8c52215c7faa22ae12fac8e0af2ed07f516f78db
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-09-11 12:50:27 -0700

    GET method correctly implemented.
    Only works when you are logged in.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,25 @@
 
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const db = require('../db/dbConfig');
 
 const router = express.Router();
+
+const sessionConfig = { //new
+    name: 'banana', // default is connect.sid
+    secret: 'nobody tosses a dwarf!',
+    cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    secure: false, // only set cookies over https. Server will not send back a cookie over http.
+    }, // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: false,
+};
+
+router.use(session(sessionConfig)); // new 
 
 router.post('/register', (req, res) => {
     const creds = req.body;
@@ -29,6 +44,8 @@ router.post('/login', (req, res) => {
         .where({ username: creds.username })
         .first().then(user => {
             if(user && bcrypt.compareSync(creds.password, user.password)) {
+                req.session.username = user.username;
+                
                 res.status(200).send('Logged in');
             } else {
                 res.status(401).json({ error: "You shall not pass!"})
@@ -37,12 +54,16 @@ router.post('/login', (req, res) => {
         .catch(err => res.status(500).send(err))
 });
 
-router.get('/users', (req, res) => {
-    db('usernames').select('id', 'username')
+router.get('/users', (req, res) => {  //new
+    if(req.session && req.session.username) {
+        db('usernames').select('id', 'username', 'password')
         .then(users => {
         res.status(200).json(users)
         })
         .catch(err => console.log(err));
+    } else {
+        res.status(401).json({ message: 'You are not logged in.' }) 
+    } 
 });
 
 module.exports = router;
