18550a58ea187603cb3129fe65e4fcb1aa8792be
adamcglee <adamcglee@gmail.com>
2018-09-11 14:29:45 -0700

    updated code to use sessions and cookies with middleware

--- index.js
+++ index.js
@@ -2,20 +2,43 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const bcrypt = require("bcryptjs");
+const session = require("express-session")
 
 const db = require('./db/dbConfig.js');
 
 const server = express();
 
+const sessionConfig = {
+    name: "banana",
+    secret: "i've got a bad feeling about this",
+    cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: false
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false,
+};
+
+server.use(session(sessionConfig));
+
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+
+function protected(req, res, next) {
+    if (req.session && req.session.userId) {
+      next();
+    } else {
+      res.status(401).json({ message: 'Unauthorized User, please log in.' });
+    }
+}
 
 server.get('/', (req, res) => {
     res.send("API is running...")
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
     db('users')
         .select('id', 'username', 'password')
         .then(users => {
@@ -44,6 +67,7 @@ server.post('/api/login', (req, res) => {
         .first()
         .then(user => {
             if(user && bcrypt.compareSync(creds.password, user.password)) {
+                req.session.name = user.userId;
                 res.status(200).send("Login Successful");
             } else {
                 res.status(401).json({ message: 'Unauthorized login attempt. Username or Password are incorrect.'})
