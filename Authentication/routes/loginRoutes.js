f831d46d3e5a3a74e4e810bdc02acbab8f4cf6d4
codejoncode <jonathanjholloway@gmail.com>
2018-09-11 14:38:51 -0500

    changed the loginRouter put the req.session.username inside of it.

--- Authentication/routes/loginRoutes.js
+++ Authentication/routes/loginRoutes.js
@@ -3,25 +3,6 @@ const bcrypt = require("bcryptjs");
 const db = require("../../db/dbConfig.js");
 const loginRouter = express.Router();
 
-
-// loginRouter.put("/:username", (req,res) => {
-//   const { username } = req.params; 
-//   db("users")
-//     .update({signedIn: false})
-//     .where({ username })
-//     .then(count => {
-//       if(count){
-//         res.status(200).json({message: `${username} signed out successfully`})
-//       }else {
-//         res.status(404).json({message: "Unable to update/ sign out"})
-//       }
-//     })
-//     .catch(error => {
-//       res.status(500).json({error})
-//     })
-// })
-
-
 loginRouter.post("/", (req, res) => {
   const creds = req.body;
   db("users")
@@ -29,27 +10,16 @@ loginRouter.post("/", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
-        req.body.signedIn = true
-        console.log(req.body); 
-        db("users")
-          .update(req.body)
-          .where({username: creds.username})
-          .then(count => {
-            res.status(200).send(`Welcome ${creds.username}`)
-          })
-          .catch(error => {
-            res.status(500).json({errorMessage: "UnAuthorized"})
-          })
-        res.status(200).send(`Welcome ${creds.username}`);
+        req.session.username = user.username; 
+        
+        res.status(200).send(`Welcome ${req.session.username}`);
       } else {
-        res.status(401).json({ errorMessage: "UnAuthorized at 28"});
+        res.status(401).json({ errorMessage: "UnAuthorized at 28" });
       }
     })
     .catch(error => {
-      res.status(500).json({error});
+      res.status(500).json({ error });
     });
 });
-
-
 
 module.exports = loginRouter;
