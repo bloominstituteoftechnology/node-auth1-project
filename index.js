cf6d5e6cf0b4c10653d88b35a2d5885541dca984
Charles Cortinas <crcortinas@gmail.com>
2018-09-11 16:09:10 -0500

    Day 2
    Day 2 MVP completed

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express');
+const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const knex = require('knex');
 const dbConfig = require('./knexfile');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = knex(dbConfig.development);
-
 const server = express();
 
+server.use(cors());
 server.use(express.json());
+server.use(
+  session({
+    name: 'notsession',
+    secret: 'nobody tosses a dwarf!',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: false,
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+      tablename: 'sessions',
+      sidfieldname: 'sid',
+      knex: db,
+      createtable: true,
+      clearInterval: 1000 * 60 * 60
+    })
+  })
+);
 
-server.get('/', (req, res) => {
-  res.status(200).json({ message: 'GOGOGOGOGO!' });
-})
+server.get('/greet', (req, res) => {
+  res.send(`hello ${req.session.username}`);
+});
 
 server.post('/api/register', (req, res) => {
   let cred = req.body;
@@ -33,6 +56,7 @@ server.post('/api/login', (req, res) => {
   db('users').where({ username: cred.username }).first()
     .then((user) => {
       if(user && bcrypt.compareSync(cred.password, user.password)) {
+        req.session.username = cred.username;
         res.status(200).json({ message: 'Logged in' });
       } else {
         res.status(401).json({ message: 'YOU SHALL NOT PASS!' });
@@ -45,15 +69,31 @@ server.post('/api/login', (req, res) => {
     })
 });
 
+server.get('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send('error logging out');
+      } else {
+        res.send('good bye');
+      }
+    });
+  }
+});
+
 server.get('/api/users', (req, res) => {
-  db('users').select('id', 'username')
-  .then((data) => {
-    res.status(200).json(data);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(404).json({ message: 'Database not found' });
-  })
+  if(req.session && req.session.username){
+    db('users').select('id', 'username')
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(404).json({ message: 'Database not found' });
+    })
+  } else {
+    res.status(401).json({ message: 'You are not authorized to view this resource' });
+  }
 });
 
 server.listen(8000, () => {
