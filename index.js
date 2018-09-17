336879ff0d0a4957ea625c82925270379a5cd31c
AI2EJAY <JHammett1314@gmail.com>
2018-09-17 16:22:52 -0500

    Server is now running and imports fixed

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require(express);
-const helmet = require(helmet);
-const bcrypt = require(bcryptjs);
+const express = require("express");
+const helmet = require("helmet");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 
 const db = require("./db/helpers");
 
+const server = express();
+
 server.use(express.json());
 server.use(helmet());
+
+
+
+const port = 9000;
+server.listen(
+  port,
+  console.log(`\n ===> Server is running on port:${port} <=== \n`)
+);
