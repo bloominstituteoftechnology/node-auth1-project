08b25419713c2c0ec9f0add086a73c8932f9af4e
Kairi <kairijb@gmail.com>
2018-09-17 14:34:43 -0500

    dependencies installed

---
+++ db/knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  development: {
+    client: 'sqlite3', // tells knex that we're using the SQLite3 driver we installed via npm
+    connection: {
+      filename: './lambda.sqlite3', // update this with the location of your database file
+    },
+    useNullAsDefault: true, // new configuration for SQLite
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    }
+  },
+};
