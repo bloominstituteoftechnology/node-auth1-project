1bbd6960a530ebc92f3e01a161dd4b19e44d0e0a
AI2EJAY <JHammett1314@gmail.com>
2018-09-17 17:51:38 -0500

    error on the Hash save. Atleast it is hitting my catch now.

--- db/helpers/index.js
+++ db/helpers/index.js
@@ -1,8 +1,14 @@
 const knex = require("knex");
 const dbConfig = require("../../knexfile");
+
 const db = knex(dbConfig.development);
 
 module.exports = {
   // We are not doing a lot of endpoints it doesn't sound like
   // but I want to be in the habbit of creating my functions here.
+  hashUser: function(action) {
+    let query = db("users");
+
+    return query.insert(action).then(([id]) => this.get(id));
+  }
 };
--- index.js
+++ index.js
@@ -9,8 +9,25 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
 
+server.get("/", (req, res) => {
+  res.send("You made it!");
+});
 
+server.post("/api/register", (req, res) => {
+  const creds = req.body;
+
+  const hash = bcrypt.hashSync(creds.password, 10);
+  creds.password = hash;
+  console.log(creds.password);
+
+  db.hashUser(creds)
+    .then(id => {
+      res.status(200).json(id);
+    })
+    .catch(err => res.status(500).send("error creating user"));
+});
 
 const port = 9000;
 server.listen(
