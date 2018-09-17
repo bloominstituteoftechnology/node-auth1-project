dea2fa333caad3a0ee11353acfb8b1c0a719c209
Munsu <dark_specineff2002@yahoo.com>
2018-09-17 15:48:07 -0700

    mvp

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const db = require('./db/dbConfig.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+// endpoints
+
+server.get('/', (req, res) => {
+    res.send('We Are Live!');
+});
+
+server.post('/api/register', (req, res) => {
+    const creds = req.body;
+
+    const hash = bcrypt.hashSync(creds.password, 10);
+    
+    creds.password = hash;
+
+    db('users')
+        .insert(creds)
+        .then(ids => {
+        const id = ids[0];
+            res.status(201).json(id);
+        })
+        .catch(err => res.status(500).send(err));
+});
+
+server.post('/api/login', (req, res) => {
+    const creds = req.body;
+
+    db('users')
+        .where({ username: creds.username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(creds.password, user.password)) {
+                res.status(200).send('Logged in');
+            } else {
+                res.status(401).json({ message: 'You shall not pass!' })
+            }
+        })
+        .catch(err => res.status(500).send(err));
+})
+
+server.get('/api/users', (req, res) => {
+    db('users')
+        .select('id', 'username', 'password')
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
+
+const port = 5000;
+server.listen(port, function() {
+    console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
