9b01ea9549aeebf013a9f470d5fbb3106e78b27b
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-09-17 18:24:21 -0500

    migrate, and now working on endpoints

--- migrations/20180917181035_users.js
+++ migrations/20180917181035_users.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('users', (tbl) => {
+    tbl
+    .increments()
+
+    tbl
+    .string('username', 128)
+    .notNullable()
+
+    tbl
+    .string('password', 128)
+    .notNullable()
+    
+    })     
 };
 
 exports.down = function(knex, Promise) {
   
+ 
 };
