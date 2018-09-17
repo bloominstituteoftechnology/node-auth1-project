892369db1fd3611f898bee2b87cf147c6aff5cbd
tulf <timzulf@gmail.com>
2018-09-17 19:41:49 -0400

    async stuff not working, swtiched back to normal  method, MVP finished.

--- index.js
+++ index.js
@@ -1,107 +1,64 @@
+//module import
 const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
-
+const bcrypt = require('bcryptjs')
+//knex config
 const dbConfig = require('./knexfile.js')
-
 const db = knex(dbConfig.development);
-
+//server config
 const server = express();
-
+//
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
-
-//---------GET REQUESTS-------//
-
-//-----obligatory welcome----///
-server.get('/', (req, res) => {
-  db('credentials')
-  .then( credentials => {
-    res.status(200).send("welcome pilgrim");
-  })
-})
-
-//All:
-
-server.get('/api/credentials', (req, res) => {
-  db('credentials')
-  .then( credentials => {
-    res.status(200).json(credentials);
-  })
-  .catch(err =>{
-    console.log(err)
-    res.status(500).json(err)
-  })
-})
-//BY ID:
-
-server.get('/api/credentials/:id', (req, res) => {
-  const  {id} = req.params;
-  db('credentials')
-  .select()
-  .where('id', id)
-  .then( credentials => {
-    res.status(200).json(credentials);
-  })
-  .catch(err =>{
-    console.log(err)
-    res.status(500).json(err)
-  })
-})
 
 //----POST ------//
+//register
+server.post("/api/register", (req, res) => {
+ const credentials = req.body;
+ const hashnum = bcrypt.hashSync(credentials.password, 14);
+ credentials.password = hashnum;
 
-server.post( '/api/credentials', (req, res) => {
-  const zoo = req.body;
+ db("credentials")
+   .insert(credentials)
+   .then(users => {
+     const id = users[0];
 
-  db.insert(zoo)
-  .into('credentials')
-  .then(credentials => {
-    res.status(201).json(credentials);
-  })
-  .catch(err => {
-    res.status(500).json(err);
-  })
-})
-
-//-------DELETE------------//
-
-server.delete('/api/credentials/:id', (req, res) => {
-  const { id } = req.params;
-   db('credentials')
-   .where({ id })
-   .del()
-   .then( credentials => {
-     res.status(200).json(credentials);
+     res.status(201).json(id);
    })
-   .catch(err => {
-     console.log(err)
-     res.status(500).json(err)
-   })
-})
-
-//-------------PUT-----------//
-server.put('/api/credentials/:id', (req, res) => {
-  const { id } = req.params;
-  const name = req.body;
-
-  db('credentials')
-  .where( { id })
-  .update(name)
-  .then( credentials => {
-    res.status(200).json(credentials)
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err);
-  })
+   .catch(err => res.status(500).send(err));
 });
 
+//login
+server.post("/api/login", (req, res) => {
+ const credentials = req.body;
+ db("credentials")
+   .where({ username: credentials.username })
+   .first()
+   .then(user => {
+     if (user && bcrypt.compareSync(credentials.password, user.password) ) {
+       res.status(200).send("Hello my friend, nice to see you again");
+     }
+     else {
+       res.status(401).json({ message: "Man you can't even type right " });
+     }
+   })
+   .catch(err => res.status(500).send(err));
+});
 
+//------GET-------//
+server.get("/api/users", (req, res) => {
+ db("credentials")
+   .select("id", "username")
+   .then(users => {
+     res.json(users);
+   })
+   .catch(err => res.status(500).send(err));
+});
 
-const port = 3300;
+//-------Listener--------//
+const port = 8000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
