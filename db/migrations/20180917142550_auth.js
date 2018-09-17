1ac47aea4c2eecc9500f2ed4ee2f4f3bed5efe36
Kairi <kairijb@gmail.com>
2018-09-17 14:48:24 -0500

    created migration

--- db/migrations/20180917142550_auth.js
+++ db/migrations/20180917142550_auth.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', function(tbl) {
+        tbl.increments();
   
+        tbl.string('name', 128).notNullable().unique('user_name');
+  
+        tbl.string('password', 128).notNullable();
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('users')
 };
