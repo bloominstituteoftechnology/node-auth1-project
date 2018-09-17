7d8f4dc7cd3e17c160c89b8f03264bb3ebef09ce
vanceleon <vanceleon44@gmail.com>
2018-09-17 18:58:24 -0400

    MVP

--- db/migrations/20180917183002_users.js
+++ db/migrations/20180917183002_users.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('users', users => {
-        users.increment();
+        users.increments();
 
         users
             .string("username", 128)
