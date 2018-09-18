76364d9260637b0bdb9da8719c44e9ad3d35f8aa
Erica Thompson <ericathompson.nc@gmail.com>
2018-09-18 14:02:16 -0700

    Save

--- server.js
+++ server.js
@@ -3,18 +3,37 @@ const knex = require('knex');
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const knexConfig = require('./knexfile');
+const session = require('express-session');
+const cors = require('cors');
 
 // Instantiations
 const server = express();
 const db = knex(knexConfig.development);
 
 // Middleware
+
+
+const sessionConfig = {
+    name: 'monkey', // default is connect.sid
+    secret: 'nobody tosses a dwarf!',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: false, // only set cookies over https. Server will not send back a cookie over http.
+    }, // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: false,
+  };
+
+
+server.use(session(sessionConfig));
 server.use(express.json());
+server.use(cors());
 
 // Endpoints
-server.get('/', (req, res) => {
-    res.status(200).send('Server is running!');
-});
+// server.get('/', (req, res) => {
+//     res.status(200).send('Server is running!');
+// });
 
 server.post('/api/register', (req, res) => {
     const creds = req.body;
@@ -54,14 +73,25 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
         if (user && bcrypt.compareSync(creds.password, user.password)){
-            res.status(200).send('Welcome');
+            req.session.username = user.username;
+            res.status(200).send(`Welcome ${req.session.username}`);
         } else {
             res.status(401).json({message: 'incorrect combination'});
         }
-    }).catch(err => {res.status(500).send(err, "Everything failed")});
+    }).catch(err => {
+        console.log('/api/login Post error:', err);
+        res.status(500).send(err, "Everything failed")});
 })
 
-
+server.get('/', (req, res) => {
+    req.session.name = 'Frodo';
+    res.send('got it');
+  });
+  
+  server.get('/greet', (req, res) => {
+    const name = req.session.username;
+    res.send(`hello ${name}`);
+  });
 
 
 
