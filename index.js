2b718e0ac7dffe059314e649f0fdff7c1e216082
vanceleon <vanceleon44@gmail.com>
2018-09-18 17:26:17 -0400

    project setup

--- index.js
+++ index.js
@@ -1,59 +1,87 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const db = require("./db/dbConfig.js");
-
+const session = require("express-session");
 const server = express();
+
+const KnexSessionStore = require("connect-session-knex")(session);
 
 server.use(express.json());
 server.use(cors());
 
+const sessionConfig = {
+  name: "banana", // default is connect.sid
+  secret: "nobody tosses a dwarf!",
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    secure: false // only set cookies over https. Server will not send back a cookie over http.
+  }, // 1 day in milliseconds
+  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    tablename: "sessions",
+    sidfieldname: "sid", //what is this line of code?
+    knex: db,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
 
-server.get('/', (req, res) => {
-    res.send("hello world");
-})
+server.use(session(sessionConfig));
 
+auth = (req, res, next) => {
+  if (req.session && req.session.username) {
+    next();
+  } else {
+    res.status(401).json({ message: "go back to the shadows!" });
+  }
+};
+
+server.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 server.post("/api/register", (req, res) => {
-    const creds = req.body;
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 12);
+  creds.password = hash;
 
-    const hash = bcrypt.hashSync(creds.password, 12);
-
-    creds.password = hash;
-
-    db("users")
-        .insert(creds)
-        .then(ids => {
-            const id = ids[0];
-            res.status(201).json(id);
-        })
-        .catch(err => res.status(500).send(err));
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json(id);
+    })
+    .catch(err => res.status(500).send(err));
 });
 
-server.post('/api/login', (req, res) => {
-    const creds = req.body;
-
-    db("users")
-        .where({username: creds.username})
-        .first()
-        .then(user => {
-            if(user && bcrypt.compareSync(creds.password, user.password)){
-                res.status(200).json(`Welcome ${creds.username}`);
-            }else {
-                res.status(401).json({message:"You shall not pass! Go Back to the Shadows!"});
-            }
-        })
-        .catch(err => res.status(500).send(err));
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        req.session.username = user.username;
+        res.status(200).json(`Welcome ${creds.username}`);
+      } else {
+        res
+          .status(401)
+          .json({ message: "You shall not pass! Go Back to the Shadows!" });
+      }
+    })
+    .catch(err => res.status(500).send(err));
 });
 
-server.get("/api/users", (req, res) => {
-    db("users")
-        .select("id", "username", "password")
-        .then(users => {
-            res.json(users);
-
-        })
-        .catch(err => res.send(err));
+server.get("/api/users", auth, (req, res) => {
+  db("users")
+    .select("id", "username", "password")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
 });
 
-server.listen(8000, () => console.log("API running on 8000"));
+server.listen(8000, () => console.log("========API running on 8000======="));
