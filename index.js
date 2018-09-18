d6e64239e7248c1307f691925919370d5e42b6f5
cubsfan46350 <cubsfan.46350@msn.com>
2018-09-18 15:29:43 -0500

    added express-session and structure for auth

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const server = express();
 const knex = require('knex');
 const dBConfig = require('./knexfile');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 //Middleware
 server.use(express.json());
-
+server.use(
+    session({
+        name: 'somesession',
+        secret: '@!!!!!%%%vilksioeknfk!!?#*',
+        cookie: {
+            maxAge: 1 * 24 * 60 * 60 * 1000,
+            secure: false
+        },
+        httpOnly: true,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 const db = knex(dBConfig.development);
 
 //Endpoints
@@ -31,7 +44,9 @@ server.post('/api/login', (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(creds.password, user.password)) {
-                res.status(200).send('Welcome');
+                req.session.username = user.username;
+
+                res.status(200).send(`Welcome ${req.session.username}`);
             } else {
                 res.status(401).json({ message: 'You are unauthorized' })
             }
@@ -39,13 +54,26 @@ server.post('/api/login', (req, res) => {
         .catch(err => res.status(500).send(err));
 });
 
+server.get('/api/name', (req, res) => {
+    req.session.name = username;
+    res.send('success!');
+});
+
+server.get('/api/greeting', (req, res) => {
+    const name = req.session.name;
+    res.send(`Hello ${name}`);
+});
+
 server.get('/api/users', (req, res) => {
+    if (req.session && req.session.username) {
     db('users')
-        .select('id', 'username', 'password')
+        .select('id', 'username')
         .then(users => {
             res.json(users);
         })
         .catch(err => res.send(err));
+    }
+    res.status(401).json({ message: 'You are not allowed!'});
 });
 
 server.listen(3000, console.log('Listening on Port 3000'));
