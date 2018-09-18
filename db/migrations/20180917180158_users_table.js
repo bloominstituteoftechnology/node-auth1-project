e03947b544b2aaeb29e1609fe97e2d4fc7886ea4
Synch3 <jonsimo@aol.com>
2018-09-18 11:09:12 -0400

    Updated

--- db/migrations/20180917180158_users_table.js
+++ db/migrations/20180917180158_users_table.js
@@ -1,14 +1,19 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable("users", function(tbl) {
-    tbl.increments();
 
-    tbl
-      .string("user", 128)
-      .notNullable()
-      .defaultTo("not provided");
-  });
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(tbl) {
+      tbl.increments();
+
+      tbl
+        .string('user', 128)
+        .notNullable()
+        .defaultTo('not provided');
+
+      tbl
+        .string('password', 128);
+        .notNullable()
+  })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable("users");
+  return knex.schema.dropTable('users');
 };
