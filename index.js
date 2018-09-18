81de6cbce2f4a061634de81673e6e13fbf4e4f68
Kairi <kairijb@gmail.com>
2018-09-18 16:14:24 -0500

    fixed a bug in the login. day 2 mvp done

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const knex = require('knex')
 const server = express();
 const dbConfig = require('./db/knexfile')
 const bcrypt = require('bcryptjs');
-
+const session = require('express-session');
 const db = knex(dbConfig.development);
 
+
+const sessionConfig = {
+    name: 'notsession', // default is connect.sid
+    secret: 'nobody tosses a dwarf!',
+    cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: false, // only set cookies over https. Server will not send back a cookie over http.
+    }, // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: false,
+}
+
+server.use(session(sessionConfig))
 server.use(express.json());
 server.use(helmet());
 
@@ -17,20 +31,53 @@ server.get('/', (req, res) => {
 })
 
 server.get('/api/user', (req, res) => {
-    db('auth')
-    .select('id', 'username', 'password')
-        .then(user => {
-            res.status(200).json(user)
-        })
-        .catch(err => res.status(500).json(err))
+    // only send the list of users if the client is logged in
+    req.session && req.session.username ?
+
+        db('auth')
+            .select('id', 'username', 'password')
+            .then(user => {
+                res.status(200).json(user)
+            })
+            .catch(err => res.status(500).json(err))
+        :
+        res.status(401).json({ msg: 'Not authorized for this action' })
 })
 
-// find the user in the database by it's username then
-// if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
-//   return res.status(401).json({ error: 'Incorrect credentials' });
-// }
+server.get('/api/admins', (req, res) => {
+    //  if (req.session && req.session.userId) {
+    //     let userId = req.session.userId
+    // db('roles as r').join('user_roles as ur', 'ur.role_id', '=', 'r.id')
+    //     .select('r.role_name')
+    //     .where('ur.user_id', userId)
+    //     .then(roles => {
+    //         if (roles.includes('admin')) {
+    //             // have access
+    //         } else {
+    //             // bounced
+    //         }
+            
+            
+    //     })
+    // }
+    // role can have many permissions
+    // a user can have many roles
+    // a user can have many permissions
+    //query the db and get the roles for the user
 
-// the user is valid, continue on
+
+    // only send the list of users if the client is logged in
+    req.session && req.session.role === 'admin' ?
+        db('auth')
+            .select('id', 'username', 'password')
+            .then(user => {
+                res.status(200).json(user)
+            })
+            .catch(err => res.status(500).json(err))
+        :
+        res.status(403).json({ msg: 'Not authorized for this action' })
+
+})
 
 server.post('/api/login', (req, res) => {
     const credentials = req.body;
@@ -40,18 +87,29 @@ server.post('/api/login', (req, res) => {
         .first()
         .then(user /* is an object */ => {
             console.log('user:', user);
-            if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
-              return res.status(401).json({ error: 'You shall not pass!' });
+            if (user || bcrypt.compareSync(credentials.password, user.password)) {
+                req.session.username = user.username
+                const name = req.session.username
+                return res.status(200).json(`Welcome ${name}`);
             }
             else {
-                return res.status(200).json({ msg: 'Logged in' });
-            }
+                return res.status(401).json({ error: 'You shall not pass!' });            }
         })
         .catch(error => {
             console.log('/api/login POST Error:', error)
             res.status(500).send('Please try again later')
         })
 })
+
+server.get('/', (req, res) => {
+    req.session.name = 'Frodo';
+    res.send('got it');
+});
+
+server.get('/api/greet', (req, res) => {
+    const name = req.session.username;
+    res.send(`hello ${name}`);
+});
 
 server.post('/api/register', (req, res) => {
     // grab credentials
