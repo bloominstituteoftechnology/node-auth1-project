70d1a25a17b05a13c0acbfd610d77b256892dc1a
bri79319 <bri79319@gmail.com>
2018-09-18 16:05:02 -0400

    fixed error in package.json

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const bcrypt = require("bcryptjs");
-
+const session = require("express-session");
 const db = require("./database/dbConfig.js");
+const KnexSessionStore = require("connect-session-knex")(session);
 
+//--------------------------------------//
+// Optional Way for configuration //
 //const knexConfig = require("./knexfile");
 //const db = knex(knexConfig.development);
+//-------------------------------------//
 
 const server = express();
 
-server.use(express.json());
-// server.use(cors());
+// session configuration
+const sessionsConfig = {
+  name: "monkey", // default is connect.sid
+  secret: "nobody tosses a dwarf!",
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000, // a day
+    secure: false // only set cookies over https. Server will not send back a cookie over http.
+  }, // 1 day in milliseconds
+  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    tablename: "sessions",
+    sidfieldname: "sid",
+    knex: db,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
 
+server.use(session(sessionsConfig));
+server.use(express.json());
+server.use(cors());
+
+// middleware
 function auth(req, res, next) {
-    
+  if (req.session && req.session.username) {
     next();
+  } else {
+    res.status(401).json({ Error: "You shall not pass!!" });
+  }
 }
 
 //============GET ENDPOINT============//
-server.get("/api/users", (req, res) => {
+server.get("/api/users", auth, (req, res) => {
   db("users")
     .select("id", "username")
     .then(users => {
