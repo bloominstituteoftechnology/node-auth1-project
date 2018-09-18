c5c4e5088a8a33f7fd91ffb3737200563277a904
rverdi642 <rverdi642@gmail.com>
2018-09-18 15:55:29 -0500

    Richard Verdier - ﬁnished mvp with luis

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const express = require('express');
+const server = express();
 const cors = require('cors');
 
 const db = require('./database/dbConfig.js');
 const bcrypt = require('bcryptjs');
-const server = express();
+const session = require('express-session');
+//configure express-session middleware
+const KnexSessionStore = require('connect-session-knex')(session);
+  const sessionConfig = {
+    name: 'notsession', // default is connect.sid
+    secret: 'nobody tosses a dwarf!',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: false, // only set cookies over https. Server will not send back a cookie over http.
+    }, // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+      tablename: 'sessions',
+      sidfieldname: 'sid',
+      knex: db,
+      createtable: true,
+      clearinterval: 100 * 60 *60,
+    })
+  };
+server.use(session(sessionConfig))
+
+
 
 
 server.use(express.json());
 
 server.use(cors());
+
+function protected(req, res, next) {
+  if (req.session && req.session.username) {
+    next();
+  } else {
+    res.status(401).json({ message: 'you shall not pass!!' });
+  }
+}
 
 
 server.get('/', (req, res) => {
@@ -45,7 +77,9 @@ server.post('/api/login', (req,res) =>{
 
 //check creds
     if(user && bcrypt.compareSync(creds.password, user.password)) {
-        res.status(200).send('Login Accepted');
+      req.session.username = user.username;
+
+        res.status(200).send(`Login Accepted ${req.session.username}`);
     } else { 
         res.status(401).json({message: 'Invalid Password/Failed Attempt'})
     }
@@ -55,14 +89,50 @@ server.post('/api/login', (req,res) =>{
 
 })
 
+server.get('/setname', (req, res) => {
+  // req.session = {};
+  req.session.name = 'Frodo';
+  res.send('got it');
+});
+
+server.get('/greet', (req, res) => {
+  // const name = req.session.name;
+  const name = req.session.username;
+  // res.send(`hello ${req.session.name}`);
+  res.send(`hello ${name}`);
+});
+
 // protect this route, only authenticated users should see it
 server.get('/api/users', (req, res) => {
+  if(req.session && req.session.username) {
   db('users')
     .select('id', 'username','password')
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
+  }
+  res.status(401).json({message: "Unauthorized attempt"});
+
+});
+
+server.get('/api/admins', (req, res) => {
+  //only see users, if you have the role of 'admin'
+  if (req.session && req.session === 'admin') {
+
+ 
+  db('users')
+    .select('id', 'username','password')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+  // }
+} else {
+  res.status(403).json({message: "Role violation - No access to resource"});
+}
+  // res.status(401).json({message: "Unauthorized attempt"});
+  // 
 });
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
