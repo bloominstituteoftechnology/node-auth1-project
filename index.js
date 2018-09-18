7ab25bd3a6036ef4a108debe492bba0b94e3b470
Fred <sohnf90@yahoo.com>
2018-09-18 13:32:05 -0700

    added express-session

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const db = require('./database/dbConfig.js');
 
 const server = express();
+const sessionConfig = {
+  name: 'monkey', // default is connect.sid
+  secret: 'nobody tosses a dwarf!',
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000, // a day
+    secure: false, // only set cookies over https. Server will not send back a cookie over http.
+  }, // 1 day in milliseconds
+  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+  resave: false,
+  saveUninitialized: false,
+};
+
+server.use(session(sessionConfig));
 
 server.use(express.json());
 server.use(cors());
@@ -15,7 +29,7 @@ server.get('/', (req, res) => {
 
 server.get('/api/users', (req, res) => {
   db('users')
-    // .select('id', 'username')
+    .select('id', 'username')
     .then(users => res.status(200).json(users))
     .catch(err => res.status(500).json({ errorMsg: 'Could not get users.' }));
 });
