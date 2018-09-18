f9ab3ace994faa44e823ea5fdaebcecc5fb113c3
AlexanderCaves <ahcaves@gmail.com>
2018-09-17 20:58:35 -0700

    day one

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./dbAccess');
 
@@ -10,7 +11,41 @@ server.use(cors());
 
 server.post('/api/register', (req, res) => {
     const creds = req.body;
-    const hash
-})
+    const hash = bcrypt.hashSync(creds.password, 10);
+    creds.password = hash;
+
+    db('users')
+        .insert(creds)
+        .then(ids => {
+            const id = ids[0];
+            res.status(201).json(id);
+        })
+        .catch(err => res.status(500).send(err));
+});
+
+server.post('/api/login', (req, res) => {
+    const creds = req.body;
+
+    db('users')
+        .where({ username: creds.username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(creds.password, user.password )) {
+                res.status(200).send({ message: 'Welcome!' });
+            } else {
+                res.status(401).json({ message: 'Unauthorized' });
+            }
+        })
+        .catch(err => res.status(500).send(err));
+});
+
+server.get('/api/users', (req, res) => {
+    db('users')
+        .select('id', 'username', 'password')
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
 
 server.listen(9000, () => console.log('\n== API on port 9k ==\n'));
