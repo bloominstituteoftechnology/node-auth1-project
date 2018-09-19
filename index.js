419fa57c105a00396886dd126c877c83b20cb80f
tulf <timzulf@gmail.com>
2018-09-18 20:56:13 -0400

    hashed the seeds and added session storage and sent back cookie to user.

--- index.js
+++ index.js
@@ -3,15 +3,62 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 const bcrypt = require('bcryptjs')
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session)
 //knex config
 const dbConfig = require('./knexfile.js')
 const db = knex(dbConfig.development);
 //server config
 const server = express();
-//
+
+// configure express-session middleware I'm not sure I like this so I'm doing to go the  session config route
+// server.use(
+//   session({
+//     name: 'login', // default is connect.sid
+//     secret: 'fly you fools!',
+//     cookie: {
+//       maxAge: 1 * 24 * 60 * 60 * 1000,
+//       secure: false, // only set cookies over https. Server will not send back a cookie over http.
+//     }, // 1 day in milliseconds
+//     httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+//     resave: false,
+//     saveUninitialized: false,
+//     store: new KnexSessionStore({
+//       tablename:"sessions",
+//       sidfieldname: 'sid',
+//       knex: db,
+//       createtable: true,
+//       clearInterval: 1000*60*60,
+//     })
+//   })
+// );
+
+//sessionConfig
+const sessionConfig = {
+  name: 'login',
+  secret: 'fly you fools',
+      cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: false, // only set cookies over https. Server will not send back a cookie over http.
+      },
+      resave:false,
+      saveUninitialized:false
+}
+//middleware configuration
 server.use(express.json());
 server.use(helmet());
+server.use(session(sessionConfig))
 
+
+//proteced middleware for protected route:
+function protected (req, res, next) {
+  if(req.session && req.session.username){
+    next();
+  }
+  else{
+    res.status(400).json({error: 'Session credentials incorrect for this user' })
+  }
+}
 
 //----POST ------//
 //register
@@ -38,7 +85,11 @@ server.post("/api/login", (req, res) => {
    .first()
    .then(user => {
      if (user && bcrypt.compareSync(credentials.password, user.password) ) {
-       res.status(200).send("Hello my friend, nice to see you again");
+       //pulling username off user into the cookie
+       req.session.username = user.username;
+       res.status(200).send(`Hello ${req.session.username}, nice to see you again`);
+       //send a cookie back here
+
      }
      else {
        res.status(401).json({ message: "Man you can't even type right " });
@@ -48,7 +99,8 @@ server.post("/api/login", (req, res) => {
 });
 
 //------GET-------//
-server.get("/api/users", (req, res) => {
+// need to make this proteced, use middleware
+server.get("/api/users", protected, (req, res) => {
  db("credentials")
    .select("id", "username")
    .then(users => {
--- seeds/users_info.js
+++ seeds/users_info.js
@@ -1,15 +1,15 @@
-
+const bcrypt = require('bcryptjs')
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('credentials').del()
     .then(function () {
       // Inserts seed entries
       return knex('credentials').insert([
-        {username: 'bobheper1', password: 'biallzabob'},
-        {username: 'ricardo', password: 'david'},
-        {username: 'Greg', password: 'mankiw'},
-        {username: 'orungatan', password: 'spatialmemory'},
-        {username: 'eric', password: 'elhricson'}
+        {username: 'bobheper1', password: bcrypt.hashSync('biallzabob',14)},
+        {username: 'ricardo', password: bcrypt.hashSync('david',14)},
+        {username: 'Greg', password: bcrypt.hashSync('mankiw',14)},
+        {username: 'orungatan', password: bcrypt.hashSync('spatialmemory',14)},
+        {username: 'eric', password: bcrypt.hashSync('elhricson',14)}
       ]);
     });
 };
