5b20c188ec1b63ea4f2a04e1121c7461f5764eae
vanceleon <vanceleon44@gmail.com>
2018-09-18 22:19:19 -0400

    router used in the backend

--- login/login.js
+++ login/login.js
@@ -7,6 +7,17 @@ const bcrypt = require("bcryptjs");
 const server = express();
 // router.get
 
+
+auth = (req, res, next) => {
+    if (req.session && req.session.username) {
+      next();
+    } else {
+      res.status(401).json({ message: "go back to the shadows!" });
+    }
+  };
+  
+
+
 router.post("/register", (req, res) => {
     const creds = req.body;
     const hash = bcrypt.hashSync(creds.password, 12);
@@ -20,4 +31,64 @@ router.post("/register", (req, res) => {
       })
       .catch(err => res.status(500).send(err));
   });
+
+  router.post("/api/login", (req, res) => {
+    const creds = req.body;
+    db("users")
+      .where({ username: creds.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)) {
+          req.session.username = user.username;
+          res.status(200).json(`Welcome ${creds.username}`);
+        } else {
+          res
+            .status(401)
+            .json({ message: "You shall not pass! Go Back to the Shadows!" });
+        }
+      })
+      .catch(err => res.status(500).send(err));
+  });
+
+
+  router.get("/api/users", auth, (req, res) => {
+    db("users")
+      .select("id", "username", "password")
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.json({ message: "Please login to access information" }));
+  });
+
+
+  server.get("/", (req, res) => {
+    res.send("hello world");
+  });
+  
+  //router post
+  
+  server.get("/api/logout", (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.send("error logging out");
+        } else {
+          res.send("good bye");
+        }
+      });
+    }
+  });
+  
+  
+  
+  server.get("/api/users", auth, (req, res) => {
+    db("users")
+      .select("id", "username", "password")
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.json({ message: "Please login to access information" }));
+  });
+
+
   module.exports = router;
