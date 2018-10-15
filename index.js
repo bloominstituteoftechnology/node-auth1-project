c3b7fe71662e48a757b748a2e50518980d8cb5b8
JeanetteFernandez <jeanfern555@gmail.com>
2018-10-15 17:20:20 -0500

    MVP - Initialized Project - > Added package.json with dependencies, knexfile.js, and index.js with skeleton.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const port = 5000;
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+    res.send('Am I Alive?'); //Yes
+});
+
+server.listen(port, () => console.log(`\n=== Listening on Port ${port} ===\n`));
+
+
