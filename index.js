3c7647305d721bfe2d9305669c6f6d22f94f0f50
m-j-hacker <mjhacker@gmail.com>
2018-10-15 15:00:24 -0500

    created GET /api/users and POST /api/register endpoints

--- index.js
+++ index.js
@@ -14,5 +14,31 @@ server.get('/', (req, res) => {
     res.send("It's allliiiiive!!!");
 });
 
+server.post('/api/register', (req, res) => {
+    const credentials = req.body;
+
+    // hash password
+    const hash = bcrypt.hashSync(credentials.password, 14);
+    credentials.password = hash;
+    // save user
+    db('users').insert(credentials).then(ids => {
+        const id = ids[0];
+        res.status(201).json({ newUserId: id });
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+// this endpoint needs to be protected, only auth'd users should see it
+server.get('/api/users', (req, res) => {
+    db('users')
+      .select('id', 'username')
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
+
 const port = 8888;
 server.listen(port, () => console.log(`API running on ${port}`));
