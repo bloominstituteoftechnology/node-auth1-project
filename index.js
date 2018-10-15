a62b644ad8781cf5e3a9cae89cb91195b4b4d355
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-15 17:21:50 -0400

    Added POST endpoints

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs'); // added this library
+
+const db = require('./dbConfig.js');
 
 const server = express();
 
@@ -12,6 +15,45 @@ server.use(cors());
 
 server.get('/', (req, res) => {
   res.send('This sshizz working?');
+});
+
+//################################### POST ########################################//
+// implented this here
+//Introduction to Authentication for FSW13 w/ Luis Hernandez (near 1:02)
+server.post('/register', (req, res) => {
+  const creds = req.body;
+  // hash the password here
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+  // then save the user
+
+  db('users')
+  .insert(creds)
+  .then(ids => {
+      const id = ids[0];
+
+      res.status(201).json(id);
+  })
+  .catch(err => {
+      res.status(500).send(err);   
+  })
+});
+
+server.post('/api/login', (req ,res) => {
+  const creds = req.body;
+
+  db('users')
+  .where({username: creds.username})
+  .first()
+  .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+          res.send(200).send('Started from the bottom now you here!');
+      } else {
+          res.status(401).json({ message: `That's wrong dude....`});
+      }
+  })
+  .catch(err => res.status(500).send(err))
+
 });
 
 const port = 9001;
