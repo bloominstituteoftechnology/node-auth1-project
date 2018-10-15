468110b92fde5fa6011640fda453ca505ec68b7c
Keiran <keirankozlowski@gmail.com>
2018-10-15 17:44:58 -0400

    Initial post and get functions.

--- index.js
+++ index.js
@@ -18,30 +18,34 @@ server.get('/', (request, response) => {
 });
 
 // user endpoints
-// server.get('/api/users', (request, response) => {
-//     projectDb('actions')
-//         .then(actions => {
-//             return response
-//                 .status(200)
-//                 .json(actions);
-//         })
-//         .catch(() => {
-//             return response
-//                 .status(500)
-//                 .json({ Error: "Could not find list of actions." })
-//         });
-// });
+server.get('/api/users', (request, response) => {
+
+
+    db('users')
+        .then(users => {
+            return response
+                .status(200)
+                .json(users);
+        })
+        .catch(() => {
+            return response
+                .status(500)
+                .json({ Error: "Could not find list of users." })
+        });
+});
 
 server.post('/api/register', (request, response) => {
-    const newUser = request.body;
+    const credentials = request.body;
 
-    db
-        .insert(newUser)
-        .into('users')
-        .then(newUser => {
+    const hashedPW = bcrypt.hashSync(credentials.password, 14);
+    credentials.password = hashedPW;
+
+    db('users')
+        .insert(credentials)
+        .then(credentials => {
             return response
                 .status(201)
-                .json(newUser);
+                .json(credentials);
         })
         .catch(() => {
             return response
@@ -57,14 +61,14 @@ server.post('/api/login', (request, response) => {
         .where({ username: credentials.username })
         .first()
         .then(user => {
-            if (user && bcrypt.compareSync(credentials.password, user.password)) {
-                return response
-                    .status(201)
-                    .json({ welcome: user.username });
-            } else {
+            if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
                 return response
                     .status(401)
                     .json({ message: "You shall not pass!" });
+            } else {
+                return response
+                    .status(200)
+                    .json({ message: `${credentials.username} logged in...` });
             }
         })
         .catch(() => {
