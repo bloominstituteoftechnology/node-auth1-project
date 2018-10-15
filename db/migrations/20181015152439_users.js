7bff95962699dbdd2bb128ea6c06e97fccc5d1dc
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-10-15 15:45:32 -0500

    users migration, login, register

--- db/migrations/20181015152439_users.js
+++ db/migrations/20181015152439_users.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+    knex.schema.createTable('users', function(tbl) {
+        users.increments();
+        users
+            .string('username', 128)
+            .unique()
+            .notNullable();
+        users.string('password', 128).notNullable()
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+    knex.schema.dropTable('users');
 };
--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const bcrypt = require('bcrypt');
+const knex = require('knex');
+const knexConfig = require('knexfile.js');
 
-const db = require('./database/dbConfig.js');
+const db = knex(knexConfig.development);
 
 const server = express();
 
-server.use(express.json(), cors());
+server.use(express.json(), cors(), helmet(), bcrypt());
 
 server.get('/', (req, res) => {
     res.send('It lives!');
 });
 
-server.get('/api/users', (req, res) => {
-    db
-        ('users')
-        .select('id',)
+server.post('api/register', (req, res) => {
+    const credentials = req.body;
+    const hash = bcrypt.hashSync(credentials.password, 12)
+    credentials.password = hash;
+    db('users')
+        .insert(credentials)
+        .then(ids => {
+            res.status(201).json(ids[0])
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
+})
+
+server.post('api/login', (req, res) => {
+    const credentials = req.body;
+    db('users')
+        .where({ username: credentials.username })
+        .first()
+        .then(user => {
+            user && bcrypt.compareSync(creds.password,user.password)?
+            res.status(200).send('Access granted'):
+            res.status(401).send('Access denied')
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
 })
