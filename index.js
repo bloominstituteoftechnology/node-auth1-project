cd309a7f10669b8c8d1f5838513d279d1d482dbe
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-10-15 15:17:48 -0700

    GET for users working

--- index.js
+++ index.js
@@ -48,9 +48,14 @@ server.post('/api/login', (req, res) => {
 	});
 });
 
-server.post('/api/users', (req, res) => {
+server.get('/api/users', (req, res) => {
     db('users')
-	.then(users)
+	.then(users => {
+	    res.status(200).json(users);
+	})
+	.catch(err => {
+	    res.status(404).json(err);
+	});
 });
 
 const port = 3300;
