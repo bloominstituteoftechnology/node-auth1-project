02ab45405fccff72a252a158344ec95a217ebd1d
m-j-hacker <mjhacker@gmail.com>
2018-10-15 16:41:45 -0500

    Added express-session and basic authorization middleware

--- index.js
+++ index.js
@@ -7,8 +7,32 @@ const db = require('./database/dbConfig.js');
 
 const server = express();
 
+const session = require('express-session');
+
+function protected(req, res, next) {
+    if (req.session && req.session.username) {
+        next();
+    } else {
+        res.status(401).json({ message: 'You shall not pass!' });
+    }
+}
+
 server.use(express.json());
 server.use(cors());
+
+server.use(
+    session({
+      name: 'notsession', // default is connect.sid
+      secret: 'nobody tosses a dwarf!',
+      cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: false, // only set cookies over https. Server will not send back a cookie over http.
+      }, // 1 day in milliseconds
+      httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
 
 server.get('/', (req, res) => {
     res.send("It's allliiiiive!!!");
@@ -22,6 +46,7 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
         if(user && bcrypt.compareSync(creds.password, user.password)) {
+            req.session.username = user.username;
             res.status(200).json({ message: `Logged in` });
         } else {
             res.status(401).json({ message: "You shall not pass!"});
@@ -47,7 +72,7 @@ server.post('/api/register', (req, res) => {
 })
 
 // this endpoint needs to be protected, only auth'd users should see it
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
     db('users')
       .select('id', 'username')
       .then(users => {
