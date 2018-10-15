76bd54dfb3c7b78ef72782bb3a029b2c8f4eb301
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-15 16:08:59 -0400

    created a route to register a user

--- users/usersRoutes.js
+++ users/usersRoutes.js
@@ -2,8 +2,26 @@ const express = require("express");
 
 const knex = require("knex");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 const knexConfig = require("../knexfile.js");
 const db = knex(knexConfig.development);
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash;
+
+  db("users")
+    .insert(credentials)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ newUserId: id });
+    })
+    .catchThrow(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
