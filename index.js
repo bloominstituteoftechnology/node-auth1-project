4f6318ececacab764ad8a2bc6c499f5219b6d57e
alejandrok93 <alejandrok93@gmail.com>
2018-10-15 14:02:03 -0700

    Completed Register / Login requirements and API endpoints

--- index.js
+++ index.js
@@ -13,19 +13,42 @@ server.use(express.json());
 server.use(cors());
 
 //Routes
-server.get("/", (req, res) => {
-  res.send("its aliveeeee");
-});
-
-server.get("/users", (req, res) => {
-  console.log(db("users"));
+server.get("/api/users", (req, res) => {
   db("users")
     .then(users => res.json(users))
     .catch(err => res.status(500).json(err));
 });
 
-server.post("/register", (req, res) => {
+server.post("/api/register", (req, res) => {
   const creds = req.body;
+  console.log(creds);
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+  console.log(creds);
+
+  db("users")
+    .insert(creds)
+    .then(id => res.send(id))
+    .catch(err => res.status(500).json(err));
+});
+
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+  console.log(creds);
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      console.log(user);
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        console.log("exists");
+        res.status(200).json({ message: "Access granted" });
+      } else res.status(401).json({ message: "You shall not pass" });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err);
+    });
 });
 
 server.listen(9000, () => {
