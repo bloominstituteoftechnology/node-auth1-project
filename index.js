e451d9b09a1881831eb4eb771012d71d8cabcff0
Timothy Hoang <timh1203@gmail.com>
2018-10-15 15:15:25 -0400

    Add login route

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.use(cors())
 server.route('/')
   .get((req, res) => res.send("En vivo"))
 
-server.route('/register')
+server.route('/api/register')
   .post((req, res) => {
     const credentials = req.body;
     const hash = bcrypt.hashSync(credentials.password, 14);
@@ -28,5 +28,17 @@ server.route('/register')
       })
       .catch(err => res.status(500).json(err));
   })
+
+server.route('/api/login')
+  .post((req, res) => {
+    const credentials = req.body
+    db('users')
+      .where({ username: credentials.username }).first()
+      .then(user => {
+        if (user && bcrypt.compareSync(credentials.password, user.password)) return res.status(200).json({ message: `Welcome ${user.username}!` })
+        return res.status(401).json({ message: 'You shall not pass!' });
+      })
+      .catch(err => res.status(500).json({ err }));
+  })  
 
 server.listen(port, () => console.log(`\n===Listening on ${port}===\n`))
