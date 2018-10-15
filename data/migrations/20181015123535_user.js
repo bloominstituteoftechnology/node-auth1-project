98d8748da8f94dd6b9b94cff06d6716fba4c8781
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-10-15 12:40:37 -0700

    user migration set up

--- data/migrations/20181015123535_user.js
+++ data/migrations/20181015123535_user.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex, Promise) {
-  
+	return knex.schema.createTable("users", function(tbl) {
+		tbl.increments();
+		tbl.string("username", 20).notNullible();
+		tbl.string("password", 255).notNullible();
+	});
 };
 
 exports.down = function(knex, Promise) {
-  
+	return knex.schema.dropTableIfExists("users");
 };
