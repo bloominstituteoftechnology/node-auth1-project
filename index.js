12dd46874e426dbba28aed215e85410bd90805cc
Timothy Hoang <timh1203@gmail.com>
2018-10-15 14:49:02 -0400

    Add register route

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const helmet = require('helmet')
+const cors = require('cors');
 const bcrypt = require('bcryptjs')
-const server = express()
 const port = 9000
+const server = express()
 
-server.use(helmet())
 server.use(express.json())
+server.use(helmet())
+server.use(cors());
 
-server.get('/', (req, res) => res.send("En vivo"))
+server.route('/')
+  .get((req, res) => res.send("En vivo"))
+
+server.route('/register')
+  .post((req, res) => {
+    const credentials = req.body;
+    const hash = bcrypt.hashSync(credentials.password, 14);
+    credentials.password = hash;
+    db('users')
+      .insert(credentials)
+      .then(ids => {
+        const id = ids[0]
+        res.status(201).json({ newUserId: id });
+      })
+      .catch(err => res.status(500).json(err));
+  })
 
 server.listen(port, () => console.log(`\n===Listening on ${port}===\n`))
