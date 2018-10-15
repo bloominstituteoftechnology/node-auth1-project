203af681200f7400085e49dea1946dfaecb023e8
Austinlp4 <apendergrast44@gmail.com>
2018-10-15 12:56:51 -0500

    Both post endpoints

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const db = require('./database/dbConfig.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send('Its Alive!');
+});
+
+server.post('/register', (req,res) => {
+  const credentials = req.body;
+  //hash the password
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash;
+  //then save the user
+  db('users').insert(credentials).then(ids => {
+    const id = ids[0];
+    res.status(200).json({ newUserId: id})
+  })
+  .catch(err => console.error(err))
+})
+
+server.post('/login', (req, res) => {
+  const creds = req.body;
+
+  db('users').where({username: creds.username}).first().then(user => {
+    if(user && bcrypt.compareSync(creds.password, user.password)) {
+      //found the user
+      res.status(200).json({welcome: user.username})
+    }else{
+      res.status(401).json({ message: 'DENIED'})
+    }
+  })
+  .catch(err => res.status(500).json({err}))
+})
+
+// protect this route, only authenticated users should see it
+server.get('/api/users', (req, res) => {
+  db('users')
+    .select('id', 'username')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+server.listen(3300, () => console.log('\nrunning on port 3300\n'));
+
