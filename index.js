1bb8bd540bb6359f269d7cabaeccd7e4a32d3c69
Csillag61 <csillagok@gmail.com>
2018-10-15 16:10:51 -0400

    Post register, login, get users

--- index.js
+++ index.js
@@ -11,5 +11,52 @@ server.use(cors());
 server.get("/", (req, res) => {
   res.send("Server Running");
 });
+function protected(req, res, next) {
+     if (req.session && req.session.username) {
+         next();
+        
+    } else {
+              res.status(401).json({ message: 'no pass!!!' });
+        
+    }
+    
+} 
+server.post('/api/register', (req, res)=>{
+    const creds=req.body;
+    const hash=bcrypt.hashSync(creds.password, 14);
+    creds.password= hash;
+    db('users')
+    .insert(creds)
+    .then(ids=>{
+        const id = ids[0];
+        res.status(201).json(id);
+    })
+    .catch(err => res.status(500).send(err));
+});
 
+server.post('/api/login',(reg, res )=>{
+    const creds=req.body;
+    db('users')
+    .where({username:creds.username})
+    .first()
+    .then(user=>{
+        if (user && bcrypt.compareSync(creds.password, user.password)){
+            req.session.username=user.username;
+            res.status(200).send(`Welcome back ${req.session.username}`);
+        }else{
+            res.status(401).json({message:'No pass...:('});
+        }
+        })
+        .catch(err => res.status(500).send(err));
+    });
+    server.get('/api/users', protected, (req, res)=>{
+        db('users')
+        .select('id', 'username', 'password')
+        .then(users=>{
+            res.json(users);
+        })
+        .catch(err=>res.send(err));
+    });
+    
+   
 server.listen(5500, () => console.log("\nrunning on port 5500\n"));
