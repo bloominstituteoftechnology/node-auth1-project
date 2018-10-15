a735159daaf6843b054f1997e1e6bb6121476826
Michael Trevino <preince560m@gmail.com>
2018-10-15 17:52:55 -0500

    finished

---
+++ database/migrations/20181015164455_user_test.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('usersTest', users => {
+        users.increments();
+    
+        users
+          .string('username', 255)
+          .notNullable()
+          .unique();
+        users.string('password', 255).notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('usersTest');
+};
