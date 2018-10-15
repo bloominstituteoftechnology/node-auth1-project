18d57e7a7ad91f94da3110f3daee9fb60bbe21df
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-10-15 18:21:46 -0400

    knex init, create users table migration

---
+++ data/migrations/20181015161624_create_users_table.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(tbl) {
+    tbl.increments();
+
+    tbl
+      .string('username', 128)
+      .notNullable()
+      .unique('username');
+
+    tbl.string('password', 128).notNullable;
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
