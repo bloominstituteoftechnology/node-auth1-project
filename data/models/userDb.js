9bbbfb294d10bae61b0b6f54290a85b2c422afcb
unknown <cornielleandres@gmail.com>
2018-10-15 16:21:25 -0400

    Add POST request handler for logging in a user.

--- data/models/userDb.js
+++ data/models/userDb.js
@@ -1,6 +1,13 @@
 const db = require('../dbConfig.js');
 
 module.exports = {
+	getUser: function(username) {
+		let query = db('users as u');
+		return query
+			.select('password')
+			.first()
+			.where({ username: username });
+	},
 	registerNewUser: function(credentials) {
 		let query = db('users');
 		return query
--- index.js
+++ index.js
@@ -8,6 +8,33 @@ const port = 5000;
 
 applyGlobalMiddleware(server);
 
+// login a user
+server.post('/api/login', (req, res) => {
+	const credentials = req.body;
+	if (!credentials.username) {
+		return res.status(401).json({ error: 'Username cannot be empty.' });
+	}
+	if (!credentials.password) {
+		return res.status(401).json({ error: 'Password cannot be empty.' });
+	}
+	return userDb
+		.getUser(credentials.username)
+		.then(user => {
+			if (user) {
+				return bcrypt
+					.compare(credentials.password, user.password)
+					.then((match) => {
+						if (match) {
+							return res.status(201).json({ message: `You are now logged in as ${ credentials.username }` });
+						}
+						return res.status(404).json({ error: 'You shall not pass!' });
+					});
+			}
+			return res.status(404).json({ error: 'You shall not pass!' });
+		})
+		.catch(err => res.status(500).json({ error: `Server failed to login user: ${ err }` }));
+});
+
 // register a new user
 server.post('/api/register', (req, res) => {
 	const credentials = req.body;
@@ -17,16 +44,17 @@ server.post('/api/register', (req, res) => {
 	if (!credentials.password) {
 		return res.status(401).json({ error: 'Password cannot be empty.' });
 	}
-	bcrypt.hash(credentials.password, 14, function(err, hash) {
-		if (err) {
-			return res.status(400).json({ error: `Registration failed: ${ err }` });
-		}
-		credentials.password = hash;
-		userDb
-			.registerNewUser(credentials)
-			.then(id => res.status(201).json(id.id[0]))
-			.catch(err => res.status(500).json({ error: `Server failed to register new user: ${ err }` }));
-	});
+	return bcrypt
+		.hash(credentials.password, 14, function(bcryptErr, hash) {
+			if (bcryptErr) {
+				return res.status(500).json({ error: `Bcrypt hashing failed: ${ bcryptErr }` });
+			}
+			credentials.password = hash;
+			return userDb
+				.registerNewUser(credentials)
+				.then(id => res.status(201).json(id.id[0]))
+				.catch(err => res.status(500).json({ error: `Server failed to register new user: ${ err }` }));
+		});
 });
 
 server.listen(port, () => { console.log(`\n=== Listening on port ${ port } ===`) });
