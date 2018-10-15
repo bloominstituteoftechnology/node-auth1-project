f77b2f054d85ce774cc39096d3ac396d33ed7ff1
mparedes003 <michelle.g.paredes@gmail.com>
2018-10-15 12:49:10 -0700

    Added POST route handler to protect GET route handler that accesses all users so that only authenticated users can see it.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const db = require('./database/dbConfig.js');
 
 const server = express();// creates the server
 
+// Add GLOBAL MIDDLEWARE
 server.use(express.json());
 server.use(cors());
 
@@ -32,6 +33,26 @@ server.post('/register', (req, res) => {
     })
     .catch(err => {
       res.status(500).json(err);
+    });
+});
+
+// Add POST ROUTE HANDLER to protect GET ROUTE HANDLER to access all users
+// so that only authenticated users can see it
+server.post('/login', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        res.status(200).json({ welcome: user.username });
+      } else {
+        res.status(401).json({ message: 'Invalid username and password' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err });
     });
 });
 
