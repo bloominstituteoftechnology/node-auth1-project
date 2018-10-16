8875f0f3dccc3838e9d235be1e04649880d0cd1c
Benny Oseguera <oseguerabenny27@gmail.com>
2018-10-16 15:01:13 -0700

    Finished Day 2 - Benny Oseguera

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 // imports
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
 // instantiate server
 const server = express();
 server.use(express.json());
 
+// middleware
+// session
+server.use(
+  session({
+    name: "notsession",
+    secret: "nobody tosses a dwarf!",
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: true
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+const protected = (req, res, next) => {
+  if (req.session && req.session.username) {
+    next();
+  } else {
+    res.status(401).json({ message: "you shall not pass!" });
+  }
+};
+
 // endpoints
 server.post("/api/register", (req, res) => {
   const credentials = req.body;
-  // hash the password
-  const hash = bcrypt.hashSync(credentials.password, 14);
+
+  const hash = bcrypt.hashSync(credentials.password, 10);
   credentials.password = hash;
-  // save the user
+
   db("users")
     .insert(credentials)
-    .then(ids => res.status(201).json(ids[0]))
-    .catch(err => express.status(500).json(err));
+    .then(ids => {
+      req.session.username = user.username;
+      res.status(201).json(ids[0]);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 server.post("/api/login", (req, res) => {
@@ -27,15 +57,16 @@ server.post("/api/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
+        req.session.username = user.username;
         res.status(200).json({ welcome: user.username });
       } else {
-        res.status(401).json({ message: "Could not login" });
+        res.status(401).json({ message: "You shall not pass!" });
       }
     })
     .catch(err => res.status(500).json(err));
 });
 
-server.get("/api/users", (req, res) => {
+server.get("/api/users", protected, (req, res) => {
   db("users")
     .then(users => res.status(201).json(users))
     .catch(err => express.status(500).json(err));
