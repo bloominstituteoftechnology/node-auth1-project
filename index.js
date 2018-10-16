f6fa2d85e6ee0b89c516f676b0c2fb48a066080d
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-10-16 13:58:46 -0700

    Main objective finished, have to handle personal stuff, but will look into tomorrow kit

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
-
 const knexConfig = require('./knexfile.js');
-
 const db = knex(knexConfig.development);
-
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+
+const sessionConfig = {
+  secret: 'you.shall.not.pass.!',
+  name: 'monkey', // defaults to --> connect.sid (session id)
+  httpOnly: true, // i don't want this to be accessed by JS
+  resave: false,
+  saveUninitialized: false, // laws !
+  cookie: {
+    secure: false, // only over HTTPS, in production it'll be set to true
+    maxAge: 1000 * 60 * 1 // when the cookie expires
+  } // cookie
+}
 
 const server = express();
-
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.get('/', (req, res) => {
   res.send('Its Alive!');
@@ -19,7 +29,7 @@ server.get('/', (req, res) => {
 
 
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protectedRoute, (req, res) => {
   db('users')
     .select('id', 'username', 'password')
     .then(users => {
@@ -48,6 +58,10 @@ server.post('/api/register', (req, res) => {
     .insert(creds)
     .then(ids => {
       const id = ids[0];
+      console.log('creds entered', creds); // is an object of your username and password, the password is hashed
+      console.log('req session', req.session); // session is an object, like the one below
+      req.session.username = creds.username;
+      console.log(req.session.username, 'req username'); // adds a username to the session object
       res.status(201).json({ newUserId: id }) // when they post, this is what shows
     })
     .catch(err => res.status(500).json(err));
@@ -63,6 +77,7 @@ server.post('/api/login', (req, res) => {
 
       if (user && bcrypt.compareSync(creds.password, user.password))
       {
+        req.session.username = user.username;
         res.status(200).json({ welcome: creds.username });
       } else 
       {
@@ -72,6 +87,30 @@ server.post('/api/login', (req, res) => {
     })
     .catch(err => res.status(500).json(err));
 });
+
+server.get('/api/logout', (req, res) => {
+  if (req.session)
+  {
+    req.session.destroy(err => {
+
+      if (err) {
+        res.send('You are stuck here mortal! Mwahahahaha');
+      } else {
+        res.send('Logout successful. Good bye!');
+      }
+
+    });
+  }
+});
+
+function protectedRoute(req, res, next) {
+  // req.session is an object
+  if (req.session && req.session.username) {
+    next(); // continue as usual
+  } else { // else if not authorized...
+    res.status(401).json({ authError: 'FBI DON\'T MOVE!' });
+  }
+}
 
 
 server.listen(4405, () => console.log('\nrunning on port 4405\n'));
