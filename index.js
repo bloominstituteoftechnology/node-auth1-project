bb45002c268e92d39de1e49233f85683c196061c
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-10-16 18:52:24 -0400

    Added use of session and cookies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const knex = require('knex');
 const knexConfig = require('./knexfile');
@@ -9,12 +10,33 @@ const db = knex(knexConfig.development);
 const server = express();
 const port = 9000;
 
+const sessionConfig = {
+  secret: 'heck-if-I.know%',
+  name: 'eagle', //defaults to connect.sid
+  httpOnly: true, // JS can't access this
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, //over httpS
+    maxAge: 1000 * 60 * 1,
+  },
+};
+server.use(session(sessionConfig));
+
+function checkAuth(req, res, next) {
+  if (req.session && req.session.username) {
+    next();
+  } else {
+    res.status(401).json({ message: 'not authorized' });
+  }
+}
+
 server.use(express.json());
 server.use(cors());
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', checkAuth, (req, res) => {
   db('users')
-    .select('users.id', 'users.username', 'users.password')
+    .select('id', 'username', 'password')
     .then((response) => {
       res.status(200).json(response);
     });
@@ -43,6 +65,7 @@ server.post('/api/login', (req, res) => {
     .first()
     .then((user) => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
+        req.session.username = user.username;
         res.status(200).send('Log in successful.');
       } else {
         res.status(401).json({ message: 'You shall not pass!' });
@@ -52,6 +75,18 @@ server.post('/api/login', (req, res) => {
       console.log(error);
       res.json(500).json({ errorMessage: 'Error logging in' });
     });
+});
+
+server.get('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.send("You can't leave");
+      } else {
+        res.send('good bye');
+      }
+    });
+  }
 });
 
 server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
