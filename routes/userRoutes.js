b27a73e1fe7477c8ae1a1521328899e320ea2bbc
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-16 14:00:22 -0400

    Authorization/Logout Created a protected function to limit access to users endpoint Created a logout endpoint for the user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,19 +14,13 @@ router.post("/register", (req, res) => {
         .insert(creds) //pass credentials within users table
         .then(ids => {
             const id = ids[0]; //array grab the first id
+            req.session.username = user.username;
             res.status(201).json(id); // sends back new user id
         })
         .catch(err => res.status(500).send(err));
 });
-   
-router.get("/users", (req, res) => {
-    db("users")
-        .select("id", "username", "password")
-        .then(ids => {
-            res.json({newUserId: ids});
-        })
-        .catch(err => res.status(500).send(err));
-});
+
+
 
 router.post("/login", (req, res) => {
     const creds = req.body; 
@@ -41,7 +35,43 @@ router.post("/login", (req, res) => {
                 res.status(401).json({ message: "Truly unfortunate, access has not been granted!" });
             }
         })
+        .catch(err => {
+            res.status(500).json({ err });
+        });
+});
+
+router.get("/users", protected, (req, res) => {
+    if (req.session && req.session.username) {
+    db("users")
+        .select("id", "username", "password")
+        .then(ids => {
+            res.json({newUserId: ids});
+        })
+        .catch(err => res.status(500).send(err));
+    }else {
+        res.status(401).send('Not Authorized.');
+      }
     
+  });
+function protected(req, res, next) {
+    if (req.session && req.session.username) {
+      next();
+    } else {
+      res.status(401).json({ message: "denied" });
+    }
+  };
+
+
+router.get('/api/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.send('logout error');
+        } else {
+          res.send('Farewell good friend!');
+        }
+      });
+    }
 });
 
 module.exports = router;
