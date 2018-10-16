bce763f883493a627981b40993862126c0f4b96d
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-16 16:53:44 -0600

    Add Better protected.js Comments

--- middleware/protected.js
+++ middleware/protected.js
@@ -1,6 +1,9 @@
+// Middleware to Prevent Access to Restricted Endpoints
 module.exports = (request, response, next) => {
-    if (request.path.search(/(\/api\/restricted)/g) >= 0) {
-        console.log(request.path)
+
+    // Check if path starts with restriced url path. 
+    if (request.path.search(/(\/api\/restricted)/g) = 0) {
+        // Execute Next Middleware if there is a current, authorized, session.
         if (request.session && request.session.username) {
             next();
         } else {
