cfb8a4a7aa4157a51a1c6a87120bba4262cdd0be
Michael Trevino <preince560m@gmail.com>
2018-10-16 17:37:17 -0500

    part 2

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session')
 const db = require('./database/databaseConfig.js');
+
 const server = express();
 
+const sessionConfig={
+  secret:'ClarissA123123!',
+  name:'Fox',
+  httpOnly:true,
+  resave:false,
+  saveUninitialized:false,
+  cookie: {
+    secure: false,
+    maxAge: 1000 * 60 * 1,
+  }
+}
+server.use(session(sessionConfig));
 server.use(express.json());
 server.use(cors());
 
@@ -36,6 +50,7 @@ server.get('/', (req, res) => {
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(creds.password, user.password)) {
+          req.session.username = user.username;
           res.status(200).json({ welcome: user.username });
         } else {
           res.status(401).json({
@@ -48,7 +63,7 @@ server.get('/', (req, res) => {
       });
   });
   
-  server.get('/api/users', (req, res) => {
+  server.get('/api/users',protected, (req, res) => {
     db('users')
       .select('id', 'username', 'password')
       .then(users => {
@@ -56,5 +71,13 @@ server.get('/', (req, res) => {
       })
       .catch(err => res.send(err));
   });
+
+  function protected(req, res, next){
+    if (req.session && req.session.username){
+      next();
+    } else {
+      res.status(401).json({message:'NOT AUTHORIZED!!!'});
+    }
+  }
   
   server.listen(3300, () => console.log('\nrunning on port 3300\n'))
