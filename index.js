fe29350fb7d83dd6930c26147a63182e041c8db3
James Cassidy <cassidyjamesw@gmail.com>
2018-10-17 13:38:08 -0400

    fixed

--- index.js
+++ index.js
@@ -1,88 +1,112 @@
 const express = require("express");
-const cors = require("cors");
 const bcrypt = require("bcryptjs");
-
+const cors = require("cors");
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
 const session = require("express-session");
-const KnexSessionStore = require("connect-session-knex")(session);
 
-const db = require("./database/dbConfig.js");
 const server = express();
-
-const sessionConfig = {
-  secret: "nobody-tosses.a%dwarf.!",
-  name: "monkey",
-  httpOnly: true,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false,
-    maxAge: 1000 * 60 * 10
-  },
-  store: new KnexSessionStore({
-    labelname: "sessions",
-    sidfieldname: "sid",
-    knex: db,
-    createtable: true,
-    clearInterval: 1000 * 60 * 60
-  })
-};
-
-server.use(session(sessionConfig));
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(cors());
 
+// configure express-session middleware
+server.use(
+  session({
+    name: "notsession", // default is connect.sid
+    secret: "nobody tosses a dwarf!",
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: true // only set cookies over https. Server will not send back a cookie over http.
+    }, // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// middleware to handled protected parts of server
+function protected(req, res, next) {
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.status(401).json({ message: "you shall not pass!!" });
+  }
+}
+
+// GET //
 server.get("/", (req, res) => {
-  res.send("Its Alive!");
+  res.send("server functional");
 });
 
-server.post("/register", (req, res) => {
-  const credentials = req.body;
+server.get("/api/users", protected, (req, res) => {
+  db("users")
+    .select("id", "username")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({ error: err });
+    });
+});
 
-  // hash the password
+// Log out fxnality
+server.get("/api/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send("error logging out");
+      } else {
+        res.send("good bye");
+      }
+    });
+  }
+});
+////
+
+// POST //
+server.post("/api/register", (req, res) => {
+  const credentials = req.body;
   const hash = bcrypt.hashSync(credentials.password, 14);
   credentials.password = hash;
-  // then save the user.
+
   db("users")
     .insert(credentials)
     .then(ids => {
+      // return the respective user's id
       const id = ids[0];
       res.status(201).json({ newUserId: id });
     })
     .catch(err => {
-      res.status(500).json(err);
+      res
+        .status(500)
+        .json({
+          errorMessage: `There was an error registering.\n`,
+          error: err
+        });
     });
 });
 
-server.post("/login", (req, res) => {
-  const creds = req.body;
-
+server.post("/api/login", (req, res) => {
+  const credentials = req.body;
   db("users")
-    .where({ username: creds.username })
+    .where({ username: credentials.username })
     .first()
     .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
-        // found the user.
-        res.status(200).json({ welcome: user.username });
-      } else {
-        res.status(401).json({
-          message: `You shall not pass... as you are not authenticated.`
-        });
-      }
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        res.status(200).json({ message: "You may enter" });
+      } else
+        res.status(401).json({ message: "YOUUUU SHALLLL NOTTTTT PASSSSS" });
     })
     .catch(err => {
-      res.status(500).json(err);
+      res.status(500).send(err);
     });
 });
+/////
 
-// protect this route, only authenticated users should see it
-server.get("/api/users", (req, res) => {
-  db("users")
-    .select("id", "username", "password")
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+// Server Listener
+server.listen(5000, () => {
+  console.log("API is running");
 });
-
-server.listen(3400, () => console.log("\nrunning on port 3400\n"));
