5867e81bf4796fb6445e1ad020ffee457b07af54
Lucas Beemer <you@example.com>
2018-10-17 10:49:00 -0700

    project folders and files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const KnexSessionStore = require('connect-session-knex')(session);
+const jwt = require('jsonwebtoken');
 const db = require('./database/dbConfig.js');
 const server = express();
 const port = 9000;
@@ -31,85 +32,97 @@ server.use(express.json());
 server.use(cors());
 
 
-// ======== PROTECTS USERS ======== //
-function protected(req, res, next) {
-  if (req.session && req.session.username) {
-    next();
-  } else {
-    res.status(401).json({ Message: '<h1>Not authorized</h1>' });
-  }
-}
-
-
 // ======== SERVER RUNNING ======== //
 server.get('/', (req, res) => {
   res.send('<h1>Its Alive!<h1>');
 });
 
 
-// ========== CREATE USER ========== //
+// ========= CREATES USER ========= //
 server.post('/register', (req, res) => {
   const credentials = req.body;
-  const hash = bcrypt.hashSync(credentials.password, 12);
+  const hash = bcrypt.hashSync(credentials.password, 10);
   credentials.password = hash;
+
   db('users')
     .insert(credentials)
     .then(ids => {
       const id = ids[0];
-      req.session.username = credentials.username;
-      res.status(201).json({ 
-        newUserId: id, 
-        message: `Welcome to the database, ${credentials.username}`});
-  })
+      res.status(201).json({ newUserId: id });
+    })
     .catch(err => {
-    res.status(500).json(err);
-  });
+      res.status(500).json(err);
+    });
 });
 
 
-// ========== LOGIN ========== //
+// ======= GENERATES TOKEN ======= //
+const jwtSecret = 'nobody tosses a dwarf!';
+
+function generateToken(user) {
+  const jwtPayload = {
+    ...user,
+    hello: 'FSW13',
+    role: 'admin'
+  };
+  const jwtOptions = {
+    expiresIn: '5m',
+  };
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+}
+
+
+// ========== LOGIN ========= //
 server.post('/login', (req, res) => {
   const creds = req.body;
+
   db('users')
     .where({ username: creds.username })
     .first()
     .then(user => {
-      if(user && bcrypt.compareSync(creds.password, user.password)) {
-        req.session.username = user.username;
-        res.status(200).json({ Welcome: user.username})
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ welcome: user.username, token });
       } else {
-        res.status(401).json({ Message: 'YOU SHALL NOT PASS!'})
-    }
-  })
-  .catch(err => {
-    res.status(500).json({ err });
-  });
+        res.status(401).json({ message: 'you shall not pass!' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
 });
 
 
-// === AUTHENTICATED USERS GET USER-LIST === //
+// AUTHENTICATED USERS SHOULD GET USER-LIST //
 server.get('/users', protected, (req, res) => {
+  console.log('\n*** DECODED TOKEN INFO ***\n', req.decodedToken);
   db('users')
     .select('id', 'username', 'password')
     .then(users => {
-      res.json({ userId: req.session.userId, users });
+      res.json({ users });
     })
     .catch(err => res.send(err));
 });
 
 
-// ========== LOGOUT ========== //
-server.get('/logout', (req, res) => {
-  if (req.session) {
-    req.session.destroy(err => {
-      if (err) {
-        res.send("You can't leave");
+// === VERIFICATION for PROTECTION === //
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+  if(token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if(err) {
+        // token verifcation failed
+        res.status(401).json({ message: 'invalid token'});
       } else {
-        res.send("Goodbye");
+        // token is valid
+        req.decodedToken = decodedToken;
+        next();
       }
     })
+  } else {
+    res.status(401).json({ message: 'no token provided' })
   }
-})
+}
 
 
 // ========= SERVER ========= //
