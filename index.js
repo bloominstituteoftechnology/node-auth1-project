b71a071af3a240f4d5f953d5e14d79957290fa75
Luisan <luisangelsc@gmail.com>
2018-10-17 13:25:14 -0500

    used sessions and cookies, users may remain logged in

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session)
+
 
 const knex = require('knex');
 const knexConfig = require('./knexfile.js');
@@ -9,10 +12,31 @@ const db = knex(knexConfig.development)
 
 const server = express();
 
+const sessionConfig = {
+  secret: 'this.is%my.secret%key',
+  name: 'chad',
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    maxAge: 1000*60*25,
+  },
+  store: new KnexSessionStore({
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    knex: db,
+    createtable: true,
+    clearInterval: 1000*60*10
+  })
+}
+
+server.use(session(sessionConfig));
+
 server.use(express.json());
 server.use(cors());
 
-server.get('/users', (req, res) => {
+server.get('/users', protected,  (req, res) => {
   db('users')
     .then(users => {
       res.status(200).json(users)
@@ -32,7 +56,7 @@ server.post('/register', (req, res) => {
   db('users')
     .insert(creds)
     .then(user => {
-      console.log(user);
+      req.session.username = creds.username
       res.status(201).json(user)
     })
     .catch(err => {
@@ -49,6 +73,7 @@ server.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
+        req.session.username = creds.username
         res.status(200).json({ welcome: user.username })
       } else {
         res.status(401).json({ message: 'You shall not pass!'})
@@ -60,6 +85,25 @@ server.post('/login', (req, res) => {
     })
 })
 
+server.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send("But you can never leave, tu du-ru-run")
+      } else {
+        res.send('Bubis-bye')
+      }
+    });
+  }
+});
+
+function protected(req, res, next) {
+  if (req.session && req.session.username){
+    next();
+  } else {
+    res.status(401).send('You shall remain unauthorized')
+  }
+}
 
 const port = 9000;
 
