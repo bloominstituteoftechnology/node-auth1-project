8d3e2087b99b41821a34184cfc7de9748466283f
mich-shrak <micah.shrak@gmail.com>
2018-10-22 14:23:02 -0700

    Created endpoints login, users, register

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
+const bcrypt = require('bcryptjs');
+
 // Database
+const db = require('./data/dbConfig.js');
 
 const server = express();
 
@@ -16,11 +19,54 @@ server.get('/', (req, res) => {
 // Register endpoint
 server.post('/api/register', (req, res) => {
 	const creds = req.body;
+
+	// hash the password (Monday)
+	const hash = bcrypt.hashSync(creds.password, 14);
+	creds.password = hash;
+
+	// then save the user (Tuesday)
+	db('users')
+		.insert(creds)
+		.then((ids) => {
+			const id = ids[0];
+			res.status(201).json({ newUserId: id });
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
 });
 
 // Login endpoint
+server.post('/api/login', (req, res) => {
+	const creds = req.body;
 
-// Users endpoint
+	db('users')
+		.where({ username: creds.username })
+		.first()
+		.then((user) => {
+			if (user && bcrypt.compareSync(creds.password, user.password)) {
+				//found user now compare password creds with user password
+				res.status(200).json({ welcome: user.username });
+			} else {
+				res.status(401).json({ message: 'you shall not pass!' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ Message: err });
+		});
+});
+
+// Users endpoint (Monday)
+server.get('/api/users', (req, res) => {
+	db('users')
+		.select('id', 'username', 'password') // never add return password in production
+		.then((users) => {
+			res.json(users);
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+});
 
 // Server Listening
 const port = 9000;
