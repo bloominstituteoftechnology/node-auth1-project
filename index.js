101aad77ec557d046b7e26a61a57f4dec9db57ae
Joseph Chretien-Golden <joechre@gmail.com>
2018-10-22 16:49:06 -0500

    USE -add object -add store

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const helmet = require('helmet')
 const knex = require('knex')
 const knexConfig = require('./knexfile.js')
 const session = require('express-session')
-const KnexSessionStore = require('connect-session-knex')(session)
+
 const port = 9000
 const db = knex(knexConfig.development)
 const server = express()
 
 server.use(express.json())
+server.use(helmet())
+
+server.use(session({
+    name: 'panda', // default is connect.sid
+    secret: 'bamboo rules',
+    cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: false, // only set cookies over https. Server will not send back a cookie over http.
+      },
