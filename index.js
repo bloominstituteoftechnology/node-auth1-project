7ac6c4fbf2054d7438eca4721bd0cb68f28c387c
lyang9 <lyang9@buffalo.edu>
2018-10-22 16:49:40 -0400

    server.get(/) to check if postman is working

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+// endpoints here
+server.get('/', (req, res) => {
+	res.send('It is working!');
+});
+
 // listening port
 const port = 5000;
 server.listen(port, function() {
