742bc40e206dfcf5f2462d426042c914f08e8cca
Sasha <taylork85@gmail.com>
2018-10-22 14:34:48 -0500

    ADD PACKAGE.JSON CONFIGURE KNEXFILE ADD DEPENDENCIES

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './lambda_authi.sqlite3'
+    },
+    migrations: {
+      filename: './data/migrations'
+    },
+    seeds: {
+      filename: './data/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
