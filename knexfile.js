e9d19452540ccf5e1a6d5eb09c0ba951a6303c41
Marious Davis <mlancelot88@gmail.com>
2018-10-23 10:18:26 -0500

    added knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/db.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+};
