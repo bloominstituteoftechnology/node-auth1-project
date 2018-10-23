b1eec1f1ca4054365f5192886506d89a8906c312
Ray Lomeli <raylom96@gmail.com>
2018-10-23 16:18:12 -0500

    added routes

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+
+const db = require("./database/dbConfig.js");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send("Its Alive!");
+});
+
+server.post("/register", (req, res) => {
+  const credentials = req.body;
+  // hash the password
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash;
+  // then save the user
+  db("users")
+    .insert(credentials)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ newUserId: id });
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+server.post("/login", (req, res) => {
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        // found the user
+        res.status(200).json({ welcome: user.username });
+      } else {
+        res.status(401).json({ message: "you shall not pass!" });
+      }
+    });
+});
+
+// protect this route, only authenticated users should see it
+server.get("/users", (req, res) => {
+  db("users")
+    .select("id", "username", "password")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+server.listen(4000, () => console.log("\nrunning on port 4000\n"));
---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./dev.sqlite3"
+    }
+  },
+
+  migrations: {
+    tableName: "knex_migrations"
+  }
+};
