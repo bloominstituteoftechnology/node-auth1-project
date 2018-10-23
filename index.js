bcacf559390a4e5d0b5d8d827f5715de147de57b
Brandon White <bwhite06@stac.edu>
2018-10-23 16:59:46 -0400

    added middware
    - working creating protected route

--- index.js
+++ index.js
@@ -6,11 +6,46 @@ const knexConfig = require('./knexfile.js');
 const db = knex(knexConfig.development);
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
-server
+server.use(
+  session({
+    name:'notsession',
+    secret:'john doe',
+    cookie:{maxAge:1*24*60*60*1000
+    secure:false,
+    },//1 day
+    httpOnly:true
+
+    resave:false,
+    saveUninitialized:false,
+  })
+);
+
+
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
+
+function protected(req,res,next){
+  return function(protected){
+db('users').select('username')
+.then(username=>{
+  if(username===req.session.username){
+    next();
+
+  } else{
+    return res.status(401).json({error: 'Incorrect Credentials'})
+  }
+});
+.catch(err=>{
+  res.send(err)
+})
+}
+
+
+
+
 
 server.get('/',(req,res)=>{
   res.send('active')
@@ -25,6 +60,7 @@ credentials.password = hash;
 db('users').insert(credentials)
 .then(ids=>{
   const id = id[0];
+//  req.session.username = user.username
   res.status(201).json({newUserId:id})
 })
 .catch(err=>{
@@ -33,6 +69,7 @@ db('users').insert(credentials)
 });
 
 server.get('/api/users',(req,res)=>{
+
   db('users')
   .select('id','username', 'password')
   .then(users =>{
@@ -48,6 +85,7 @@ server.post('/login', (req,res)=>{
   db('users').where({username: credentials.username}).first()
   .then(users => {
     if (users&&bcrypt.compareSync(credentials.password, users.password)){
+  req.session.username = users.username
       res.status(200).json({welcome: users.username})
     } else {
       res.status(401).json({
