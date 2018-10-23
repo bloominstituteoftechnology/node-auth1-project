329ef9bab144b311b7da5d47606e853b062c5150
Joseph Chretien-Golden <joechre@gmail.com>
2018-10-23 17:25:37 -0500

    LOGIN/OUT - add login and out

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const helmet = require('helmet')
-
+const cors = require('cors')
 const knex = require('knex')
 const knexConfig = require('./knexfile.js')
 const session = require('express-session')
@@ -11,11 +11,71 @@ const server = express()
 
 server.use(express.json())
 server.use(helmet())
-
 server.use(session({
-    name: 'panda', // default is connect.sid
+    name: 'panda', 
     secret: 'bamboo rules',
-    cookie: {
-        maxAge: 1 * 24 * 60 * 60 * 1000,
-        secure: false, // only set cookies over https. Server will not send back a cookie over http.
+    cookie: { maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: false, 
       },
+      store: new KnexSessionStore({
+        tablename: 'sessions',
+        sidfield: 'sid',
+        knex: db,
+        createtable: true,
+        clearInterval: 1000 * 60 * 60 // removes only expired sessions
+      }),
+      httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+server.route('/api/setname')
+  .get((req, res) => {
+    req.session.username = 'Kitty';
+    res.send(`Set username to ${req.session.username}`);
+  })
+
+server.route('/api/getname')
+  .get((req, res) => {
+    res.send(`Hey ${req.session.username}, good to see you!`);
+  })
+
+server.route('/api/register')
+  .post((req, res) => {
+    const credentials = req.body;
+    const hash = bcrypt.hashSync(credentials.password, 14);
+    credentials.password = hash;
+    db('users')
+      .insert(credentials)
+      .then(ids => {
+        const id = ids[0]
+        req.session.username = credentials.username
+        return res.status(201).json({ newUserId: id });
+      })
+      .catch(err => res.status(500).json(err));
+  })
+
+server.route('/api/login')
+  .post((req, res) => {
+    const credentials = req.body
+    db('users')
+      .where({ username: credentials.username }).first()
+      .then(user => {
+        if (user && bcrypt.compareSync(credentials.password, user.password)) {
+          req.session.username = user.username
+          return res.status(200).json({ message: `Welcome ${user.username}!` })
+        }
+        return res.status(401).json({ message: 'You shall not pass!' });
+      })
+      .catch(err => res.status(500).json({ err }));
+  });
+  server.route('/api/logout')
+  .get((req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) return res.send('Error logging out!');
+        return res.send('Good bye!');
+      })
+    }
+  })
