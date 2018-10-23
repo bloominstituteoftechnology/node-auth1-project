befaf8a31c7c47ed8bec91e4acf8fd649b30316b
MichelleOk <umbralcompendium@gmail.com>
2018-10-23 16:33:32 -0600

    dependencies set up, still testing cookies

--- index.js
+++ index.js
@@ -3,18 +3,44 @@ console.log('index.js says hi....hi!');
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs'); //bring in the bcryptjs
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = require('./data/dbConfiguration.js');
 
 const server = express();
 
+//setting up the Cookies
+const sessionConfig = {
+  secret: 'bananas.are.gross',
+  name: 'monkeybutts',
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    maxAge: 1000 * 60 * 1 //cookie timer
+  },
+  //need to add store!!!!!!!!!!!
+  store: new KnexSessionStore({
+    tablename: 'session',
+    sidfieldname: 'sid',
+    knex: db,
+    createtable: true,
+    clearInterval: 100 * 60 * 60,
+  }),
+};
+
+server.use(session(sessionConfig));
 server.use(express.json());
 server.use(cors());
 
+//server tester message
 server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
 
+//MVP Login/Register
 server.post('/register', (req, res) => { 
   const credentials = req.body;
 
@@ -33,8 +59,11 @@ server.post('/register', (req, res) => {
 server.post('/login', (req, res) => {
   const credentials = req.body;
   db('users').where({username: credentials.username}).first().then(user => {
-//compareSync is how we figure that out, compares the given password and the actual pasword
+//compareSync is how we figure that out, compares the given password and the 
+//actual password
 if(user && bcrypt.compareSync(credentials.password, user.password)) {
+  //checking session username matches given
+  req.session.username = user.username
   res.status(200).json({ welcome: user.username })
     } else {
       res.status(401).json({ message: "Entry Denied!" })
@@ -43,13 +72,34 @@ if(user && bcrypt.compareSync(credentials.password, user.password)) {
   .catch(err => res.status(500).json({ err }));
 });
 
-server.get('/users', (req, res) => {
+//grab all users
+server.get('/users', protected, (req, res) => {
   db('users')
     .select('id', 'username', 'password')// we normally wouldn't have it return the password
     .then(users => {
-      res.json(users);
+      res.json({ userId: req.session.userId, users });
     })
     .catch(err => res.send(err));
 });
+
+server.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send("Nope! You can't leave ever! Mwhahaahha!");
+      } else {
+        res.send("I guess you can leave if you want...")
+      }
+    });
+  }
+});
+
+function protected(req, res, next) {
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.status(401).json({ message: "Not Authorized"});  
+  }
+}
 
 server.listen(9000, () => console.log('\n API running mad circles on port 9000\n'));
