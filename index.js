e4e55403a6446d8886460aa98a93df2e8a5c7e76
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-21 08:16:12 -0600

    created hashed registered account

--- index.js
+++ index.js
@@ -10,4 +10,30 @@ server.get('/', (req, res) => {
     res.send('im running')
 })
 
+server.post('/api/register', (req, res) => {
+    const credentials = req.body
+
+    const hash = bcrypt.hashSync(credentials.password, 14)
+    credentials.password = hash;
+
+    db('users')
+        .insert(credentials)
+        .then(ids => {
+            const id = ids[0]
+            res.status(201).json({ userId: id })
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error registering to the server' })
+        })
+})
+
+server.get('/api/users', (req, res) => {
+    db('users')
+      .select('id', 'username', 'password')
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
+
 server.listen(9000, () => console.log('server is running'))
