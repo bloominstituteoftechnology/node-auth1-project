f819091d91ff7c78501076c4603fa077770ca0c1
unknown <brandongrych@gmail.com>
2018-11-26 15:07:43 -0600

    added get for users, working

--- index.js
+++ index.js
@@ -30,6 +30,17 @@ server.post('/register', (req, res) => {
     });
 });
 
+server.get("/users", (req, res) => {
+    db('users')
+    .select('id', 'username')
+    .then(users => {
+        res.json(users);
+    })
+    .catch(err => res.send (err));
+        
+});
+
+
 const port = 9000;
 server.listen(port, function() {
 console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
