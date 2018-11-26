0aa8b84488999ee9e30b8691dd9a4590983ff0e8
kyranmccann <kyran.mccann@gmail.com>
2018-11-26 11:27:54 -0800

    day one mvp

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const bcrypt = require('bcryptjs');
+
+const db = require('./database/dbConfig.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+//sanity check
+server.get('/', (req, res) => {
+  res.send({ message: 'it is alive' });
+});
+
+//register new users
+server.post('/api/register', (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+  db('users').insert(creds).then(id => res.status(201).json(id)).catch(err => res.json(err))
+})
+
+//login existing users
+server.post('/api/login', (req, res) => {
+  const creds = req.body;
+  db('users').where({ username: creds.username }).first().then(user => {
+    if (user && bcrypt.compareSync(creds.password, user.password)){
+      //passwords match and correct username
+      res.status(200).json({ message: 'welcome to the thunderdome!' });
+    } else {
+      //messed up
+      res.status(401).json({ message: 'you shall not pass'})
+    }
+  }).catch(err => res.json(err))
+})
+
+//list of users if logged in
+server.get('/api/users', (req, res) => {
+  db('users')
+    .select('id', 'username')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.json(err)); 
+})
+
+server.listen(9000, () => {
+  console.log('\nrunning on port 9000\n');
+})
