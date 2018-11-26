65e757d1835d954aeaafc7c0188fc508843384a9
Chad Jemmett <chad.jemmett@gmail.com>
2018-11-26 15:32:31 -0700

    Can check for user password

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const knexConfig = require('./knexfile.js')
 const db = knex(knexConfig.development)
 const bcrypt = require('bcryptjs')
 const server = express();
-
+// don't forget this in the future
 server.use(express.json())
 
 server.get('/', (req, res) => {
@@ -25,9 +25,19 @@ server.post('/api/users', (req, res) => {
   db('users').insert(creds).then(ids => {
     res.status(201).json(ids)
   }).catch(err => res.json(err))
-
-
 });
+
+server.post('/api/login', (req, res) => {
+  const creds = req.body
+  db('users').where({username: creds.username}).first()
+  .then(user => {
+    if(user && bcrypt.compareSync(creds.password, user.password)) {
+      res.status(200).json({message: 'welcome'})
+    } else {
+      res.status(401).json({message: 'you shall not pass'})
+    }
+  })
+})
 
 const port = 3300;
 server.listen(port, function() {
