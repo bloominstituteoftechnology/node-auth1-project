34daa3bdb8d6a64105cc6d7a36a64f22e51129dc
Gannon <gkleblanc@gmail.com>
2018-11-26 16:27:24 -0500

    added endpoints in index.js and fixed knexfile

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = require('./database/dbConfig')
 
 //set up server with dependencies
 const server = express();
-server.use(express.json);
+server.use(express.json());
 server.use(cors());
 server.use(morgan());
 server.use(helmet());
@@ -18,6 +18,59 @@ server.get('/', (req, res) => {
     console.log('testing to see if this works')
     res.send({ message: 'do not forget to add the correct url info' })
 })
+
+
+//endpoints
+
+
+//register new users
+server.post('/api/register', (req, res) => {
+    // grab username and password from body
+    const creds = req.body;
+    // generate the hash from the user's password
+    const hash = bcrypt.hashSync(creds.password, 8);
+    // override the user.password with the hash
+    creds.password = hash;
+    // save the user to the database
+    db('users')
+        .insert(creds)
+        .then(id => res.status(201).json(id))
+        .catch(err => res.status(500).json(err))
+})
+
+//login 
+server.post('/api/login', (req, res) => {
+    // grab username and password from body
+    const creds = req.body;
+
+    db('users')
+        .where({ username: creds.username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(creds.password, user.password)) {
+                //passwords match and has correct username too
+                res.status(200).json({ message: 'Welcome to Asgard mortal' })
+            } else {
+                //they don't match
+                res.status(401).json({ message: 'YOU SHALL NOT PASS!' })
+            }
+        })
+        .catch(err => res.status(500).json(error))
+})
+
+
+//List of users
+server.get('/api/users', (req, res) => {
+    db('users')
+        .select('id', 'username')
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.json(err));
+})
+
+
+//port
 
 server.listen(9000, () => {
     console.log('\nrunning on port 9000\n');
--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,14 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: { filename: './database/users.sqlite3' },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
