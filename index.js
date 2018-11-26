b3755a1b3fa8802f02a5d8553b1b60c5aed45e63
Daniel Ferrer <danfrrr13@gmail.com>
2018-11-26 14:25:15 -0500

    created register endpoint

--- index.js
+++ index.js
@@ -7,4 +7,22 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/api/register', (req, res) => {
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, 14);
+
+    creds.password = hash;
+
+    db('users')
+        .insert(creds)
+        .then(ids => {
+            res.status(201).json(ids);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Authentication error' });
+        });
+});
+
+
+
 server.listen(3000, () => console.log('\n Server is running on port 3000 \n' ));
