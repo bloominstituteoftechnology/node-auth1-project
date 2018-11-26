d0c10b629266d45d3267d514fdfd4a207d1f8872
David247365 <ds051128@gmail.com>
2018-11-26 15:52:30 -0600

    set up endpoint for registering users and GET request for users

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 const db = require("./database/dbConfig.js");
 
 const server = express();
 
+server.use(express.json());
+
 port = 3000;
 
 server.listen(port, () => console.log(`\n listening on port: ${port} \n`));
+
+// Get users
+
+server.get("/api/users", (req, res) => {
+  db("users")
+    .select("id", "username", "password")
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+// Post to users
+
+server.post("/api/register", (req, res) => {
+  // grab username and password from body
+  const creds = req.body;
+
+  // generate the hash  from user's passwords
+  const hash = bcrypt.hashSync(creds.password, 14);
+
+  // override the user.password with the hash
+  creds.password = hash;
+
+  // save the user to the database
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
+});
