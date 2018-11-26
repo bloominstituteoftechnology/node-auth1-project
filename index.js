f638140dfc14cae4106c009789e69fc28149b784
Lee Len Formento <leebatef@gmail.com>
2018-11-26 11:45:12 -0500

    POST login

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.get('/', (req,res) => {
     res.send('I Am Alive!');
 })
 
-server.post('/register', (req, res) => {
+server.post('/api/register', (req, res) => {
     const credentials = req.body;
     // hash the password
     const hash = bcrypt.hashSync(credentials.password, 14) // 2^14 times
@@ -27,7 +27,23 @@ server.post('/register', (req, res) => {
     .catch(err => {
       res.status(500).json(err)
     })
+  })
+
+server.post('/login', (req, res) => {
+    const creds = req.body;
+    db('users')
+    .where({ username: creds.username})
+    .first()
+    .then(user => {
+      // found user - right password or not (compare sync) -- compare to user password (hash same, found)
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        res.status(200).json({ welcome: user.username})
   
+      } else {
+        res.status(404).json({ message: 'You shall not pass!'})
+      }
+    })
+    .catch(err => res.status(500).json(err))
   })
 
 server.listen(3700, () => console.log('\n Party at part 3700 '))
