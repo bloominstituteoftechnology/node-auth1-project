b5adaf867d3fb7d445d16cce5772eb64d3d872ea
jsmnglng <jsmn.glng@gmail.com>
2018-11-26 13:10:45 -0800

    Created retrieving users endpoint; expected output: []

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
+const db = require('./database/dbConfig');
+
 const server = express();
 const port = 8080;
 
+// middleware
+server.use(express.json());
+
 // test api
 server.get('/', (_, res) => res.send('API is running...'));
+
+// ===================== ENDPOINTS =====================
+// retrieve users
+server.get('/api/users', (_, res) => {
+  db('users')
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 server.listen(port, () => console.log(`Listening to port: ${port}`));
