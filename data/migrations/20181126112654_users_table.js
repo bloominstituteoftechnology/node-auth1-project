89153058d46e1f9e96cedc46ba3848e1f8ed65b4
Lee Len Formento <leebatef@gmail.com>
2018-11-26 11:32:28 -0500

    Installed two bcrypts - bcryptjs is the correct one

--- data/migrations/20181126112654_users_table.js
+++ data/migrations/20181126112654_users_table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+	return knex.schema.createTable('users', function(tbl) {
+        tbl.increments();
+        
+		tbl
+			.string('username', 150)
+			.unique()
+            .notNullable();
+            
+		tbl.string('password').notNullable();
+	});
 };
-
-exports.down = function(knex, Promise) {
-  
+ exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('users');
 };
