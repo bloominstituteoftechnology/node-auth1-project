628c10f2dbf6e7b86622626ef274b903fa999741
Yanrong Xu <xuyanrong@icloud.com>
2018-11-26 16:11:25 -0500

    create a database file

--- database/migrations/20181126160528_users.js
+++ database/migrations/20181126160528_users.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('users', users => {
-        users.increment();
+        users.increments();
 
         users
             .string('username', 128)
