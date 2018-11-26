d21e967db1aac42a7658098932e1c08d5df69509
Michael Checo <checomichael2@gmail.com>
2018-11-26 15:01:17 -0500

    register users endpoint

--- index.js
+++ index.js
@@ -10,5 +10,20 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Test')
 })
+
+app.post('/api/register', (req, res) => {
+ const creds = req.body;
+
+ const hash = bcrypt.hashSync(creds.password, 16)
+
+ creds.password = hash;
+
+ db('users').insert(creds).then(ids => {
+     res.status(201).json(ids)
+ })
+ .catch(err => json(err))
+
+})
+
 const PORT = 3500
 app.listen(PORT, console.log(`==^_^== ${PORT} ==^_^==`));
