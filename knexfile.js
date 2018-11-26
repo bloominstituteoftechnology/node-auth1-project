3fe9eed11f98366f42668c7632ced7310a898fc6
Peter Pham <phamhere@gmail.com>
2018-11-26 15:25:28 -0700

    added knexfile

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/lambda.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+};
