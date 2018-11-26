1de6e4bfb38bacd57d6c4d31c3171a81acc6e90c
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-11-26 15:07:24 -0800

    post - login and register

--- data/seeds/users.js
+++ data/seeds/users.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex("users").insert([
-        {username: "user1", password: "123abc"},
-        {username: "user2", password: "1234abcd"},
-        {username: "user3", password: "12345abcde"}
+        {id: 1, username: "user1", password: "123abc"},
+        {id: 2, username: "user2", password: "1234abcd"},
+        {id: 3, username: "user3", password: "12345abcde"}
       ]);
     });
 };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const knex = require("knex");
 const bcrpyt = require("bcrypt");
+const knex = require("knex");
+const cors = require("cors");
 
 const knexConfig = require("./knexfile.js");
 const db = knex(knexConfig.development);
@@ -8,5 +9,37 @@ const server = express();
 const PORT = 3500;
 
 server.use(express.json());
+server.use(cors());
+
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+  console.log(creds);
+  db("users")
+    .where({username: creds.username})
+    .first()
+    .then(user => {
+      console.log("user", user);
+      // console.log(bcrpyt.compareSync(creds.password, user.password));
+      if (user && bcrpyt.compareSync(creds.password, user.password)) {
+        res.status(200).json({message: "welcome!"});
+      } else {
+        res.status(401).json({message: "access denied..."});
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post("/api/register", (req, res) => {
+  const creds = req.body;
+
+  const hash = bcrpyt.hashSync(creds.password, 8);
+
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => res.status(201).json(ids))
+    .catch(err => res.status(500).json(err));
+});
 
 server.listen(PORT, () => console.log(`PORT ${PORT}`));
