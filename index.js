844eb1cafd63cb5b797f746abfd3ab32c734449d
Hunter315 <hunter.mckay.smith@gmail.com>
2018-11-26 14:48:03 -0700

    configured database for autincrement ID, added Login, Register, and Get Users requests

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
-const knex = require('knex');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const express = require('express');
 const bcrypt = require('bcryptjs');
 
-
+const db = require('./database/dbConfig.js');
 const server = express();
 
 server.use(express.json());
+server.use(morgan());
+server.use(helmet());
 
 
+server.get('/', (req, res) => {
+    res.send('My server can respond');
+});
 
 
+//==========CREATE A NEW USER(REGISTER)=========
+server.post('/api/register', (req, res) => {
+    //grabs username and password from body
+        const creds = req.body;
 
+    // generate hash from the user's password
+        const hash = bcrypt.hashSync(creds.password, 14);
 
+    //override the user.password with the hash
+        creds.password = hash;
 
+    //save the user to the database
+        db('users').insert(creds)
+        .then(ids => {
+        res.status(201).json(ids);
+        })
+        .catch(err => res.status(401).json(err));
+});
+
+//==========LOGIN A USER==========
+server.post('/api/login', (req, res) => {
+    //grabs username and password from body
+        const creds = req.body;
+    db('users').where({ username: creds.username}).first().then(user => {
+        if(user && bcrypt.compareSync(creds.password, user.password)) {
+        //paswords match and username exists
+        res.status(200).json({message: 'Welcome!'});
+    
+    
+        } else {
+        res.status(401).json({message: 'You will not get this info'})
+        }
+        
+    }).catch(err => res.json(err))
+});
+
+//==========GET USER INFO==========
+server.get('/api/users', (req, res) => {
+    db('users')
+      .select('id', 'username')
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+});
+ 
 
 server.listen(8000, () => console.log('\n====Server running on port 8000====\n'));
