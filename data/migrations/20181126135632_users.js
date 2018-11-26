8a7018bb88998dbfaa18c7745b4a9d90a3f872a2
Brandon Vilaychith <blvilaychith@outlook.com>
2018-11-26 16:20:38 -0600

    Endpoints finished

---
+++ data/migrations/20181126135632_users.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments();
+
+    tbl
+      .string('username')
+      .unique()
+      .notNullable();
+
+    tbl.string('password', 128).notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
