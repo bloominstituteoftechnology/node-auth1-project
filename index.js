d472df920885e7e314572487c687e1d743f2d923
koennecker <koenneckerb@gmail.com>
2018-11-26 14:55:13 -0700

    Create database and API endpoints.

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const db = require("./database/dbConfig.js");
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        res.status(200).json({ message: "Login successful." });
+      } else {
+        res.status(401).json({ message: "Invalid username or password." });
+      }
+    })
+    .catch(err => res.json(err));
+});
+
+server.post("/api/register", (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 4);
+  creds.password = hash;
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
+});
+
+server.get("/api/users", (req, res) => {
+  db("users")
+    .select("id", "username", "password")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+server.get("/", (req, res) => {
+  res.send("Its Alive!");
+});
+
+server.listen(3300, () => console.log("\nrunning on port 3300\n"));
