8e352dcca00b5e3a8c22c3f3da926d63cf6f3f21
Chad Jemmett <chad.jemmett@gmail.com>
2018-11-26 15:00:33 -0700

    can hash a password

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
-// const knex = require('knex')
-// const knexConfig = require('./knexfile.js')
-// const db = knex(knexConfig.development)
+const knex = require('knex')
+const knexConfig = require('./knexfile.js')
+const db = knex(knexConfig.development)
+const bcrypt = require('bcryptjs')
 const server = express();
+
+server.use(express.json())
 
 server.get('/', (req, res) => {
   res.send("hello")
 })
+
+server.get('/api/users', (req, res) => {
+  db('users')
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json(err))
+})
+
+
+server.post('/api/users', (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 2)
+  creds.password = hash;
+  db('users').insert(creds).then(ids => {
+    res.status(201).json(ids)
+  }).catch(err => res.json(err))
+
+
+});
 
 const port = 3300;
 server.listen(port, function() {
