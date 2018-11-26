e9cdb1f570d982048fb1e9c2e4f1f46e7d6ed165
jsmnglng <jsmn.glng@gmail.com>
2018-11-26 13:36:19 -0800

    Created register a user endpoint

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig');
 
@@ -15,8 +16,32 @@ server.get('/', (_, res) => res.send('API is running...'));
 // retrieve users
 server.get('/api/users', (_, res) => {
   db('users')
+    .select('id', 'username') // exclude password col
     .then(users => {
       res.status(200).json(users);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// register a new user
+server.post('/api/register', (req, res) => {
+  // initial value: {}
+  // will populate it with the appropriate props when creating a new user
+  // props: username, password
+  console.log(req.body)
+  const creds = req.body;
+
+  // generates a hash for the user's password
+  const hash = bcrypt.hashSync(creds.password, 8); // rounds to 2^X; in this case, X is 8
+
+  // overrides the password prop with the hash
+  creds.password = hash;
+
+  // save the new user to the database
+  db('users')
+    .insert(creds)
+    .then(id => {
+      res.status(201).json({ message: `ID ${id} created` });
     })
     .catch(err => res.status(500).json(err));
 });
