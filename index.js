ab5d5f0a6cf46776d9afd78671971004c7169fe1
SavannahGreuel <smallidgesavannah@gmail.com>
2018-11-26 15:32:37 -0700

    added login endpoint

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
-const cors = require('cors');
+
 
 const bcrypt = require('bcryptjs'); // 
 const db = require('./database/dbConfig.js');
 
 const server = express();
 server.use(express.json());
-server.use(cors());
 
+
+server.post('/api/login', (req,res) => {
+    const creds = req.body;
+
+    db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        // passwords match and user exists by that username
+        res.status(200).json({ message: 'you have logged in successfuly' });
+      } else {
+        // either username is invalid or password is wrong
+        res.status(401).json({ message: 'You have entered the wrong login credentials' });
+      }
+    })
+    .catch(err => res.json(err));
+});
 
 server.post('/api/register', (req,res) => {
     const creds = req.body;
@@ -27,7 +44,7 @@ server.get('/api/users', (req, res) => {
     db('users')
     .select('id','username','password')
     .then(users => {
-        res.status(200).json(users);
+        res.json(users);
     })
     .catch(error => res.status(500).json(error))
 })
@@ -35,5 +52,5 @@ server.get('/api/users', (req, res) => {
 server.get('/', (req, res) => {
     res.send('Its Alive!');
   });
-  
+
 server.listen(8000, () => console.log('\nrunning on port 8000\n'));
--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     useNullAsDefault: true, // used to avoid warning on console
     migrations: {
       directory: './database/migrations',
-      tableName: 'dbmigrations',
+      
     },
    
   },
