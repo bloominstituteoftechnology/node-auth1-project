569a3bd87f9f8efe9e396185eab14313366b3d2d
rytwalker <rytwalker@gmail.com>
2018-11-26 15:24:25 -0500

    Added register endpoint

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 
+const db = require('./data/dbConfig');
+
 const server = express();
+
+server.use(express.json());
+// REGISTER
+server.post('/api/register', async (req, res) => {
+  // get form input values
+  const registrationData = req.body;
+  // generate a hash
+  const hash = bcrypt.hashSync(registrationData.password, 10);
+  // replace plain text with hash
+  registrationData.password = hash;
+  // save to db
+  try {
+    const newUserId = await db('users').insert(registrationData);
+    res.status(201).json(newUserId);
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong with the request.' });
+  }
+});
 
 server.get('/', (req, res) => {
   res.send('Helllo there.');
--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,7 @@ module.exports = {
     connection: {
       filename: './data/auth-users.sqlite3'
     },
+    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
     }
