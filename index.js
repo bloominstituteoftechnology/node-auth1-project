989b79ff0e9225893b82a24cecb6c63873e095d2
Katie Cleary <katiecleary@airmaster.lan>
2018-11-26 17:07:40 -0500

    tweaked/updated code and tested API for MVP ability. MVP as described, is reached. Next, adding some conditionals for input errors or users who already exsist

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const db = require('./database/dbConfig.js');
 const server = express();
 server.use(express.json());
 
+server.post('/api/register', (req, res) => {
+    // grab username and password from body
+    const creds = req.body;
+    // generate the hash from the user's password
+    const hash = bcrypt.hashSync(creds.password, 4); // rounds is 2^X
+    // override the user.password with the hash
+    creds.password = hash;
+    // save the user to the database
+    db('users')
+      .insert(creds)
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => {
+        console.log(err)
+         res.json(err);
+  });
+});
+
 server.post('/api/login', (req, res) => {
     // grab username and password from body
     const creds = req.body;
@@ -24,38 +43,6 @@ server.post('/api/login', (req, res) => {
       .catch(err => res.json(err));
   });
   
-  server.post('/api/register', (req, res) => {
-    // grab username and password from body
-    const creds = req.body;
-  
-    // generate the hash from the user's password
-    const hash = bcrypt.hashSync(creds.password, 4); // rounds is 2^X
-  
-    // override the user.password with the hash
-    creds.password = hash;
-  
-    // save the user to the database
-    db('users')
-      .insert(creds)
-      .then(ids => {
-        res.status(201).json(ids);
-      })
-      .catch(err => json(err));
-  });
-  
-  // protect this route, only authenticated users should see it
-  server.get('/api/users', (req, res) => {
-    db('users')
-      .select('id', 'username', 'password') // ***************************** added password to the select
-      .then(users => {
-        res.json(users);
-      })
-      .catch(err => res.send(err));
-  });
-  
-  server.get('/', (req, res) => {
-    res.send('Its Alive!');
-  });
   
   server.listen(3300, () => console.log('\nrunning on port 3300\n'));
   
