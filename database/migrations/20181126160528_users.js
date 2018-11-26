33bbeeb2c5d412e280c412311978c8092cbdd5d1
Yanrong Xu <xuyanrong@icloud.com>
2018-11-26 16:10:16 -0500

    change package json to star nodemon, and change migration file

--- database/migrations/20181126160528_users.js
+++ database/migrations/20181126160528_users.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('users', users => {
+        users.increment();
+
+        users
+            .string('username', 128)
+            .notNullable()
+            .unique();
+        users.string('password', 128).notNullable()
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('users')
 };
