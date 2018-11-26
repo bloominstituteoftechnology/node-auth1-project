d56ff55c5e4895ce69aa598baba269ae975022b6
ssmodish <smodish@hotmail.com>
2018-11-26 16:48:15 -0500

    register users working

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.get('/', (req, res) => {
   res.send('API UP!');
 });
 
-// registered users
+// return registered users
 server.get('/api/users', (req, res) => {
   db('users')
     .select('id', 'username', 'password')
@@ -22,6 +22,20 @@ server.get('/api/users', (req, res) => {
       res.json(users);
     })
     .catch(err => res.send(err));
+});
+
+// register a user
+server.post('/api/register', (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
 });
 
 server.listen(8300, () => console.log('\nServer running on 8300\n'));
