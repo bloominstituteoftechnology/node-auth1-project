574d9c02b003c5f92148a6663c663152c690925e
Yanrong Xu <xuyanrong@icloud.com>
2018-11-26 16:28:55 -0500

    add line need for session and cookie

--- index.js
+++ index.js
@@ -9,10 +9,30 @@ const db = require('./database/dbConfig.js')
 
 const server = express();
 
+const sessionConfig = {
+    secret: 'animal kindom',
+    name: 'lambda_users',
+    httpOnly: true,
+    resave: false,
+    savaUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 1000*60*1
+    }
+}
+
+server.use(session(sessionConfig));
+
 server.use(express.json());
+server.use(cors())
 
 server.get('/', (req, res) => {
     res.send('Server is running')
+});
+
+
+server.get('/api/users', (req, res) => {
+    
 });
 
 
