3a34b528d95cc6ae2c9d3260ba2cea2ba4965622
KHollobaugh <khollobaugh@gmail.com>
2018-11-26 14:53:32 -0500

    login functionality working

--- index.js
+++ index.js
@@ -21,19 +21,30 @@ server.get('/api/users', (req, res) => {
 })
 
 server.post('/api/register', (req, res) => {
-
     const creds = req.body;
-
     const hash = bcrypt.hashSync(creds.password, 3);
-
     creds.password = hash;
-    
     db('users')
     .insert(creds)
     .then(ids => {
         res.status(201).json(ids);
     })
     .catch(err => res.status(500).json(err));
+})
+
+server.post('/api/login', (req, res) => {
+    const creds = req.body;
+    db('users')
+    .where({ username: creds.username })
+    .first()
+    .then( user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)){
+            res.status(200).json({ message: "welcome user!"})
+        } else {
+            res.status(401).json({ message: 'invalid username or password' })
+        }
+    })
+    .catch(err => res.json(err))
 })
 
 server.listen(3300, () => console.log('\nserver is running on port 3300\n'))
