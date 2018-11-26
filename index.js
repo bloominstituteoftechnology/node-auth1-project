2e71361aa134cf7ce50a8d5e2d9ae107a9b9674c
Steven O'Neill <oneillsteven957@gmail.com>
2018-11-26 15:35:15 -0600

    added the register endpoint and get users endpoint

--- index.js
+++ index.js
@@ -13,17 +13,32 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+server.post('/api/register', (req, res) => {
+    const cred = req.body;
+
+    const hash = bcrypt.hashSync(cred.password, 12);
+
+    cred.password = hash;
+
+    db('users')
+        .insert(cred)
+        .then(ids => {
+            res.status(201).json(ids)
+        })
+        .catch(err => err);
+})
+
 server.get('/', (req, res) => {
     res.send('running');
   });
   
-// server.get('/api/users', (req, res) => {
-//     db('users')
-//     .select('id', 'username')
-//     .then(users => {
-//         res.json(users);
-//     })
-//     .catch(err => res.send(err));
-//   });
+server.get('/api/users', (req, res) => {
+    db('users')
+    .select('id', 'username', 'password') // ****** added password to see if it worked
+    .then(users => {
+        res.json(users);
+    })
+    .catch(err => res.send(err));
+  });
   
   server.listen(8300, () => console.log('\nrunning on port 8300\n'));
