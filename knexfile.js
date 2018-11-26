ec8aacf6435948ffd6f73d1b4208494bfb08dcc9
Drewsup123c <drewsup123c@gmail.com>
2018-11-26 13:28:57 -0700

    deleted postegl from knexfile, added nodemon as dev dependency, and added server script to start nodemon

--- knexfile.js
+++ knexfile.js
@@ -8,37 +8,4 @@ module.exports = {
       filename: './dev.sqlite3'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
