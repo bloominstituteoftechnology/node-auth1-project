faaa711599a934b65ea47a663ac07dca90f1a4c2
theryanclausen <theryanclausen@gmail.com>
2018-11-26 15:34:40 -0700

    login

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 const port = 3334;
 
-const db = require('./config/dbConfig');
-const middleware = require('./config/middleware')
+const db = require("./config/dbConfig");
+const middleware = require("./config/middleware");
 const server = express();
 
 middleware(server);
 
-const sendError = (res, message = 'Server Error', code = 500)=>{
-    return res.status(code).json({error:message})
-}
+const sendError = (res, message = "Server Error", code = 500) => {
+  return res.status(code).json({ error: message });
+};
 
-server.get('/', (req,res) =>{
-    res.send('<h1>Built by Ryan Clausen</h1>')
-})
+server.get("/", (req, res) => {
+  res.send("<h1>Built by Ryan Clausen</h1>");
+});
 
-server.get('/api/users', async(req, res) =>{
-    const users = await db('users');
-    return res.status(200).json(users)
-})
+server.get("/api/users", async (req, res) => {
+  const users = await db("users");
+  return res.status(200).json(users);
+});
 
-server.post('/api/register', async(req,res) =>{
-    const credentials = req.body;
-    if (!credentials.username || !credentials.password){
-        return sendError(res, 'Username and password required!', 400)
-    }
-    const hash = await bcrypt.hash(credentials.password,14);
-    credentials.password = hash;
-    try{
-    const id = await db('users').insert(credentials)
-    return res.status(201).json(id)
+server.post("/api/register", async (req, res) => {
+  const credentials = req.body;
+  if (!credentials.username || !credentials.password) {
+    return sendError(res, "Username and password required!", 400);
+  }
+  const hash = await bcrypt.hash(credentials.password, 14);
+  credentials.password = hash;
+  try {
+    const id = await db("users").insert(credentials);
+    return res.status(201).json(id);
+  } catch (err) {
+    sendError(res, err);
+  }
+});
+
+server.post("/api/login", async (req, res) => {
+  const credentials = req.body;
+  if (!credentials.username || !credentials.password) {
+    return sendError(res, "Username and password required!", 400);
+  }
+  try{
+  const user = await db('users').where({username:credentials.username}).first();
+  if (user && bcrypt.compareSync(credentials.password, user.password)){
+    return res.status(200).json({message:'You did good'})
+  }
+  else{
+      return sendError(res, 'get outta here', 401)
+  }
 }catch(err){
-    sendError(res,err)
+    return sendError(res)
 }
-})
+});
 
-server.listen(port, ()=> console.log(`We hear you ${port}`))
+server.listen(port, () => console.log(`We hear you ${port}`));
