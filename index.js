4b0143ac45e837b4baaaebb3bc59797bdb83bbcd
Yanrong Xu <xuyanrong@icloud.com>
2018-11-26 16:38:11 -0500

    add register endpoint

--- index.js
+++ index.js
@@ -44,5 +44,21 @@ server.get('/api/users', (req, res) => {
     }
 });
 
+server.post('/api/register', (req, res) => {
+    const credentials = req.body;
+
+    const hash = bcrypt.hashSync(credentials.password, 14)
+    credentials.password = hash;
+
+    db('users')
+        .insert(credentials)
+        .then(ids => {
+            const id = ids[0];
+            res.status(201).json({newUserId: id})
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
 
 server.listen(9000, () => console.log('\nRunning on port 9000'));
