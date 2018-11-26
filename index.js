bd76cf197ec82762dd966474fb194cd725319c3a
adamhinckley <adamhinckley@mac.com>
2018-11-26 14:50:27 -0700

    MVP done

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
 const express = require("express");
-const server = express();
 const bcrypt = require("bcryptjs");
 const db = require("./database/dbConfig");
 const cors = require("cors");
 
+const server = express();
 server.use(express.json());
 server.use(cors());
 
+server.post("/api/register", (req, res) => {
+  // grab username and password from body
+  const creds = req.body;
+
+  // generate the hash from the user's password
+  const hash = bcrypt.hashSync(creds.password, 1); // rounds is 2^X
+
+  // override the user.password with the hash
+  creds.password = hash;
+
+  // save the user to the database
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 server.post("/api/login", (req, res) => {
-  //get username and password from body
-  const creds = res.body;
+  // grab username and password from body
+  const creds = req.body;
 
   db("users")
     .where({ username: creds.username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
-        //passwords match and user exists
-        res.status(200).json({ message: "welcome" });
+        // passwords match and user exists by that username
+        res.status(200).json({ message: "welcome!" });
       } else {
-        res
-          .status(401)
-          .json({ message: "invalid username or password" })
-          .catch(err => json(err));
+        // either username is invalid or password is wrong
+        res.status(401).json({ message: "you shall not pass!!" });
       }
+    })
+    .catch(err => {
+      res.status(500).json(err);
     });
 });
 
-server.listen(4500, () => console.log("\n== Port 4500 ==\n"));
+server.get("/api/users", (req, res) => {
+  db("users")
+    .select("id", "username", "password") // ***************************** added password to the select
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+server.listen(4500, () => console.log("\n== Port 5k ==\n"));
