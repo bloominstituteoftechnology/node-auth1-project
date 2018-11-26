1bcfb77b31b3fe93ca72fd421d35147dcb5b91c7
KHollobaugh <khollobaugh@gmail.com>
2018-11-26 14:47:55 -0500

    register functionality working

--- database/migrations/20181126141852_users.js
+++ database/migrations/20181126141852_users.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', users => {
+      users.increments();
+      users.string('username', 100).notNullable().unique();
+      users.string('password', 100).notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('users');
 };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./database/dbConfig');
+const bcrypt = require('bcryptjs');
 const server = express();
 
 server.use(express.json());
@@ -17,6 +18,22 @@ server.get('/api/users', (req, res) => {
         res.json(users);
     })
     .catch(err => res.send(err))
+})
+
+server.post('/api/register', (req, res) => {
+
+    const creds = req.body;
+
+    const hash = bcrypt.hashSync(creds.password, 3);
+
+    creds.password = hash;
+    
+    db('users')
+    .insert(creds)
+    .then(ids => {
+        res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json(err));
 })
 
 server.listen(3300, () => console.log('\nserver is running on port 3300\n'))
