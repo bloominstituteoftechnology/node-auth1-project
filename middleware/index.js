d721ff633e05a1b86afbdc428f96ecc09759adcf
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-11-27 10:16:37 -0800

    tacos session

--- middleware/index.js
+++ middleware/index.js
@@ -1,24 +1,27 @@
 require('dotenv').config()
-
+//* Third-party middleware
 const express = require('express')
 const logger = require('morgan')
 const helmet = require('helmet')
+const cors = require('cors')
 const session = require('express-session')
 
 module.exports = server => {
   server.use(express.json())
   server.use(logger('dev'))
   server.use(helmet())
+  server.use(cors())
 
   //* memory store
   server.use(
     session({
+      name: 'tacos',
       secret: process.env.SECRET,
       httpOnly: true,
       resave: false,
       saveUninitialized: false,
       cookie: {
-        maxAge: 24 * 60 * 60 * 1000
+        maxAge: 5 * 1000
       }
     })
   )
