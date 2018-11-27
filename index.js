65cdc8ce4ec66cc32e3294870a77231c3c0d4aef
Hunter315 <hunter.mckay.smith@gmail.com>
2018-11-27 14:38:02 -0700

    added protected middleware

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
+
 
 const db = require('./database/dbConfig.js');
 const server = express();
 
+const sessionConfig = {
+    name: 'Monster',
+    secret: 'hunter',
+    cookie: {
+      maxAge: 1000 * 60 * 10, // 10 minutes
+      secure: false // only set it over https; in production you want this true
+    },
+    httpOnly: true, //no js can touch this cookie
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+      tablename: 'sessions',
+      sidfieldname: 'sid',
+      knex: db,
+      createtable: true,
+      clearInterval: 1000 * 60 * 60
+    })
+}
+
+  server.use(session(sessionConfig));
 server.use(express.json());
 server.use(morgan());
 server.use(helmet());
@@ -39,28 +63,49 @@ server.post('/api/register', (req, res) => {
 server.post('/api/login', (req, res) => {
     //grabs username and password from body
         const creds = req.body;
-    db('users').where({ username: creds.username}).first().then(user => {
-        if(user && bcrypt.compareSync(creds.password, user.password)) {
-        //paswords match and username exists
-        res.status(200).json({message: 'Logged in'});
+    db('users').where({ username: creds.username})
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(creds.password, user.password)) {
+                req.session.userId = user.id;
+         //paswords match and username exists
+            res.status(200).json({message: 'Logged in'});
     
     
         } else {
-        res.status(401).json({message: 'You shall not pass!'})
+            res.status(401).json({message: 'You shall not pass!'})
         }
         
     }).catch(err => res.status(401).json({message: "You shall not pass!"}, err))
 });
 
+function protected(req, res, next) {
+    if (req.session && req.session.user) {
+        next();
+    } else {
+        res.status(401).json({message: "You shall not pass!"})
+    }
+}
 //==========GET USER INFO==========
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected,  (req, res) => {
     db('users')
-      .select('id', 'username')
+      .select('id', 'username') //to get password add ", 'password' "
       .then(users => {
         res.json(users);
       })
       .catch(err => res.status(401).json({message: 'You shall not pass!'}));
 });
  
+server.get('/api/logout', (req, res) => {
+    if(req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.send('you can never leave');
+        } else {
+          res.send('bye');
+        }
+      })
+    }
+  })
 
 server.listen(8000, () => console.log('\n====Server running on port 8000====\n'));
