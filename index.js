4e9b1477ff3c15f5da076cc33f0d620a629abe8e
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-11-27 12:37:44 -0800

    added dependencies and setup coockie config

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 const express = require("express");
 const knex = require("knex");
+const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const knexConfig = require("./knexfile.js");
-
 const db = knex(knexConfig.development);
 
 const server = express();
+
+const sessionConfig = {
+    name: 'authCookie',
+    secret: 'abcdthgsaqw23rrsefsd54tdsfsf3rghjb;desf',
+    cookie: {
+        maxAge: 1000 * 60 * 10,
+        secure: false,
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+        tablename: 'sessions',
+        sidfieldname: 'sid',
+        knex: db,
+        createable: true,
+        clearInterval: 1000 * 60 * 60,
+    }),
+};
+
+server.use(session(sessionConfig));
 server.use(express.json());
+server.use(cors());
+
+function protected(req, res, next) {
+    if(req.session && req.session.user) {
+        next();
+    } else {
+        res.status(401).json({ message: 'Access Denied...' });
+    }
+}
 
 // get all users
-server.get('/api/users', (req, res) => {
-    db('users').select('id', 'username', 'password').then(users => {
+server.get('/api/users', protected, (req, res) => {
+    db('users').select('id', 'username').then(users => {
         res.json(users);
-    }).catch(err => res.send(err));
+    }).catch(err => res.status(400).json(err));
 });
 
 // register user
@@ -24,7 +56,7 @@ server.post('/api/register', (req, res) => {
 
     db('users').insert(creds).then(ids => {
         res.status(201).json(ids);
-    }).catch(err => json(err));
+    }).catch(err => res.status(400).json(err));
 })
 
 // login user
@@ -37,7 +69,7 @@ server.post('/api/login', (req, res) => {
         } else {
             res.status(401).json({ message: 'Wrong username or password. Try again...' });
         }
-    }).catch(err => res.json(err));
+    }).catch(err => res.status(400).json(err));
 });
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
