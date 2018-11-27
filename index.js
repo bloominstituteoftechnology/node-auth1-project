c32846f8daf956f6ae99a5104ed88b20a709a02d
ssmodish <smodish@hotmail.com>
2018-11-27 17:15:42 -0500

    Tuesday MVP complete, stretching

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'apeapi',
+  secret: 'this is bananas',
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+  },
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    tablename: 'kong',
+    sidfieldname: 'donkey',
+    knex: db,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+};
+
+server.use(session(sessionConfig));
 server.use(express.json());
 server.use(cors());
 
@@ -16,12 +38,16 @@ server.get('/', (req, res) => {
 
 // return registered users
 server.get('/api/users', (req, res) => {
-  db('users')
-    .select('id', 'username', 'password')
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+  if(req.session && req.session.user){
+    db('users')
+      .select('id', 'username', 'password')
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  } else {
+    res.status(401).json({ you: 'shall not pass!!' });
+  }
 });
 
 // register a user
@@ -47,11 +73,30 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
       if(user && bcrypt.compareSync(creds.password, user.password)) {
+        req.session.user = user.id;
         res.status(200).json({ message: 'logged in' });
       } else {
         res.status(401).json({ message: 'login failed' });
       }
     }).catch(err => res.json(err));
 });
+
+// logout - destroys session
+server.get('/api/logout', (req,res) => {
+  if(req.session) {
+    req.session.destroy(err => {
+      if(err) {
+        res.send('Welcome to the Hotel California!');
+      } else {
+        res.send('l8r');
+      }
+    });
+  } else {
+    res.end();
+  }
+});
+
+// protected middleware
+// function protected = 
 
 server.listen(8300, () => console.log('\nServer running on 8300\n'));
