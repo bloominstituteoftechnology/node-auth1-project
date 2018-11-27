af68891065ce8f283a317c93c4232c6b25333413
txiong000 <tkx422@gmail.com>
2018-11-27 17:29:02 -0600

    day 2 mvp finish, session,cookie

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = require('./database/dbConfig.js');
 
 const server = express();
 
+const sessionConfig = {
+    name: 'lambda',
+    secret: '0jhda978dfv78fdva67nbgv78ngf76a56',
+    cookie: {
+        maxAge: 1000 * 60 * 10,
+        secure: false,
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+        tablename: 'sessions',
+        sidfieldname: 'sid',
+        knex: db,
+        createtable: true,
+        clearInterval: 1000 * 60 * 60,
+    }),
+}
+
+server.use(session(sessionConfig));
 server.use(express.json());
 server.use(cors());
 
@@ -13,20 +35,62 @@ server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
 
-server.get('/api/users', (req, res) => {
-    db('users')
-      .select('id', 'username')
-      .then(users => {
-        res.json(users);
-      })
-      .catch(err => res.send(err));
+function protected(req, res, next) {
+    if(req.session && req.session.userId) {
+        next();
+
+    } else {
+        res.status(401).json({message: 'you shall not pass!'})
+    }
+
+}
+
+
+
+server.get('/api/me', protected, (req, res) => {
+    if(req.session && req.session.userId) {
+        // they are logged in, go ahead and provide access
+        db('users')
+        .where({ id: req.session.userId })
+        .first()
+            .select('id', 'username')
+            .then(users => {
+            res.json(users);
+            })
+            .catch(err => res.send(err));
+    } else {
+        // bounce them
+        res.status(401).json({message: 'you shall not pass!'})
+    }
+   
+  });
+
+  
+server.get('/api/users', protected, (req, res) => {
+    if(req.session && req.session.userId) {
+        // they are logged in, go ahead and provide access
+        db('users')
+            .select('id', 'username')
+            .then(users => {
+            res.json(users);
+            })
+            .catch(err => res.send(err));
+    } else {
+        // bounce them
+        res.status(401).json({message: 'you shall not pass!'})
+    }
+   
   });
 
 server.post('/api/login', (req, res) => {
     const creds = req.body;
 
-    db('users').where({ username: creds.username }).first().then(user =>{
+    db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user =>{
         if(user && bcrypt.compareSync(creds.password, user.password)) {
+            req.session.userId = user.id
             res.status(200).json({message: 'welcome!'})
         } else {
             res.status(401).json({ message: 'you shall not pass!'})
@@ -34,6 +98,19 @@ server.post('/api/login', (req, res) => {
 
     }).catch()
     
+})
+
+server.get('/api/logout', (req, res) => {
+    console.log(req.session)
+    if(req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.send('error')
+            } else {
+                res.send('logged out')
+            }
+        })
+    }
 })
 
 server.post('/api/register', (req, res) => {
