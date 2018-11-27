8ce916c33defdb1077c6abfb48c9a322070441f0
David247365 <ds051128@gmail.com>
2018-11-27 17:16:08 -0600

    Working on session and cookies middleware

--- index.js
+++ index.js
@@ -1,25 +1,77 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
 const db = require("./database/dbConfig.js");
 
 const server = express();
 
+// Remeber that this is a secret
+const sessionConfig = {
+  name: "monkey",
+  secret: "This is a unique secret",
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false // only set it over https; ...in production you want this to be true
+  },
+  httpOnly: true, // no js can touch this
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    table: "sessions",
+    sidfieldname: "sid",
+    knex: db,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
+
+server.use(session(sessionConfig)); // Wires up session management
 server.use(express.json());
 
-port = 3000;
+// Provide a configuration object and implemet it as a middleware
 
-server.listen(port, () => console.log(`\n listening on port: ${port} \n`));
+// Login
+
+server.post("/api/login", (req, res) => {
+  // grab username and password from body
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        //passwords match and user exists
+        req.session.userId = user.id;
+        res.status(200).json({ message: "Welcome!" });
+      } else {
+        //   either username or password is invalid
+        res.status(401).json({ message: "Username/Password does not exist" });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ message: "There was an error, please try again" })
+    );
+});
 
 // Get users
 
+// Protect this route and only allow authenticated users to see it
 server.get("/api/users", (req, res) => {
-  db("users")
-    .select("id", "username", "password")
-    .then(users => {
-      res.status(200).json(users);
-    })
-    .catch(err => res.send(err));
+  if (req.session && req.session.userId) {
+    // for every client that has successfully logged in to the client we are going to add in a session
+    // They are logged in
+    db("users")
+      .select("id", "username", "password")
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(err => res.send(err));
+  } else {
+    res.status(401).json({ message: "You shall not pass!" });
+  }
 });
 
 // Post to users
@@ -43,25 +95,6 @@ server.post("/api/register", (req, res) => {
     .catch(err => json(err));
 });
 
-// Login
+port = 3000;
 
-server.post("/api/login", (req, res) => {
-  // grab username and password from body
-  const creds = req.body;
-
-  db("users")
-    .where({ username: creds.username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
-        //passwords match and user exists
-        res.status(200).json({ message: "Welcome!" });
-      } else {
-        //   either username or password is invalid
-        res.status(401).json({ message: "Username/Password does not exists" });
-      }
-    })
-    .catch(err =>
-      res.status(500).json({ message: "There was an error, please try again" })
-    );
-});
+server.listen(port, () => console.log(`\n listening on port: ${port} \n`));
