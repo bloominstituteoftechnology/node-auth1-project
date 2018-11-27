0020bdccb67d6323a84edbba16b09e906f54a2cc
Hargobind Atwal <ethaipath@gmail.com>
2018-11-26 16:02:47 -0800

    Added cookie middleware

--- api/middleware.js
+++ api/middleware.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser')
 
 module.exports = server => {
+  server.use(helmet());
+  server.use(cookieParser())
   server.use(express.json());
   server.use(morgan('dev'));
-  server.use(helmet());
 }
