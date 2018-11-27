16ef40b5f536e466ad1100ef243a00fc4e849ede
Chad Jemmett <chad.jemmett@gmail.com>
2018-11-27 15:32:18 -0700

    Sessions work

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 const express = require('express');
 const knex = require('knex')
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session)
 const knexConfig = require('./knexfile.js')
 const db = knex(knexConfig.development)
 const bcrypt = require('bcryptjs')
 const server = express();
-// don't forget this in the future
+
+const sessionConfig = {
+  name: 'monkey',
+  secret: 'hello world',
+  cookie: {
+    maxAge: 1000 * 60 * 20 * 2,
+    secure: false,
+  },
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    knex: db,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+}
+
+server.use(session(sessionConfig))
 server.use(express.json())
 
 server.get('/', (req, res) => {
-  res.send("hello")
+  res.send(`Hello users number: ${req.session.user}`)
 })
 
-server.get('/api/users', (req, res) => {
-  db('users')
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(500).json(err))
-})
+protected = (req, res, next) => {
+  if(req.session && req.session.user) {
+    next()
+  } else {
+    res.status(401).json({message: 'You shall not pass!'})
+  }
+}
 
+server.get('/api/users', protected, (req, res) => {
+      db('users')
+        .then(users => res.status(200).json(users))
+        .catch(err => res.status(500).json(err))
+})
 
 server.post('/api/users', (req, res) => {
   const creds = req.body;
@@ -32,11 +61,26 @@ server.post('/api/login', (req, res) => {
   db('users').where({username: creds.username}).first()
   .then(user => {
     if(user && bcrypt.compareSync(creds.password, user.password)) {
-      res.status(200).json({message: 'welcome'})
+      req.session.user = user.id
+      res.status(200).json({message: `Welcome ${user.username}`})
     } else {
       res.status(401).json({message: 'you shall not pass'})
     }
   })
+})
+
+server.get('/api/logout', (req, res) => {
+  if(req.session) {
+    req.session.destroy(err => {
+      if(err) {
+        res.send('you can never leave')
+      } else {
+        res.send('bye')
+      }
+    })
+  } else {
+    res.end()
+  }
 })
 
 const port = 3300;
