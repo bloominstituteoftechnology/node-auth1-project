4f77f7c0894d0d8384424ff30f2d4f5459bc8093
Lola <lolahef@gmail.com>
2018-11-27 17:53:38 -0500

    sessions & cookies added to protect routes; logout added: MVP part 2 complete

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 const express = require('express')
 const cors = require('cors')
+const session = require('express-session')
+const KnexSessionStore = require('connect-session-knex')(session)
 const helmet = require('helmet')
 const bcrypt = require('bcrypt')
 const db = require('./database/config.js')
 
 const server = express()
 
+const sessionConfig = {
+    name: 'session',
+    secret: 'bees are underrated',
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+        tablename: 'sessions',
+        sidfieldname: 'sid',
+        knex: db,
+        createtable: true,
+        clearInterval: 1000 * 60 * 60
+    })
+}
+
 server.use(express.json())
 server.use(cors())
+server.use(session(sessionConfig))
 server.use(helmet())
 
-server.post('/api/login', (req, res) => {
-    const creds = req.body
-    db('users')
-        .where({ username: creds.username })
-        .first()
-        .then(user => {
-            user && bcrypt.compareSync(creds.password, user.password) ?
-            res.status(200).json({ message: 'login successful!' }) :
-            res.status(401).json({ message: 'better luck next time >:[' })
-        })
-        .catch(err => res.json(err))
-})
+
 
 server.post('/api/register', (req, res) => {
     const creds = req.body
@@ -33,11 +44,55 @@ server.post('/api/register', (req, res) => {
         .catch(err => res.status(500).json(err))
 })
 
-server.get('/api/users', (req, res) => {
+server.post('/api/login', (req, res) => {
+    const creds = req.body
     db('users')
-        .select('id', 'username')
-        .then(users => res.status(200).json(users))
-        .catch(err => res.status(500).json(err))
+        .where({ username: creds.username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(creds.password, user.password)) {
+                req.session.user = user.id
+                res.status(200).json({ message: 'login successful!' })
+            }
+            else {
+                res.status(401).json({ message: 'better luck next time >:[' })
+            }
+        })
+        .catch(err => res.json(err))
+})
+
+function protected(req, res, next) {
+    req.session && req.session.user ?
+    next() :
+    res.status(401).json({ message: 'scram' })
+}
+
+server.get('/api/users', protected, (req, res) => {
+    db('users')
+    .select('id', 'username')
+    .then(users => res.json(users))
+    .catch(err => res.send(err))
+})
+
+server.get('/api/me', protected, (req, res) => {
+    db('users')
+    .select('id', 'username')
+    .where({ id: req.session.user })
+    .first()
+    .then(user => res.json(user))
+    .catch(err => res.send(err))
+})
+
+server.get('/api/logout', (req, res) => {
+    if(req.session) {
+        req.session.destroy(err => {
+            err ?
+            res.send('you can never leave') :
+            res.send('see you next time')
+        })
+    } else {
+        res.end()
+    }
 })
 
 server.listen(3300, () => console.log('\n servin up on port 3300\n'));
