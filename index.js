acaf8635d00c0d25210fff65b96122a374ea9a1b
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-27 16:28:42 -0500

    implementes cookies and sessions

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 const bcrypt = require('bcryptjs');
 
 const knex = require('knex');
 const knexConfig = require('./knexfile');
+
 const db = knex(knexConfig.development);
 
 const server = express();
+
+const sessionConfig = {
+    name: "whoknows",
+    secret: "asfjaofuwruq04r3oj;ljg049fjq30j4jlajg40j40tjojasl;kjg",
+    cookie: {
+        maxAge: 1000 * 60 * 10,
+        secure: false 
+    },
+    httpOnly: true, 
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+        tablename: "sessions",
+        sidfieldname: "sid",
+        knex: db,
+        createtable: true,
+        clearInterval: 1000 * 60 * 60
+    })
+};
+server.use(session(sessionConfig));
 server.use(express.json());
 server.use(cors());
 
@@ -32,6 +55,7 @@ server.post('/api/login', (req, res) => {
         .where({username: creds.username }).first()
         .then(user => {
             if(user && bcrypt.compareSync(creds.password, user.password)){
+
                 res.status(200).json({ message: 'WELCOME!'})
             }else{
                 res.status(401).json({ message: 'WRONG CREDENTIALS!!'})
