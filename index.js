742fcbc16c2b5e515a51ab9fb36aedc2516f9388
Elvis Ibarra <ielvisd@gmail.com>
2018-11-27 15:22:12 -0800

    MVP + Stretch (middleware) completed

--- index.js
+++ index.js
@@ -1,11 +1,102 @@
-const server = require('./api/server.js');
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const bcrypt = require('bcryptjs'); // *************************** added package and required it here
+
 const db = require('./database/dbConfig.js');
 
-const bcrypt = require('bcryptjs');
-
+const server = express();
 const port = 9000;
 
-server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
+const sessionConfig = {
+  name: 'monkey',
+  secret: 'asfjaofuwruq04r3oj;ljg049fjq30j4jlajg40j40tjojasl;kjg',
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false // only set it over https; in production you want this true.
+  },
+  httpOnly: true, // no js can touch this cookie
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    knex: db,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
+
+server.use(session(sessionConfig)); // wires up session management
+server.use(express.json());
+server.use(cors());
+
+server.post('/api/login', (req, res) => {
+  // grab username and password from body
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        // passwords match and user exists by that username
+        req.session.user = user.id;
+        res.status(200).json({ message: 'Welcome!', userid: req.session.user });
+      } else {
+        // either username is invalid or password is wrong
+        res.status(401).json({ message: 'you shall not pass!!' });
+      }
+    })
+    .catch(err => res.json(err));
+});
+
+function protected(req, res, next) {
+  if (req.session && req.session.user) {
+    // they're logged in, go ahead and provide access
+    next();
+  } else {
+    // bounce them
+    res.status(401).json({ you: 'shall not pass!!' });
+  }
+}
+
+// protect this route, only authenticated users should see it
+server.get('/api/me', protected, (req, res) => {
+  db('users')
+    .select('id', 'username', 'password') // ***************************** added password to the select
+    .where({ id: req.session.user })
+    .first()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+// protect this route, only authenticated users should see it
+server.get('/api/users', protected, (req, res) => {
+  db('users')
+    .select('id', 'username', 'password') // ***************************** added password to the select
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+server.get('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send('you can never leave');
+      } else {
+        res.send('bye');
+      }
+    });
+  } else {
+    res.end();
+  }
+});
 
 server.post('/api/register', (req, res) => {
   //grab username and password from body
@@ -23,30 +114,8 @@ server.post('/api/register', (req, res) => {
     .catch(err => console.log(err));
 });
 
-server.post('/api/login', (req, res) => {
-  //grab username and password from body
-  const creds = req.body;
-
-  db('users')
-    .where({ username: creds.username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
-        // passwords match and user exists by that username
-        res.status(200).json({ message: 'welcome!' });
-      } else {
-        res.status(401).json({ message: 'You shall not pass!!' });
-      }
-    })
-    .catch(err => res.json(err));
+server.get('/', (req, res) => {
+  res.send('Its Alive!');
 });
 
-// protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
-  db('users')
-    .select('id', 'username', 'password')
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.json(err));
-});
+server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,12 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: {
-      filename: './database/users.sqlite3'
+    connection: { filename: './database/auth.sqlite3' }, // change this if you want a different name for the database
+    useNullAsDefault: true, // used to avoid warning on console
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations'
     },
-    useNullAsDefault: true
+    seeds: { directory: './database/seeds' }
   }
 };
