39cb9a5306bb34538da61a66d715efd0ccb4072d
DaftBeowulf <ajmboosh@gmail.com>
2018-11-27 13:31:53 -0600

    day1 and day2 MVP complete: implemented secured endpoints that require user to be logged in based on session & cookie created upon login, and created logout endpoint to test session destruction

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
 // external imports
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session); // returns a curried function and uses the imported session, so needs to come after
 
 //init db and server
 const db = require("./database/dbConfig");
 const server = express();
 
+// configure session object to be called later in middleware for better readability of middleware
+
+const sessionConfig = {
+  name: "foobar banana", //literally a foobar banana, unrelated if possible, helps avoid attacks who target the default value
+  secret: "*secret treasure found sound effect*",
+  cookie: {
+    maxAge: 1000 * 60 * 10, // session times out after 10 minutes
+    secure: false // only set it over https if true: should be true for production
+  },
+  httpOnly: true, // no js can touch this cookie (such as extensions)
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    // can be abstracted in a config like sessionConfig also
+    tablename: "sessions", //every prop is all-lowercase except for clearInterval
+    sidfieldname: "sid", // foobar banana but stands for session id here
+    knex: db,
+    createtable: true, //automatically creates the table for us instead of having to implement migration
+    clearInterval: 1000 * 60 * 60 // interval between clearing out timed-out rows in sessions table (otherwise you could end up with a table with thousands of rows over time)
+  })
+};
+
 //necessary middleware
+server.use(helmet());
+server.use(session(sessionConfig));
 server.use(express.json());
 server.use(cors());
-server.use(
-  session({
-    name: "linksession",
-    secret: "it's dangerous to go alone",
-    cookie: {
-      maxAge: 60 * 1000,
-      secure: true
-    },
-    httpOnly: true,
-    resave: false,
-    saveUninitialized: false
-  })
-);
-//endpoints
 
-server.get("/", (req, res) => {
-  console.log("hey I'm here");
-  res.send("working");
-});
+// endpoints
 
 // create new user (register)
 server.post("/api/register", (req, res) => {
   // grab username and password from body
   const creds = req.body;
-
   //verify username and password was submitted
   if (!creds.username || !creds.password) {
     res.status(400).json({ message: "Submit both username and password" });
@@ -74,44 +82,65 @@ server.post("/api/register", (req, res) => {
 
 // authenticate user
 server.post("/api/login", (req, res) => {
-  //   //see if session implementation is working
-  //   req.session.name = "Link";
-
   const creds = req.body;
 
-  db("users")
-    .where({ username: creds.username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
-        res.status(200).json({ message: "Login successful" });
-      } else {
-        res.status(401).json({ message: "No login for you!" });
-      }
-    })
-    .catch(err =>
-      res.status(500).json({ error: "Error during login attempt: ", err })
-    );
+  if (!creds.username || !creds.password) {
+    res
+      .status(400)
+      .json({ message: "Username and password required to login." });
+  } else {
+    db("users")
+      .where({ username: creds.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)) {
+          //after verifying user exists and pasword matches, attach userID field onto session to be checked against in protected endpoints
+          req.session.userId = user.id;
+          res.status(200).json({ message: "Login successful" });
+        } else {
+          res.status(401).json({ message: "No login for you!" });
+        }
+      })
+      .catch(err =>
+        res.status(500).json({ error: "Error during login attempt: ", err })
+      );
+  }
 });
 
-//TODO: get list of all users IF user is logged in
+// get list of all users ONLY IF user is logged in
 server.get("/api/users", (req, res) => {
-  //   //check for session name?
-  //   const name = req.session.name;
-  //   if (!name || name === undefined) {
-  //     res.status(401).json({ message: "Imposters not allowed!" });
-  //   } else {
+  // check for session && userId (created upon valid login)
 
-  db("users")
-    .select("id", "username")
-    .orderBy("id")
-    .then(users => res.status(200).json(users))
-    .catch(err =>
-      res
-        .status(500)
-        .json({ error: "Error occurred while retrieving users: ", err })
-    );
-  //   }
+  if (req.session && req.session.userId) {
+    //user is currently logged in, so provide access to endpoint hit
+    db("users")
+      .select("id", "username")
+      .orderBy("id")
+      .then(users => res.status(200).json(users))
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: "Error occurred while retrieving users: ", err })
+      );
+  } else {
+    //not logged in, bounce 'em!
+    res.status(401).json({ message: "You shall not pass!" });
+  }
+});
+
+// manual logout
+server.get("/api/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send("You can never leave *guitar solo*");
+      } else {
+        res.send("Later tater");
+      }
+    });
+  } else {
+    res.send("Need to log in to log out, bro");
+  }
 });
 
 server.listen(5500, () => console.log("\nrunning on Port 5500\n"));
