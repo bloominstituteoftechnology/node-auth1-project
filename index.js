dbbc4c9082e5a80f7ec0ed1d5b0de0c5c01282bd
MEBonham <programmer@mckaybonham.com>
2018-11-27 16:40:43 -0700

    Added GET api/users and GET api/logout endpoints, protected

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 const bcrypt = require('bcryptjs');
 
 const db = require('./data/dbConfig.js');
 
 const server = express();
+
+const sessionConfig = {
+    name: "monkey",
+    secret: "wq3or8sbo98ew092j3rbvn",
+    cookie: {
+        maxAge: 1000 * 60 * 10, // 10 minutes
+        secure: false // are we using https?
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+        tablename: "sessions",
+        sidfieldname: "sid",
+        knex: db,
+        createtable: true,
+        clearInterval: 1000 * 60 * 60 // 1 hour
+    })
+};
+
+server.use(session(sessionConfig));
 server.use(express.json());
 server.use(cors());
 
 const port = 9000;
 const rounds = 14;
+
+function protected(req, res, next) {
+    if (req.session && req.session.userId) {
+        next();
+    } else {
+        res.status(401).json({ message: "You shall not pass!" });
+    }
+}
 
 server.get("/", (req, res) => {
     res.send('Connected!');
@@ -51,6 +82,7 @@ server.post("/api/login", (req, res) => {
         .then(user => {
             if (user && bcrypt.compareSync(creds.password, user.password)) {
                 // Start session, cookie
+                req.session.userId = user.id;
                 res.status(200).json({ message: "Welcome!" });
             } else {
                 res.status(401).json({ message: "You shall not pass!" });
@@ -59,6 +91,31 @@ server.post("/api/login", (req, res) => {
         .catch(err => {
             res.status(500).json({ error: err });
         });
+});
+
+server.get("/api/users", protected, (req, res) => {
+    db("users")
+        .select('id', 'username', 'password')
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ error: err });
+        });
+});
+
+server.get("/api/logout", (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.send("You're stuck");
+            } else {
+                res.send("Bye!");
+            }
+        });
+    } else {
+        res.end();
+    }
 });
 
 server.listen(port, () => console.log(`Server live on port ${port}`));
