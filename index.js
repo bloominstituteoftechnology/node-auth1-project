680d7311cd699a497d5bbd7206dc4f1aa1e9213a
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-11-27 17:57:40 -0500

    login auth for users endpoint

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
 const knex = require("knex");
 const bcrypt = require("bcryptjs");
 
+const sessionConfig = {
+  secret: "uniquesecret",
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false
+  },
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false
+};
 const knexConfig = require("./knexfile.js");
 const db = knex(knexConfig.development);
 const server = express();
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.post("/login", (req, res) => {
   const creds = req.body;
@@ -16,12 +28,13 @@ server.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
+        req.session.userId = user.id;
         res.status(200).json({ message: "Authentication succesful" });
       } else {
         res.status(401).json({ message: "Failed to authenticate" });
       }
     })
-    .catch(err => res.json(err));
+    .catch(err => res.status(500).json(err));
 });
 
 server.post("/register", (req, res) => {
@@ -33,7 +46,7 @@ server.post("/register", (req, res) => {
     .then(ids => {
       res.status(201).json(ids);
     })
-    .catch(err => json(err));
+    .catch(err => res.status(500).json(err));
 });
 
 server.get("/", (req, res) => {
@@ -41,12 +54,16 @@ server.get("/", (req, res) => {
 });
 
 server.get("/users", (req, res) => {
-  db("users")
-    .select("id", "username")
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+  if (req.session && req.session.userId) {
+    db("users")
+      .select("id", "username")
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  } else {
+    res.status(401).json({ message: "login failed" });
+  }
 });
 
 server.listen(3300, () => console.log("\nrunning on port 3300\n"));
