0da4707f61f54a1c2a94ed8b2a7f53e21292ea3e
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-28 09:03:15 -0600

    adds helmet and some comments

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const session = require('express-session');
+const helmet = require('helmet');
 const knexSessionStore = require('connect-session-knex')(session);
 const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig');
-
-
 
 const server = express();
 
@@ -25,16 +24,18 @@ const sessionConfig = {
     sidfieldname: 'sid',
     knex: db,
     createtable: true,
-    clearInterval: 1000 * 60 * 10
+    clearInterval: 1000 * 60 * 60
   })
 }
 
 server.use(session(sessionConfig))
+server.use(helmet());
 server.use(express.json());
 
       
-
+//users login
 server.post('/api/login', (req, res) => {
+  //gets username and password
   const creds = req.body;
 
   db('users')
@@ -51,6 +52,8 @@ server.post('/api/login', (req, res) => {
     .catch(error => res.json(error))
 })
 
+
+//middleware
 function protect(req, res, next) {
   if (req.session && req.session.user) {
     next();
@@ -59,6 +62,7 @@ function protect(req, res, next) {
   }
 } 
 
+//only logged in users should see this list of users
 server.get('/api/users', protect, (req, res) => {
   db('users')
     .select('id', 'username')
@@ -80,13 +84,16 @@ server.get('/api/restricted/other', protect, (req, res) => {
     .catch(err => res.send(err));
 });
 
+
+//registers new users
 server.post('/api/register', (req,res) => {
+  //gets username and password
   const creds = req.body;
-
+  //generate the hash
   const hash = bcrypt.hashSync(creds.password, 14);
-
+  //overwrite the password with the hash
   creds.password = hash;
-
+  //save user to DB
   db('users')
     .insert(creds)
     .then(ids => {
@@ -95,7 +102,7 @@ server.post('/api/register', (req,res) => {
     .catch(error => json(error))
 })
 
-
+//just to test if it works
 server.get('/', (req, res) => {
   res.send('It\'s Alive!');
 });
