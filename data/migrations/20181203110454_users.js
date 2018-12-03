baf164138e9e5d62654dcb5c885ea474278d1137
Troy Amlee <troyamlee19@gmail.com>
2018-12-03 11:38:26 -0600

    Troy Amlee [mvp] initial commit

---
+++ data/migrations/20181203110454_users.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', users => {
+      users.increments();
+      users.string('username', 255).notNullable().unique();
+      users.string('password', 255).notNullable().unique();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const helmet = require('helmet');
+const knex = require('knex');
+const cors = require('cors');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
+const server = express()
+
+server.use(express.json())
+server.use(helmet());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.json({ message: 'API Running!' });
+});
+
+server.post('api/register', (req, res) => {
+    const credentials = req.body
+    const hash = bcrypt.hashSync(credentials.password, 14);
+    credentials.password = hash
+    db('users').insert(credentials)
+    .then(user => res.status(201).json(user))
+    .catch(error => res.status(500).json({ message: "You done F'd Up", error }))
+});
+
+server.post('api/login', (req, res) => {
+    const logger = req.body
+
+    
+    // db('users').insert(logger)
+    // .then(user => res.status(201).json(user))
+    // .catch(error => res.status(500).json({ message: "You done F'd Up", error }))
+});
+
+server.get('api/users', (req, res) => {
+    db('users')
+    .select('username', 'id', 'password')
+    .then(user => res.status(200).json(user))
+    .catch(error => res.status(500).json({ message: 'Could Not Retrieve Users', error }));
+})
+
+
+
+server.listen(7777, () => console.log('\n === API Running On Port 7777 => http://localhost:7777 ===\n'))
