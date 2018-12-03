45fe2fd77ee9b8aac36a0b33c2600bf79d31692c
Troy Amlee <troyamlee19@gmail.com>
2018-12-03 12:19:55 -0600

    [mvp] Welcome User

--- data/migrations/20181203110454_users.js
+++ data/migrations/20181203110454_users.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', users => {
       users.increments();
       users.string('username', 255).notNullable().unique();
-      users.string('password', 255).notNullable().unique();
+      users.string('password', 255).notNullable();
   })
 };
 
--- index.js
+++ index.js
@@ -17,32 +17,31 @@ server.get('/', (req, res) => {
     res.json({ message: 'API Running!' });
 });
 
-server.post('api/register', (req, res) => {
-    const credentials = req.body
+server.post('/api/register', (req, res) => {
+    const credentials = req.body;
     const hash = bcrypt.hashSync(credentials.password, 14);
-    credentials.password = hash
+    credentials.password = hash;
     db('users').insert(credentials)
-    .then(user => res.status(201).json(user))
+    .then(user => {res.status(201).json(user)})
     .catch(error => res.status(500).json({ message: "You done F'd Up", error }))
 });
 
-server.post('api/login', (req, res) => {
+server.post('/api/login', (req, res) => {
     const logger = req.body
 
     db('users')
-    .where({ username: creds.username }).first().then(user => {
+    .where({ username: logger.username }).first().then(user => {
         if (user && bcrypt.compareSync(logger.password, user.password)){
-            res.status(200).json({ message: "Welcome User!" })
+            res.status(200).json({ message: `Logged In: Welcome ${user.username}!` })
         } else {res.status(401).json({ message: 'You Shall Not Pass!' })}
-    }).catch(error => res.status(500).json({ message: 'error', error }))
-    ;
+    }).catch(error => res.status(500).json({ message: 'error', error }));
     
     // db('users').insert(logger)
     // .then(user => res.status(201).json(user))
     // .catch(error => res.status(500).json({ message: "You done F'd Up", error }))
 });
 
-server.get('api/users', (req, res) => {
+server.get('/api/users', (req, res) => {
     db('users')
     .select('username', 'id', 'password')
     .then(user => res.status(200).json(user))
