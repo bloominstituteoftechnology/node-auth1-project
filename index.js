d31d80dd54c963385e05afd67bb359b0d24e1cf1
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-12-03 18:54:39 -0500

    Get users working

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-
+server.get('/api/users', (req, res) => {
+  db('users')
+  .select('id', 'username', 'password')
+  .then(users => {
+    res.json(users)
+  })
+  .catch(err => res.json(err));
+})
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
