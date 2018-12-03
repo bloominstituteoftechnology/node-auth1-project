3c2513a8739a9e9703133e87edfb59225bb263be
Angelo Deleon <rudy209@comcast.net>
2018-12-03 15:19:10 -0800

    created user table

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const server = express();
+
+
+
+
 server.get('/', (req, res) => {
     res.send('Helllo there.');
 });
+
+
+
 server.listen(3300, () => console.log('Server started on port 3300'));
