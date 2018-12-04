020d9b078144031617b78723f9ab8a83aa4aef8b
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-12-03 19:17:12 -0500

    Delete user by id

--- index.js
+++ index.js
@@ -20,6 +20,44 @@ server.get('/api/users', (req, res) => {
   .catch(err => res.json(err));
 })
 
+server.post('/api/register', (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.json(err));
+})
+
+server.post('/api/login', (req, res) => {
+  const creds = req.body;
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        res.status(200).json({ message: 'Welcome!' });
+      } else {
+        res.status(401).json({ message: 'Username or Password is incorrect!'});
+      }
+    })
+    .catch(err => res.json(err));
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  db('users')
+  .where({ id: id })
+  .del()
+  .then(count => {
+    res.status(200).json({ count });
+  })
+  .catch(err => res.status(500).json(err));
+})
 
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
