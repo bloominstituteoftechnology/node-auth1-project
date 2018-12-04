ab7c56e399e1990c4dcb4d4625c0597f267c0eee
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-12-04 16:32:43 -0600

    -added client, but not connected to API yet -added 'get' endpoint

--- middleware/loginRouter.js
+++ middleware/loginRouter.js
@@ -1,6 +1,34 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 const router = express.Router();
 const db = require('../data/dbConfig');
+
+router.post('/', (req, res) => {
+  // grab username and password from body
+  const credentials = req.body;
+  db('users')
+    .where({
+      username: credentials.username
+    })
+    .first() // returns the first match, works for unique usernames
+    .then(user => {
+      if(user && bcrypt.compareSync(credentials.password, user.password)) {
+        //passwords match! and user exists by that username
+        res
+          .status(200)
+          .json({
+            message: `Welcome, ${credentials.username}`
+          })
+      } else {
+        // either username is invalid or password is wrong
+        res
+          .status(401)
+          .json({
+            message: "Sorry, I can't let you in!"
+          })
+      }
+    })
+})
 
 module.exports = router;
--- middleware/userRouter.js
+++ middleware/userRouter.js
@@ -3,4 +3,15 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 const db = require('../data/dbConfig');
 
+router.get('/', (req, res) => {
+  db('users')
+    .select('id', 'username')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => 
+      res
+        .json({errorMessag: "Sorry, we're having trouble getting the list of users..."}))
+})
+
 module.exports = router;
