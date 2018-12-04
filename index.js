93ce18cd68935a4e6ff4211fb00c9e0064f94338
jaklundun <govi.escudero@gmail.com>
2018-12-04 07:52:49 -0800

    Register and Login api endpoints completed. Day 1 MVP attained.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const knexConfig = require('./knexfile');
 const knex = require('knex');
-
+const bcrypt = require('bcryptjs');
 
 const server = express();
 
@@ -16,6 +16,52 @@ server.use(helmet(), express.json());
 server.get('/', (request, response) => {
     response.send("Let's GO!")
 });
+
+
+server.post('/api/register', (req, res) => {
+    
+    const creds = req.body;
+    
+    const hash = bcrypt.hashSync(creds.password, 16); 
+   
+    creds.password = hash;
+    
+    db('users')
+    .where({ username: creds.username })
+    .then(user => {
+        if (user) {
+        res.status(409).json({ message: 'username is already taken' });
+        } else {
+        db('users')
+        .insert(creds)
+        .then(ids => {  
+            res.status(201).json(ids);
+        })
+        }
+    })
+    .catch(err => res.json(err));
+});
+
+server.post('/api/login', (req, res) => {
+    
+    const creds = req.body;
+  
+    db('users')
+      .where({ username: creds.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)) {
+         
+          res.status(200).json({ message: 'Benvenuto!' });
+        } else {
+          
+          res.status(401).json({ message: 'Access Denied!' });
+        }
+      })
+      .catch(err => res.json(err));
+  });
+  
+  
 
 
 server.listen(8888, () => console.log('\nrunning on port 8888\n'));
