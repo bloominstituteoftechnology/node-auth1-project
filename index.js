ce6802cbcd969dd8b4dc674c2699e356c0c44cbc
paulapivat <paul.apivat@gmail.com>
2019-01-16 15:13:53 +0700

    add POST request endpoint for login

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 //const cors = require('cors');   //necessary for react frontend
 const bcrypt = require('bcryptjs'); //open source Hashing
 
-const knex = require('knex');   // pre-Helpers
-const dbConfig = require('./knexfile'); // pre-Helpers
+// const knex = require('knex');   // pre-Helpers
+// const dbConfig = require('./knexfile'); // pre-Helpers
 
 //add helpers later
-//const db = require('./database/dbHelpers.js');
+const db = require('./database/dbHelpers.js');
 
 const server = express();
-const db = knex(dbConfig.development); // pre-Helpers
+//const db = knex(dbConfig.development); // pre-Helpers
 const PORT = 3000;
 
 server.use(express.json());
@@ -23,7 +23,8 @@ server.post('/api/register', (req , res) => {
     const user = req.body;
     //bcrypt goes here
     user.password = bcrypt.hashSync(user.password, 14);
-    db('users').insert(user)
+    //db('users').insert(user)
+    db.insertUser(user)   //dbHelpers
     .then(ids => {
         res.status(201).json({id: ids[0]});
     })
@@ -32,10 +33,20 @@ server.post('/api/register', (req , res) => {
     });
 });
 
-// server.post('/api/login', (req, res) => {
-//     const bodyUser = req.body;
-//     .then
-// })
+server.post('/api/login', (req, res) => {
+    const bodyUser = req.body;
+    db.findByUsername(bodyUser.username) //dbHelpers
+    .then(users => {
+        if (users.length && bcrypt.compareSync(bodyUser.password, users[0].password)){
+            res.json({ info: "success: you're logged in"})
+        } else {
+            res.status(404).json({err: "Intentionally Generic: Invalid username and/or password"});
+        }
+    })
+    .catch(err => {
+        res.status(500).send(err);
+    })
+})
 
 
 server.listen(PORT, () => console.log(`running on port ${PORT}`));
