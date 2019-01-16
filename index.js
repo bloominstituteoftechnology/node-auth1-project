f043b2f397b89d833004913d8b6c679a40284eee
Denise Escobar <descobar1490@gmail.com>
2019-01-16 13:09:35 -0600

    Added my index.js file and set up my server boilerplate

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+
+const db = knex(dbConfig.development)
+const PORT = process.env.PORT || 8999;
+
+server.use(express.json());
+
+//POST	/api/register
+
+//POST	/api/login
+
+//GET	/api/users
+
+server.listen(PORT, () =>{
+    console.log(`Server is listening on ${PORT}`)
+})
