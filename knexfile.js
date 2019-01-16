9f98fc161966305858987debe0bcfb272dfaadc6
BryceEvans <evans.bryce@gmail.com>
2019-01-15 20:58:15 -0700

    Initial setup complete.

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/auth.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
+
+};
