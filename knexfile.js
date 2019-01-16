7546df37c59d0eee1a563fe45d4b33022ca86dae
Kendra Williams <kmonique.williams@gmail.com>
2019-01-15 22:48:19 -0500

    project set up completed

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/database.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    }
+  },
+};
