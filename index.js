b1a45397386dd270eb6aa8fa3e71087fa3fbdb05
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-16 00:02:41 -0500

    required bcrypt required dbConfig.js added post endpoint to create new user

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const logger = require('morgan');
+const bcrypt = require('bcryptjs');
+
+const db = require('./data/dbConfig');
 
 const app = express();
 
@@ -12,6 +15,19 @@ app.use(logger('dev'));
 
 app.get('/', (req, res) => {
   res.json({message: 'your app is running!'});
+});
+
+app.post('/api/register', (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 14);
+  console.log('user', user);
+  db('users').insert(user)
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 app.listen(PORT, () => {
