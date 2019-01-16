7fb00a2137f60cf2a59d9741bfaa63f63956bf86
Kendra Williams <kmonique.williams@gmail.com>
2019-01-15 23:13:54 -0500

    migration for users table completed, structure file updated

---
+++ data/migrations/20190115230824_users_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("users", table => {
+   table.increments();
+   table.string("username").notNullable().unique();
+   table.string("password").notNullable();
+   table.boolean("loggedIn").defaultTo(false);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("users");
+};
