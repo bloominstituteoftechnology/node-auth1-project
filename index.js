d1136de11bd98e935e6a24f8be91a11593a7b3e4
Glenn-David Daniel <d2rd@outlook.com>
2019-01-16 12:55:07 -0800

    Adding middleware knex, bcrypt.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const cors = require('cors');
+const server = express();
+const knex = require('knex');
 const bcrypt = require('bcryptjs');
+// const cors = require('cors');
 
 // const db = require('./database/dbConfig.js');
 
-const server = express();
+const PORT = process.env.PORT || 3300;
 
 server.use(express.json());
-server.use(cors());
+// server.use(cors());
 
 server.get('/', (req, res) => {
-  res.send('I live therefore I AM!');
+  res.send('I am responding to your GET request, Dave!');
 });
 
-server.listen(3300, () => console.log(`I live therefore I AM running on port 3300`));
+// server.listen(3300, () => console.log('I live therefore I AM running on port 3300'));
+server.listen(PORT, () => {
+  console.log(`I live therefore I AM running on port ${PORT}`);
+});
