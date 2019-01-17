45834b9fc63120caff54a352fc9da16a06b23ebf
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-01-16 21:29:28 -0500

    initialized index js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const db = require('./database/dbHelpers.js')
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.listen(3300, () => console.log('\nrunning on port 3300\n'));
