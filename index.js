ce5111e844ced33821f1be3b4a81eafdfb9e7fd6
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-01-16 23:17:22 -0500

    added the ability to save cookies and made a global middleware

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+// const knexSession = require('connect-session-knex')(session);
 
 const server = express();
 const db = require('./data/helpers/dbHelpers')
 
 const PORT = 3500;
+
+const sessionConfig = {
+  name: 'user_session', 
+  secret: 'the-undetermined-sailor',
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+  },
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+}
+
 server.use(
   express.json(),
-  cors()
+  cors(),
+  session(sessionConfig)
 )
 
-server.get('/api/users', (req, res) => {
-  res.send('It works')
-})
 //registering a new user
 server.post('/api/register', (req, res) => {
   const newUser = req.body;
@@ -30,16 +43,12 @@ server.post('/api/register', (req, res) => {
 //authenticating a user when logging in
 server.post('/api/login', (req, res) => {
   const userCreds = req.body;
-
+  
   db.authorize(userCreds.username)
     .then(user => {
       if(user.length && bcrypt.compareSync(userCreds.password, user[0].password)) {
-        db.getUsers()
-          .then(users => {
-            res.json(users)
-          }).catch( err => {
-            res.status(500).json({errorMessage: "unable to get users"})
-          })
+        req.session.auth = userCreds.username;
+        res.json({message: "you are logged in. Welcome"});
       } else {
         res.status(404).json({errorMessage: "You shall not pass"});
       }
@@ -48,6 +57,24 @@ server.post('/api/login', (req, res) => {
       res.status(500).json({errorMessage: "unable to process authorization"})
     })
 });
+
+//getting all users
+server.get('/api/users', autho, (req, res) => {
+  db.getUsers()
+    .then(users => {
+      res.json(users)
+    }).catch( err => {
+      res.status(500).json({errorMessage: "unable to get users"})
+    })
+})
+
+function autho(req, res, next) {
+  if(req.session && req.session.auth) {
+    next();
+  }else {
+    res.status(401).json({errorMessage: 'you do not have access'})
+  }
+}
 
 server.listen(PORT, () => {
   console.log(`<==== running server on port ${PORT} ===>`)
