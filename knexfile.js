da738f9d8f80abd0a7deb8f86fcad424a8761016
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-01-16 19:20:54 -0600

    Sert up knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/userDb.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  
+
+};
