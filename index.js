34b4ee38f33cfb57f8d4b3a74898229a4d06dfa5
TheGameFreak720 <luis.alvarez@commithub.com>
2019-01-16 21:44:31 -0500

    created post route

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const knex = require('knex');
 const helmet = require('helmet');
+const bcrypt = require('bcryptjs');
+const dbConfig = require('./knexfile');
 
 const server = express();
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(helmet());
+
+server.post('/api/register', (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 15);
+
+    db('users').insert(user)
+        .then(id => {
+            res.status(201).json(id);
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Failed to insert users' });
+        }) 
+});
 
 const port = 3300;
 server.listen(port, function() {
