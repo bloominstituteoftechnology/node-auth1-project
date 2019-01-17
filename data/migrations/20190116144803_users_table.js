cb6c8ddd5020693ada478f1a0bf4462b98c91418
Dewayne87 <dewaynelindsay87@gmail.com>
2019-01-16 17:12:43 -0800

    finished day 1 of assignment

---
+++ data/migrations/20190116144803_users_table.js
@@ -0,0 +1,18 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('users', table => {
+        table
+            .increments();
+        table
+            .string('username', 128)
+            .notNullable()
+            .unique();
+        table
+            .string('password', 128)
+            .notNullable();
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('users')
+};
--- index.js
+++ index.js
@@ -12,26 +12,48 @@ server.disable("etag");
 
 server.post('/api/register', (req, res) => {
     const newUser = req.body;
-    if (!!newUser.password || !!newuser.username) {
-        return res.status(404).json('Please provide Username and/or password')
+    console.log(newUser)
+    if (!newUser.password || !newUser.username) {
+        return res
+            .status(404)
+            .json('Please provide Username and/or password')
     }
     newUser.password = bcrypt.hashSync(newUser.password, 14);
     dbhelper.registerUser(newUser)
-        .then(res.status(201).json())
-        .catch(res.status(500).json())
+        .then(id => {
+            res
+                .status(201)
+                .json(id)
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json(err)
+        })
 })
 
 server.post('/api/login', (req, res) => {
     const bodyUser = req.body;
+    if (!bodyUser.password || !bodyUser.username) {
+        return res
+            .status(404)
+            .json('Please provide Username and/or password')
+    }
     dbhelper.findByUser(bodyUser.username)
         .then(user => {
             if (user.length && bcrypt.compareSync(bodyUser.password, user[0].password)) {
-                res.json({ message: 'Logged In!' })
+                res
+                    .json({ message: 'Logged In!' })
             } else {
-                res.json({ message: 'You shall not pass!' })
+                res
+                    .json({ message: 'You shall not pass!' })
             }
         })
-        .catch(res.status(500).json())
+        .catch(error => {
+            res
+                .status(500)
+                .json(error)
+        })
 })
 
 server.listen(PORT, err => {
