ff16fac7f7a632eb2f6957c6270d04825eddffc9
Alex Martin <atonymartin20@yahoo.com>
2019-01-16 21:51:35 -0600

    Finished post endpoint for api/register

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const morgan = require('morgan');
 
+// const knex = require('knex');
+// const knexConfig = require('./knexfile.js');
+// const db = knex(knexConfig.development);
+
 const db = require('./data/dbHelpers.js');
 const server = express();
 const PORT = 4500;
@@ -13,15 +17,22 @@ server.use(morgan('dev'));
 
 server.post('/api/register', (req,res) => {
     const user = req.body;
+    console.log(user.username, ' and ', user.password);
     user.password = bcrypt.hashSync(user.password, 16);
-    db.insert('user')
-        .then( ids => {
-            res.status(201).json({ id: ids[0] });
-        })
-        .catch(err => {
-            res.status(500).json({ errorMessage: 'Failed to add user '});
-        });
-});
+    console.log('new pass', user.password);
+    if(!user.username || !user.password || user.username === '' || !user.password === '') {
+        res.status(400).json({ errorMessage: 'Please make sure you input your username and password.' });
+    } else {
+        db.insert(user)
+            .then( ids => {
+                res.status(201).json({ id: ids[0] });
+            })
+    
+            .catch(err => {
+                res.status(500).json({ errorMessage: 'Failed to add user '});
+            });
+        };
+    });
 
 
 server.listen(PORT, () => {
