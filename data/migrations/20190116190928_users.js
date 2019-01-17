8b19d0ea2e80b2f43f62bfeae1018fd7a04e5e73
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-01-16 19:22:09 -0600

    Created migration for user table

---
+++ data/migrations/20190116190928_users.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', table =>{
+      table.increments();
+      table
+        .string('username')
+        .notNullable()
+        .unique()
+      table
+        .string('password')
+        .notNullable(); 
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
