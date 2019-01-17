a12b8f039c672babff630ee075eecfeaa4225e2d
vy3191 <venkyafhs@gmail.com>
2019-01-16 20:41:44 -0500

    Added migrations

---
+++ data/migrations/20190116203620_clients.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+   return knex.schema.createTable('clients', function (clients) {
+    clients.increments();
+    clients.string('username',128)
+    .notNullable()
+    .unique();
+    clients.string('password',128)
+    .notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+   return knex.schema.dropTableIfExists('clients');
+};
--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './user-application.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
