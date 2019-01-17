5abaab1b1334d2c45ce5b2633c951ed20819a252
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-01-16 19:36:13 -0700

    Added endpoint to register new user with hashed password and also created helper method to find user by username

--- data/dbHelpers.js
+++ data/dbHelpers.js
@@ -5,8 +5,14 @@ const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
 module.exports = {
+    // helper method to insert new user into table
     insert: (user) => {
         return db('users_table')
             .insert(user);
+    },
+    // helper method to find database entry using username
+    findByUsername: (username) => {
+        return db('users_table')
+            .where('username', username);
     }
-}
+} 
--- index.js
+++ index.js
@@ -8,4 +8,36 @@ const PORT = 4400;
 
 server.use(express.json());
 
+// register new user with hashed password
+
+server.post('/api/register', (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 14);
+    if (user.password && user.username) {
+        db
+        .insert(user)
+        .then(id => {
+            res
+                .status(201)
+                .json({id: id[0]})
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({message: `Registration could not be completed at this time.`})
+        });
+    }
+    else {
+        res
+            .status(400)
+            .json({message: `Please provide both a username and password for registration.`})
+    }
+});
+
+// log in with hashed password
+
+server.post('/api/login', (req, res) => {
+    const user = req.body;
+});
+
 server.listen(PORT, () => console.log(`Running on ${PORT}`));
