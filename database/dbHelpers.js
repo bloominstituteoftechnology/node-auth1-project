f644d91241cbb6fdc51d1e3e104ffd09352e0239
BryceEvans <evans.bryce@gmail.com>
2019-01-16 20:32:20 -0700

    Seems like part 1 is acheived.

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -4,3 +4,14 @@ const knexConfig = require('../knexfile');
 
 const db = knex(knexConfig.development);
 
+// this file contains my helpers which is basically the knex logic.
+
+module.exports = {
+  insert: (user) => {
+    return db('users').insert(user);
+  },
+
+  findByUserName: (username) => {
+    return db('users').where('username', username);
+  }
+}
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
-// const db = require('./database/dbHelpers.js');
+const db = require('./database/dbHelpers.js');
 
 const server = express();
 
@@ -11,6 +11,40 @@ server.use(cors());
 
 server.get('/', (req, res) => {
   res.send('Up and running!');
+});
+
+// Register a user
+server.post('/api/register', (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password);
+
+  db.insert(user)
+  .then(id => {
+    res.status(201).json({ id: id[0] });
+  })
+  .catch(err => {
+    res.status(500).send(err);
+  })
+})
+
+server.post('/api/login', (req, res) => {
+  const bodyUser = req.body;
+
+  db.findByUserName(bodyUser.username)
+  .then(users => {
+
+    // check if username is valed
+    // check is password from client === password from db
+    // if (users.length && bodyUser.password === users[0].password)
+    if (users.length && bcrypt.compareSync(bodyUser.password, users[0].password)) {
+      res.json({ info: "Logged in" });
+    } else {
+      res.status(404).json({ err: "You shall not pass!"});
+    }
+  })
+  .catch(err => {
+    res.status(500).send(err);
+  });
 });
 
 
