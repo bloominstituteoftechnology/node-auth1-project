5dfce48c5c71924cf0039af6adaff8e645b09126
Julie Jonak <juliejonak10@gmail.com>
2019-01-16 18:47:49 -0600

    Add create account POST with error handling

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const server = express();
 
-const knex = require('knex');
-const dbConfig = require('./knexfile');
-const dbOne = knex(dbConfig.development);
-
 const loginRouter = require('./routes/loginRoutes');
 
 const PORT = process.env.PORT || 3800;
@@ -12,16 +8,6 @@ const PORT = process.env.PORT || 3800;
 server.use(express.json());
 
 server.use('/api', loginRouter);
-
-// server.get('/api/user', (req, res)=>{
-//     dbOne('Users')
-//         .then(response => {
-//             res.json(response)
-//         })
-//         .catch(err => {
-//             res.status(500).json({ message: "Could not fetch users" })
-//         })
-// })
 
 //SERVER
 
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,25 +1,40 @@
 const express = require('express');
 const router = express.Router();
-
-
-const knex = require('knex');
-const dbConfig = require('../knexfile');
-const dbOne = knex(dbConfig.development);
-
+const bcrypt = require('bcryptjs');
 
 const dbLogin = require('../data/userModel');
 
-// router.post('/register', (req, res) => {
-//     //hash password
-//     //insert user with hashed password
-//     //password must be at least 12 characters long
-// })
+router.post('/register', (req, res) => {
+    //hash password
+    //insert user with hashed password
+    //password must be at least 12 characters long
 
-// .post('/login', (req, res) => {
-//     //post request to cross check username and then hashed passwords
-// })
+    const user = req.body;
+    if(user.Password.length > 12){
+        user.Password = bcrypt.hashSync(user.Password, 10)
+        if(user.Username){
+            dbLogin.add(user)
+            .then(response => {
+                console.log(response);
+                res.status(201).json({ message: "Account created successfully!" })
+            })
+            .catch(err => {
+                res.status(500).json({ message: "Unable to add new account" })
+            })
+        } else{
+            res.status(400).json({ message: "New accounts require a Username" })
+        }
+        
+    } else {
+        res.status(400).json({ message: "Password must be at least 12 characters long"})
+    }
+})
 
-router.get('/users', (req, res) => {
+.post('/login', (req, res) => {
+    //post request to cross check username and then hashed passwords
+})
+
+.get('/users', (req, res) => {
     //return all users if logged in properly
     //else 'You shall not pass!'
     
