d592534b3a29049a5e61690fdf16facdc9497f41
Robin U Familara <robin.familara@cgius.com>
2019-01-17 01:33:02 -0600

    Setup the boilerplates

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const server = express();
+
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
+
+const PORT = process.env.PORT || 3500;
+
+server.use(express.json());
+
+
+//SERVER
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`)
+});
