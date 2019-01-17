9b4570f11a49870b79f67b7af6c4a42bb846b419
shannonhealy <sahealy525@gmail.com>
2019-01-16 22:58:32 -0500

    added logic for registering users

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
+const bcrypt = require("bcryptjs");
+
+const server = express();
+
+server.use(express.json());
+
+server.post("/api/register", (req, res) => {
+    if (req.body.username && req.body.password && typeof req.body.username === "string" && typeof req.body.password === "string") {
+        let user = req.body;
+        user.password = bcrypt.hashSync(user.password);
+        db("users")
+            .insert(user)
+            .then(ids => {
+                res.status(201).json(ids[0]);
+            }).catch(error => {
+                res.status(500).json({message: "Error registering user", error: error});
+            });
+    } else {
+        res.status(400).json({ error: "Incorrectly formatted user data" });
+    }
+});
+
+const port = 3300;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
