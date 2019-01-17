d9345420e7523d8db33bd312118fe68cd473c0a3
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-01-16 19:22:39 -0600

    Created seed for testing and user database

---
+++ data/seeds/users.js
@@ -0,0 +1,11 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {username: 'test-name', password: 'test-password'}
+      ]);
+    });
+};
