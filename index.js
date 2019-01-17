f479d7b97c955415e7ec074a76e070046e63f86d
paulapivat <paul.apivat@gmail.com>
2019-01-17 13:16:07 +0700

    first pass sessions and cookies

--- index.js
+++ index.js
@@ -10,6 +10,39 @@ const db = require('./database/dbHelpers.js');
 
 const server = express();
 //const db = knex(dbConfig.development); // pre-Helpers
+
+const session = require('express-session');
+
+/// GLOBAL MIDDLEWHERE SESSION COOKIES
+// configure express-session middleware
+server.use(
+    session({
+        name: 'notsession', // default is connect.sid
+        secret: 'nobody tosses a dwarf!',
+        cookie: {
+            maxAge: 1 * 24 * 60 * 60 * 1000,   // 1 day in milliseconds
+            secure: true, // only set cookies over https. Server will not send back a cookie over http.
+        }, // 1 day in milliseconds
+        httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+// global middleware ensures a user is logged in when accessing any route prefixed by /api/restricted/
+// /api/restricted/      /api/restricted/something...
+server.get('/setname', (req,res) => {
+    req.session.name = 'Frodo';
+    res.send('got it');
+});
+
+server.get('/getname', (req, res) => {
+    const name = req.session.name;
+    res.send(`hello ${req.session.name}`);
+});
+
+
+
 const PORT = 3000;
 
 server.use(express.json());
@@ -18,6 +51,12 @@ server.use(express.json());
 server.get('/', (req , res) => {
     res.send("LIVE FROM BKK!!");
 });
+
+
+
+
+
+
 
 server.post('/api/register', (req , res) => {
     const user = req.body;
@@ -48,6 +87,22 @@ server.post('/api/login', (req, res) => {
     })
 })
 
+//MIDDLEWARE TO RESTRICT ACCESS TO PROTECTED ROUTES
+function protected(req, res, next) {
+    if (req.session && req.session.username){
+        next();
+    } else {
+        res.status(401).json({ message: 'you shall not pass!!'});
+    }
+}
+
+server.get('/api/restricted', protected, (req,res) => {
+    db('users')
+    .then(users => res.json(users))
+    .catch(err => res.json(err));
+});
+
+
 // only authenticated users should see this
 server.get('/api/users', (req,res) => {
     db.getUsers()
@@ -57,6 +112,7 @@ server.get('/api/users', (req,res) => {
     })
     .catch(err => res.send(err));
 })
+
 
 
 
