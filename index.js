587088e1a5d2a06e9078f20285130d1ab025e142
Alex Martin <atonymartin20@yahoo.com>
2019-01-16 21:08:43 -0600

    created user table and started created post endpoint for api/register

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ server.use(express.json());
 server.use(cors());
 server.use(morgan('dev'));
 
+server.post('/api/register', (req,res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 16);
+    db.insert('user')
+        .then( ids => {
+            res.status(201).json({ id: ids[0] });
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Failed to add user '});
+        });
+});
+
+
 server.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
 });
