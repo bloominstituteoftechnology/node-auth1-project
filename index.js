676fa7a733f28a69f6af0975828ee9eefd40fe97
shannonhealy <sahealy525@gmail.com>
2019-01-17 16:10:35 -0500

    created API endpoint for login

--- index.js
+++ index.js
@@ -24,6 +24,26 @@ server.post("/api/register", (req, res) => {
     }
 });
 
+server.post("/api/login", (req, res) => {
+    if (req.body.username && req.body.password && typeof req.body.username === "string" && typeof req.body.password === "string") {
+        let user = req.body;
+        db("users")
+            .where("username", user.username)
+            .then(dbUsers => {
+                if (dbUsers.length 
+                    && bcrypt.compareSync(user.password, dbUsers[0].password)) {
+                        res.status(200).json({ message: "Logged In Successfully" });
+                    } else {
+                        res.status(422).json({ error: "Incorrect username or password" })
+                    }
+            }).catch(error => {
+                res.status(500).json({message: "Error logging in", error: error});
+            });
+    } else {
+        res.status(400).json({ error: "Incorrectly formatted user data" });
+    }
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
