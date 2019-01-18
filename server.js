325acde0d5ed7ee52e34e3140ffc550e1e3a743d
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-01-17 23:54:19 -0700

    Backend Requests GET and POST created
    Users can be retrieved with GET, Users can be created with POST, Users can be authenticated using POST and login endpoint

--- server.js
+++ server.js
@@ -9,6 +9,45 @@ server.use(express.json())
 
 const PORT = 4444;
 
+server.get('/api/users', (req, res) => {
+    db('users')
+    .then(info => {
+        res.json(info)})
+        .catch(err => {
+            res.status(500).json({err: `${err} Something went wrong`})
+        })
+})
+
+server.post('/api/register', (req,res) => {
+    const user = req.body;
+    const missing = ['username', 'password', 'registered'].filter(item => {return user.hasOwnProperty(item) === false})
+    if(missing.length===0)
+    {db('users').insert(user)
+    .then(ids => {
+        res.status(201).json(res.json({message: `user ${user.username} added`}))})
+    .catch(err => {
+        res.status(500).json({err: `Could not add user! ${err}`})
+    })
+    }
+
+    else {res.status(500).json({message: `missing info: ${missing}`})}
+    })
+
+    server.post('/api/login', (req,res) => {
+        const user = req.body;
+        
+        db('users').where('username', user.username)
+        .then(users => {
+            if (users.length && user.password === users[0].password){
+                res.json({message: `Success!`})}
+            else {
+                res.json({message: `invalid username or password`})
+            }})
+        .catch(err => {
+            res.status(500).json({message: `Could not login`})
+        })})
+
+
 
 server.listen(PORT, (err) => {
     if (err) {console.log(err)}
