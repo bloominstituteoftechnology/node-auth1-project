e7aa5b4c16fd7e41daa24aa7a576a0ebe31aea71
efleming111 <efleming111@yahoo.com>
2019-01-17 21:36:11 -0700

    Added sessions and cookies

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const knex = require('knex');
 const knexConfig = require('./knexfile');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const server = express();
 const db = knex(knexConfig.development);
 
 server.use(express.json());
+server.use(session({
+    name: 'notsession',
+    secret: 'Thats eighty hundred',
+    cookie:{
+        maxAge: 1 * 24 * 60 * 60 * 1000
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false
+}));
 
 server.post('/api/register', (req, res)=>{
     const user = req.body;
@@ -33,6 +44,7 @@ server.post('/api/login', (req, res)=>{
         .where('username', user.username)
         .then(users=>{
             if(users.length && bcrypt.compareSync(user.password, users[0].password)){
+                req.session.userId = users[0].id;
                 res.json({info: `Welcome ${user.username}`});
             }
             else{
@@ -48,15 +60,31 @@ server.post('/api/login', (req, res)=>{
     }
 })
 
+server.post('/api/logout', (req, res)=>{
+    req.session.destroy(error=>{
+        if(error){
+            res.status(500).json({errorMessage: 'Logout Failed'});
+        }
+        else{
+            res.json({info: 'Logged Out'});
+        }
+    })
+})
+
 server.get('/api/users', (req, res)=>{
-    db('users')
-    .select('id', 'username', 'password') // PASSWORD ONLY FOR TESTING
-    .then(users=>{
-        res.json(users);
-    })
-    .catch(error=>{
-        res.status(500).json({error: 'Failed to return users'})
-    })
+    if(req.session && req.session.userId){
+        db('users')
+        .select('id', 'username')
+        .then(users=>{
+            res.json(users);
+        })
+        .catch(error=>{
+            res.status(500).json({error: 'Failed to return users'})
+        })
+    }
+    else{
+        res.status(401).json({errorMessage: 'Access Denied'});
+    }
 })
 
 server.listen(3300, ()=>console.log('Starting server'));
