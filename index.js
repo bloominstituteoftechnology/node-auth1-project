f002be4c243337493db4f072ed0631574732bf77
Jess <jessdev83@gmail.com>
2019-01-17 23:29:00 -0500

    Day 2 Complete
    sessions added, middleware stretch goal complete.

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
+const session = require("express-session");
 
 const PORT = 3300;
 const db = require("./data/dbConfig.js");
 
 const server = express();
 
+const protect = (req, res, next) => {
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.status(401).send("access denied");
+  }
+};
+
 server.use(express.json());
 server.use(cors());
+server.use(
+  session({
+    name: "notsession",
+    secret: "nobody tosses a dwarf!",
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 server.post("/api/register", (req, res) => {
   const newUser = req.body;
@@ -32,16 +53,37 @@ server.post("/api/login", (req, res) => {
         users.length &&
         bcrypt.compareSync(user.password, users[0].password)
       ) {
-        res.status(202).json({ message: "login successful" });
+        req.session.userId = users[0].id;
+        res.status(202).send("login successful");
       } else {
-        res.status(401).json({ message: "You shall not pass!" });
+        res.status(401).send("You shall not pass!");
       }
     })
     .catch(err => {
-      res.status(500).json(err);
+      res.status(500).send(err);
     });
 });
 
+server.get("/api/restricted/users", protect, (req, res) => {
+  db("users")
+    .select("id", "username")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+server.post("/api/logout", (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send("login successful");
+    }
+  });
+});
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
