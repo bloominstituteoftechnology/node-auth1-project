ae8cce2246b1daff3bfcccbfa313330f72c42569
Ben <bennybooftw@gmail.com>
2019-01-18 18:44:55 -0500

    Added sessions to login endpoint. Created protected functionality. Added get request for logging out and for displaying users if session is active.

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+server.use(
+  session({
+      name: 'session1',
+      secret: 'lambda secret',
+      cookie: {
+          maxAge: 1*24*60*60*1000,
+          secure: false,
+      },
+      httpOnly: true,
+      resave: false,
+      saveUninitialized: false,
+      store: new KnexSessionStore({
+          tablename: 'sessions',
+          sidfieldname: 'sid',
+          knex: db,
+          createtable: true,
+          clearInterval: 60*60*1000,
+      })
+  })
+);
+
+const protected = (req, res, next) => {
+  if (req.session && req.session.username) {
+      next();
+  } else {
+      res.status(401).json({ message: 'You must be logged in to access this page.' });
+  }
+}
+
 server.get('/', (req, res) => {
   res.send("Hello!");
 })
@@ -32,6 +61,17 @@ server.post('/api/register', (req, res) => {
     })
 })
 
+server.get('/api/users', protected, (req, res) => {
+  db('users')
+      .select('id', 'username', 'password')
+      .then(users => {
+          res.status(200).json(users);
+      })
+      .catch(err => {
+          res.status(500).json({ error: 'There was an error fetching the users', err });
+      });
+});
+
 server.post('/api/login', (req, res) => {
   const credentials = req.body;
   db('users')
@@ -39,6 +79,7 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        req.session.username = user.username;
         res.status(200).json({ message: "You have successfully logged in." })
       } else {
         res.status(400).json({ message: "Incorrect credentials." })
@@ -48,3 +89,15 @@ server.post('/api/login', (req, res) => {
       res.status(500).json(err)
     })
 })
+
+server.get('/api/logout', protected, (req, res) => {
+  if (req.session) {
+      req.session.destroy(err => {
+          if (err) {
+              res.status(500).json({ error: 'Error logging out', err });
+          } else {
+              res.status(200).json({ message: 'Logout successful.' });
+          }
+      });
+  }
+});
