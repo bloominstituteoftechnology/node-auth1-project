be1f2fe65d4bb974bbdf0b240b94742a1e2ac50c
Matthew Meitl <darksamus6@gmail.com>
2019-01-19 11:14:39 -0600

    added apis and loggin info

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 const express = require("express");
+const server = express();
 const knex = require("knex");
-const server = express(); 
 const dbConfig = require('./knexfile');
+const bcrypt = require('bcryptjs');
 const db = knex(dbConfig.development); 
 const PORT = 4000;
 
 server.use(express.json());
 
 
+
+server.get('/api/users', (req, res) => {
+    db('users').then( user => {
+        res.status(200).json(user)
+    }) 
+    .catch(error => { res.status(400).json({error: "there was an error"})})
+})
+
+
+
+server.post('/api/register', (req, res) => {
+    const user = req.body;
+    if(user.username && user.password){
+        user.password = bcrypt.hashSync(user.password);
+        db('users').insert(user)
+        .then( ids => {
+            res.status(201).json({id: ids[0]})
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        })
+    } else {
+        res.status(400).json({errorMessage: "Please provide a username and password"})
+
+    }
+});
+
+server.post('/api/login', (req, res) => {
+    const user = req.body;
+    if(user.username && user.password){
+        db('users').where('username', user.username)
+        .then(users => {
+            if(users.length && bcrypt.compareSync(user.password, users[0].password)){
+                res.json({info: "You are now logged in"})
+            } else {
+                res.status(404).json({error: 'Incorrect username or password'})
+            }
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        })
+    } else {
+        res
+        .status(400).json({errorMessage: "Please provide your username and password"})
+    }
+}); 
 
 server.listen(PORT, () => {
     console.log("server is running in port " + PORT)
--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,9 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/lambda.sqlite3'
+      filename: './lambda.sqlite3'
     },
     useNullAsDefault:true
   },
