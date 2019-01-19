782f3e3db638e58af95328faa1d9538ed59a0b84
unknown <kyleteeter12@gmail.com>
2019-01-19 12:41:54 -0600

    Integrating with react App

---
+++ App/app/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    fetch('/api/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users}));
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        <ul>
+          {this.state.users.map(user =>
+            <li key={user.id}>{user.username}</li>
+            )}
+        </ul>
+         
+      </div>
+    );
+  }
+}
+
+export default App;
