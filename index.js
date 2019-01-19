71d1997500304099df355cf70cf82e2dff918687
Emmanium <pradoe.ep@gmail.com>
2019-01-19 11:29:41 -0700

    protected users endpoint with protectMiddleware

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const db = require('./database/dbHelpers.js');
 
 const server = express();
 
+function protectMiddleware(req, res, next) {
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.status(400).send('You shall not pass!')
+  }
+}
+
 server.use(express.json());
 server.use(cors());
+server.use(session({
+  name: 'notsession', // default is connect.sid
+  secret: 'nobody tosses a dwarf!',
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000
+  }, // 1 day in milliseconds
+  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+  resave: false,
+  saveUninitialized: false,
+}))
 
 server.post('/api/register', (req, res) => {
   const user = req.body;
@@ -35,8 +55,14 @@ server.post('/api/login', (req, res) => {
     })
 })
 
-server.get('/api/users', (req, res) => {
-
+server.get('/api/users', protectMiddleware, (req, res) => {
+  db.getUsers()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Server go boom' });
+    })
 })
 
 const PORT = 3300;
