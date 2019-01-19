9c1f89af3f590f1e2e30c97a82aadb34c8bd9965
Julie Jonak <juliejonak10@gmail.com>
2019-01-19 12:06:27 -0600

    Setting up Register component

--- login/src/components/register.js
+++ login/src/components/register.js
@@ -2,30 +2,66 @@ import React from 'react';
 import '../App.css';
 
 
-const Register = () => {
+class Register extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    inputHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleKeyUp = (e) => {
+        if(e.keyCode === 13) return this.register()
+    }
+
+    register = (e) => {
+        e.preventDefault();
+        const newUser = {
+            Username: this.state.username,
+            Password: this.state.password
+        }
+        this.props.register(newUser);
+
+        
+    }
+
+    render(){
     return(
         <div>
             <h1>Register for the Authenticator!</h1>
-            <form>
+            <form onSubmit={this.register} onKeyUp={this.handleKeyUp}>
                 <div>
-                    <label for="name">
-                        Username:
-                    </label>
-                    <input type ="text" id="username" name="username"></input>
+                    <input 
+                        onChange={this.inputHandler}
+                        type ="text" 
+                        placeholder="Username"
+                        value={this.state.username}
+                        name="username"
+                    ></input>
                 </div>
 
                 <div>
-                    <label for="password">
-                        Password:
-                    </label>
-                    <input type="text" id="password" name="password"></input>
+                    <input 
+                        onChange={this.inputHandler}
+                        type ="password" 
+                        placeholder="Password"
+                        value={this.state.password}
+                        name="password"
+                    ></input>
                 </div>
+
                 <div class="button">
                     <button type="submit">Submit</button>
                 </div>
             </form>
         </div>
     )
+    }
 }
 
 export default Register;
