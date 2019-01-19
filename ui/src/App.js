fc5df23138b3d23608bef967e96f33713f37f6ee
Jason Hedrick <jasonleehedrick@gmail.com>
2019-01-19 11:16:09 -0800

    Began working on the react UI but I wont be able to complete it in time.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Link class='main-links' to='/register'>Register</Link>
+        <Link class='main-links' to='/login'>Login</Link>
+        <Link class='main-links' to='/users'>Users</Link>
       </div>
     );
   }
