3a4c65a3f08a01419a816d2c1ffbb4028022f7e7
shannonhealy <sahealy525@gmail.com>
2019-01-23 12:44:44 -0500

    fixed wrong table name in exports.down

--- data/migrations/20190116222311_users.js
+++ data/migrations/20190116222311_users.js
@@ -8,5 +8,5 @@ exports.up = function (knex, Promise) {
 };
 
 exports.down = function (knex, Promise) {
-    return knex.schema.dropTableIfExists("projects");
+    return knex.schema.dropTableIfExists("users");
 };
