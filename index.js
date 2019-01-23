1c8ad1c41d883f4c3bc063a603967d955f7385ed
chasefulks <chase.fulks@gmail.com>
2019-01-22 18:03:14 -0600

    mvp done

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+
+ const restrictedRoutes = require('./restrictedRoutes');
+
+ const server = express();
+const db = knex(knexConfig.development);
+
+ server.use(express.json());
+server.use(session({
+    name: 'notsession',
+    secret: 'getit',
+    cookie:{
+        maxAge: 1 * 24 * 60 * 60 * 1000
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false
+}));
+
+ server.use('/api/restricted', restrictedRoutes);
+
+ server.post('/api/register', (req, res)=>{
+    const user = req.body;
+    if(user.username && user.password){
+        user.password = bcrypt.hashSync(user.password);
+        db('users')
+        .insert(user)
+        .then(ids=>{
+            res.status(201).json({id: ids[0]});
+        })
+        .catch(error=>{
+            res.status(500).json({error: 'Failed to add user'});
+        })
+    }
+    else{
+        res.status(400).json({errorMessage: 'Please include a username and password'});
+    }
+})
+
+ server.post('/api/login', (req, res)=>{
+    const user = req.body;
+    if(user.username && user.password){
+        db('users')
+        .where('username', user.username)
+        .then(users=>{
+            if(users.length && bcrypt.compareSync(user.password, users[0].password)){
+                req.session.userId = users[0].id;
+                res.json({info: `Welcome ${user.username}`});
+            }
+            else{
+                res.status(404).json({errorMessage: 'Invalid username or password'});
+            }
+        })
+        .catch(error=>{
+            res.status(500).json({error: 'Failed'});
+        })
+    }
+    else{
+        res.status(400).json({errorMessage: 'Please include a username and password'});
+    }
+})
+
+ server.post('/api/logout', (req, res)=>{
+    req.session.destroy(error=>{
+        if(error){
+            res.status(500).json({errorMessage: 'Logout Failed'});
+        }
+        else{
+            res.json({info: 'Logged Out'});
+        }
+    })
+})
+
+ server.get('/api/users', (req, res)=>{
+    if(req.session && req.session.userId){
+        db('users')
+        .select('id', 'username')
+        .then(users=>{
+            res.json(users);
+        })
+        .catch(error=>{
+            res.status(500).json({error: 'Failed'})
+        })
+    }
+    else{
+        res.status(401).json({errorMessage: 'Access Denied'});
+    }
+})
+
+ server.listen(5000, ()=>console.log('Starting server'));
---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+module.exports = {
+
+  development: {
+   client: 'sqlite3',
+   connection: {
+     filename: './data/users.sqlite3'
+   },
+   useNullAsDefault: true,
+   migrations:{
+     directory: './data/migrations'
+   }
+ }
+
+};
