c141486b175288630f2f60d0a443a8e849626454
Tommmy Collison <tommy@collison.ie>
2019-01-29 19:20:13 -0800

    users and register endpoints

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs')
 
 const server = express();
 
@@ -9,5 +10,29 @@ server.use(cors());
 server.get('/', (req, res) => {
     res.send(`I'm alive!`)
 });
+
+server.get('/api/users', (req, res) => {
+    db('users')
+    .select('id', 'username', 'password')
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => res.send(err))
+})
+
+server.post('/api/register', (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 14)
+    db('users').insert(user)
+    .then(ids => {
+        res.status(201).json({id: ids[0]})
+    })
+    .catch(error => {
+        res.status(500).send(error)
+    })
+})
+
+server.post('/api/login', (req, res) => {
+})
 
 server.listen(2525, () => console.log(`I'm alive!`))
