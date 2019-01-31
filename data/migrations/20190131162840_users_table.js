b4673f2488d6084a3bbecd262ff8a857964a0b83
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-01-31 16:33:46 -0600

    spelling mistake fix on knex migration

--- data/migrations/20190131162840_users_table.js
+++ data/migrations/20190131162840_users_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users',
     table => {
-      tablele.increments()
+      table.increments()
       table.string('user_name', 128).notNullable().unique()
       table.string('password').notNullable()
     })
