bcf4ce152c9c9ea65ba062bf42fff2da4c0f2255
Tommmy Collison <tommy@collison.ie>
2019-01-30 19:49:01 -0800

    login parameters added and tested

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
 
 const db = require('./database/dbConfig');
 
@@ -8,22 +9,39 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
+server.use(session({
+    // configure express-session middleware
+      name: 'notsession', // default is connect.sid
+      secret: 'nobody expects the Spanish inquisition!',
+      cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+      }, // 1 day in milliseconds
+      httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+      resave: false,
+      saveUninitialized: false,
+  }))
+  
 
 server.get('/', (req, res) => {
     res.send(`I'm alive!`)
 });
 
 server.get('/api/users', (req, res) => {
-    db('users')
-    .select('id', 'username', 'password')
-    .then(users => {
-        res.json(users)
-    })
-    .catch(err => res.send(`You shall not pass!`))
+    if(req.session && req.session.userId){
+        db('users')
+        .select('id', 'username', 'password')
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => res.send(`You shall not pass!`))
+    } else {
+        res.status(400).send(`access denied`)
+    }
 })
 
 server.post('/api/register', (req, res) => {
     const user = req.body;
+    console.log(`Tommy's session in the register post req:`, req.session)
     user.password = bcrypt.hashSync(user.password, 14)
     db('users').insert(user)
     .then(ids => {
@@ -39,6 +57,7 @@ server.post('/api/login', (req, res) => {
     db('users').where('username', userBody.username)
     .then(users => {
         if(users.length && bcrypt.compareSync(userBody.password, users[0].password)){
+            req.session.userId = users[0].id
             res.json(`Correct`)
         } else {
             res.status(404).json(`You shall not pass!`)
