c5c35e52611871ccd16749950d2c26e4a587e638
Mitchell Robles <mitchellr92@live.com>
2019-01-30 18:23:44 -0800

    helper file created

---
+++ data/dbHelpers.js
@@ -0,0 +1,13 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development);
+
+module.exports = {
+    insertUser: (user) => {
+        return db('users').insert(user);
+    },
+
+    findByUsername: (username) => {
+        return db('users').where('username', username);
+    }
+};
