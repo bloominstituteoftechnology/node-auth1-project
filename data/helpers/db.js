22a7343bed1a48a054155e9fc5e014457f90dbd3
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-01-31 17:26:41 -0600

    authRoute and dbHelper done

--- data/helpers/db.js
+++ data/helpers/db.js
@@ -4,5 +4,11 @@ const dbConfig = require('../../knexfile')
 const db = knex(dbConfig.development)
 
 module.exports = {
-  
+  register: (creds) => {
+    return db('users').insert(creds)
+  },
+
+  login: (user_name) => {
+    return db('users').where('user_name', user_name)
+  }
 }
--- data/routes/authRoute.js
+++ data/routes/authRoute.js
@@ -1,8 +1,49 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 
 const db = require('../helpers/db')
 
 //endpoints
+router.post('/register', (req, res) => {
+  const creds = req.body //grab username/password
+  const hash = bcrypt.hashSync(creds.user_name, 14) // hash password
+  creds.password = hash
+  
+  db.register(creds)
+    .then(id => {
+      res
+        .status(201)
+        .json(id)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({message: 'registration failed'})
+    })
+})
+
+router.post('/login', (req, res) => {
+  const creds = req.body
+  db.login(creds.user_name)
+    .then(user => {
+      if(user && bcrypt.compareSync(creds.password, user.password)) {
+        res
+          .status(201)
+          .json({message: 'Welcome'})
+      } else {
+        res
+          .status(401)
+          .json({message: 'You shall not pass!'})
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({message: 'Login Failed'})
+    })
+})
+
+
 
 module.exports = router;
