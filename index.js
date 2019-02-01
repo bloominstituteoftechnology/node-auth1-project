6a5ad7222da7912867ac6e503eed03a31f7f306d
cocoitali <cmburatto@gmail.com>
2019-02-01 10:24:54 -0700

    MVP : added cors as dependency, added to server

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const db = knex(knexConfig.development)
 const PORT = 5100
 const server = express()
 server.use(express.json())
+server.use(cors)
+
+
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
