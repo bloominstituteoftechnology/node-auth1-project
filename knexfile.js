90156a73edd8322cad2fb50a17988064cc2882ac
cocoitali <cmburatto@gmail.com>
2019-02-01 10:54:28 -0700

    MVP : created db Config file to clean up index.js

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/dev.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
