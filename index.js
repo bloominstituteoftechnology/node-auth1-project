cf78361616bedd5121b696224d22f9b990dfb4bf
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-01-31 23:07:25 -0500

    Added bcrypt, added boilerplate to index.

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express") ;
+const helmet = require('helmet') ;
+const logger = require('morgan') ;
+const port = process.env.port || 3254 ;
+const server = express.server() ;
+
+server.use(
+ helmet(),
+ logger(),
+ express.json()
+)
+
+server.use('/api/register',)
+server.use('/api/login', )
+
+server.listen(port, () => {
+ console.log(`Server is running live on ${port}`)
+})
