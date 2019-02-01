2affa4a15f31c8597744aebdb0d0ddc976f07d57
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-01-31 23:56:50 -0500

    Adding more boilerplate/requirements as necessary.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express") ;
 const helmet = require('helmet') ;
 const logger = require('morgan') ;
+const bcrypt = require('bcryptjs') ;
+const loginRouter = require('./data/routes/loginRoute') ;
+const registerRouter = require('./data/routes/registerRoute') ; 
 const port = process.env.port || 3254 ;
 const server = express.server() ;
 
@@ -8,11 +11,11 @@ server.use(
  helmet(),
  logger(),
  express.json()
-)
+) ;
 
-server.use('/api/register',)
-server.use('/api/login', )
+server.use('/api/register',) ;
+server.use('/api/login', ) ;
 
 server.listen(port, () => {
  console.log(`Server is running live on ${port}`)
-})
+}) ;
