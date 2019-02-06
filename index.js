733de92f6cd8e5847c2b535cb9fa2e85c9bd4822
Mitchell Robles <mitchellr92@live.com>
2019-02-05 20:19:27 -0800

    added sessions and cookies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const db = require('./data/dbHelpers.js');
 
@@ -9,21 +10,39 @@ const server = express();
 server.use(express.json())
 server.use(cors())
 
+server.use(
+    session({
+        name: "session1",
+        secret: "nobody tosses a dwarf!",
+        cookie: {
+            maxAge: 1 * 24 * 60 * 60 * 1000
+        },
+        httpOnly: true,
+        resave: false,
+        saveUninitialized: false
+    })
+)
+
 const PORT = 4400;
 
 server.get('/api/users', (req, res) => {
-    db.getUsers()
-        .then(users => {
-            res.json(users)
-        })
-        .catch(err => {
-            res.status(500).send(err)
-        });
+    if (req.session && req.session.userId) {
+        db.getUsers()
+            .then(users => {
+                res.json(users)
+            })
+            .catch(err => {
+                res.status(500).send(err)
+            });
+    } else {
+        res.status(400).send('You shall not pass!');
+    }
 });
 
 server.post('/api/register', (req, res) => {
     const user = req.body;
-    user.password = bcrypt.hashSync(user.password, 14);
+    console.log('session', req.session);
+    user.password = bcrypt.hashSync(user.password, 10);
     db.insertUser(user)
         .then(([id]) => {
             res.status(201).json({ id });
@@ -34,10 +53,13 @@ server.post('/api/register', (req, res) => {
 });
 
 server.post('/api/login', (req, res) => {
+
+
     const bodyUser = req.body;
     db.findByUsername(bodyUser.username)
         .then(users => {
             if (users.length && bcrypt.compareSync(bodyUser.password, users[0].password)) {
+                req.session.userId = users[0].id;
                 res.json({ info: 'Logged in' })
             } else {
                 res.status(404).json({ err: 'You shall not pass!' })
@@ -47,5 +69,6 @@ server.post('/api/login', (req, res) => {
             res.status(500).send(err);
         })
 })
+// whatever
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
