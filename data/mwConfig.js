019ddfb9d56a4cdf4a9f1b56af8d6a3e6b42c457
cocoitali <cmburatto@gmail.com>
2019-02-05 23:56:14 -0700

    MVP : added mw to protect Get /users endpoint, only logged in users see data

--- data/mwConfig.js
+++ data/mwConfig.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const helmet= require('helmet')
 const logger = require('morgan')
-const cors = require('cors')
+// const cors = require('cors')
 const session = require('express-session')
 
 const sessionConfig = {
     name: 'notsession', // default is connect.sid, change it to something else
     secret: 'nobody tosses a dwarf!',
     cookie: {
-      maxAge: 1 * 24 * 60 * 60 * 1000,// 1 day in milliseconds
+      maxAge:  1000 * 15,// 1 day in milliseconds 1 * 24 * 60 * 60 * 1000
       secure:  false //set to true during production, false during development // only set cookies over https. Server will not send back a cookie over http.
     }, 
     httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
@@ -17,6 +17,6 @@ const sessionConfig = {
   }
 
 module.exports = server => {
-    server.use(express.json(),logger('tiny'), cors(), helmet())
+    server.use(express.json(),logger('tiny'), helmet()) //cors(),
     server.use(session(sessionConfig))
 }
--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require('express')
 const bcrypt = require('bcryptjs')
 const mwConfig = require('./data/mwConfig')
 const db = require('./data/dbConfig.js')
+// const session = require('express-session')
+
+// const sessionConfig = {
+//   name: 'notsession', // default is connect.sid, change it to something else
+//   secret: 'nobody tosses a dwarf!',
+//   cookie: {
+//     maxAge: 1 * 15, // 1 day in milliseconds 1 * 24 * 60 * 60 * 1000
+//     secure: false //set to true during production, false during development // only set cookies over https. Server will not send back a cookie over http.
+//   },
+//   httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+//   resave: false, //for compliance with US law
+//   saveUninitialized: true
+// }
 
 const PORT = 5100
 const server = express()
-server.use(express.json())
-
+// server.use(express.json())
+// server.use(session(sessionConfig))
 mwConfig(server)
 
 server.post('/register', (req, res) => {
@@ -30,16 +43,28 @@ server.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
-        req.session.userId = user.id 
+        req.session.userId = user.id
         res.status(200).json({ message: `${user.username} is logged in` })
       } else {
         res.status(401).json({ message: 'You shall not pass!' })
       }
     })
-    .catch(() => res.status(500).json({message: 'Please try logging in again.'}))
+    .catch(() =>
+      res.status(500).json({ message: 'Please try logging in again.' })
+    )
 })
 
-server.get('/users', (req, res) => {
+function protected(req, res, next) {
+  if (req.session && req.session.userId) {
+    next()
+  } else {
+    res.status(401).json({ message: 'You shall not pass, not authenticated.' })
+  }
+}
+
+//protect this endpoint so only logged in users can see the data
+server.get('/users', protected, (req, res) => { //  add : async -----if uncomment the below code
+  // const users = await db('users')
   db('users')
     .select('id', 'username', 'password') //<----NEVER EVER SEND THE PASSWORD BACK TO THE CLIENT, THIS IS WHAT NOT TO DO!!!
     .then(users => {
