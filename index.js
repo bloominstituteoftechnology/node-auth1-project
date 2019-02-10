390a1753c81c4c6aedc4fe2bfd8221720ed5a398
Obed Lorisson <Obed.lorisson@gmail.com>
2019-02-10 04:26:18 -0500

    add express-session configure endpoint to use sessions and cookie created middleware to protect endpoint from unauthenthicatedusers add connect-session-knex to persist  the sessions and add to the dataabse

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan')
-const cors = require('cors')
+const express  = require('express');
+const session  = require('express-session');
+const sessionStore = require('connect-session-knex')(session);
+const helmet   = require('helmet');
+const morgan   = require('morgan')
+const cors     = require('cors')
 
-const bcrypt = require('bcryptjs');
-const db = require('./data/dbConfig.js')
+const bcrypt   = require('bcryptjs');
+const db       = require('./data/dbConfig.js')
 
-const PORT = 5050;
+const sessionsConfig = {
+  name: 'notsession', // default is connect.sid
+  secret: 'nobody tosses a dwarf!',
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    secure: false, // only set cookies over https. Server will not send back a cookie over http.
+  }, // 1 day in milliseconds
+  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+  resave: false,
+  saveUninitialized: false,
+  store: new sessionStore({
+    tablename:'sessions',
+    sidfieldname: 'sid',
+    knex : db,
+    createtable: true,
+    clearInterval: 1000 * 60 * 10,
+  }),
+}
+
+const PORT   = 5050;
 
 const server = express();
 server.use(express.json()); 
@@ -15,25 +36,16 @@ server.use(
             helmet(),
             morgan('dev'),
           );
+server.use(session(sessionsConfig))
 //endpoints
 
-server.get('/api/users',(req, res) =>{
-  db('users')
-  .select('id', 'username')
-  .then(users => {
-    res.json(users);
-  })
-  .catch(err => res.send(err));
-})
-
 server.post("/api/login",(req, res) =>{
-  const user = req.body;
+  const creds = req.body;
   db("users")
-    .where("username", user.username)
-    .then(users =>{
-    if( users.length && 
-        bcrypt.compareSync(user.password, users[0].password)
-    ){
+    .where("username", creds.username).first()
+    .then(user => { 
+    if( user && bcrypt.compareSync(creds.password, user.password)){
+      req.session.user = user
       res.status(202).send("user logged in");
     }
     else{
@@ -57,6 +69,37 @@ server.post("/api/register", (req, res) => {
       res.status(500).send(err);
     });
 });
+
+function protected(req, res, next ){
+  if (req.session && req.session.user){
+  next();
+  }else{
+    res.status(401).json({msg:"Not authenticated"})
+  }
+}
+
+server.get('/api/users',protected ,(req, res) =>{
+  db('users')
+  .select('id', 'username')
+  .then(users => {
+    res.json(users);
+  })
+  .catch(err => res.send(err));
+})
+
+server.get('/api/logout', (req, res) => {
+  if(req.session){
+    req.session.destroy(err =>{
+      if(err){
+        res.status(500).send("server error unable to logout ")
+      }else{
+        res.status(200).send("user completely logout")
+      }
+    })
+  }else{
+    res.json({msg:"already logoff"})
+  }
+})
 
 
 //listen
