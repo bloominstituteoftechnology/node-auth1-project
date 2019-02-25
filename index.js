162dca1298701c25705df766e564ad53f9a7465e
Matt Basile <matthewbasile2@gmail.com>
2019-02-25 14:18:01 -0500

    Adding users with encrypted pws

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const knex = require('knex');
 const knexConfig = require('./knexfile')
-
+const bcrypt = require('bcryptjs');
 const db = knex(knexConfig.development);
+const Users = require('./users/users-model.js');
 const server = express();
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send("It's alive!");
+  });
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+    // generate hash from user's password
+    const hash = bcrypt.hashSync(user.password, 12); // 2^ n
+    // override user.password with hash
+    user.password = hash 
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
 
 
---
+++ users/users-model.js
@@ -0,0 +1,30 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile')
+const db = knex(knexConfig.development);
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+};
+
+function find() {
+  return db('users').select('id', 'username', 'password');
+}
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
+
+async function add(user) {
+  const [id] = await db('users').insert(user);
+
+  return findById(id);
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
