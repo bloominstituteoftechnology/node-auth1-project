8d4cca30581ddece171b51dbd9e10123e2a4b990
RachelKolk <rach.m.obrien@gmail.com>
2019-02-25 16:49:28 -0500

    users model

--- users/users-model.js
+++ users/users-model.js
@@ -1,6 +1,32 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
-
-
+  find,
+  findBy,
+  add,
+  findById,
 };
+
+function find() {
+    return db('users')
+      .select('id', 'username', 'password');
+}
+
+function findBy(filter) {
+    return db('users')
+      .where(filter);
+}
+
+async function add(user) {
+    const [id] = await dr('users')
+      .insert(user);
+
+    return findById(id);
+}
+
+function findById(id) {
+    return db('users')
+      .where({id})
+      .first();
+}
+
