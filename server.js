27242acbd18cc831c1e750ebbff8d62cc7608be4
RachelKolk <rach.m.obrien@gmail.com>
2019-02-25 17:16:58 -0500

    both post endpoints - /api/register & api/login

--- server.js
+++ server.js
@@ -11,8 +11,43 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+
 server.get('/', (req, res) => {
     res.send("We did the mash ---- we did the Monster Mash!");
+});
+
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 12);
+
+    user.password = hash;
+
+    Users.add(user)
+      .then(saved => {
+          res.status(201).json(saved);
+      })
+      .catch(error => {
+          res.status(500).json(error);
+      });
+});
+
+server.post('/api/login', (req, res) => {
+    let {username, password} = req.body;
+
+    Users.findBy({username})
+      .first()
+      .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+              res.status(200).json({message: `Welcome ${user.username}!`});
+          } else {
+              res.status(401).json({message: 'You shall not pass!'});
+          }
+      })
+      .catch(error => {
+          res.status(500).json(error);
+      });
 });
 
 module.exports = server;
