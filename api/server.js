6c8797f6ce30e72445ed7deb26e0017a0cf7ac2e
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-25 15:03:25 -0700

    GET endpoint to display user data, restrict access by header data

--- api/server.js
+++ api/server.js
@@ -13,6 +13,27 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+// check if user is logged in
+async function restricted(req, res, next) {
+  let { username, password } = req.headers;
+  try {
+    if (username && password) {
+      const userData = await db('users')
+        .where({ username })
+        .first();
+      if (userData && bcrypt.compareSync(password, userData.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'Invalid credentials.' });
+      }
+    } else {
+      res.status(400).json({ message: 'No credentials provided.' });
+    }    
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}
+
 server.get('', (req, res) => {
   res.status(200).json({ message: "Server up!" });
 });
@@ -39,14 +60,13 @@ server.post('/api/register', async (req, res) => {
 
 // endpoint to login a user
 server.post('/api/login', async (req, res) => {
-  const loginData = req.body;
+  const { username, password } = req.body;
   try {
-    console.log("loginData.username: ", loginData.username);
     const userData = await db('users')
-      .where({ username: loginData.username })
+      .where({ username })
       .first();
     console.log("userData: ", userData);
-    if (userData && bcrypt.compareSync(loginData.password, userData.password)) {
+    if (userData && bcrypt.compareSync(password, userData.password)) {
       res.status(200).json({ message: `Welcome ${userData.username}!`});
     } else {
       res.status(401).json({ message: `Invalid login, try again.` });
@@ -55,5 +75,15 @@ server.post('/api/login', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+// endpoint to get user data IF user is logged in
+server.get('/api/users', restricted, async (req, res) => {
+  try {
+    const usersData = await db('users');
+    res.status(200).json(usersData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+})
 
 module.exports = server;
