204c912884472cca06a3d37527b6712413a8e04b
Matt Basile <matthewbasile2@gmail.com>
2019-02-25 14:19:22 -0500

    Added login functionality for verified users.

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ server.post('/api/register', (req, res) => {
         res.status(500).json(error);
       });
   });
+server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        // check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
 
 
