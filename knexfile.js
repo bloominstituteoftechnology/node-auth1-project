90568a69a9171b7e8c26cffc0a8d6271cd0b897c
RachelKolk <rach.m.obrien@gmail.com>
2019-02-25 15:17:51 -0500

    dependencies and knex file

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/authentication.db3',
+    },
+    useNullAsDefault: true,
+  },
+
+
+};
