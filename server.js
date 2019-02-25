bfc825cb7b117462f99d147e39738002bfcfb24b
Brandon <Blent1050@yahoo.com>
2019-02-25 17:22:21 -0500

    MVP completed

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs');
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-module.js');
 
+// const Authenticate = require('./Auth/authenticate');
 const server = express();
 
 server.use(helmet());
@@ -40,5 +41,34 @@ server.post('/api/login', (req, res) => {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
     });
+});
+
+function authenticate(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: ' Server Error' });
+      });
+  } else {
+    res.status(400).json({ message: 'No credentials Provided' });
+  }
+}
+
+server.get('/api/users', authenticate, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
 });
 module.exports = server;
