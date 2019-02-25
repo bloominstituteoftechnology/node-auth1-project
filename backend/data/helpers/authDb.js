ec5ac50fc79ff83230ec3268bcffffee521cc19b
stellarluminant <stellarluminant@gmail.com>
2019-02-25 11:39:40 -0800

    Authorization: session keys
    This is sooo not going to work I feel.

--- backend/data/helpers/authDb.js
+++ backend/data/helpers/authDb.js
@@ -4,19 +4,100 @@ const authConst = require('./authConst');
 
 // Attempts to create a new user entry in the database.
 const register = (email, username, password) => {
+    const user = {
+        email: email,
+        username: username,
+        password: authConst.generatePasswordHash(password),
+        permission: authConst.DEFAULT_ACCOUNT_PERMS
+    };
 
+    return db('users').insert(user);
 };
+
+// Returns a promise that checks if the email/passwords match.
+// If resolved, returns user id.
+const verify = (email, password) => {
+    return db('users').where({ email: email }).select('id', 'password').first()
+        .then(user => {
+            // Inside this then block, we have a matching email.
+            if (!authConst.checkPassword(password, user.password)) {
+                return Promise.reject('Invalid credentials')
+            }
+
+            return Promise.resolve(user.id);
+        })
+        .catch(_ => {
+            return Promise.reject('Invalid credentials');
+        });
+}
+
+// Returns a promise for deleting a session.
+const deleteSession = userId => {
+    return db('sessions').where({ user_id: userId }).del();
+}
+
+// Returns a promise for inserting a session.
+const insertSession = (userId, sessionKey) => {
+    const entry = {
+        user_id: userId,
+        session_key: sessionKey,
+        expire_date: authConst.getExpireDate()
+    };
+
+    return db('sessions').insert(entry);
+}
+
+// Checks if there is a valid session for this user id, and returns it.
+// Otherwise, returns a new session.
+const getSessionKey = userId => {
+    return db('sessions').where({ user_id: userId }).first()
+        .then(result => {
+            if (result.expire_date && !authConst.isDateExpired(result.expire_date)) {
+                // If the existing key hasn't expired, just use it.
+                return Promise.resolve(result.session_key);
+            }
+
+            // Delete the expired entry. 
+            return deleteSession(userId)
+                .then(_ => {
+                    // Generate a new session.
+                    let key = authConst.generateSessionKey();
+                    return insertSession(userId, key)
+                        .then(_ => {
+                            return Promise.resolve(key);
+                        });
+                });
+                
+        })
+        .catch(_ => {
+            // Generate a new session.
+            let key = authConst.generateSessionKey();
+            return insertSession(userId, key)
+                .then(_ => {
+                    return Promise.resolve(key);
+                });
+        })
+}
 
 // Attempts to create a new session.
 // If a session is valid and exists, return that session key.
 // Otherwise, create a new session key.
 const login = (email, password) => {
-
+    return verify(email, password)
+        .then(id => {
+            return getSessionKey(id);
+        });
 };
 
 // Checks if a specified session key exists, is not expired, and meets the permission requirement.
 const isAuthorized = (sessionKey, requirement) => {
-
+    return db('sessions').where({ session_key: sessionKey }).first()
+        .then(session => {
+            return db('users').where({ id: session.user_id }).select('permission').first()
+                .then(perm => {
+                    return authConst.permsAuthorized(perm, requirement) ? Promise.resolve() : Promise.reject();
+                });
+        });
 };
 
 // Module exports
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -12,14 +12,38 @@ const router = express.Router();
 // req.body.username - The username that the user is requesting.
 // req.body.password - The password of the new user.
 router.post('/register', (req, res) => {
+    if (!req.body || !req.body.email || !req.body.username || !req.body.password) {
+        res.status(400).json({ error: 'Must include email, username, and password in body' });
+        return;
+    }
 
+    db.register(req.body.email , req.body.username, req.body.password)
+        .then(_ => {
+            res.status(201).end();
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Unable to register an account' });
+        });
 });
 
 // POST /login - The user is requesting to access other routes, and to 'create a session'. Returns 201 with { sessionKey: [data] }.
 // req.body.email - The email of the user.
 // req.body.password - The password of the user.
 router.post('/login', (req, res) => {
+    if (!req.body || !req.body.email || !req.body.password) {
+        res.status(400).json({ error: 'Must include email and password in body' });
+        return;
+    }
 
+    db.login(req.body.email, req.body.password)
+        .then(sessionKey => {
+            res.status(201).json({ sessionKey: sessionKey });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Unable to login' });
+        });
 });
 
 // Router export for other files
