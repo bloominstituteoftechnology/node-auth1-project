4c6327973c50b3e8347ee0694064ac7ffbbdff00
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-26 15:52:20 -0700

    implement sessions, session storage

--- api/server.js
+++ api/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
+const db = require('../data/dbConfig.js');
 const Users = require('../users/users-module.js');
 const errors = {
   '19': 'Username taken, pick a different one.'
@@ -10,9 +13,28 @@ const errors = {
 
 const server = express();
 
+const sessionConfig = {
+  name: 'newSession',
+  secret: 'some big secret here',
+  cookie: {
+    maxAge: 1000 * 60,
+    secure: false,
+  },
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: 'sessions',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+};
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 const restricted = require('../custom-middlewares/auth.js');
 
@@ -30,6 +52,7 @@ server.post('/api/register', async (req, res) => {
     userData.password = hash;
     try {
       const userId = await Users.addUser(userData);
+      req.session.user = userData.username;
       res.status(201).json({ userId });
     } catch (error) {
       const msg = errors[error.errno] || error;
@@ -45,8 +68,9 @@ server.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
   try {
     const userData = await Users.findUserBy({ username });
-    console.log("userData: ", userData);
+    // console.log("userData: ", userData);
     if (userData && bcrypt.compareSync(password, userData.password)) {
+      req.session.user = userData.username;
       res.status(200).json({ message: `Welcome ${userData.username}!`});
     } else {
       res.status(401).json({ message: `You shall not pass!` });
