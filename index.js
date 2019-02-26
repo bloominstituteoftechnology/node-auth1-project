888703a43062df31eed45dd4601d5ec1f4d896ec
Randy Wilson <phoenixrr2113@gmail.com>
2019-02-26 18:44:44 -0500

    played around with sessions and found some cool bugs

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ const express = require('express');
 const helmet = require('helmet');
 // const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = require('./data/dbConfig.js');
 const Users = require('./userHelper/userHelpers');
 
 const server = express();
+
+const sessionConfig = {
+	name: 'auth-i_session',
+	secret: 'Shhhhhhhhhh',
+	cookie: {
+		maxAge: 1000 * 60 * 60,
+		secure: false,
+	},
+	httpOnly: true,
+	resave: false,
+	saveUninitialized: false,
+
+	store: new KnexSessionStore({
+		knex: db,
+		tablename: 'sessions',
+		sidfieldname: 'sid',
+		createTable: true,
+		clearInterval: 1000 * 60 * 60,
+	}),
+};
+
+server.use(helmet());
+server.use(express.json());
+// server.use(cors());
+server.use(session(sessionConfig));
 
 const auth = (req, res, next) => {
 	const { username, password } = req.body;
@@ -15,6 +42,7 @@ const auth = (req, res, next) => {
 		.first()
 		.then(user => {
 			if (user && bcrypt.compareSync(password, user.password)) {
+				req.session.user = user;
 				return next();
 			} else {
 				res.status(401).json('invalid creds');
@@ -23,9 +51,14 @@ const auth = (req, res, next) => {
 		.catch(err => res.status(500).json('server error'));
 };
 
-server.use(helmet());
-server.use(express.json());
-// server.use(cors());
+const restricted = (req, res, next) => {
+	console.log(req.session);
+	if (req.session && req.session.user) {
+		next();
+	} else {
+		res.status(401).json({ message: 'You shall not pass!' });
+	}
+};
 
 server.get('/', (req, res) => {
 	res.send("It's alive!");
@@ -39,6 +72,7 @@ server.post('/api/register', (req, res) => {
 
 	Users.add(user)
 		.then(newUser => {
+			req.session.user = newUser;
 			res.status(201).json(newUser);
 		})
 		.catch(err => res.status(500).json(err));
@@ -52,6 +86,30 @@ server.post('/api/login', auth, (req, res) => {
 			res.status(201).json(authenticatedUser);
 		})
 		.catch(err => res.json(err));
+});
+
+server.get('/api/users', restricted, (req, res) => {
+	Users.find()
+		.then(users => {
+			res.json(users);
+		})
+		.catch(err => res.send(err));
+});
+
+server.get('/api/logout', (req, res) => {
+	if (req.session) {
+		req.session.destroy(err => {
+			if (err) {
+				res.send(
+					'you can checkout any time you like, but you can never leave....',
+				);
+			} else {
+				res.send('bye, thanks for playing');
+			}
+		});
+	} else {
+		res.end();
+	}
 });
 
 const port = process.env.PORT || 5000;
