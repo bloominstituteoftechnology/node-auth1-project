e513181857ebdc0e93ad1c3b1ddfb9121cb2aa03
leianne louis <leilouis112@gmail.com>
2019-02-26 16:53:49 -0500

    completed restricted on user route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,27 +2,6 @@ const express = require('express');
 const db = require('../data/helpers/usersModel');
 const router = express();
 const bcrypt = require('bcryptjs');
-const session = require('express-session');
-const KnexSessionStore =require('connect-session-knex')(session);
-
-const sessionConfig = {
-    name: '__sess',
-    secret: 'you are awesome!',
-    cookie: {
-        maxAge: 1000 * 60 * 60,
-        secure: false
-    },
-    httpOnly: true,
-    resave: false,
-    saveUninitialized: false,
-    store: new KnexSessionStore({
-        knex: db,
-        tablename: 'session',
-        createTable: true,
-        clearInterval: 1000 * 60 * 60
-    })
-}
-
 
 const checkRegisterInfo = (req, res, next) =>{
     if(req.body.username && req.body.password) {
@@ -32,7 +11,16 @@ const checkRegisterInfo = (req, res, next) =>{
     }
 }
 
-router.get('/users', async (req, res) => {
+const restricted = (req, res, next) =>{
+    console.log(req.session.username)
+    if(req.session.username){
+        next()
+    } else {
+        res.status(404).json({message: "Please login!"})
+    }
+}
+
+router.get('/users', restricted, async (req, res) => {
     try{
         const users = await db.getUsers();
         if(users.length > 0){
@@ -66,10 +54,10 @@ router.post('/register', checkRegisterInfo, async (req, res) => {
 
 router.post('/login', async (req, res) => {
     const {username, password} = req.body;
-    
     try{
         const user = await db.findUser(username)   
         if(user && bcrypt.compareSync(password, user.password)) {
+            req.session.username = user.username;
             res.status(200).json({message: 'Credentials verified!'})
         } else{
             res.status(401).json({message: 'Invalid Credentials! Please try again!'})
--- server.js
+++ server.js
@@ -3,11 +3,26 @@ const logger = require('morgan');
 const helmet = require('helmet');
 const server = express();
 const authRoutes = require('./routes/authRoutes');
+const session = require('express-session');
+const KnexSessionStore =require('connect-session-knex')(session);
+const db = require('./data/helpers/usersModel');
+
+const sessionConfig = {
+    name: '__cookiee',
+    secret: 'you are awesome!',
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false
+}
 
 server.use(logger());
 server.use(helmet());
 server.use(express.json());
-
+server.use(session(sessionConfig))
 server.use('/api', authRoutes);
 
 module.exports = server;
