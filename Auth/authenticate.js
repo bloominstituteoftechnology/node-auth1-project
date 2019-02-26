b4e1ba2a1cb5e08bd1860ada0263581ded079ea5
Brandon <Blent1050@yahoo.com>
2019-02-26 15:32:52 -0500

    day 2 MVP completed - sessions and cookies added

--- Auth/authenticate.js
+++ Auth/authenticate.js
@@ -5,25 +5,10 @@ module.exports = {
   authenticate,
 };
 
-const Users = require('../users/users-module');
-
 function authenticate(req, res, next) {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({ message: ' Server Error' });
-      });
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(400).json({ message: 'No credentials Provided' });
+    res.status(401).json({ message: 'You shall not pass!' });
   }
 }
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-module.js');
@@ -11,9 +13,30 @@ const { authenticate } = require('./Auth/authenticate');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'Alpha-Romeo-Juliet',
+  secret: 'Check',
+  cookie: {
+    maxAge: 1000 * 60 * 60, // in ms
+    secure: false, // used over https only
+  },
+  httpOnly: true, // cannot access the cookie from js using document.cookie
+  resave: false,
+  saveUninitialized: false, // GDPR laws against setting cookies automatically
+
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60, // in ms
+  }),
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use('/api/restricted', authenticate, restrictedRouter);
 
@@ -27,10 +50,11 @@ server.post('/api/register', (req, res) => {
   user.password = hash;
   Users.add(user)
     .then(saved => {
+      req.session.user = saved;
       res.status(201).json(saved);
     })
     .catch(err => {
-      res.status(500).json(error);
+      res.status(500).json(err);
     });
 });
 
@@ -40,6 +64,7 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
