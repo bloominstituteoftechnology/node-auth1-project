1201a3384551e77dcb9cddb5fbbbafa6c2a836b9
umbur <work1415@gmail.com>
2019-03-05 15:24:04 -0700

    installed/configured necessary dependencies as well as created logout endpoint for storing sessions/cookies in order to login/logout

--- index.js
+++ index.js
@@ -2,93 +2,112 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const bcrypt = require('bcryptjs')
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = require('./helpers/model.js');
 const Users = require('./helpers/users-model')
 const server = express();
 
+const sessionConfig = {
+  name: 'cookies',
+  secret: 'the secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+  },
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+}
+
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+server.use(session(sessionConfig));
 
-server.post("/api/login", (req, res) => {
-    const creds = req.body;
-  
-    db("users")
-      .where({ username: creds.username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(creds.password, user.password)) {
-          res.status(200).json({ message: "Logged in" });
-        } else {
-          res.status(401).json({ message: "You shall not pass!" });
-        }
-      })
-      .catch(err => {
-          res.status(500).json({
-            error: "Error logging user into the database."
-          });
-        });
-  });
-  
-  server.post("/api/register", (req, res) => {
-    const creds = req.body;
-  
-    const hash = bcrypt.hashSync(creds.password, 8);
-  
-    creds.password = hash;
-  
-    db("users")
-      .insert(creds)
-      .then(ids => {
-        res.status(201).json(ids);
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: "Error registering user to the database."
-        });
-      });
-  });
+server.get('/', (req, res) => {
+  res.send("It's alive!");
+});
 
-  function restricted (req, res, next) {
-    // we'll read the username and password from headers
-    // when testing the endpoint add these headers in Postman
-    const { username, password } = req.headers
-  
-    if ( username && password ) {
-      Users.findBy({ username })
-      .first()
-      .then(user => {
-        //here user is the object being passed in which is why you use user dot password to check it.
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "Invalid Creds"})
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error)
-      })
+server.post('/api/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      // check that passwords match
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user; // 4. this
+        res
+          .status(200)
+          .json({ message: `Welcome ${user.username}!, have a cookie...` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(() => {
+      res.status(500).send('Please make sure you have provided username and password');
+    });
+});
+ 
+server.post('/api/register', (req, res) => {
+  let user = req.body;
+
+  // generate hash from user's password
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+
+  // override user.password with hash
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      req.session.user = saved;
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+ 
+  function restricted(req, res, next) {
+    if(req.session && req.session.user) {
+      next();
     } else {
-      res.status(400).json({ messages: "No Creds Provided"})
+      res.status(401).json({ message: 'You shall not pass!' })
     }
   }
 
-server.get("/api/users", restricted, (req, res) => {
-  Users
-    .find()
-    .then(users => {
-      if (users) {
-        res.status(200).json(users);
-      } else {
-        res.status(401).json({ message: "You shall not pass!" });
-      }
-    })
-    .catch(err =>
-      res
-        .status(500)
-        .json({ error: "User information could not be retrieved." })
-    );
-});
+  server.get('/api/users', restricted, (req, res) => {
+    Users.find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
+
+  server.get('/api/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.send(
+            'you can checkout any time you like, but you can never leave....'
+          );
+        } else {
+          res.send('bye, thanks for playing');
+        }
+      });
+    } else {
+      res.end();
+    }
+  });
 
 server.listen(5000, () => console.log('\nrunning on port 5000\n'));
