56eb867aa6df768fa23cf3b7fad76574c401a521
Kseniya Platonava <ksu2008@gmail.com>
2019-03-27 22:10:06 -0400

    Add migarions and Registration

--- index.js
+++ index.js
@@ -16,7 +16,23 @@ server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
+//Add POST user and password with hash password
 
+server.post('/api/register', (req, res) =>{
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash;
+  Users.add(user)
+  .then(saved => {
+    res.status(201).json(saved);
+  })
+  .catch(err => {
+    res.status(500).json({err: 'Something went wrong!'})
+  });
+
+});
+
+//Add POST login  function 
 
 const port = process.env.PORT || 8080;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
