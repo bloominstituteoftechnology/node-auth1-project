6f4d400bda91fe70e7721b53367d0d1796458ae8
Kseniya Platonava <ksu2008@gmail.com>
2019-03-27 23:04:34 -0400

    Middleware and GET Option

--- index.js
+++ index.js
@@ -49,6 +49,41 @@ server.post('/api/login', (req, res)=>{
 .catch(err=>{
   res.status(500).json({err: 'You shall not pass!'})
 })
-})
+});
+
+//Middleware 
+
+function restricted(req, res, next){ 
+  const {username, password } = req.headers;
+  if (username && password) {
+      Users.findBy({username})
+      .first()
+      .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)){
+          next();
+  
+        }else{
+      res.status(401).json({message:'You shall not pass'})
+  
+        }
+      })
+      .catch(err =>{
+        res.status(500).json(err);
+      })
+     }else{
+  res.status(400).json({message:'You shall not pass'})
+      }
+  };
+  
+  server.get('/api/users', restricted, (req, res) => {
+
+    Users.find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
+
+
 const port = process.env.PORT || 8080;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
