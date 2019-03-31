ab5f8e16f4be39ef6c2a5996a1135473a4f50c74
Kseniya Platonava <ksu2008@gmail.com>
2019-03-31 18:25:58 -0400

    Add Session, Cookie, and Logout Endpoint

--- index.js
+++ index.js
@@ -2,12 +2,34 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+const KnexSessionStore = require("connect-session-knex")(session);
 
 const db = require('./data/dbConfig.js');
 const Users = require('./users/users-model.js');
 
 const server = express();
 
+const sessionOptions ={
+name: 'webauth1challenge', //name 
+secret: 'this is a secret',//password
+cookie: {
+  maxAge: 1000*60*60,
+  secure: false
+  },
+  httpOnly:true,
+  resave:false,
+  saveUninitialized: false, // ask to save the cookie
+  store:new KnexSessionStore({
+    knex: require('./data/dbConfig.js') ,
+    tablename: 'sessons',
+    sidfieldname: "sid",
+    createtable:true,
+    clearInterval: 1000 *60 * 60
+  })
+}
+
+server.use(session(sessionOptions)) //initiation of the session
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
@@ -36,12 +58,18 @@ server.post('/api/register', (req, res) =>{
 
 server.post('/api/login', (req, res)=>{
 
+  console.log(req.session)
+
+
   let {username, password } = req.body;
   Users.findBy({username})
   .first()
   .then(user => {
     if (user && bcrypt.compareSync(password, user.password)){
-      res.status(200).json({ message: `${user.username} is logged in!` });
+
+      req.session.user = user //that what we add for session
+
+      res.status(200).json({ message: `${user.username} is logged in and has a cookie!` });
     }else{
       res.status(401).json({ message: 'You shall not pass!' });
     }
@@ -54,26 +82,12 @@ server.post('/api/login', (req, res)=>{
 //Middleware 
 
 function restricted(req, res, next){ 
-  const {username, password } = req.headers;
-  if (username && password) {
-      Users.findBy({username})
-      .first()
-      .then(user => {
-        if(user && bcrypt.compareSync(password, user.password)){
-          next();
-  
-        }else{
-      res.status(401).json({message:'You shall not pass'})
-  
-        }
-      })
-      .catch(err =>{
-        res.status(500).json(err);
-      })
-     }else{
-  //res.status(400).json({message:'You shall not pass'})
-      }
-  };
+  if(req.session && req.session.user){
+      next()
+  }else{
+    res.status(401).json({message: 'You shall not pass!'})
+  }
+};
   
   //GET option
   server.get('/api/users', restricted, (req, res) => {
@@ -85,6 +99,20 @@ function restricted(req, res, next){
       })
       .catch(err => res.send(err));
   });
+
+  server.get('/api/logout', (req, res) => {
+    if(req.session){
+      req.session.destroy( err =>{
+        if(err) {
+          res.send('you can never leave')
+        }else{
+          res.send('Bey, bey!')
+        }
+      })
+    }else{
+      res.end()
+    }
+    })
 
 
 const port = process.env.PORT || 8080;
