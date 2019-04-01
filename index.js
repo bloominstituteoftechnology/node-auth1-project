2c7a2dbdea38b2708a6b592b9f4d94f8c3a8583a
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-01 12:43:28 -0400

    API is functional

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./server.js');
 const port = 5000;
 
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+  console.log(`\n=== API running on http://localhost:${port} ===\n`);
 });
