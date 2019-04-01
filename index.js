56b7c484d2d3cd71186d2da9041456502f70d208
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-01 14:09:12 -0700

    server-created-and-running

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+//server moduke
 const express = require('express');
+//security module
+const helmet = require('helmet');
+
+const cors = require('cors');
+//password cryptography
+// const bcrypt = require('bcryptjs');
+
+//database model
+// const Users = require('./data/users/users-model.js');
+
+//server initialize
+const server = express();
+
+//middleware
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+//CRUD
+//root GET
+server.get('/', (req, res) => {
+    res.send("Server Running");
+});
+
+//server port
+const port = process.env.PORT || 7900;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+
+
+
