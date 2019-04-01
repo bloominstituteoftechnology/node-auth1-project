6ce40a847874f5995aaa1ba197da3c9a36a74490
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-01 17:46:52 -0400

    Knexfile updated

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./data/users.sqlite3"
+      filename: "./data/auth.sqlite3"
     },
     useNullAsDefault: true,
     migrations: {
