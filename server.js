8829d7fd63576ffad5008cac4067ad8111210ffb
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-03 13:22:11 -0700

    session-knex-store-created-and-works

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
-//server moduke
+//server
 const express = require("express");
-//security module
+//security
 const helmet = require("helmet");
 //cors?
 const cors = require("cors");
-//session-express module
-const session = require('express-session');
-//Routing 
-const authRouter = require('./database/authentication/auth-router.js');
-const usersRouter = require('./database/users/users-router.js');
+//session using express
+const session = require("express-session");
+//session store using knex
+const KnexSessionStore = require("connect-session-knex")(session); //currying
+//Routing
+const authRouter = require("./database/authentication/auth-router.js");
+const usersRouter = require("./database/users/users-router.js");
+const knexstoreconfig = require("./database/dbConfig.js");
 // const sessionConfig = require('./database/authentication/session-config.js');
+//config of session using express
 const sessionConfig = {
-    name: 'excalibur',
-    secret: 'sword of destiny',
-    resave: false, //avoid recreating unchanged sessions
-    saveUninitialized: false, //GDPR compliance
-    cookie: {
-        maxAge: 1000 * 60 * 10, //milliseconds
-        secure: false, //use for https
-        httpOnly:  true, //can JS access the cookie on the client
-    },
+  name: "excalibur",
+  secret: "sword of destiny",
+  resave: false, //avoid recreating unchanged sessions
+  saveUninitialized: false, //GDPR compliance
+  cookie: {
+    maxAge: 1000 * 60 * 10, //milliseconds, session time
+    secure: false, //use for https
+    httpOnly: true //can JS access the cookie on the client
+  },
+  //store is using session-express to be transferred to Knexsessionstore
+  store: new KnexSessionStore({
+    // knex: require('./database/dbConfig.js'), dont do this, lol
+    knex: knexstoreconfig,
+    tablename: "sessiondata",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 30 //milliseconds, deletes expired sessions
+    //it works
+  })
 };
 //server initialize
 const server = express();
@@ -29,12 +43,12 @@ server.use(express.json());
 server.use(cors());
 server.use(session(sessionConfig));
 //Routers
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 //server CRUD
 //root GET
 server.get("/", (req, res) => {
-    res.send("Server Running");
-  });
+  res.send("Server Running");
+});
 //export server!
 module.exports = server;
