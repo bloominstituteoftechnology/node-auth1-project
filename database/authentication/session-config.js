6df4c95eb0af1716dd983345fdadd5b7f112b39c
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-03 14:12:17 -0700

    restructure-sessions

---
+++ database/authentication/session-config.js
@@ -0,0 +1,28 @@
+//session using express
+const session = require("express-session");
+//session store using knex
+const KnexSessionStore = require("connect-session-knex")(session); //currying
+//routing
+const knexstoreconfig = require("../dbConfig.js");
+
+module.exports = {
+  name: "excalibur",
+  secret: "sword of destiny",
+  resave: false, //avoid recreating unchanged sessions
+  saveUninitialized: false, //GDPR compliance
+  cookie: {
+    maxAge: 1000 * 60 * 10, //milliseconds, session time
+    secure: false, //use for https
+    httpOnly: true //can JS access the cookie on the client
+  },
+  //store is using session-express to be transferred to Knexsessionstore
+  store: new KnexSessionStore({
+    // knex: require('./database/dbConfig.js'), dont do this, lol
+    knex: knexstoreconfig,
+    tablename: "sessiondata",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 30 //milliseconds, deletes expired sessions
+    //it works again
+  })
+};
--- server.js
+++ server.js
@@ -6,35 +6,11 @@ const helmet = require("helmet");
 const cors = require("cors");
 //session using express
 const session = require("express-session");
-//session store using knex
-const KnexSessionStore = require("connect-session-knex")(session); //currying
 //Routing
 const authRouter = require("./database/authentication/auth-router.js");
 const usersRouter = require("./database/users/users-router.js");
-const knexstoreconfig = require("./database/dbConfig.js");
-// const sessionConfig = require('./database/authentication/session-config.js');
 //config of session using express
-const sessionConfig = {
-  name: "excalibur",
-  secret: "sword of destiny",
-  resave: false, //avoid recreating unchanged sessions
-  saveUninitialized: false, //GDPR compliance
-  cookie: {
-    maxAge: 1000 * 60 * 10, //milliseconds, session time
-    secure: false, //use for https
-    httpOnly: true //can JS access the cookie on the client
-  },
-  //store is using session-express to be transferred to Knexsessionstore
-  store: new KnexSessionStore({
-    // knex: require('./database/dbConfig.js'), dont do this, lol
-    knex: knexstoreconfig,
-    tablename: "sessiondata",
-    sidfieldname: "sid",
-    createtable: true,
-    clearInterval: 1000 * 60 * 30 //milliseconds, deletes expired sessions
-    //it works
-  })
-};
+const sessionConfig = require('./database/authentication/session-config.js');
 //server initialize
 const server = express();
 //middleware
@@ -52,3 +28,4 @@ server.get("/", (req, res) => {
 });
 //export server!
 module.exports = server;
+//test
