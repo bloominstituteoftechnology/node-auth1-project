e2b13927064f5c9969e86201df3d994681f38994
Patrick <patrick.schwindt14@gmail.com>
2019-04-08 12:04:20 -0600

    Commit

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+module.exports = {
+    development: {
+      client: 'sqlite3',
+      useNullAsDefault: true,
+      connection: {
+        filename: './database/auth.db3',
+      },
+      pool: {
+        afterCreate: (conn, done) => {
+          conn.run('PRAGMA foreign_keys = ON', done);
+        },
+      },
+      migrations: {
+        directory: './database/migrations',
+      },
+      seeds: {
+        directory: './database/seeds',
+      },
+    },
+  };
