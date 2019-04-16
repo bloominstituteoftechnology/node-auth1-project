110c05ddf36f538e39cdcae2296d057a22656f8b
Marc Thiongo <marcthuo@gmail.com>
2019-04-16 17:47:50 -0400

    edit

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -24,6 +24,7 @@ router.post('/login', (req, res) => {
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
+          req.session.user = user
           res.status(200).json({ message: `Welcome ${user.username}, thank you for using our services!` });
         } else {
           res.status(401).json({ message: 'Invalid Credentials, Please try again' });
