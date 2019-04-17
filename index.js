7b3604c96e788725c634c733527b9f393215689b
Marc Thiongo <marcthuo@gmail.com>
2019-04-16 23:28:42 -0400

    edit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 6000;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+const port = 6000;
+server.listen(port, () => {
+    console.log(`\n** Running on port ${port} **\n`);
+});
