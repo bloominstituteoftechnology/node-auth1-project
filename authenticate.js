8c358faa16f338bbea7c4988f99c1278af78419c
Alexnadria Supriana <alexa9410@gmail.com>
2019-05-06 17:54:42 -0400

    Create authenticate.js

---
+++ authenticate.js
@@ -0,0 +1,24 @@
+function authenticate(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Server Error" });
+      });
+  } else {
+    res.status(400).json({ message: "No credentials Provided" });
+  }
+}
+
+module.exports = {
+  authenticate
+};
