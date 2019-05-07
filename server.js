b795f62eeceaf56d0b42a84172f765dc6e7d9fa0
Alexnadria Supriana <alexa9410@gmail.com>
2019-05-07 17:52:32 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+
+const db = require("./dbConfig.js");
+const Users = require("./users.js");
+const restrictedRouter = require("./routes/restricted");
+
+const { authenticate } = require("./authenticate");
+
+const server = express();
+
+const sessionConfig = {
+  name: "Jane-Doe",
+  secret: "Check",
+  cookie: {
+    maxAge: 1000 * 60 * 60, // in ms
+    secure: false
+  },
+  httpOnly: true, // cannot access the cookie from js using document.cookie
+  resave: false,
+  saveUninitialized: false, // GDPR laws against setting cookies automatically
+
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60 // in ms
+  })
+};
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
+
+server.use("/api/restricted", authenticate, restrictedRouter);
+
+server.get("/", (req, res) => {
+  res.send("It's alive!");
+});
+
+server.post("/api/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      req.session.user = saved;
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post("/api/login", (req, res) => {
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    });
+});
+
+server.get("/api/users", authenticate, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+module.exports = server;
