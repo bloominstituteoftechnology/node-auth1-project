405d20affe2b9fba4ead04486b4a3e337e376b2d
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-08-21 23:47:21 -0400

    finished writing out endpoints

--- index.js
+++ index.js
@@ -30,5 +30,39 @@ server.post('/api/register', (req, res) => {
         });
 });
 
+server.post('/api/login', validate, (req, res) => {
+    let { username } = req.headers;
+    res.status(200).json({ message: `Welcome ${username}!` });
+});
+
+server.get('/api/users', validate, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
+
+function validate(req, res, next) {
+    const { username, password } = req.headers;
+
+    if (username && password) {
+        Users.findBy({username})
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    next();
+                } else {
+                    res.status(401).json({ message: 'Invalid credentials' });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ message: 'Unexpected error' });
+            });
+    } else {
+        res.status(400).json({ message: 'No credentials provided' });
+    }
+};
+
 const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
