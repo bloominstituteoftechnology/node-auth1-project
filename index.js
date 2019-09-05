77979ee3701cb62e039227c24b20ba2d54cf2184
Kelsy <kelsywells@gmail.com>
2019-09-04 19:30:47 -0700

    for stinky pull request

--- index.js
+++ index.js
@@ -17,3 +17,37 @@ server.post('/api/register', (req, res) => {
         res.status(500).json(error);
       });
   });
+
+  server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    password= bcrypt.hashSync(user.password, 10); 
+
+    Users.findBy({ username, password })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Logged in, ${user.username}` });
+        } else {
+          res.status(401).json({ message: 'You shall not pass!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+  if (username && password) {
+    Users.findBy({ username})
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+          res.json(users.username)
+      } else { err => {
+          res.json({message: 'You shall not pass!' })
+      }}
+    })
+    .catch( err => {
+        res.status(500).json({ err:err})
+    })
+}
