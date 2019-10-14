9b4e482c980f19f03210964d619bca40d286aa99
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-14 15:20:47 -0400

    UsersRouter set up.

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const helmet = require('helmet');
 
 const db = require('./data/db-config');
 
+const UsersRouter = require('./users/users-router');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use('/api/users', UsersRouter)
 
 module.exports = server;
