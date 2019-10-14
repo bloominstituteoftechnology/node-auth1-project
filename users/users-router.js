fb67352f29853ba5bdb4e891b0bc45fd6ea339b0
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-14 16:19:12 -0400

    Created GET for users and confirmed it works via Insomnia.

--- users/users-router.js
+++ users/users-router.js
@@ -42,4 +42,29 @@ router.post('/login', (req, res) => {
     } else res.status(400).json({ message: 'Please provide valid credentials '})
 })
 
+router.get('/', protected, (req, res) => {
+    Users.find()
+        .then(users => res.json(users))
+        .catch(err => res.send(err))
+})
+
+function protected (req, res, next) {
+    const { username, password } = req.headers;
+
+    if (username && password) {
+        Users.findBy({ username })
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    next();
+                } else {
+                    res.status(401).json({ message: 'Invalid Credentials' })
+                }
+            })
+            .catch(err => res.status(500).json(err))
+    } else {
+        res.status(400).json({ message: 'Please provide valid credentials' })
+    }
+};
+
 module.exports = router;
