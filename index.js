78d17e022758fd683073d0744fcb103138fa8cb8
MichaelBaynon <baynonmichael@gmail.com>
2019-10-15 17:21:02 -0400

    cookie and session added

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
+const session = require('express-session')
 
 const db = require("./data/dbConfig");
 const Users = require("./users/users-model");
 
 const server = express();
 
+const sessionConfig = {
+  name: 'superCookie',
+  secret: 'super duper secret',
+  cookie: {
+    maxAge: 1000 * 60,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+}
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig))
 
 server.get("/", (req, res) => {
   res.send("It's working!");
@@ -40,6 +54,7 @@ server.post("/api/login", (req, res) => {
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
+          req.session.user = user
           res.status(200).json({ message: `Welcome ${user.username}!` });
         } else {
           res.status(401).json({ message: "must provide valid credentials" });
@@ -64,21 +79,22 @@ server.get("/api/users", protected, (req, res) => {
 function protected(req, res, next) {
   let { username, password } = req.headers;
 
-  if (username && password) {
-    Users.FindBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "You shall not pass!" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
+  if (req.session && req.session.user) {
+    // Users.FindBy({ username })
+    //   .first()
+    //   .then(user => {
+    //     if (user && bcrypt.compareSync(password, user.password)) {
+    //       next();
+    //     } else {
+    //       res.status(401).json({ message: "You shall not pass!" });
+    //     }
+    //   })
+    //   .catch(error => {
+    //     res.status(500).json(error);
+    //   });
+    next()
   } else {
-    res.status(400).json({ message: "please provide credentials" });
+    res.status(401).json({ message: "you are not authorized to access this info" });
   }
 }
 
