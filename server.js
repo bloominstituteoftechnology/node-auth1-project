8ff479220b2b9d66dd4ff6e4a4a43574f8e87961
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-15 15:38:16 -0400

    installed express-session and added session config object to server.js.

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
+const sessions = require('express-session');
 
 const db = require('./data/db-config');
 
 const UsersRouter = require('./users/users-router');
 
+const sessionConfig = {
+    name: 'dogfather',
+    secret: 'Dog spells God backwards.',
+    cookie: {
+        httpOnly: true, // No cookies in JS for security reasons
+        maxAge: 1000 * 60 * 60, // Cookie only valid for 1 hour
+        secure: false, // Change to "true" during production
+    },
+    resave: false, // To prevent excessive churning
+    saveUninitialized: true, // False during production (or you get sued)
+};
+
 const server = express();
 
+server.use(sessions(sessionConfig));
 server.use(helmet());
 server.use(express.json());
+sessionConfig.use(cors());
+
 server.use('/api/users', UsersRouter)
 
 module.exports = server;
