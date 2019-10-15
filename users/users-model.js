464d5ff787d75f0c1dc1eeebea8f82190f260b36
zachary-peasley <zpman95@hotmail.com>
2019-10-15 09:12:38 -0500

    models

--- users/users-model.js
+++ users/users-model.js
@@ -2,9 +2,11 @@ const db = require('../data/db-config')
 
 module.exports = {
     find,
-
+    add,
+    findBy,
+    findByIb
 }
-// jkjh
+
 
 function find() {
     return db('users')
@@ -14,3 +16,23 @@ function find() {
         'password'
     )
 };
+
+function add(user) {
+    return db('users')
+    .insert(user, 'id')
+    .then(ids => {
+        const [id] = ids
+        return findById(id)
+    })
+}
+
+function findBy(filter) {
+    return db('users')
+    .where(filter)
+}
+
+function findById(id) {
+    return db( 'users ')
+    .where({ id })
+    .first();
+}
