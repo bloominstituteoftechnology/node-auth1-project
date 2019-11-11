4709d33ae8a963eecde6e119c3bd2916bc2f871d
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-11 17:30:13 -0600

    ran migration for schema

---
+++ data/migrations/20191111172812_schema.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl =>{
+      tbl.increments()
+      tbl
+        .string('username')
+        .notNullable()
+        .unique()
+      tbl
+        .string('password')
+        .notNullable()
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('users')
+};
