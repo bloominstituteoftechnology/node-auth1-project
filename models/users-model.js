434cac665d263acacb03bf59d49685e79e1298a7
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-11 17:47:32 -0600

    finished models

---
+++ models/users-model.js
@@ -0,0 +1,37 @@
+const db = require('../data/dbconfig.js')
+module.exports = {
+    get,
+    add, 
+    getById,
+    searchBy,
+
+}
+function get(){
+    return db('users')
+}
+
+async function add(newUser){
+  try{
+    [id] = await db('users').insert(newUser, "id") 
+
+    addedUser = await db('users').getById(id)
+    return addedUser
+  }
+  catch(error){
+      console.error(error)
+  }
+}
+
+function getById(id){
+    return get().where({ id }).first()
+}
+
+async function searchBy(thisData){
+    try{
+        const finding = get().where(thisData).first()
+        return finding
+    }
+    catch(error){
+        console.error(error)
+    }
+}
--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,3 +1,9 @@
 const router = require('express').Router;
+const Users = require('../models/users-model.js')
+const bcrypt = require('bcryptjs') 
+
+
+
+
 
 module.exports = router
