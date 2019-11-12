53aa5c43e12f4f8e7fb4fbb0b795adb6b67607e4
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-11-12 16:27:03 -0500

    corrected parenthesis error

--- auth/requires-auth-middleware.js
+++ auth/requires-auth-middleware.js
@@ -5,20 +5,22 @@ module.exports = (req, res, next) => {
   let { username, password } = req.headers;
 
   if (username && password) {
-    Users.findBy({ username})
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) 
-        next();
-        //check that the password is valid
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+          //check that the password is valid
         } else {
-        res.status(401).json({ message: "Invalid Credentials" })
-      }
-    })
-    .catch(error => {
-      res.status(500).json({message: "ran into an error, please try later"})
-    }
-    } else {
-        res.status(400).json({message: 'please provide credentials'})
-    }
-}
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ message: "ran into an error, please try later" });
+      });
+  } else {
+    res.status(400).json({ message: "please provide credentials" });
+  }
+};
