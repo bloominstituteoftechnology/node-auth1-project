74e5fc5d34472a816c6c01792b6ffadbaf34d668
Angela Flowers <me@angela.flowers>
2019-11-13 15:34:54 -0500

    server creates session and sends cookie on login

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 
 const userRouter = require('./users/user-router.js');
 
@@ -9,6 +10,18 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use(session({
+    name: 'user_session',
+    secret: 'four score and seven JS errors ago...',
+    cookie: {
+        maxAge: 24 * 3600 * 1000,
+        secure: false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: true
+}));
 
 server.use('/api', userRouter);
 
