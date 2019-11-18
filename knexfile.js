e419360c64e8411efeee6e84a068ed182b9b8b9a
Martins-O-U <martinsonyedikachi@gmail.com>
2019-11-18 07:51:09 +0100

    Added Postgress dependencies for production

--- knexfile.js
+++ knexfile.js
@@ -20,4 +20,14 @@ module.exports = {
       directory: './database/seeds',
     },
   },
+  production:{
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    }
+  }
 };
