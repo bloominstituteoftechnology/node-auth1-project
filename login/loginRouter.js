3f0dcde7869ed8c4f8da4b43a9e5e330b01447a0
ruwaidah <Ruwidah.riyadh@gmail.com>
2019-12-15 17:55:30 -0600

    update the error msg for login

--- login/loginRouter.js
+++ login/loginRouter.js
@@ -6,21 +6,28 @@ const router = express.Router();
 const Loginuser = require("./login-model");
 
 router.post("/", (req, res) => {
-  Loginuser.findUser(req.body)
-    .then(user => {
-      if (user && bcryptjs.compareSync(req.body.password, user.password))
-        res.status(200).json({ message: "user Logged In", user });
-      else {
-        res.status(401).json({
-          message: "You shall not pass!"
+  const { username, password } = req.body;
+  if (username && password) {
+    Loginuser.findUser(req.body)
+      .then(user => {
+        if (user && bcryptjs.compareSync(req.body.password, user.password))
+          res.status(200).json({ message: "user Logged In", user });
+        else {
+          res.status(401).json({
+            message: "You shall not pass!"
+          });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: "Error Getting Data"
         });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({
-        message: "Error Getting Data"
       });
+  } else {
+    res.status(404).json({
+      message: "please Provide Username and Password"
     });
+  }
 });
 
 module.exports = router;
