effb4b8580e2d119b3588742b5c78a5d3de100ce
Ale <alearanibar@gmail.com>
2019-12-16 12:49:26 -0800

    api-router

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,14 @@
 const router = require = require('express').Router()
 
 const authRouter = require('../auth/auth-router')
-const userRouter = require('./')
+const userRouter = require('../users/users.router')
+
+router.use ('/auth', authRouter)
+router.use('./users', usersRouter)
+
+router.get('/', (req, res) => {
+    res.json({api: `It's Alive`})
+})
+
+module.exports = router
 
