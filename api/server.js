e93cb35f0dfa2a35ab0693d9c0a6341ac6543120
Carlos <miklo.concepcion@gmail.com>
2020-01-14 18:11:31 -0600

    setting up basic api. then moving into data folder set up

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = requreire("express");
+const db = require("../../data/dbconfig");
+// SERVER
+const server = express();
+
+// SERVER API
+server.get("/", (req, res) => {
+  res.status(200).json({
+    message: "web auth1 project api says hi."
+  });
+});
+// SERVER GET ANOTHER_BRANCH
+server.get("/branchName", async (req, res) => {
+  // bringing in a database file aqui.
+  const branchName = await db("ANOTHER_BRANCH");
+  try {
+    res.status(200).json({
+      message: "welcome to a new branch"
+    });
+  } catch (error) {
+    res.status(500).json({ message: "ay dios mios" });
+  }
+});
