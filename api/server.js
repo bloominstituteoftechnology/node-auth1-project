d84345fc33788638ac7d13c3e26cf6633c1f93b6
Carlos <miklo.concepcion@gmail.com>
2020-01-15 20:23:31 -0600

    broke it already. i need to finish establishing what the password is and tweak the logic in the api

--- api/server.js
+++ api/server.js
@@ -4,12 +4,12 @@ const express = require("express");
 const server = express();
 
 // Hashing a password with bcryptjs. it is a npm i.
-// const bcrypt = require("bcryptjs");
-
-// | POST   | /api/login    |
-// | GET    | /api/users    |
-// If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.
-
+const bcrypt = require("bcryptjs");
+// to hash a password
+const credentials = req.body;
+const hash = bcrypt.hashSync(credentials.password, 14);
+credentials.password = hash;
+// move on to save the user.
 //////////////////////////////// SERVER API
 server.get("/", (req, res) => {
   res.status(200).json({
@@ -19,22 +19,38 @@ server.get("/", (req, res) => {
 
 //////////////////////////////// POST REGISTER USER /api/register
 // Creates a `user` using the information sent inside the `body` of the request. **Hash the password** before saving the user to the database.                                                                                                                                                         |
-
 server.post("/api/register", async (req, res) => {
   // bringing in a database file aqui.
   // const branchName = await db("ANOTHER_BRANCH");
+  let { username, password } = req.body;
+  //   try {
+  //     res.status(200).json({
+  //       message: "Register in this form!"
+  //     });
+  //   } catch (error) {
+  //     res.status(500).json({ message: "ay dios mios Register" });
+  //   }
+  // });
   try {
-    res.status(200).json({
-      message: "Register in this form!"
-    });
+    URLSearchParams.findBy({ username })
+      .first()
+      .then(user => {
+        // check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          // we will return 401 if the password or username are invalid
+          // we don't want to let attackers know when they have a good username
+          res.status(401).json({ message: "You shall not pass!" });
+        }
+      });
   } catch (error) {
-    res.status(500).json({ message: "ay dios mios Register" });
+    res.status(500).json({ message: "You shall not pass!" });
   }
 });
 
 //////////////////////////////// POST LOGIN USER /api/login
-// Use the credentials sent inside the `body` to authenticate the user. On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id. If login fails, respond with the correct status code and the message: 'You shall not pass!' |
-
+// Use the credentials sent inside the `body` to authenticate the user. On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id. If login fails, respond with the correct status code and the message: 'You shall not pass!'
 server.post("/api/login", async (req, res) => {
   // bringing in a database file aqui.
   // const branchName = await db("ANOTHER_BRANCH");
@@ -49,7 +65,6 @@ server.post("/api/login", async (req, res) => {
 
 //////////////////////////////// GET USER /api/users
 // If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.
-
 server.get("/api/user", async (req, res) => {
   // bringing in a database file aqui.
   // const branchName = await db("ANOTHER_BRANCH");
