abf533c3ba12bf6accf7bfeb65252c36b44d4b6b
Carlos <miklo.concepcion@gmail.com>
2020-01-17 20:25:10 -0600

    re-adding database and user sqlite3 files. then moving back into sessions and tokents with JWT tokens.

--- api/server.js
+++ api/server.js
@@ -9,13 +9,13 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 // Hashing a password with bcryptjs. it is a npm i.
-const bcrypt = require("bcryptjs");
-// to hash a password
-const credentials = req.body;
-const hash = bcrypt.hashSync(credentials.password, 14);
-credentials.password = hash;
-// move on to save the user.
-
+// const bcrypt = require("bcryptjs");
+// // to hash a password
+// const credentials = req.body;
+// const hash = bcrypt.hashSync(credentials.password, 14);
+// credentials.password = hash;
+// // move on to save the user.
+// let { username, password } = req.body;
 //////////////////////////////// SERVER API
 server.get("/", (req, res) => {
   res.status(200).json({
@@ -26,10 +26,6 @@ server.get("/", (req, res) => {
 //////////////////////////////// POST REGISTER USER /api/register
 // Creates a `user` using the information sent inside the `body` of the request. **Hash the password** before saving the user to the database.                                                                                                                                                         |
 server.post("/api/register", async (req, res) => {
-  // bringing in a database file aqui.
-  // const branchName = await db("ANOTHER_BRANCH");
-  let { username, password } = req.body;
-
   try {
     URLSearchParams.findBy({ username })
       .first()
@@ -51,8 +47,6 @@ server.post("/api/register", async (req, res) => {
 //////////////////////////////// POST LOGIN USER /api/login
 // Use the credentials sent inside the `body` to authenticate the user. On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id. If login fails, respond with the correct status code and the message: 'You shall not pass!'
 server.post("/api/login", async (req, res) => {
-  // bringing in a database file aqui.
-  // const branchName = await db("ANOTHER_BRANCH");
   try {
     res.status(200).json({
       message: "Register in this Login!"
