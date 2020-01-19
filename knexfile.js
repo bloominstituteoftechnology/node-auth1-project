f27f7603d89b156c451b653b94eef86ef88a562f
Carlos <miklo.concepcion@gmail.com>
2020-01-18 23:17:27 -0600

    still cleaning up knexfile.js. adding better notes to refer back on.

--- knexfile.js
+++ knexfile.js
@@ -2,20 +2,25 @@
 
 module.exports = {
   development: {
+    // the DBMS driver
     client: "sqlite3",
+    // location of the db
     connection: {
       filename: "./dev.sqlite3"
     },
-    // always have to have this line 11.
+    // necessary when using sqlite3 .
     useNullAsDefault: true,
+    // migrations file path
     migrations: {
       directory: "./data/migrations"
     },
+    // seeds file path
     seeds: {
       directory: "./data/seeds"
     }
   },
   pool: {
+    // apply to be able to use foreign keys
     afterCreate: (conn, done) => {
       // runs after a connection is made to the sqlite engine.
       conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
