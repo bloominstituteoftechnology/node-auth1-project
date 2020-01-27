dee0becd160ef0e825e547bd8ba05b84326b74fb
Annette Odingo <odingoannette@gmail.com>
2020-01-27 16:19:43 +0000

    Import cookieparser

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const cookieParser = require('cookie-parse');
 
 module.exports = server => {
   server.use(helmet());
   server.use(express.json()); 
+  server.use(cookieParser());
   server.use(cors());
 };
