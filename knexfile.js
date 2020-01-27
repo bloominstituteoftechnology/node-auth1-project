c818294eabaf8b5d1f534ede0697ab3c36c49249
Annette Odingo <odingoannette@gmail.com>
2020-01-27 13:14:24 +0000

    Generate a knexfile.js and renamed database and add migration and seeding folders

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/aut.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  }
+
+};
