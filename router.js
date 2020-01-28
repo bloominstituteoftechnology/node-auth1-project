a9ea755ef85380b19abe53c706099e461c11286d
Aasa-Christian <aasachristian@yahoo.com>
2020-01-28 17:57:27 -0500

    completed day1 mvp

--- router.js
+++ router.js
@@ -4,4 +4,17 @@ const router = express.Router();
 
 const db = require('./model')
 
+router.get('/', (req, res) => {
+    res.json({ api: "It's alive" });
+  });
+
+
+  router.get('/user', (req, res) => {
+    db.find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./router');
+const authRouter = require('./authRouter')
 const configureMiddleware = require('./config-mid');
 
 const server = express();
@@ -7,5 +8,6 @@ const server = express();
 configureMiddleware(server);
 
 server.use('/api', router);
+server.use('/api', authRouter);
 
 module.exports = server;
