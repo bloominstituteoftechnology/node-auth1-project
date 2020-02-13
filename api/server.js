ae10f9d05dd5fca29e5b208e8696b558155d3596
Denise Chaloupka <denisechaloupka@yahoo.com>
2020-02-12 20:54:35 -0600

    MVP

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const apiRouter = require("./api-router.js");
+const configureMiddleware = require("./configure-middleware.js");
+
+const server = express();
+
+configureMiddleware(server);
+
+server.use("/api", apiRouter);
+
+module.exports = server;
---
+++ users/users-model.js
@@ -0,0 +1,34 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById
+};
+
+function find() {
+  return db("users").select("id", "username");
+}
+
+function findBy(filter) {
+  return db("users")
+    .select("id", "username", "password")
+    .where(filter);
+}
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db("users")
+    .select("id", "username")
+    .where({ id })
+    .first();
+}
