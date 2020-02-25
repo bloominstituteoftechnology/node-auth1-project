3f8d36f976f582b970910b7c8eda77c1bb14f453
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-02-24 21:47:23 -0500

    added validation for when the user registers

--- register/registerRouter.js
+++ register/registerRouter.js
@@ -6,15 +6,22 @@ const bcrypt = require("bcryptjs");
 router.post("/", (req, res) => {
   const newUser = req.body;
 
-  const hash = bcrypt.hashSync(newUser.password, 8);
-  newUser.password = hash;
+  if (newUser.username && newUser.password) {
+    const hash = bcrypt.hashSync(newUser.password, 8);
+    newUser.password = hash;
 
-  Register.addUser(newUser)
-    .then(user => {
-      res.status(201).json(user);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+    Register.addUser(newUser)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  } else {
+    res
+      .status(400)
+      .json({ message: `Please enter a username and a password.` });
+  }
 });
+
 module.exports = router;
