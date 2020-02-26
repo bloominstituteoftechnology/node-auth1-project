78000b10cb0d7f9688c545c320889160705b3a90
arashhaji <56742302+arashhaji@users.noreply.github.com>
2020-02-26 16:02:36 -0500

    mvp

--- server.js
+++ server.js
@@ -2,17 +2,35 @@ const express = require('express');
 
 const db = require('./data/dbHelp');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
+const session = require('express-session');
+
+const restricted = require('./middelware/restricted-middleware');
 
 const server = express();
 
 server.use(express.json());
 
+const sessionConfig = {
+    name: "cookie",
+    secret: "secret",
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 1,
+      secure: false
+    },
+    resave: false,
+    saveUninitialized: true
+  };
+  
+  server.use(session(sessionConfig));
+
 server.get('/', (req, res) => {
     res.send('<h1> Its Nice, I like, How Much <h1>')
 })
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
     db.find()
       .then(user => {
         res.status(200).json(user);
@@ -29,6 +47,7 @@ server.get('/api/users', (req, res) => {
 
       db.add(users)
         .then(user => {
+            req.session.loggedIn = true
             res.status(200).json(user);
         })
         .catch(err => {
@@ -38,10 +57,12 @@ server.get('/api/users', (req, res) => {
 
   server.post('/api/login', (req, res) => {
     const { username, password } = req.body;
-    db.login({ username: username })
+    db.login({ username })
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
+          req.session.loggedIn = true
+          req.session.username = user.username;
           res.status(200).json({ message: `Welcome ${user.username}` });
         } else {
           res.status(404).json({ message: "Invaild Login" });
@@ -50,6 +71,20 @@ server.get('/api/users', (req, res) => {
       .catch(err => {
         res.status(500).json(err);
       });
+  });
+
+  server.get("/api/logout", (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.send("error logging out");
+        } else {
+          res.send("Logged Out");
+        }
+      });
+    } else {
+      res.end();
+    }
   });
 
 module.exports = server;
