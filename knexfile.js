4326d18452763b79e4353e704954c9ae1131c242
Kemberly Eliscar <kemberlyeliscar@gmail.com>
2020-03-15 02:17:01 -0400

    set up schema table and knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,19 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/auth.db3',
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done)
+      },
     }
   },
 };
