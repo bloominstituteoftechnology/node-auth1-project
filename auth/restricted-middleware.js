7ed10ec11bcbfed7d444e805fc89188eebed83cc
Michael Lowe <mrlowememphis@yahoo.com>
2020-05-06 18:56:36 -0500

    Restricted-middleware.js created

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,8 @@
+module.exports = (req, res, next) => {
+    
+    if (req.session && req.session.loggedIn) {
+      next();
+    } else {
+      res.status(401).json({ you: "cannot pass!!" });
+    }
+  };
