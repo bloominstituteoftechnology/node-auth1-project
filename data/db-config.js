8be39359d3fe944edb1de1a96646c8511c37914a
Your name <a.m.verdine@gmail.com>
2020-05-06 22:26:29 -0400

    Create and connect knexfile and db-config

---
+++ data/db-config.js
@@ -0,0 +1,4 @@
+const knex = require("knex");
+const knexfile = require("../knexfile");
+
+module.exports = knex(knexfile.development);
---
+++ knexfile.js
@@ -0,0 +1,10 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/users.db3",
+    },
+  },
+};
