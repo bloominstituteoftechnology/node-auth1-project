b63c52b8300096787b137546713d6c56383bd5e0
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-05-18 06:49:39 -0700

    migration create_user created

--- data/migrations/20200517133531_create_user.js
+++ data/migrations/20200517133531_create_user.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments()
+    tbl.string('username')
+      .unique()
+      .notNullable()
+    tbl.string('password')
+      .notNullable()
+  })
 
 };
 
 exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users')
 
 };
