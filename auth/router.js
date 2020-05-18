e535fce839dba50141b42eec3356d5846c6a1260
robertelias <robert.elias@live.com>
2020-05-18 12:54:47 -0700

    router

---
+++ auth/router.js
@@ -0,0 +1,77 @@
+const bcryptjs = require("bcryptjs");
+
+const router = require("express").Router();
+
+const Users = require("../users/users-model.js");
+const { isValid } = require("../users/users-service.js");
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 16;
+
+    // hash the password
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    // save the user to the database
+    Users.add(credentials)
+      .then(user => {
+        req.session.loggedIn === true;
+
+        res.status(201).json({ data: user });
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
+});
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          // we can save information about the client inside the session (req.session)
+          req.session.loggedIn = true;
+          req.session.user = user;
+
+          res.status(200).json({ message: "Welcome to our API" });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ message: "we could not log you out, try later please" });
+      } else {
+        res.status(204).end();
+      }
+    });
+  } else {
+    res.status(204).end();
+  }
+});
+
+module.exports = router;
