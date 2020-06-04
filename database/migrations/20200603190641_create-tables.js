de9ff3361c36372ca6266b08b9bc609a463ceb9c
Veto Ramirez <vetorobert3@gmail.com>
2020-06-03 19:52:49 -0700

    Created all files and sownloaded all dependancoes. Working on API calls.

--- database/migrations/20200603190641_create-tables.js
+++ database/migrations/20200603190641_create-tables.js
@@ -13,5 +13,5 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
   return knex.schema  
-    .dropTableIfExist('users');
+    .dropTableIfExists('users');
 };
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n** Running on port ${PORT} **\n`));
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
