9a72ae0b4c39f203b4744daa4ac2eeea5dd16f46
lex-marie790 <lexi.marie790@gmail.com>
2020-08-05 20:23:47 -0700

    seeds completed

--- database/seeds/01-init.js
+++ database/seeds/01-init.js
@@ -1,13 +1,6 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {username: 'johnsnow', password: 'youknownothing'}
       ]);
-    });
 };
