39d3cc3a0deefaf07e6d9b2166f1b049c8e52c41
lex-marie790 <lexi.marie790@gmail.com>
2020-08-05 20:45:53 -0700

    server.js updated, restricted-middleware completed

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const knexSessionStore = require('connect-session-knex')(session);
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const restricted = require('../auth/restricted-middleware.js');
+const restricted = require('../auth/restricted-middleware');
 
 //routers
 const usersRouter = require('../users/users-router.js');
@@ -39,3 +39,15 @@ const sessionConfig = {
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
+
+server.use('api/users', restricted, usersRouter);
+server.use('/api/auth', authRouter);
+
+server.get('/', (req, res) => {
+    res.json({
+        api: 'up'
+    });
+});
+
+module.exports = server;
