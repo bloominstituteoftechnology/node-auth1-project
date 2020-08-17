d6b2ee645721042e395702e7d601145dd17ae2c3
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-08-17 15:19:19 -0700

    NPM i .. rewatching lecture

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+const server = require("./server.js");
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
