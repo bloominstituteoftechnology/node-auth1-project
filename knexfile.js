051ef93aed6345994fe8b490401541e233b9c580
Vish Singh <vishalicious213@gmail.com>
2020-09-02 13:49:46 -0700

    created knexfile, removed staging & production objects, added db path/name, useNullAsDefault, migrations path

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/projectDB.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    }
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
