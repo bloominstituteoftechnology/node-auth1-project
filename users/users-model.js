6d1e7611fb911067c82548d81f09b7b8c8903eef
Vish Singh <vishalicious213@gmail.com>
2020-09-02 15:14:27 -0700

    ADD user works. added add, findById & findBy to model. added /register endpoint to router

--- users/users-model.js
+++ users/users-model.js
@@ -5,6 +5,30 @@ function findAll() {
 	return db("users").select("id", "username")
 }
 
+// ADD user
+async function add(user) {
+    const [id] = await db('users').insert(user)
+    return findById(id)
+}
+
+// FIND user by ID
+function findById(id) {
+    return db('users')
+        .select('id', "username")
+        .where({ id })
+        .first()
+}
+
+// FIND user by filter (username, in this case)
+function findBy(filter) {
+	return db("users")
+		.select("id", "username", "password")
+		.where(filter)
+}
+
 module.exports = {
-    findAll
+    add,
+    findAll,
+    findBy,
+    findById
 }
--- users/users-router.js
+++ users/users-router.js
@@ -3,12 +3,36 @@ const Users = require('./users-model')
 
 const router = express.Router()
 
+// GET all users
 router.get('/users', async (req, res, next) => {
     try {
         res.json(await Users.findAll())
     } catch(error) {
         next(error)
     }
+})
+
+// ADD user
+router.post("/register", async (req, res, next) => {
+	try {
+		const { username, password } = req.body
+		const user = await Users.findBy({ username }).first()
+
+		if (user) {
+			return res.status(409).json({
+				message: "Username is already taken",
+			})
+		}
+
+		const newUser = await Users.add({
+			username,
+			password,
+		})
+
+		res.status(201).json(newUser)
+	} catch(err) {
+		next(err)
+	}
 })
 
 module.exports = router
