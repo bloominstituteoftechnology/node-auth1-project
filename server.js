d6c491aba33f44e78219047934b67cc63f990a5f
Vish Singh <vishalicious213@gmail.com>
2020-09-02 17:37:02 -0700

    installed connect-session-knex

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const server = express();
 const usersRouter = require('./users/users-router')
+const session = require('express-session')
+const knexSessionStore = require('connect-session-knex')(session)
+const db = require('./data/dbConfig')
 
 server.use(express.json());
+server.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'bah weep grana weep ninny bon',
+    store: new knexSessionStore({
+        knex: db,
+        createtable: true,
+    })
+}))
+
 server.use('/api', usersRouter);
 
 module.exports = server;
