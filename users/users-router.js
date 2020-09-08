be1a0c0f25a23849b6484c09f7f89ee2133fe59f
c00kamunga <tsothalanti2@yahoo.com>
2020-09-08 13:39:20 -0700

    created an http endpoint for creating a user

--- users/users-router.js
+++ users/users-router.js
@@ -10,7 +10,27 @@ router.get("/users", async (req, res, next) => {
     }
 })
 
+router.post('/users', async (req, res, next) => {
+    try {
+        const { username, password } = req.body
+        const user = await Users.findBy({ username })
 
+        if (user) {
+            return res.status(409).json({
+                message: "The username has already been taken"
+            })
+        }
+
+        const newUser = await Users.add({
+            username,
+            password,
+        })
+        
+        res.status(201).json(newUser)
+    } catch (err) {
+        next(err)
+    }
+})
 
 
 
