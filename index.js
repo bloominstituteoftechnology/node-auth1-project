798cba1826f5e44e708ed5f22b0494360ef683d3
c00kamunga <tsothalanti2@yahoo.com>
2020-09-08 02:03:48 -0700

    created index file, imported packages, created a port

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+//const usersRouter = require('./users/usersRouter')
+const server = express()
+const port = process.env.PORT || 6936
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+//server.use(usersRouter)
+
+server.use((err, req, res, next) => {
+    console.log(err)
+
+    res.status(500).json({
+        message: "something went wrong"
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+})
