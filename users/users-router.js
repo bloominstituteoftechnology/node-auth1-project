039f09e51389b0ff4bf15549caa06db4a5854b37
c00kamunga <tsothalanti2@yahoo.com>
2020-09-08 13:33:46 -0700

    created users-router file

---
+++ users/users-router.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const Users = require('./users-model')
+const router = express.Router()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
