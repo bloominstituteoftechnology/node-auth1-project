3e529da7ad617e860c04da356379dbd8acbe31cf
c00kamunga <tsothalanti2@yahoo.com>
2020-09-08 13:35:49 -0700

    created http method for getting an array of users

--- users/users-router.js
+++ users/users-router.js
@@ -2,7 +2,13 @@ const express = require('express')
 const Users = require('./users-model')
 const router = express.Router()
 
-
+router.get("/users", async (req, res, next) => {
+    try {
+        res.json(await Users.find())
+    } catch(err) {
+        next(err)
+    }
+})
 
 
 
