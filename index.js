de4d2bc8aa204bdaf04d0225bab99c9556f010ba
c00kamunga <tsothalanti2@yahoo.com>
2020-09-08 17:08:56 -0700

    imported knexsessionstore to allow the session to be persisted

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 const session = require('express-session')
+const knexSessionStore = require('connect-session-knex')(session)
 const usersRouter = require('./users/users-router')
+const db = require('./database/config')
 const server = express()
 const port = process.env.PORT || 6936
 
@@ -12,7 +14,11 @@ server.use(express.json())
 server.use(session({
 resave: false, //avoids recreating sessions that have not changed
 saveUninitialized: false, //to comply with GDPR laws
-secret: "keep it secret, keep it safe" //crpytographically sign the cookie
+secret: "keep it secret, keep it safe", //crpytographically sign the cookie
+store: new knexSessionStore({
+    knex: db, //configured instance of knex
+    createtable: true, // if the sessions table doesnt exist, create it automatically
+}),
 }))
 
 server.use(usersRouter)
