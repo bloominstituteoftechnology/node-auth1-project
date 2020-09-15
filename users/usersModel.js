c7f39db9224bf361f0157b5b11fecd20bebee8a4
Ash <ashley.bergsma42@gmail.com>
2020-09-15 06:29:45 -0700

    User can register, and login if correct credentials are given. Now session support must be added to persist the user's authorization

--- users/usersModel.js
+++ users/usersModel.js
@@ -1,9 +1,32 @@
-const users = require('../data/db-config'); 
+const db = require('../data/db-config'); 
 
 module.exports = {
-    find
+    find, 
+    findById, 
+    add, 
+    findBy
 }
 
 function find() {
-    return users('users'); 
+    return db('users'); 
+}
+
+function findById(id){
+    return db('users')
+        .where({ id })
+        .first(); 
+}
+
+function add(user){
+    return db('users')
+        .insert(user, 'id')
+            .then(([id]) => {
+                return findById(id);
+            });
+}
+
+function findBy(username){
+    return db('users')
+        .where({ username })
+        .first(); 
 }
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,10 +1,11 @@
-const express = require('express'); 
-
-const db = require('./usersModel'); 
+const express = require("express"); 
+const bcrypt = require("bcryptjs")
+const db = require("./usersModel"); 
+const { orWhereNotExists } = require("../data/db-config");
 
 const router = express.Router(); 
 
-//* 🎆 Get all users 🎆 *// 
+//* 🎆 GET all users 🎆 *// 
 //TODO 🧵 a piece of middleware validating the username and password will need to be created and added to this end point! // 
 router.get("/", (req, res) => {
     db.find()
@@ -14,6 +15,36 @@ router.get("/", (req, res) => {
         .catch(error => {
             res.status(500).json({ message: "Error finding users" });
         });
+}); 
+
+//* 🎆 POST and register a new user 🎆 *// 
+//TODO 🧵 bcryptjs needs to be installed as a dependency to hash the user's password! // 
+router.post("/register", (req, res) => {
+    const { username, password } = req.body; 
+    const rounds = process.env.BCRYPT_ROUNDS || 4;
+    db.add({ username, password: bcrypt.hashSync(password, rounds) })
+        .then(user => {
+            res.status(201).json({ message: `Welcome ${username}!` }); 
+        })
+        .catch(error => {
+            res.status(500).json(error); 
+        });
+});
+
+//* 🎆 POST and login an existing user 🎆 *// 
+router.post("/login", (req, res) => {
+    const { username, password } = req.body; 
+    db.findBy(username)
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({ message: `Welcome to back ${username}!` })
+            } else {
+                res.status(401).json({ message: "Credentials unauthorized" }); 
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Unable to login user" }); 
+        }); 
 }); 
 
 
