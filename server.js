2445b9c996ba44057b61cbcb075117d607d6b7d2
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-20 07:23:08 -0400

    Serving user routes and /api/users endpoint

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ server.get("/", (req, res)=>{
     res.send("the api is working");
 });
 
+//routing
+const userRoutes = require("./users/usersRouter");
+server.use("/api/users", userRoutes);
+
 module.exports = server;
