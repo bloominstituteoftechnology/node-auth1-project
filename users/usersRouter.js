4ea7a7dea51341699ad78ebf6f67af20fbb6d2fb
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-20 07:34:55 -0400

    Created /register endpoint for users

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -3,9 +3,22 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const db = require("../data/db");
 
+router.post("/register", (req, res)=>{
+    if(!req.body || !req.body.email || !req.body.first_name || !req.body.last_name || !req.body.password){
+        return res.status(400).json({message: "All fields are required"})
+    }
+    const {email, first_name, last_name} = req.body;
+    const password = bcrypt.hashSync(req.body.password, 14);
+    db("users").insert({email, first_name, last_name, password}).then(numCreated=>{
+        res.status(201).json({email, first_name, last_name});
+    }).catch(err=>{
+        res.status(500).json({message: "A server error occurred"});
+    })
+});
+
 router.post("/login", (req, res)=>{
     if(!req.body || !req.body.email || !req.body.password) {
-        return res.status(401).json({message: "email and password are required"});
+        return res.status(400).json({message: "email and password are required"});
     }
     const {email, password} = req.body;
     db("users").where({email: email}).first().then(user=>{
