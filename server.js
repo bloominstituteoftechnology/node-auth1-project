1fd80d9a69130e7f9713aaf289a8fb8cd4af1ff9
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-21 01:38:14 -0400

    Made auth middleware global'

--- server.js
+++ server.js
@@ -27,20 +27,21 @@ server.use("/api/", authRoutes);
 
 const db = require("./data/db");
 
-server.get("/api/users", protected, (req, res)=>{
+server.use(function(req, res, next){
+    if(req.path.indexOf('/api/users') !== -1){
+        if(req.session && req.session.user) return next();
+        res.status(401).json({message: "You shall not pass!"});
+    }else {
+        res.status(401).json({message: "You shall not pass!"});
+    };  
+});
+
+server.get("/api/users", (req, res)=>{
     db("users").then(users=>{
         res.status(200).json(users);
     }).catch(err=>{
         return res.status(500).json({message: "A server error has occurred"});
     })
-})
-
-function protected(req, res, next){
-    if(req.session && req.session.user){
-        next();
-    }else {
-        res.status(401).json({message: "You shall not pass"})
-    }
-}
+});
 
 module.exports = server;
