139a6602744ca60970609642332c17238d7563ec
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-20 20:28:13 -0400

    Added /api/users endpoint'

--- server.js
+++ server.js
@@ -11,4 +11,13 @@ server.get("/", (req, res)=>{
 const authRoutes = require("./auth/authRouter");
 server.use("/api/", authRoutes);
 
+const db = require("./data/db");
+server.get("/api/users", (req, res)=>{
+    db("users").then(users=>{
+        res.status(200).json(users);
+    }).catch(err=>{
+        return res.status(500).json({message: "A server error has occurred"});
+    })
+})
+
 module.exports = server;
