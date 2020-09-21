0c9d572e32c5c84f76d75d6ae3e17a3eefbaccdf
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-21 00:21:57 -0400

    Edited session config. Created auth middleware. Using auth middleware on /api/users/

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const server = express();
-const session = require("express-session")
+const session = require('express-session');
 
-sessionConfig = {
-    name: "asessname",
-    secret: "as;lfj349iwhaakeshrhwafw9hfskdfh289",
+const sessionConfig = {
+    name: 'notsession',
+    secret: 'nobody tosses a dwarf!',
     cookie: {
         maxAge: 1 * 24 * 60 * 60 * 1000,
-        secure: true
-    },
-    httpOnly: true,
+        secure: process.env.NODE_ENV === "production"? true: false,
+        httpOnly: true
+    },  
     resave: false,
     saveUninitialized: false
 }
@@ -26,12 +26,21 @@ const authRoutes = require("./auth/authRouter");
 server.use("/api/", authRoutes);
 
 const db = require("./data/db");
-server.get("/api/users", (req, res)=>{
+
+server.get("/api/users", protected, (req, res)=>{
     db("users").then(users=>{
         res.status(200).json(users);
     }).catch(err=>{
         return res.status(500).json({message: "A server error has occurred"});
     })
 })
+
+function protected(req, res, next){
+    if(req.session && req.session.user){
+        next();
+    }else {
+        res.status(401).json({message: "You shall not pass"})
+    }
+}
 
 module.exports = server;
