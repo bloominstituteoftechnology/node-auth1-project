c7366945217852f3562a3a92bfd9209e13d92864
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-10-30 14:05:05 -0500

    created knex file

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: '.data/project.db3'
+    }
+  },
+
+  useNullAsDefault: true,
+  
+  migrations: {
+    directory: '.data/migrations'
+    }
+  
+
+};
