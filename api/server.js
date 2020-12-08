694b7e39b634bb88b405e45b51db4b3ee4affc9d
e-licea <elicea0412@gmail.com>
2020-12-08 03:09:40 -0600

    password hashed before inserting to db

--- api/server.js
+++ api/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const server = express();
+const helmet = require('helmet');
+const bcrypt = require('bcryptjs')
+const cors = require('cors');
 
+
+//db
+const db = require('../data/dbConfig')
+
+//middlewares
+server.use(express.json(), helmet(), cors())
 
 
 
@@ -10,21 +19,49 @@ const server = express();
 
 //POST -- Register new user
 
-server.post('/api/register', (req, res)=>{
-    const newUser = req.body
+server.post('/api/register', async (req, res, next)=>{
+
+
+    try{
+
+        const newUser = await (req.body);
+        const hash = bcrypt.hashSync(newUser.Password, 12)
+        
+        newUser.Password = hash
+
+        await db.insert({
+            Username: `${newUser.Username}`,
+            Password: `${newUser.Password}`
+        })
+        .into('user_table')
+
+        res.json({message: 'user created',
+                newUser: newUser})
+
+    }
+    
+    
+    catch (err){
+        next(err);
+    }
+
+
 })
 
 
 //POST -- log in user
 
 server.post('/api/login', (req, res)=>{
+    
     const loggedUser = req.body
+
+    
 })
 
 //GET -- get users
 
 server.get('/api/users', (req, res)=>{
-    
+    res.json({message: Hello})
 })
 
 
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require('./api/server');
 
 const PORT = 2000;
 
+server.get('/api', (req, res)=>{
+
+    res.json({message: 'Hello'})
+})
 
 server.listen(PORT, ()=>{
     console.log(`Server is listening on PORT : ${PORT}`)
