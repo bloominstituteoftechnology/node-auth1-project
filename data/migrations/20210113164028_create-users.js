e855dc643dfb9c2f6a8da3c2682879ab51f66d67
Tucker <leachtucker@gmail.com>
2021-01-13 16:49:29 -0800

    Database with users table setup

---
+++ data/migrations/20210113164028_create-users.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('users', table => {
+            table.increments('user_id');
+            table.text('username', 20)
+                .notNullable()
+                .unique()
+                .index();
+            table.text('password')
+                .notNullable();
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExits('users');
+};
---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    usNullAsDefault: true,
+    connection: {
+      filename: './data/auth1.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+};
