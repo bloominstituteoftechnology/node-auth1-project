9a50ecd235d95ba7791c0d0fc28f58005b382c5e
Diego Roman <diegormnv209@gmail.com>
2021-02-22 13:01:36 -0800

    added files and folders to be ready to type code

---
+++ data/migrations/20210222125233_firstmigration.js.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('roles', tbl =>{
+      tbl.increments();
+      tbl.string('name', 130).notNullable().unique();
+  })
+  .createTable('users', tbl =>{
+      tbl.increments();
+      tbl.string('username', 130).notNullable().unique.index();
+      tbl.string('password', 260).notNullable();
+      tbl.integer('role')
+        .unsigned()
+        .reference('roles.id')
+        .inTable('roles')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('users')
+  .dropTableIfExists('roles')
+};
