2b8d3a491fd0229c6bc92f850df83eb673d084ac
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-01 22:45:42 +0100

    readme

--- api/server.js
+++ api/server.js
@@ -6,12 +6,12 @@ const cors = require("cors");
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
   This is achieved by setting 'saveUninitialized' to false, and by not
-  changing `req.session` unless the user authenticates.
+  changing the `req.session` object unless the user authenticates.
 
   Users that do authenticate should have a session persisted on the server,
   and a cookie set on the client. The name of the cookie should be "chocolatechip".
 
-  The session can be persisted in memory (not adecuate for production)
+  The session can be persisted in memory (would not be adecuate for production)
   or you can use a session store like `connect-session-knex`.
  */
 
