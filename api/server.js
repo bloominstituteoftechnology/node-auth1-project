4f47ec39494657f06dc86c6ae1fbafd8d58259d0
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-25 14:53:04 -0700

    server.js done

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session'); // added
+const KnexSessionStore = require('connect-session-knex')(session); 
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -16,6 +18,24 @@ const cors = require("cors");
  */
 
 const server = express();
+server.use(session({
+  name: "chocolatechip",
+  secret: "lol",
+  cookie: {
+    maxAge: 1000 * 10 * 1000,
+    secure: false,
+    httpOnly: false,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: require('../data/db-config'), // configured instance of knex
+    tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+    sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+    createtable: true, // if the table does not exist, it will create it automatically
+    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+  })
+}));
 
 server.use(helmet());
 server.use(express.json());
