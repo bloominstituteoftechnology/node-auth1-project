42a4ee7b5eeb5c0697a3d2b51c942c9ef0b3033a
ngwatso <ngwatson79@yahoo.com>
2021-04-07 10:36:01 -0400

    created middleware functions

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,49 +1,87 @@
+const Users = require('../users/users-model');
+const bcrypt = require('bcryptjs');
+
 /*
-  If the user does not have a session saved in the server
+  * If the user does not have a session saved in the server
 
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
+  * status 401
+  * {
+  *   "message": "You shall not pass!"
+  * }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+	if (req?.session?.user) {
+		next();
+	} else {
+		res.status(401).json({ message: 'You shall not pass!' });
+	}
 }
 
 /*
-  If the username in req.body already exists in the database
+  * If the username in req.body already exists in the database
 
-  status 422
-  {
-    "message": "Username taken"
-  }
+  * status 422
+  * {
+  *   "message": "Username taken"
+  * }
 */
-function checkUsernameFree() {
+async function checkUsernameFree(req, res, next) {
+	try {
+		let { username } = req.body;
+		const user = await Users.findBy({ username }).first();
+		if (!user) {
+			next();
+		} else {
+			res.status(422).json({ message: 'Username taken' });
+		}
+	} catch (err) {
+		next(err);
+	}
+}
+/*
+  * If the username in req.body does NOT exist in the database
 
+  * status 401
+  * {
+  *   "message": "Invalid credentials"
+  * }
+*/
+async function checkUsernameExists(req, res, next) {
+	try {
+		let { username, password } = req.body;
+		const user = await Users.findBy({ username }).first();
+		if (user && bcrypt.compareSync(password, user.password)) {
+			next();
+		} else {
+			res.status(401).json({ message: 'Invalid credentials' });
+		}
+	} catch (err) {
+		next(err);
+	}
 }
 
 /*
-  If the username in req.body does NOT exist in the database
+  * If password is missing from req.body, or if it's 3 chars or shorter
 
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
+  * status 422
+  * {
+  *   "message": "Password must be longer than 3 chars"
+  * }
 */
-function checkUsernameExists() {
-
+function checkPasswordLength(req, res, next) {
+	const user = req.body;
+	if (!user.password || user.password.length < 4) {
+		res.status(422).json({
+			message: 'Password must be longer than 3 chars',
+		});
+	} else {
+		next();
+	}
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
-function checkPasswordLength() {
-
-}
-
-// Don't forget to add these to the `exports` object so they can be required in other modules
+// !! Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+	checkUsernameFree,
+	checkUsernameExists,
+	checkPasswordLength,
+};
