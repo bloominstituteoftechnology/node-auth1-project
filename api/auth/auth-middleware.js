83856c9511d34ce1b1ccf7863dcbb4d2e534d6a1
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-06-14 21:04:41 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -27,7 +27,7 @@ async function checkUsernameFree( req, res, next){
       next()
     }
     else{
-       next({"message": "Username taken"})
+       next({message: "Username taken", status: 422})
       }
   }catch(err){next(err)}
 }
@@ -40,8 +40,16 @@ async function checkUsernameFree( req, res, next){
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists(req, res, next) {
-  next()
+async function checkUsernameExists(req, res, next) {
+  try{
+    const users = await User.findBy({username: req.body.username})
+    if(users.length){
+      next()
+    }
+    else{
+       next({message: "Invalid credentials", status: 401})
+      }
+  }catch(err){next(err)}
 }
 
 /*
