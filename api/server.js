54e3d75377405916337630057f446875d4d489f2
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-07-07 08:03:43 -0400

    added dependencies to package.json

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const cors = require("cors");
   Users that do authenticate should have a session persisted on the server,
   and a cookie set on the client. The name of the cookie should be "chocolatechip".
 
-  The session can be persisted in memory (would not be adecuate for production)
+  The session can be persisted in memory (would not be adequate for production)
   or you can use a session store like `connect-session-knex`.
  */
 
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,22 +1,26 @@
+const db = require("../../data/db-config.js")
+
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-
+  return db("users")
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
-function findBy(filter) {
-
+const findBy = (filter) => {
+  return db("users")
+    .where(filter)
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
-function findById(user_id) {
-
+const findById = (user_id) => {
+  return db("users")
+    .where([user_id])
 }
 
 /**
@@ -27,3 +31,4 @@ function add(user) {
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = { find, findBy}
