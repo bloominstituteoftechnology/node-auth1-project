650d8eeddebe40962bc389f927d3b7062d4c8344
Corie Stewart <coriestewart1989>
2021-07-07 20:29:35 -0400

    getting there

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,22 +1,24 @@
+const db = require("../../data/db-config.js")
+
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-
+  return db("users")
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
-
+  return db("users").where("filter", filter)
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+  return db("users").where("user_id", user_id).first()
 }
 
 /**
@@ -27,3 +29,9 @@ function add(user) {
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports ={
+  find,
+  findBy,
+  findById,
+  add
+}
