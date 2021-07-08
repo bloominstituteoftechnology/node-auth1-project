120141886c1e3d701ebd022357c506b16ec240aa
yonathan Admasu <admyoni4@yahoo.com>
2021-07-07 17:14:49 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,4 @@
+const User = require('../users/users-model')
 /*
   If the user does not have a session saved in the server
 
@@ -18,8 +19,17 @@ function restricted(req,res,next) {
     "message": "Username taken"
   }
 */
-function checkUsernameFree(req,res,next) {
-  next()
+async function checkUsernameFree(req,res,next) {
+  try {
+    const users = await User.findBy({username: req.body.username })
+    if(!users.length){
+      next()
+    }else {
+      next({message: "Username taken", status: 422})
+    }
+  } catch (e) {
+    next(e)
+  }
 }
 
 /*
@@ -30,8 +40,17 @@ function checkUsernameFree(req,res,next) {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists(req,res,next) {
-  next()
+async function checkUsernameExists(req,res,next) {
+  try {
+    const users = await User.findBy({username: req.body.username })
+    if(users.length){
+      next()
+    }else {
+      next({message: "Invalid credentials", status: 401})
+    }
+  } catch (e) {
+    next(e)
+  }
 }
 
 /*
