bf57e5ceaccc22068e5008f65fadf6e45ca89ffe
Rigo <limon1285@gmail.com>
2021-07-13 23:22:41 -0500

    updated weather api

--- weather/weather-router.js
+++ weather/weather-router.js
@@ -1,20 +1,24 @@
+const axios = require("axios");
 
-const axios = require('axios');
+const router = require("express").Router();
 
-const router = require('express').Router();
-
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" },
   };
 
   axios
-    .get('https://www.metaweather.com/api/location/2487956/', requestOptions)
-    .then(response => {
-      res.status(200).json(response.data);
+    .get("https://www.metaweather.com/api/location/2487956/", requestOptions)
+    .then((response) => {
+      console.log(response.data);
+      let maps = response.data.consolidated_weather.map((item) => {
+        console.log("item---->", item);
+        return item.humidity;
+      });
+      res.status(200).json(maps);
     })
-    .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Weather', error: err });
+    .catch((err) => {
+      res.status(500).json({ message: "Error Fetching Weather", error: err });
     });
 });
 
