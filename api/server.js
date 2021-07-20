79bf2282fd21d9db164f6a9e3a5f2712a54815a4
abemm7787 <mirambeaux7@gmail.com>
2021-07-19 20:07:02 -0500

    nodemom is giving me the heads up to proceed

--- api/server.js
+++ api/server.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-
-/**
-  Do what needs to be done to support sessions with the `express-session` package!
-  To respect users' privacy, do NOT send them a cookie unless they log in.
-  This is achieved by setting 'saveUninitialized' to false, and by not
-  changing the `req.session` object unless the user authenticates.
-
-  Users that do authenticate should have a session persisted on the server,
-  and a cookie set on the client. The name of the cookie should be "chocolatechip".
-
-  The session can be persisted in memory (would not be adecuate for production)
-  or you can use a session store like `connect-session-knex`.
- */
-
 const server = express();
+const session = require( "express-session")
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'keep it secret', // .env file
+  cookie: {
+    maxAge: 1000 * 80 * 80,
+    secure: false, 
+    httpOnly: false, 
+  },
+  resave: false, 
+  saveUninitialized: false, 
+}))
+
+
+
 server.get("/", (req, res) => {
-  res.json({ api: "up" });
+  res.json({ api: "Are we there yet?" });
 });
 
 server.use((err, req, res, next) => { // eslint-disable-line
--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
+
