99141e9a3c00fc536cc7bc79738d7d7ccd71c977
Lilia Ovcharenko <lilia.ovcharenko@gmail.com>
2021-09-03 03:45:36 -0700

    fixed

--- api/server.js
+++ api/server.js
@@ -3,13 +3,14 @@ const helmet = require("helmet");
 const cors = require("cors");
 const usersRouter = require('./users/users-router');
 const authRouter = require('./auth/auth-router');
-const session = require('express-session')
-const Store = require('connect-session-knex')(session) 
+const session = require('express-session');
+const Store = require('connect-session-knex')(session);
+const knex = require('../data/db-config')
 
 const server = express();
 
 server.use(session({
-  name: 'chocolatechip',
+  name: 'chocolatechip', 
   secret: 'shh',
   resave: false,
   store: new Store({
