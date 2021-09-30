ec5bbfa1b75abb726a4d56d4adf4165b52b78f59
Stan <stan2dor@gmail.com>
2021-09-29 18:20:39 -0700

    router end-points

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,63 +1,76 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
+const User = require("../users/users-model");
 
+const {
+  checkPasswordLength,
+  checkUsernameExists,
+  checkUsernameFree,
+} = require("./auth-middleware");
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
-  }
-
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
-  }
-
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
+  response: status 200 {"user_id": 2, "username": "sue"}
+  response on username taken: status 422{"message": "Username taken"}
+  response on password three chars or less: status 422 {"message": "Password must be longer than 3 chars"}
  */
-
+router.post(
+  "/register",
+  checkPasswordLength,
+  checkUsernameFree,
+  (req, res, next) => {
+    const { username, password } = req.body;
+    const passwordHash = bcrypt.hashSync(password, 8);
+    User.add({ username, password: passwordHash })
+      .then((saved) => res.status(201).json(saved))
+      .catch(next);
+  }
+);
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
-  }
-
-  response on invalid credentials:
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
+  response: status 200 {"message": "Welcome sue!"}
+  response on invalid credentials: status 401 { "message": "Invalid credentials" }
  */
-
+router.post("/login", checkUsernameExists, (req, res, next) => {
+  const { password } = req.body;
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user;
+    res.json({ message: `Welcome ${req.user.username}` });
+  } else {
+    next({ status: 401, message: "Invalid credentials" });
+  }
+});
 
 /**
   3 [GET] /api/auth/logout
-
-  response for logged-in users:
-  status 200
-  {
-    "message": "logged out"
-  }
-
-  response for not-logged-in users:
-  status 200
-  {
-    "message": "no session"
-  }
+  response for logged-in users: status 200 {"message": "logged out"}
+  response for not-logged-in users: status 200 {"message": "no session"}
  */
+router.get("/logout", (req, res, next) => {
+  if (req.session.user) {
+    req.session.destroy((err) => {
+      if (err) {
+        next(err);
+      } else {
+        res.status(200).json({ message: "logged out" });
+      }
+    });
+  } else {
+    res.status(200).json({ message: "no session" });
+  }
+});
 
- 
+// eslint-disable-next-line
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+    cusomeMessage: "Something went wrong inside the auth router",
+  });
+});
+
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
+const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+const knex = require("../data/db-config");
+
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -17,15 +24,47 @@ const cors = require("cors");
 
 const server = express();
 
+server.use(
+  session({
+    name: "chocolatechip",
+    secret: "shhh",
+    saveUninitialized: false,
+    resave: false,
+    store: new KnexSessionStore({
+      knex,
+      createTable: true,
+      clearInterval: 1000 * 60 * 10,
+      tablename: "sessions",
+      sidfieldname: "sid",
+    }),
+    cookies: {
+      maxAge: 100 * 60 * 10,
+      secure: false,
+      httpOnly: false,
+      // sameSite: 'none'
+    },
+  })
+);
+
+server.use(express.static(path.join(__dirname, "../client")));
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: "not found!" });
+});
+
+// eslint-disable-next-line
+server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
