fcce5a976f7ab2197b9bf8dc81b9759f7a645028
Stan <stan2dor@gmail.com>
2021-09-29 18:09:39 -0700

    auth-middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -2,48 +2,75 @@
   If the user does not have a session saved in the server
 
   status 401
-  {
-    "message": "You shall not pass!"
-  }
+  { "message": "You shall not pass!" }
 */
-function restricted() {
+const User = require("../users/users-model");
 
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    next({ status: 401, message: "You shall not pass!" });
+  }
 }
 
 /*
   If the username in req.body already exists in the database
 
   status 422
-  {
-    "message": "Username taken"
-  }
+  { "message": "Username taken" }
 */
-function checkUsernameFree() {
-
+async function checkUsernameFree(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username });
+    if (!users.length) {
+      next();
+    } else {
+      next({ message: "Username taken", status: 422 });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
   If the username in req.body does NOT exist in the database
 
   status 401
-  {
-    "message": "Invalid credentials"
-  }
+  { "message": "Invalid credentials" }
 */
-function checkUsernameExists() {
-
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username });
+    if (users.length) {
+      req.user = users[0];
+      next();
+    } else {
+      next({ message: "Invalid Credentials", status: 401 });
+    }
+  } catch (err) {
+    next();
+  }
 }
 
 /*
   If password is missing from req.body, or if it's 3 chars or shorter
 
   status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
+  { "message": "Password must be longer than 3 chars" }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req, res, next) {
+  if (!req.body.password || req.body.password.length < 3) {
+    next({ message: "Password must be longer than 3 chars", status: 422 });
+  } else {
+    next();
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+};
