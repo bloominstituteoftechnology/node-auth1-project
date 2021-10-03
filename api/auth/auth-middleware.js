2ec6c45f385957d506ebf10429a4e2482e6c105e
Joey Bertschler <joey.bertschler@gmail.com>
2021-10-03 18:46:42 +0200

    middle ware restriction enhancement

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -9,8 +9,11 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  console.log('restricted')
-  next()
+   if (req.session.user ) {
+    next()
+  } else {
+    next({ status: 402, message: "Wrong password"})
+  }
 }
 
 /*
