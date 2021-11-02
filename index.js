df5e13c7cb3774d7221c65941967caa41a5b5191
Ladrillo <gabriel@cabrejas.net>
2021-11-02 14:42:09 +0100

    port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
