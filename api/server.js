f30e5e7556b4e5cf351360b5d0c41ad60e112205
joey-bertschler <joey.bertschler@gmail.com>
2022-01-13 10:47:57 +0400

    wip8

--- api/server.js
+++ api/server.js
@@ -3,6 +3,10 @@ const helmet = require("helmet");
 const cors = require("cors");
 const usersRouter = require("./users/users-router.js");
 const authRouter = require("./auth/auth-router.js");
+const session = require("express-session");
+const Store = require("connect-session-knex")(session);
+const knex = require('../data/dbConfig.js');
+
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -18,6 +22,27 @@ const authRouter = require("./auth/auth-router.js");
 
 const server = express();
 
+server.use(session ({
+  name: "chocolatechip",
+  secret: "shh",
+  saveUninitialized: false,
+  resave: false,
+  store: new Store({
+    knex,//: require("../db/knex.js"),
+    createTable: true,
+    clearInterval: 1000 * 60 * 60, //clear expired sessions every hour
+    tablename: "sessions",
+    sidfieldname: "sid",
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 10, //10 minutes
+    secure: false,
+    httpOnly: true, // this means the cookie is only accessible by the server / the browser (not the client)
+    sameSite: 'none', // this means the cookie is not accessible by javascript, only by https (not http)
+    //sameSite: "lax", // this means the cookie is accessible by the server and the client
+
+  }
+}) )
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
