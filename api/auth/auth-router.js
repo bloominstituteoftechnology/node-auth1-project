952153dbba13fa12207a28aa654a98d93e19c347
Dionne <mylife33@hotmail.com>
2022-01-23 16:25:38 -0600

    mvp. all tests passing!!

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -78,7 +78,18 @@ router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next
   }
  */
   router.get('/logout', (req, res, next) => {
-    res.json('logout')
+    if(req.session.user) {
+      req.session.destroy(err => {
+        if (err) {
+          next(err)
+        } else {
+          res.json({ message: "logged out" })
+        }
+      })
+    } else {
+      res.json({ message: 'no session'})
+    }
+    
   })
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
