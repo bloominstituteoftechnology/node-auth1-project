d63e07616441189e653ec9129e8ca9ba93ab51ff
Dionne <mylife33@hotmail.com>
2022-01-23 15:17:41 -0600

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -26,7 +26,7 @@ async function checkUsernameFree(req, res, next) {
     if (!users.length)  {
       next()
     } else {
-      next({"status": "422", "message": "Username taken"})
+      next({status: "422", message: "Username taken"})
     }
   } catch (err){
     next(err)
@@ -41,8 +41,17 @@ async function checkUsernameFree(req, res, next) {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists(req, res, next) {
-  next()
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({username: req.body.username})
+    if (users.length)  {
+      next()
+    } else {
+      next({status: "401", message: "Invalid credentials"})
+    }
+  } catch (err){
+    next(err)
+  }
 }
 
 /*
@@ -54,7 +63,11 @@ function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  next()
+  if (!req.body.password || req.body.password.length < 3) {
+    next({status: "422", message: "Password must be longer than 3 chars"})
+  } else {
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
