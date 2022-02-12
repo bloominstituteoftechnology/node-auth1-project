29592ed56e9c78f7c0921b1b3d08ab0087f39bba
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-02-12 11:59:17 -0800

    auth middleware and users router completed

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-// const usersRouter = require('./users/users-router')
+const usersRouter = require('./users/users-router')
 // const authRouter = require('./auth/auth-router')
-// const session = require('express-session')
-// const Store = require('connect-session-knex')(session)
-// const knex = require('../data/db-config')
+const session = require('express-session')
+const Store = require('connect-session-knex')(session)
+const knex = require('../data/db-config')
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -21,31 +21,31 @@ const cors = require("cors");
  */                 
 const server = express();
 
-// server.use(session({
-//   name: 'chocolatechip',
-//   secret: 'shh',
-//   saveUninitialized: false,
-//   resave: false,
-//   store: new Store({
-//     knex, 
-//     createTable: true,
-//     clearInterval: 1000 * 60 * 10,
-//     tableName: 'sessions',
-//     sidfieldname: 'sid'
-//   }),
-//   cookie: {
-//     maxAge: 100 * 60 * 10,
-//     secure: false,
-//     httpOnly: true,
-//     // sameSite: 'none'
-//   }
-// })
-// )
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'shh',
+  saveUninitialized: false,
+  resave: false,
+  store: new Store({
+    knex, 
+    createTable: true,
+    clearInterval: 1000 * 60 * 10,
+    tableName: 'sessions',
+    sidfieldname: 'sid'
+  }),
+  cookie: {
+    maxAge: 100 * 60 * 10,
+    secure: false,
+    httpOnly: true,
+    // sameSite: 'none'
+  }
+})
+)
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-// server.use('/api/users', usersRouter)
+server.use('/api/users', usersRouter)
 // server.use('/api/auth', authRouter)
 
 server.get("/", (req, res) => {
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,7 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-
+const router = require('express').Router()
+const { restricted } = require('../auth/auth-middleware')
+const User = require('./users-model')
 
 /**
   [GET] /api/users
@@ -23,6 +25,15 @@
     "message": "You shall not pass!"
   }
  */
-
+router.get('/', restricted, (req, res, next) => {
+  User.find()
+  .then(users => {
+    res.json(users)
+  })
+  .catch(err => {
+    next(err)
+  })
+})
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router
