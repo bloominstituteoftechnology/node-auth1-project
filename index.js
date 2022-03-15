140a465402273a26b97c89842d7ddbc6565b05fb
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-03-15 16:29:53 -0700

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 9000;
 
+const NAME = alieze
+
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
