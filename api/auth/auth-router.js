ff2ba2722e7c028dc38a949994055ecc34305bb0
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-03-15 22:11:46 -0700

    finally all tests passing

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -67,7 +67,7 @@ router.post('/register', checkPasswordLength, checkUsernameFree,  (req, res, nex
       // set the cookie on the client
       // store session with session id 
       req.session.user = req.user
-      res.json({ message: `Welcome ${req.user.username}`})
+      res.json({ message: `Welcome ${req.user.username}!`})
     } else {
       next({ status: 401, message: 'Invalid credentials'})
     }
@@ -89,7 +89,18 @@ router.post('/register', checkPasswordLength, checkUsernameFree,  (req, res, nex
   }
  */
   router.get('/logout', (req, res, next) => {
-    res.json('logout')
+    // res.json('logout')
+   if(req.session.user){
+    req.session.destroy(err => {
+      if (err) {
+        next(err)
+      } else {
+        res.json({ message: 'logged out '})
+      }
+    })
+   } else {
+     next({ status: 200, message: 'no session' })
+   }
   })
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
