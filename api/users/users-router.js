d958bccb663511c84acaecc08d174d7608341f11
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-03-15 17:13:31 -0700

    scaffolded endpoints

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,5 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-
+const router = require('express').Router()
 
 /**
   [GET] /api/users
@@ -24,5 +24,9 @@
   }
  */
 
+  router.get('/', (req, res, next) => {
+    res.json('users')
+  })
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router 
