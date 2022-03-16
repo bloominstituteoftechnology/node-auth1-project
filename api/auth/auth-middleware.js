451711751eb348016ad8931cde36fa1edc0cc653
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-03-15 21:46:55 -0700

    one more route to go

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -10,8 +10,13 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  console.log('restricted')
-  next()
+  //console.log('restricted')
+  //next()
+  if(req.session.user) {
+    next()
+  } else {
+    next({ status: 401, message: 'You shall not pass!'})
+  }
 }
 
 /*
